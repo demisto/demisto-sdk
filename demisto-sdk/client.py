bf93f3c0673bb285353317fd11b36210d8719f3c
liorblob <lblobstein@paloaltonetworks.com>
2019-11-05 14:44:56 +0200

    client

---
+++ demisto-sdk/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.7
+
+# Client
+# Python client that invokes functions in the Demisto SDK.
+# To use the client, you should generate an API key under Settings->Integrations->API Keys
+#
+# Author:       Lior
+# Version:      0.1
+#
+
+
+class Client:
+    def __init__(self):
+        pass

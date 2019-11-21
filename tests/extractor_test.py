6b2a97e7f3901175249c5b86f23b7901fdbe2fb2
guyfreund <gfreund@paloaltonetworks.com>
2019-11-21 19:35:11 +0200

    updated & added tests

--- tests/extractor_test.py
+++ tests/extractor_test.py
@@ -1,37 +1,10 @@
 from pytest import raises
 from mock import patch
 from demisto_sdk.common.configuration import Configuration
+from demisto_sdk.common.constants import DEFAULT_IMAGE_BASE64
 import os
+import shutil
 import base64
-
-
-DEFAULT_IMAGE_BASE64 = b'iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAMAAAC5zwKfAAACYVBMVEVHcEwAT4UAT4UAT4YAf/8A//8AT4UAf78AT4U' \
-                       'AT4UAT4UAUYcAT4YAT4YAT48AXIsAT4UAT4UAUIUAUIUAT4UAT4UAVaoAW5EAUIYAWYwAT4UAT4UAT4UAUIgAT4YAUo' \
-                       'UAUIYAUIUAT4YAVY0AUIUAT4UAUIUAUocAUYUAT4UAT4UAT4UAUIYAT4UAUIUAT4cAUYUAUIUAUIYAUocAT4UAUIUAT' \
-                       '4YAUY4AUIUAUIYAT4UAVYgAT4UAT4UAT4YAVYUAT4UAT4UAT4YAT4cAT4UAT4UAUYYAZpkAWIUAT4UAT4gAbZEAT4UA' \
-                       'UIYAT4UAUIUAT4cAUYgAT4UAZpkAT4UAT4UAT4UAVaoAUIUAT4UAWIkAT4UAU4kAUIUAUIUAU4gAT4UAT4UAT4UAVYg' \
-                       'AUIUAT4YAVYkAUYUAT4UAU4cAUIYAUIUAT4gAUIYAVYsAT4YAUocAUYUAUIYAUYgAT4UAT4UAT4UAT4UAUYUAU4UAUY' \
-                       'gAT4UAVY0AUIUAUIUAT4UAT4cAT4oAVY0AUYcAUIcAUIUAUIYAUIcAUYcAUIUAT4UAT4UAUIUAT4UAX58AT4UAUIUAU' \
-                       'IYAT4UAUIYAUIgAT4UAT4UAUIUAT4UAUIUAT4YAT4UAUIYAT4YAUYkAT4UAUYYAUIUAT4UAT4YAT4YAT4YAT4cAUokA' \
-                       'T4UAT4YAUIUAT4UAT4YAUIUAT4UAUIoAT4YAT4UAT4UAT4UAT4UAUIUAT4UAT4YAT4UAUYYAT4YAUYUAT4UAT4YAT4U' \
-                       'AUoUAT4UAT4UAUIYAT4YAUIcAYokAT4UAT4UA65kA0ZYAu5PCXoiOAAAAx3RSTlMA+nO6AgG5BP799i9wShAL9/uVzN' \
-                       'rxAw6JFLv08EmWKLyPmhI/x88+ccjz4WjtmU1F76VEoFbXGdKMrh71+K0qoZODIMuzSAoXni0H4HnjfnccQwXDjT0Gi' \
-                       '/wa5zSCaSvBsWMPb9EnLMoxe3hHOSG+Ilh/S1BnzvJULjimCayy6UAwG1VPta91UVLNgJvZCNBcRuVsPIbb37BllNjC' \
-                       'fTLsbrjukKejYCVtqb/5aqiXI9W0tnad4utdt2HEa1ro5EHWpBOBYg3JeEoS2QAAA5lJREFUGBmtwQN7Y0sABuAvbZK' \
-                       'T1Ha3tt2ubdu2vXu517Zt27a+TH/VbXgmaTIz53nyvtDaV1+JdDrxHVvzkD43D5BsyUe6bKxmUP0qJNM2Y/Pxud9bMH' \
-                       'd5DsNmlmGa/E8ZsvgumHqikFHzPUhgVTGipBxmun20LUCCw4zZAiPtjPMs4r3MmGvbYGA9E6yD7CwlN0FvPac5CckDl' \
-                       'LRBK4dJPAxbDiXvQ+c9H5OZQMwW2lZDJ7eQyQ1vQsR+2j6ARnYnU6nKQ8gdtA1Co6mLqXX1AXBf72GUa6EbGmuotCvT' \
-                       'u4tRBcOfQ+sATQ2cqoSBF2go6xiMtNNQA8zkH6GZ0zBU/mLFYEcBtbbCiVtrM6lxEA6NVFOpHk6d9lPpbjjVSKWCvXB' \
-                       'oHzUyFyG1vuFzM3Yi3rfUqL5/E5Jzv8spz+chjpdao7VIag9D3kAcLw14szHd7h0MGfVAVkITvj/PI4H1OCNyITlPQ6' \
-                       '7eDYjTzqirFmy9NDZnwRhsy0sZsw4xzX46kDVRiahHaPNleBD2+wDJSSGZpNK1v8sRstJP2StDFoDsXh+niIBEUOM/h' \
-                       'NzLBDWtD/UwTAQkghr/IGgrFURAIqg2WoagzVQQAYmg2nUELaWKCEgEla56EFRMFRGQCCpdQtBlKomARFClA0GecSqJ' \
-                       'gERQZSOCLlBNBCSCCucQZJVQTQQkggpnEHSFGiIgEQx76nhrDRPch5BiaoiARHCKv6gOgNW/n7LCOoT8e7GUSpNCMkm' \
-                       'y5xmEeTJ8tBUh6q+K2XTA34yYPYx5qxK25Q0FNFYEmzXOqJ8RZ2eRi2Z8syDpY8RiNxIsmu+niSOQuR9liCsb0638ig' \
-                       'a+RJwMhpxCUv1fUGsJ4jSt5ZRGpGBldFKjBPHOznjzmyGkNusHahyFQ1eyqPQZnHqQSv4n4VQVlTovwKGD1Mi89Bica' \
-                       'KZWVsstFd35MLSUZoqXwcxLNJQBI699TENzYWDs4mya+hBadYOFjFp9YMlaKuVAw5rYwagb93gA1HYxtefKoeaeyRjf' \
-                       'GYTkeZlK6TxofE2bFxHWCibn6oeG+zfatiOmgsn4foHOPEqehu1VJrEXWkOU5EKyhtPkQO9OSjZAdpIJDsOAVcOYccR' \
-                       'bSJnvExjZzphuJGigzf8jzBz6gxG3u5HAs4JRrhGYGmthkK9xFaYpu41hWbkwVzbyTsdHb59AMtsyGVTahnRZ9hPJ13' \
-                       'cjfQ4V89djSKcm71Ho/A9KDXs8/9v7cAAAAABJRU5ErkJggg=='
 
 
 def test_get_yml_type():
@@ -64,12 +37,12 @@ def test_extract_long_description():
         extractor.yml_path = 'script'
         assert extractor.extract_long_description('output_path') is None
         # Test opening the file and writing to it
-        extractor.yml_path = 'tests/tests_data/integration-Ipinfo.yml'
-        extractor.extract_long_description('tests/tests_data/temp_file.txt')
-        with open('tests/tests_data/temp_file.txt', 'rb') as temp_file:
-            file_data = temp_file.read()
-            assert file_data == b'detaileddescription'
-        os.remove('tests/tests_data/temp_file.txt')
+        extractor.yml_path = 'tests/test_files/integration-Zoom.yml'
+        extractor.extract_long_description('tests/test_files/temp_file.txt')
+        with open('tests/test_files/temp_file.txt', 'rb') as temp_file:
+            file_data = temp_file.read().decode('utf-8')
+            assert file_data == 'detaileddescription'
+        os.remove('tests/test_files/temp_file.txt')
 
 
 def test_extract_image():
@@ -82,10 +55,57 @@ def test_extract_image():
         extractor.yml_path = 'script'
         assert extractor.extract_image('output_path') is None
         # Test opening the file and writing to it
-        extractor.yml_path = 'tests/tests_data/integration-Ipinfo.yml'
-        extractor.extract_image('tests/tests_data/temp_image.png')
-        with open('tests/tests_data/temp_image.png', 'rb') as temp_image:
+        extractor.yml_path = 'tests/test_files/integration-Zoom.yml'
+        extractor.extract_image('tests/test_files/temp_image.png')
+        with open('tests/test_files/temp_image.png', 'rb') as temp_image:
             image_data = temp_image.read()
-            image = base64.b64encode(image_data)
+            image = base64.b64encode(image_data).decode('utf-8')
             assert image == DEFAULT_IMAGE_BASE64
-        os.remove('tests/tests_data/temp_image.png')
+        os.remove('tests/test_files/temp_image.png')
+
+
+def test_extract_code():
+    from demisto_sdk.yaml_tools.extractor import Extractor
+    configuration = Configuration()
+    with patch.object(Extractor, '__init__', lambda a, b, c, d, e, f, g: None):
+        extractor = Extractor('', '', False, False, '', configuration)
+        extractor.yml_type = ''
+        extractor.common_server = True
+        extractor.demisto_mock = True
+        extractor.yml_path = 'tests/test_files/integration-Zoom.yml'
+        extractor.dest_path = 'tests/test_files/temp_file.txt'
+        extractor.extract_code('tests/test_files/temp_file.txt')
+        with open('tests/test_files/temp_file.txt', 'rb') as temp_file:
+            file_data = temp_file.read().decode('utf-8')
+            assert 'import demistomock as demisto\n' in file_data
+            assert 'from CommonServerPython import *\n' in file_data
+            assert file_data[-1] == '\n'
+        os.remove('tests/test_files/temp_file.txt')
+        extractor.common_server = False
+        extractor.demisto_mock = False
+        extractor.extract_code('tests/test_files/temp_file.txt')
+        with open('tests/test_files/temp_file.txt', 'rb') as temp_file:
+            file_data = temp_file.read().decode('utf-8')
+            assert 'import demistomock as demisto\n' not in file_data
+            assert 'from CommonServerPython import *\n' not in file_data
+            assert file_data[-1] == '\n'
+        os.remove('tests/test_files/temp_file.txt')
+
+
+def test_migrate():
+    from demisto_sdk.yaml_tools.extractor import Extractor
+    configuration = Configuration()
+    yml_path = 'tests/test_files/integration-Zoom.yml'
+    dest_path = 'tests/test_files/Zoom'
+    extractor = Extractor(yml_path=yml_path, dest_path=dest_path, add_demisto_mock=True, add_common_server=True,
+                          yml_type='', configuration=configuration)
+    extractor.migrate()
+    assert os.path.isdir('tests/test_files/Zoom')
+    assert os.path.isfile('tests/test_files/Zoom/CHANGELOG.md')
+    assert os.path.isfile('tests/test_files/Zoom/Pipfile')
+    assert os.path.isfile('tests/test_files/Zoom/Pipfile.lock')
+    assert os.path.isfile('tests/test_files/Zoom/Zoom.py')
+    assert os.path.isfile('tests/test_files/Zoom/Zoom.yml')
+    assert os.path.isfile('tests/test_files/Zoom/Zoom_description.md')
+    assert os.path.isfile('tests/test_files/Zoom/Zoom_image.png')
+    shutil.rmtree('tests/test_files/Zoom')

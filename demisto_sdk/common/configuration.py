0b5ec1d0d0553e753f5bae59b1af1e0cdbfe2273
Bar Hochman <11165655+jochman@users.noreply.github.com>
2019-12-02 13:23:35 +0200

    fix dev_env_dir and mypy runner (#44)
    * fix dev_env_dir and mypy runner
    
    * dingle qutoe

--- demisto_sdk/common/configuration.py
+++ demisto_sdk/common/configuration.py
@@ -6,7 +6,7 @@ class Configuration:
     def __init__(self, env_dir=None, log_verbose=False, logging_level=logging.INFO):
         logging.basicConfig(level=logging_level)
         self.log_verbose = log_verbose
-        self.sdk_env_dir = os.path.dirname(os.path.abspath(os.path.join(__file__, "../../")))
+        self.sdk_env_dir = os.path.dirname(os.path.abspath(os.path.join(__file__, '/../')))
         if not env_dir:
             self.env_dir = os.getcwd()
         else:

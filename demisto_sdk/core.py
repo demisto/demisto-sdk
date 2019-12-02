d3952e56d7ac30b262bc3fc78ec81f1fce2d992c
Jochman <bhochman@paloaltonetworks.com>
2019-12-02 17:23:17 +0200

    fix pylint

--- demisto_sdk/core.py
+++ demisto_sdk/core.py
@@ -50,7 +50,6 @@ class DemistoSDK:
 
         if args.env_dir:
             self.configuration.env_dir = args.env_dir
-
         if args.command == 'extract':
             if args.migrate:
                 self.migrate_file(args.infile, args.outfile, args.demistomock, args.commonserver, args.type)

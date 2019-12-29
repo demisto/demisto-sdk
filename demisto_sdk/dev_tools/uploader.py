61f1af5c1844eafe910eebe06f14812fb8de6172
Dan Tavori <38749041+dantavori@users.noreply.github.com>
2019-12-29 15:52:09 +0200

    Update uploader.py

--- demisto_sdk/dev_tools/uploader.py
+++ demisto_sdk/dev_tools/uploader.py
@@ -1,33 +1,18 @@
 from demisto_sdk.common.tools import print_color, LOG_COLORS, print_v
-import demisto_client
-import os
+from demisto_sdk.common.demisto_client import Client
 
 
-class Uploader:
-    DEMISTO_API_KEY_ENV = 'DEMISTO_API_KEY'
+class Uploader(Client):
+    """Upload the integration specified in self.infile to the remote Demisto instance.
+        Attributes:
+            infile (str): The path of the file to be uploaded.
+            log_verbose (bool): Whether to output a detailed response.
+        """
 
     def __init__(self, infile: str, url: str, insecure: bool = False, verbose: bool = False):
         self.infile = infile
-        self.base_url = url
         self.log_verbose = verbose
-        self.client = demisto_client.configure(base_url=self.base_url,
-                                               api_key=self.get_api_key(),
-                                               verify_ssl=not insecure)
-
-    def get_api_key(self):
-        """Retrieve the API Key
-
-        Raises:
-            RuntimeError: if the API Key environment variable is not found
-
-        Returns:
-            str: API Key
-        """
-        ans = os.environ.get(self.DEMISTO_API_KEY_ENV, None)
-        if ans is None:
-            raise RuntimeError(f'Error: Environment variable {self.DEMISTO_API_KEY_ENV} not found')
-
-        return ans
+        Client.__init__(self, url, insecure)
 
     def upload(self):
         """Upload the integration specified in self.infile to the remote Demisto instance.
@@ -41,7 +26,7 @@ class Uploader:
     def add_sub_parser(subparsers):
         from argparse import ArgumentDefaultsHelpFormatter
         description = f"""Upload integration to Demisto instance.
-        {Uploader.DEMISTO_API_KEY_ENV} environment variable should contain a valid Demisto API Key."""
+        {Client.DEMISTO_API_KEY_ENV} environment variable should contain a valid Demisto API Key."""
         parser = subparsers.add_parser('upload', help=description, formatter_class=ArgumentDefaultsHelpFormatter)
         parser.add_argument("-i", "--infile", help="The yml file to with the integration to upload", required=True)
         parser.add_argument("-u", "--url", help="Base URL of the Demisto instance", required=True)

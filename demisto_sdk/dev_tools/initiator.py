c1f15e0d2544b0956aa9d30ef2d93f9df6ec91ba
rsagi <rsagi@paloaltonetworks.com>
2019-12-31 14:37:19 +0200

    code review corrections

--- demisto_sdk/dev_tools/initiator.py
+++ demisto_sdk/dev_tools/initiator.py
@@ -6,74 +6,60 @@ import yamlordereddictloader
 from typing import Dict
 from distutils.dir_util import copy_tree
 from demisto_sdk.common.tools import print_error, print_color, LOG_COLORS
-
-INTEGRATIONS_DIR = '/Integrations'
-SCRIPT_DIR = '/Scripts'
-INCIDENT_FIELDS_DIR = '/IncidentFields'
-INCIDENT_TYPES_DIR = '/IncidentTypes'
-INDICATOR_FIELDS_DIR = '/IndicatorFields'
-PLAYBOOKS_DIR = '/Playbooks'
-LAYOUTS_DIR = '/Layouts'
-TEST_PLAYBOOKS_DIR = '/TestPlaybooks'
-CLASSIFIERS_DIR = '/Classifiers'
-CONNECTIONS_DIR = '/Connections'
-DASHBOARDS_DIR = '/Dashboards'
-MISC_DIR = '/Misc'
-REPORTS_DIR = '/Reports'
-WIDGETS_DIR = '/Widgets'
-
-HELLO_WORLD_INTEGRATION = 'HelloWorld'
-HELLO_WORLD_SCRIPT = 'HelloWorldScript'
+from demisto_sdk.common.constants import INTEGRATIONS_DIR, SCRIPTS_DIR, INCIDENT_FIELDS_DIR, INCIDENT_TYPES_DIR, \
+    INDICATOR_FIELDS_DIR, PLAYBOOKS_DIR, LAYOUTS_DIR, TEST_PLAYBOOKS_DIR, CLASSIFIERS_DIR, CONNECTIONS_DIR, \
+    DASHBOARDS_DIR, MISC_DIR, REPORTS_DIR, WIDGETS_DIR
 
 
 class Initiator:
     """Initiator creates a new pack/integration/script.
 
-    Attributes:
-        output_dir (str): The directory in which init will create the new pack/integration/script
-        name (str): The name for the new pack/integration/script
-        integration (bool): Indicates whether to create an integration.
-        script (bool): Indicates whether to create a script.
-        full_path (str): The full path to the newly created pack/integration/script
+       Attributes:
+           output_dir (str): The directory in which init will create the new pack/integration/script
+           name (str): The name for the new pack/integration/script
+           integration (bool): Indicates whether to create an integration.
+           script (bool): Indicates whether to create a script.
+           full_path (str): The full path to the newly created pack/integration/script
     """
 
     def __init__(self, output_dir: str, name: str, integration: bool = False, script: bool = False):
-        if output_dir:
-            self.output_dir = output_dir
-
-        else:
-            self.output_dir = ''
-
+        self.output_dir = output_dir if output_dir is not None else ''
         self.name = name
-        self.integration = integration
-        self.script = script
-        self.full_path = ''
+        self.is_integration = integration
+        self.is_script = script
+
+        self.full_output_path = ''
+
+    HELLO_WORLD_INTEGRATION = 'HelloWorld'
+    HELLO_WORLD_SCRIPT = 'HelloWorldScript'
+    DIR_LIST = [INTEGRATIONS_DIR, SCRIPTS_DIR, INCIDENT_FIELDS_DIR, INCIDENT_TYPES_DIR, INDICATOR_FIELDS_DIR,
+                PLAYBOOKS_DIR, LAYOUTS_DIR, TEST_PLAYBOOKS_DIR, CLASSIFIERS_DIR, CONNECTIONS_DIR, DASHBOARDS_DIR,
+                MISC_DIR, REPORTS_DIR, WIDGETS_DIR]
 
     def init(self):
         """Starts the init command process.
 
         """
-        if self.integration:
-            self.check_name(created_object="integration")
+        if self.is_integration:
+            self.get_created_object_name(created_object="integration")
             self.integration_init()
 
-        elif self.script:
-            self.check_name(created_object="script")
+        elif self.is_script:
+            self.get_created_object_name(created_object="script")
             self.script_init()
 
         else:
-            self.check_name(created_object="pack")
+            self.get_created_object_name(created_object="pack")
             self.pack_init()
 
-    def check_name(self, created_object: str):
+    def get_created_object_name(self, created_object: str):
         """Makes sure a name is given for the created object
 
         Args:
             created_object (str): the type of the created object (integration/script/pack)
         """
         while self.name is None:
-            print(f"Please input the name of the initialized {created_object}:")
-            self.name = str(input())
+            self.name = str(input(f"Please input the name of the initialized {created_object}:"))
 
     def pack_init(self) -> bool:
         """Creates a pack directory tree.
@@ -83,57 +69,45 @@ class Initiator:
         """
         # if an output directory given create the pack there
         if len(self.output_dir) > 0:
-            self.full_path = self.output_dir + '/' + self.name
+            self.full_output_path = self.output_dir + '/' + self.name
 
         # content-descriptor file indicates we are in "content" repository
         # thus we will create the pack under Packs directory
         elif os.path.isfile('content-descriptor.json'):
-            self.full_path = "Packs/" + self.name
+            self.full_output_path = "Packs/" + self.name
 
         # if non of the above conditions apply - create the pack in current directory
         else:
-            self.full_path = self.name
+            self.full_output_path = self.name
 
         if not self.create_new_directory():
             return False
+        for dir in self.DIR_LIST:
+            path = os.path.join(self.full_output_path, dir)
+            os.mkdir(path=path)
 
-        os.mkdir(self.full_path + INCIDENT_FIELDS_DIR)
-        os.mkdir(self.full_path + INDICATOR_FIELDS_DIR)
-        os.mkdir(self.full_path + CLASSIFIERS_DIR)
-        os.mkdir(self.full_path + CONNECTIONS_DIR)
-        os.mkdir(self.full_path + DASHBOARDS_DIR)
-        os.mkdir(self.full_path + INCIDENT_TYPES_DIR)
-        os.mkdir(self.full_path + MISC_DIR)
-        os.mkdir(self.full_path + REPORTS_DIR)
-        os.mkdir(self.full_path + WIDGETS_DIR)
-        os.mkdir(self.full_path + INTEGRATIONS_DIR)
-        os.mkdir(self.full_path + LAYOUTS_DIR)
-        os.mkdir(self.full_path + PLAYBOOKS_DIR)
-        os.mkdir(self.full_path + SCRIPT_DIR)
-        os.mkdir(self.full_path + TEST_PLAYBOOKS_DIR)
-
-        with open(self.full_path + '/CHANGELOG.md', 'a') as fp:
+        with open(self.full_output_path + '/CHANGELOG.md', 'a') as fp:
             fp.write("## [Unreleased]")
 
-        fp = open(self.full_path + '/README.md', 'a')
+        fp = open(self.full_output_path + '/README.md', 'a')
         fp.close()
 
-        with open(self.full_path + '/pack-metadata.json', 'a') as fp:
+        with open(self.full_output_path + '/metadata.json', 'a') as fp:
+            # TODO fill this once metadata.json script is ready
             fp.write('[]')
 
-        fp = open(self.full_path + '/.secrets-ignore', 'a')
+        fp = open(self.full_output_path + '/.secrets-ignore', 'a')
         fp.close()
 
-        fp = open(self.full_path + '/.pack-ignore', 'a')
+        fp = open(self.full_output_path + '/.pack-ignore', 'a')
         fp.close()
 
-        print_color(f"Successfully created the pack {self.name} in: {self.full_path}", LOG_COLORS.GREEN)
+        print_color(f"Successfully created the pack {self.name} in: {self.full_output_path}", LOG_COLORS.GREEN)
 
-        print("\nDo you want to create an integration in the pack? Y/N")
-        create_integration = str(input()).lower()
+        create_integration = str(input("\nDo you want to create an integration in the pack? Y/N")).lower()
         if create_integration == 'y':
-            self.check_name(created_object="integration")
-            self.output_dir = self.full_path + INTEGRATIONS_DIR
+            self.get_created_object_name(created_object="integration")
+            self.output_dir = self.full_output_path + "/" + INTEGRATIONS_DIR
             return self.integration_init()
 
         return True
@@ -146,29 +120,28 @@ class Initiator:
         """
         # if output directory given create the integration there
         if len(self.output_dir) > 0:
-            self.full_path = self.output_dir + '/' + self.name
+            self.full_output_path = self.output_dir + '/' + self.name
 
-        # the file pack-metadata indicates we are in a pack directory
-        # thus we will create the integration under the Integrations directory of the pack
-        elif os.path.isfile("pack-metadata.json"):
-            self.full_path = 'Integrations/' + self.name
+        # will create the integration under the Integrations directory of the pack
+        elif os.path.isdir(INTEGRATIONS_DIR):
+            self.full_output_path = 'Integrations/' + self.name
 
         # if non of the conditions above apply - create the integration in the local directory
         else:
-            self.full_path = self.name
+            self.full_output_path = self.name
 
         if not self.create_new_directory():
             return False
 
         hello_world_path = os.path.normpath(os.path.join(__file__, "..", "..", 'common', 'templates',
-                                                         HELLO_WORLD_INTEGRATION))
+                                                         self.HELLO_WORLD_INTEGRATION))
 
-        copy_tree(str(hello_world_path), self.full_path)
-        if self.name != HELLO_WORLD_INTEGRATION:
-            self.rename(current_suffix=HELLO_WORLD_INTEGRATION)
-            self.yml_reformatting(current_suffix=HELLO_WORLD_INTEGRATION)
+        copy_tree(str(hello_world_path), self.full_output_path)
+        if self.name != self.HELLO_WORLD_INTEGRATION:
+            self.rename(current_suffix=self.HELLO_WORLD_INTEGRATION)
+            self.yml_reformatting(current_suffix=self.HELLO_WORLD_INTEGRATION)
 
-        print_color(f"Finished creating integration: {self.full_path}.", LOG_COLORS.GREEN)
+        print_color(f"Finished creating integration: {self.full_output_path}.", LOG_COLORS.GREEN)
 
         return True
 
@@ -180,29 +153,28 @@ class Initiator:
         """
         # if output directory given create the script there
         if len(self.output_dir) > 0:
-            full_path = self.output_dir + '/' + self.name
+            self.full_output_path = self.output_dir + '/' + self.name
 
-        # the file pack-metadata indicates we are in a pack directory
-        # thus we will create the script under the Scripts directory of the pack
-        elif os.path.isfile("pack-metadata.json"):
-            full_path = 'Scripts/' + self.name
+        # will create the script under the Scripts directory of the pack
+        elif os.path.isdir(SCRIPTS_DIR):
+            self.full_output_path = 'Scripts/' + self.name
 
         # if non of the conditions above apply - create the integration in the local directory
         else:
-            full_path = self.name
+            self.full_output_path = self.name
 
         if not self.create_new_directory():
             return False
 
         hello_world_path = os.path.normpath(os.path.join(__file__, "..", "..", 'common', 'templates',
-                                                         HELLO_WORLD_SCRIPT))
+                                                         self.HELLO_WORLD_SCRIPT))
 
-        copy_tree(str(hello_world_path), full_path)
-        if self.name != HELLO_WORLD_SCRIPT:
-            self.rename(current_suffix=HELLO_WORLD_SCRIPT)
-            self.yml_reformatting(current_suffix=HELLO_WORLD_SCRIPT)
+        copy_tree(str(hello_world_path), self.full_output_path)
+        if self.name != self.HELLO_WORLD_SCRIPT:
+            self.rename(current_suffix=self.HELLO_WORLD_SCRIPT)
+            self.yml_reformatting(current_suffix=self.HELLO_WORLD_SCRIPT)
 
-        print_color(f"Finished creating script: {full_path}", LOG_COLORS.GREEN)
+        print_color(f"Finished creating script: {self.full_output_path}", LOG_COLORS.GREEN)
 
         return True
 
@@ -212,19 +184,19 @@ class Initiator:
         Args:
             current_suffix (str): The yml file name (HelloWorld or HelloWorldScript)
         """
-        yml_dict = self.get_yml_data_as_dict(file_path=f"{self.full_path}/{current_suffix}.yml")
+        yml_dict = self.get_yml_data_as_dict(file_path=f"{self.full_output_path}/{current_suffix}.yml")
         yml_dict["commonfields"]["id"] = self.name
         yml_dict['name'] = self.name
         yml_dict["display"] = self.name
 
-        with open(f"{self.full_path}/{self.name}.yml", 'w') as f:
+        with open(f"{self.full_output_path}/{self.name}.yml", 'w') as f:
             yaml.dump(
                 yml_dict,
                 f,
                 Dumper=yamlordereddictloader.SafeDumper,
                 default_flow_style=False)
 
-        os.remove(f"{self.full_path}/{current_suffix}.yml")
+        os.remove(f"{self.full_output_path}/{current_suffix}.yml")
 
     def rename(self, current_suffix: str):
         """Renames the python, description, test and image file in the path to fit the newly created integration/script
@@ -232,11 +204,13 @@ class Initiator:
         Args:
             current_suffix (str): The yml file name (HelloWorld or HelloWorldScript)
         """
-        os.rename(f"{self.full_path}/{current_suffix}.py", f"{self.full_path}/{self.name}.py")
-        os.rename(f"{self.full_path}/{current_suffix}_description.md", f"{self.full_path}/{self.name}_description.md")
-        os.rename(f"{self.full_path}/{current_suffix}_test.py", f"{self.full_path}/{self.name}_test.py")
-        if self.integration:
-            os.rename(f"{self.full_path}/{current_suffix}_image.png", f"{self.full_path}/{self.name}_image.png")
+        os.rename(f"{self.full_output_path}/{current_suffix}.py", f"{self.full_output_path}/{self.name}.py")
+        os.rename(f"{self.full_output_path}/{current_suffix}_description.md",
+                  f"{self.full_output_path}/{self.name}_description.md")
+        os.rename(f"{self.full_output_path}/{current_suffix}_test.py", f"{self.full_output_path}/{self.name}_test.py")
+        if self.is_integration:
+            os.rename(f"{self.full_output_path}/{current_suffix}_image.png",
+                      f"{self.full_output_path}/{self.name}_image.png")
 
     def create_new_directory(self,) -> bool:
         """Creates a new directory for the integration/script/pack.
@@ -245,21 +219,20 @@ class Initiator:
             bool. True if directory was successfully created, False otherwise.
         """
         try:
-            os.mkdir(self.full_path)
+            os.mkdir(self.full_output_path)
 
         except FileExistsError:
-            print_error(f"The directory {self.full_path} already exists.\nDo you want to overwrite it? Y/N")
-            to_delete = str(input()).lower()
+            to_delete = str(input(f"The directory {self.full_output_path} "
+                                  f"already exists.\nDo you want to overwrite it? Y/N")).lower()
             while to_delete != 'y' and to_delete != 'n':
-                print_error(f"Your response was invalid.\nDo you want to delete it? Y/N")
-                to_delete = str(input()).lower()
+                to_delete = str(input(f"Your response was invalid.\nDo you want to delete it? Y/N").lower())
 
             if to_delete == 'y':
-                shutil.rmtree(path=self.full_path, ignore_errors=True)
-                os.mkdir(self.full_path)
+                shutil.rmtree(path=self.full_output_path, ignore_errors=True)
+                os.mkdir(self.full_output_path)
 
             else:
-                print_error(f"Pack not created in {self.full_path}")
+                print_error(f"Pack not created in {self.full_output_path}")
                 return False
 
         return True

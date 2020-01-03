cd5929b1b61cbd6a0a8c0b2e7454d46eea8234eb
ronykoz <rkozakish@paloaltonetworks.com>
2020-01-03 21:03:30 +0200

    fix uploader

--- demisto_sdk/dev_tools/uploader.py
+++ demisto_sdk/dev_tools/uploader.py
@@ -13,7 +13,6 @@ class Uploader:
             unify (bool): Whether to unify a package.
             client (DefaultApi): Demisto-SDK client object.
         """
-    TEMP_FILE_PREFIX = "temp_"
 
     def __init__(self, path: str, insecure: bool = False, verbose: bool = False):
         self.path = path
@@ -24,23 +23,27 @@ class Uploader:
     def upload(self):
         """Upload the integration specified in self.infile to the remote Demisto instance.
         """
+        try:
+            if self.unify:  # Create a temporary unified yml file
+                try:
+                    unifier = Unifier(self.path, outdir=self.path)
+                    self.path = unifier.merge_script_package_to_yml()[0][0]
+                except IndexError:
+                    print_color('Error: Path input is not a valid package directory.', LOG_COLORS.RED)
+                    return 1
 
-        if self.unify:  # Create a temporary unified yml file
-            try:
-                unifier = Unifier(self.path, outdir=self.TEMP_FILE_PREFIX + self.path)
-                self.path = unifier.merge_script_package_to_yml()[0][0]
-            except IndexError:
-                print_color('Error: Path input is not a valid package directory.', LOG_COLORS.RED)
-                return 1
+            # Upload the file to Demisto
+            result = self.client.integration_upload(file=self.path)
 
-        # Upload the file to Demisto
-        result = self.client.integration_upload(file=self.path)
+            # Print results
+            print_v(f'Result:\n{result.to_str()}', self.log_verbose)
+            print_color(f'Uploaded \'{result.name}\' successfully', LOG_COLORS.GREEN)
 
-        # Print results
-        print_v(f'Result:\n{result.to_str()}', self.log_verbose)
-        print_color(f'Uploaded \'{result.name}\' successfully', LOG_COLORS.GREEN)
+        except Exception as ex:
+            raise ex
 
-        if self.unify:  # Remove the temporary file
-            os.remove(self.TEMP_FILE_PREFIX + self.path)  # todo: check this
+        finally:
+            if self.unify and os.path.exists(self.path):  # Remove the temporary file
+                os.remove(self.path)
 
         return 0

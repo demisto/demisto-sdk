eb2bbbda4c320eefd172a599491ed8da87a39c88
esharf <esahrf@paloaltonetworks.com>
2020-01-13 08:22:47 +0200

    adding --use-git to format command

---
+++ demisto_sdk/git_tools.py
@@ -0,0 +1,28 @@
+from demisto_sdk.common.tools import run_command
+from typing import Callable, List
+import re
+
+
+class Git:
+
+    @staticmethod
+    def get_current_working_branch() -> str:
+        branches = run_command('git branch')
+        branch_name_reg = re.search(r'\* (.*)', branches)
+        return branch_name_reg.group(1)
+
+    @staticmethod
+    def get_changed_files(from_branch: str = 'master', filter_results: Callable = None):
+        temp_files = run_command(f'git diff --name-status {from_branch}').split('\n')
+        files: List = []
+        for file in temp_files:
+            if file:
+                files.append({
+                    'name': file[2:],
+                    'status': file[0]
+                })
+
+        if filter_results:
+            filter(filter_results, files)
+
+        return files
--- demisto_sdk/main.py
+++ demisto_sdk/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import click
+import re
 from pkg_resources import get_distribution
 
 from demisto_sdk.core import DemistoSDK
@@ -20,10 +21,14 @@ from demisto_sdk.yaml_tools.content_creator import ContentCreator
 from demisto_sdk.yaml_tools.update_playbook import PlaybookYMLFormat
 from demisto_sdk.json_to_outputs.json_to_outputs import json_to_outputs
 from demisto_sdk.yaml_tools.update_integration import IntegrationYMLFormat
-from demisto_sdk.common.constants import SCRIPT_PREFIX, INTEGRATION_PREFIX
+from demisto_sdk.common.constants import SCRIPT_PREFIX, INTEGRATION_PREFIX, YML_ALL_INTEGRATION_REGEXES, \
+    YML_ALL_SCRIPTS_REGEXES, YML_ALL_PLAYBOOKS_REGEX
 
+
+from demisto_sdk.git_tools import Git
 from demisto_sdk.test_playbook_generator.test_playbook_generator import TestPlaybookGenerator
-
+from demisto_sdk.common.tools import run_command
+from demisto_sdk.common.tools import checked_type
 pass_config = click.make_pass_decorator(DemistoSDK, ensure=True)
 
 
@@ -288,20 +293,36 @@ def lint(config, dir, **kwargs):
 )
 @click.option(
     "-t", "--file-type", type=click.Choice(["integration", "script", "playbook"]),
-    help="The type of yml file to be formatted.", required=True)
+    help="The type of yml file to be formatted.")
 @click.option(
-    "-s", "--source-file", help="The path of the script yml file", required=True)
+    "-s", "--source-file", help="The path of the script yml file")
 @click.option(
     "-o", "--output-file-name", help="The path where the formatted file will be saved to")
-def format_yml(file_type, **kwargs):
+@click.option(
+    '-g', '--use-git', is_flag=True, show_default=True,
+    default=False, help='Format changed files using git - this will format your branch changes and will run only on them.')
+def format_yml(**kwargs):
     file_type_and_linked_class = {
         'integration': IntegrationYMLFormat,
         'script': ScriptYMLFormat,
         'playbook': PlaybookYMLFormat
     }
-    if file_type in file_type_and_linked_class:
-        format_object = file_type_and_linked_class[file_type](**kwargs)
+    if kwargs.get('file_type') in file_type_and_linked_class:
+        format_object = file_type_and_linked_class[kwargs['file_type']](**kwargs)
         return format_object.format_file()
+
+    elif kwargs.get('use_git'):
+        files = Git.get_changed_files(filter_results=lambda _file: not _file.pop('status') == 'D')
+        for _file in files:
+            _file = _file['name']
+            file_type = 'integration' if checked_type(_file, YML_ALL_INTEGRATION_REGEXES)\
+                else 'script' if checked_type(_file, YML_ALL_SCRIPTS_REGEXES)\
+                else 'playbook' if checked_type(_file, YML_ALL_PLAYBOOKS_REGEX)\
+                else None
+            if file_type:
+                res = file_type_and_linked_class[file_type](source_file=_file).format_file()
+                if res:
+                    return res
 
     return 1
 

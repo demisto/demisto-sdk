879880c8cb2179a2139173537ff7754adf7454a6
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-01-24 00:25:14 +0200

    fix description header

--- demisto_sdk/common/scripts/dev_scripts/run_dev_tasks_pwsh.sh
+++ demisto_sdk/common/scripts/dev_scripts/run_dev_tasks_pwsh.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-# Run pylint and pytest in the current directory.
-# Used by pkg_dev_test_tasks.py to run pylint and pytest
+# Run ScriptAnalayzer (lint) and Pester (test) in the current directory.
 # inside a docker. Since this is meant to run inside a minimal docker
 # image it uses sh and not bash. Additionally, script tries to keep it
 # simply and not use any shell utilities that may be missing in a minimal docker.
 
 # Env variables:
-# PYLINT_FILES: file names to pass to pylint
-# PYLINT_SKIP: if set will skip pylint
-# PYTEST_SKIP: if set will skip pytest
-# PYTEST_FAIL_NO_TESTS: if set will fail if no tests are defined
-# CPU_NUM: number of CPUs to run tests on
+# PS_LINT_FILES: file names to pass to ScriptAnalayzer
+# PS_LINT_SKIP: if set will skip lint
+# PS_TEST_SKIP: if set will skip testing
 
 pslint_return=0
 if [ -z "${PS_LINT_SKIP}" ]; then

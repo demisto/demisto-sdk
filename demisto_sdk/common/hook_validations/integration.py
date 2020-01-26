7650ef0bf50b909b6378a85689c45d6fe9614539
Bar Hochman <11165655+jochman@users.noreply.github.com>
2020-01-26 15:59:11 +0200

    Fix wrong term in dockerimage validation (#171)
    * added prints to validate file path
    fixed term in docker image validator
    
    * fix docker validation for scripts too

--- demisto_sdk/common/hook_validations/integration.py
+++ demisto_sdk/common/hook_validations/integration.py
@@ -482,7 +482,7 @@ class IntegrationValidator(BaseValidator):
     def is_docker_image_valid(self):
         # type: () -> bool
         docker_image_validator = DockerImageValidator(self.file_path, is_modified_file=True, is_integration=True)
-        if not docker_image_validator.is_docker_image_valid():
+        if docker_image_validator.is_docker_image_valid():
             return True
         self.is_valid = False
         return False

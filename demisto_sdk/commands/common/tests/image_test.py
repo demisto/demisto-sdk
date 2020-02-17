e22c83a40897db856b37e3928b107b47f67b0b3a
bark@demisto.com <bkatzir@paloaltonetworks.com>
2020-02-17 10:16:40 +0200

    use image from init path

--- demisto_sdk/commands/common/tests/image_test.py
+++ demisto_sdk/commands/common/tests/image_test.py
@@ -16,8 +16,8 @@ def test_is_not_default_image():
     image_validator = image.ImageValidator(image_path)
     assert image_validator.is_not_default_image() is False
 
-    image_path = os.path.normpath(os.path.join(__file__, f'{git_path()}/demisto_sdk/tests', 'test_files',
-                                               'HelloWorld_image.png'))
+    image_path = os.path.normpath(os.path.join(__file__, f'{git_path()}/demisto_sdk/commands/init/templates',
+                                               'HelloWorld', 'HelloWorld_image.png'))
     image.YML_INTEGRATION_REGEXES.append(image_path)
     image_validator = image.ImageValidator(image_path)
     assert image_validator.is_not_default_image() is False

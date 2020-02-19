cc7f9ac49227f9f9f8afd6f984b2277a73fc4e3c
bark@demisto.com <bkatzir@paloaltonetworks.com>
2020-02-19 11:10:45 +0200

    yaakovi cr 102

--- demisto_sdk/commands/create_artifacts/content_creator.py
+++ demisto_sdk/commands/create_artifacts/content_creator.py
@@ -6,7 +6,6 @@ import json
 import shutil
 import zipfile
 from typing import List
-import yaml
 from ruamel.yaml import YAML
 
 from demisto_sdk.commands.unify.unifier import Unifier
@@ -141,7 +140,7 @@ class ContentCreator:
         shutil.copyfile(path, out_path)
 
     @staticmethod
-    def copy_content_yml(path, out_path, yml_info, yml_unified):
+    def copy_content_yml(path, out_path, yml_info):
         """
         Copy content ymls (except for playbooks) to the out_path (presumably a bundle)
         """
@@ -151,7 +150,7 @@ class ContentCreator:
             if parent_dir_name != SCRIPTS_DIR:
                 script_obj = yml_info['script']
             unifier = Unifier(os.path.dirname(path), parent_dir_name, out_path)
-            out_map = unifier.write_yaml_with_docker(yml_unified, yml_info, script_obj)
+            out_map = unifier.write_yaml_with_docker(yml_info, script_obj)
 
             if len(out_map.keys()) > 1:
                 print(" - yaml generated multiple files: {}".format(out_map.keys()))
@@ -175,19 +174,17 @@ class ContentCreator:
             if len(os.path.basename(path)) >= self.file_name_max_size:
                 self.long_file_names.append(path)
 
-            with open(path, 'r') as file_:
-                yml_info = yaml.safe_load(file_)
+            ryaml = YAML()
+            ryaml.allow_duplicate_keys = True
+            with io.open(path, mode='r', encoding='utf-8') as file_:
+                yml_info = ryaml.load(file_)
             ver = yml_info.get('fromversion', '0')
             print(f' - processing: {ver} ({path})')
 
             if dir_name in ['Playbooks', 'TestPlaybooks']:
                 self.copy_playbook_yml(path, os.path.join(bundle, os.path.basename(path)))
             else:
-                ryaml = YAML()
-                ryaml.allow_duplicate_keys = True
-                with io.open(path, mode='r', encoding='utf-8') as file_:
-                    yml_unified = ryaml.load(file_)
-                self.copy_content_yml(path, os.path.join(bundle, os.path.basename(path)), yml_info, yml_unified)
+                self.copy_content_yml(path, os.path.join(bundle, os.path.basename(path)), yml_info)
             content_files += 1
         print(f' - total files: {content_files}')
 

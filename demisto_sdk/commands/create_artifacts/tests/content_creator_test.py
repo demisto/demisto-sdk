fbf272121a5a4934b1c3c7b7acda75b246fd069c
bark@demisto.com <bkatzir@paloaltonetworks.com>
2020-02-19 11:43:18 +0200

    tmpdir twik

--- demisto_sdk/commands/create_artifacts/tests/content_creator_test.py
+++ demisto_sdk/commands/create_artifacts/tests/content_creator_test.py
@@ -1,4 +1,6 @@
 import filecmp
+from tempfile import mkdtemp
+
 from demisto_sdk.commands.create_artifacts.content_creator import *
 from demisto_sdk.commands.common.git_tools import git_path
 
@@ -8,15 +10,15 @@ class TestContentCreator:
         current_dir = os.path.normpath(os.path.join(__file__,
                                                     f'{git_path()}/demisto_sdk/commands/create_artifacts', 'tests'))
         self.scripts_full_path = os.path.join(current_dir, 'test_files', 'content_repo_example', 'Scripts')
-        self.content_bundle = os.path.join(current_dir, 'test_files', 'content_repo_example', 'bundle_content')
+        self._dir = mkdtemp()
         self.content_bundle_full_path = os.path.join(current_dir, 'test_files', 'content_repo_example',
                                                      'bundle_content')
         self.content_repo = os.path.join(current_dir, 'test_files', 'content_repo_example')
 
     def teardown(self):
         # delete all files in the content_bundle
-        for filename in os.listdir(self.content_bundle_full_path):
-            file_path = os.path.join(self.content_bundle_full_path, filename)
+        for filename in os.listdir(self._dir):
+            file_path = os.path.join(self._dir, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.unlink(file_path)
@@ -36,10 +38,11 @@ class TestContentCreator:
         - ensure no !!omap are added(due to ordereddict yaml problems)
         """
         content_creator = ContentCreator(artifacts_path=self.content_repo, content_version='2.5.0',
+                                         content_bundle_path=self._dir,
                                          preserve_bundles=False)
         print(self.scripts_full_path)
-        print(self.content_bundle_full_path)
+        print(self._dir)
         content_creator.copy_dir_files(self.scripts_full_path, content_creator.content_bundle)
 
         assert filecmp.cmp(f'{self.scripts_full_path}/script-Sleep.yml',
-                           f'{self.content_bundle_full_path}/script-Sleep.yml')
+                           f'{self._dir}/script-Sleep.yml')

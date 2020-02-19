760d8a761747f27a87f751446a67dd5fa4382579
bark@demisto.com <bkatzir@paloaltonetworks.com>
2020-02-19 17:18:24 +0200

    added more test cases and edge cases2

--- demisto_sdk/commands/common/hook_validations/integration.py
+++ demisto_sdk/commands/common/hook_validations/integration.py
@@ -391,10 +391,9 @@ class IntegrationValidator(BaseValidator):
         current_command_to_context_paths = self._get_command_to_context_paths(self.current_file)
         old_command_to_context_paths = self._get_command_to_context_paths(self.old_file)
 
-        if not current_command_to_context_paths and not old_command_to_context_paths:
+        if not old_command_to_context_paths:
             return False
-        if (not current_command_to_context_paths and old_command_to_context_paths) or \
-                (current_command_to_context_paths and not old_command_to_context_paths):
+        if not current_command_to_context_paths and old_command_to_context_paths:
             return True
         for old_command, old_context_paths in old_command_to_context_paths.items():
             if old_command in current_command_to_context_paths.keys():

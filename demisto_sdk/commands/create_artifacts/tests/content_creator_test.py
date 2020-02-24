0889f7fa669936fb7912c17b9f16047739d3b0ee
Bar Katzir <37335599+bakatzir@users.noreply.github.com>
2020-02-24 11:28:59 +0200

    add playbook- prefix to test playbooks in Packs (#216)
    * add playbook- prefix to test playbooks in Packs
    
    * add missing file to unit test
    
    * rename file
    
    * rm file
    
    * add changelog

--- demisto_sdk/commands/create_artifacts/tests/content_creator_test.py
+++ demisto_sdk/commands/create_artifacts/tests/content_creator_test.py
@@ -11,6 +11,7 @@ class TestContentCreator:
         self.scripts_full_path = os.path.join(tests_dir, 'test_files', 'content_repo_example', 'Scripts')
         self.integrations_full_path = os.path.join(tests_dir, 'test_files', 'content_repo_example', 'Integrations')
         self.TestPlaybooks_full_path = os.path.join(tests_dir, 'test_files', 'content_repo_example', 'TestPlaybooks')
+        self.Packs_full_path = os.path.join(tests_dir, 'test_files', 'content_repo_example', 'Packs')
         self._bundle_dir = mkdtemp()
         self._test_dir = mkdtemp()
         self.content_repo = os.path.join(tests_dir, 'test_files', 'content_repo_example')
@@ -32,7 +33,7 @@ class TestContentCreator:
     def test_copy_dir_files(self):
         """
         Given
-        - valid script-<name>.yml file
+        - valid content dir, including Scripts, Integrations and TestPlaybooks sub-dirs
         When
         - copying the content folder to a content bundle(flatten files)
         Then
@@ -43,16 +44,40 @@ class TestContentCreator:
                                          content_bundle_path=self._bundle_dir,
                                          test_bundle_path=self._test_dir,
                                          preserve_bundles=False)
+
         # test Scripts repo copy
         content_creator.copy_dir_files(self.scripts_full_path, content_creator.content_bundle)
         assert filecmp.cmp(f'{self.scripts_full_path}/script-Sleep.yml',
                            f'{self._bundle_dir}/script-Sleep.yml')
+
         # test Integrations repo copy
         content_creator.copy_dir_files(f'{self.integrations_full_path}/Securonix', content_creator.content_bundle)
         print(f'{self._bundle_dir}/Panorama.yml')
         assert filecmp.cmp(f'{self.integrations_full_path}/Securonix/Securonix_unified.yml',
                            f'{self._bundle_dir}/Securonix_unified.yml')
+
         # test TestPlaybooks repo copy
         content_creator.copy_dir_files(self.TestPlaybooks_full_path, content_creator.test_bundle)
         assert filecmp.cmp(f'{self.TestPlaybooks_full_path}/script-Sleep-for-testplaybook.yml',
                            f'{self._test_dir}/script-Sleep-for-testplaybook.yml')
+
+    def test_copy_packs_content_to_packs_bundle(self):
+        """
+        Given
+        - valid content dir, including Packs sub-dir
+        When
+        - copying the content folder to a content bundle(flatten files)
+        Then
+        - ensure files are being flatten correctly
+        - ensure no !!omap are added(due to ordereddict yaml problems)
+        - ensure TestPlaybooks without the playbook-* prefix are valid
+        """
+        content_creator = ContentCreator(artifacts_path=self.content_repo, content_version='2.5.0',
+                                         content_bundle_path=self._bundle_dir,
+                                         test_bundle_path=self._test_dir,
+                                         preserve_bundles=False)
+
+        # test Packs repo, TestPlaybooks repo copy without playbook- prefix
+        content_creator.copy_packs_content_to_old_bundles([f'{self.Packs_full_path}/FeedAzure'])
+        assert filecmp.cmp(f'{self.Packs_full_path}/FeedAzure/TestPlaybooks/FeedAzure_test.yml',
+                           f'{self._test_dir}/playbook-FeedAzure_test.yml')

80a8958a3fa3cdbf40895fadfe25d9dbe24d8eb9
bark@demisto.com <bkatzir@paloaltonetworks.com>
2020-02-27 13:08:19 +0200

    add import

--- demisto_sdk/commands/common/hook_validations/incident_field.py
+++ demisto_sdk/commands/common/hook_validations/incident_field.py
@@ -1,6 +1,7 @@
 """
 This module is designed to validate the correctness of incident field entities in content.
 """
+from distutils.version import LooseVersion
 from demisto_sdk.commands.common.constants import Errors
 from demisto_sdk.commands.common.hook_validations.base_validator import BaseValidator
 from demisto_sdk.commands.common.tools import print_error

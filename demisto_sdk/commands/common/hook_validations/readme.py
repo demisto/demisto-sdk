99a86a94dabb8b3675800176434802e43373d1d9
yorhov <Orekhova97229!>
2020-02-27 13:08:59 +0200

    added try except

--- demisto_sdk/commands/common/hook_validations/readme.py
+++ demisto_sdk/commands/common/hook_validations/readme.py
@@ -9,20 +9,24 @@ class ReadMeValidator:
 
     def is_file_valid(self):
         mdx_parse = f'{os.path.dirname(os.path.abspath(__file__))}/../mdx-parse.js'
-        is_node = subprocess.run(['node', '-v'], text=True, timeout=10, capture_output=True)
-        is_mdx = subprocess.run(['npm', 'ls', '@mdx-js/mdx'], text=True, timeout=10, capture_output=True)
-        is_fs_extra = subprocess.run(['npm', 'ls', 'fs-extra'], text=True, timeout=10, capture_output=True)
-        is_commander = subprocess.run(['npm', 'ls', 'commander'], text=True, timeout=10, capture_output=True)
-        if is_node.returncode == 0 and is_mdx.returncode == 0 and is_fs_extra.returncode == 0 and\
-                is_commander.returncode == 0:
-            res = subprocess.run(['node', mdx_parse, '-f', self.file_path], text=True, timeout=10, capture_output=True)
-            if res.returncode != 0:
-                print_error(f'Failed verfiying: {self.file_path}. Error: {res.stderr}')
-                return False
-        else:
-            print_warning(
-                f'There is no node or packages installed in your system.\n status of node {is_node.returncode}\n '
-                f'status of mdx package {is_mdx.returncode}\n status of fs_extra {is_fs_extra.returncode}\n '
-                f'status of commander {is_commander.returncode}')
+        try:
+            is_node = subprocess.run(['node', '-v'], text=True, timeout=10, capture_output=True)
+            is_mdx = subprocess.run(['npm', 'ls', '@mdx-js/mdx'], text=True, timeout=10, capture_output=True)
+            is_fs_extra = subprocess.run(['npm', 'ls', 'fs-extra'], text=True, timeout=10, capture_output=True)
+            is_commander = subprocess.run(['npm', 'ls', 'commander'], text=True, timeout=10, capture_output=True)
+            if is_node.returncode == 0 and is_mdx.returncode == 0 and is_fs_extra.returncode == 0 and\
+                    is_commander.returncode == 0:
+                res = subprocess.run(['node', mdx_parse, '-f', self.file_path], text=True, timeout=10, capture_output=True)
+                if res.returncode != 0:
+                    print_error(f'Failed verfiying: {self.file_path}. Error: {res.stderr}')
+                    return False
+            else:
+                print_warning(
+                    f'There is no node or packages installed in your system.\n status of node {is_node.returncode}\n '
+                    f'status of mdx package {is_mdx.returncode}\n status of fs_extra {is_fs_extra.returncode}\n '
+                    f'status of commander {is_commander.returncode}')
+                return None
+            return True
+        except Exception as err:
+            print_warning(err)
             return None
-        return True

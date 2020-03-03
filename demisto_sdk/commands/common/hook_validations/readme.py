1077d73ac2bebd892f8d846453e8079d21f58279
yorhov <Orekhova97229!>
2020-03-03 13:14:15 +0200

    added comments

--- demisto_sdk/commands/common/hook_validations/readme.py
+++ demisto_sdk/commands/common/hook_validations/readme.py
@@ -7,27 +7,34 @@ class ReadMeValidator:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def is_file_valid(self):
+    def is_file_valid(self) -> bool:
+        is_valid = True
         mdx_parse = f'{os.path.dirname(os.path.abspath(__file__))}/../mdx-parse.js'
         try:
+            # check if requiring modules in node exist
             is_node = subprocess.run(['node', '-v'], text=True, timeout=10, capture_output=True)
             is_mdx = subprocess.run(['npm', 'ls', '@mdx-js/mdx'], text=True, timeout=10, capture_output=True)
             is_fs_extra = subprocess.run(['npm', 'ls', 'fs-extra'], text=True, timeout=10, capture_output=True)
             is_commander = subprocess.run(['npm', 'ls', 'commander'], text=True, timeout=10, capture_output=True)
             if is_node.returncode == 0 and is_mdx.returncode == 0 and is_fs_extra.returncode == 0 and \
                     is_commander.returncode == 0:
+                # run the java script mdx parse validator
                 res = subprocess.run(['node', mdx_parse, '-f', self.file_path], text=True, timeout=10,
                                      capture_output=True)
                 if res.returncode != 0:
-                    print_error(f'Failed verfiying README.md, Path: {self.file_path}. Error Message is: {res.stderr}')
-                    return False
+                    print_error(f'Failed verifying README.md, Path: {self.file_path}. Error Message is: {res.stderr}')
+                    is_valid = False
             else:
-                print_warning(
-                    f'Status of node {is_node.returncode}\n '
-                    f'Status of mdx package {is_mdx.returncode}\n status of fs_extra {is_fs_extra.returncode}\n '
-                    f'Status of commander {is_commander.returncode}')
-                return None
-            return True
-        except Exception as err:
-            print_warning(f'There is no node installed on the machine, Test Skipped, error {err}')
-            return None
+                # if modules are not installed, the readme file would not be validated
+                print_warning(f'There are some modules missing, Test Skipped,\n'
+                              f'Status of node {is_node.returncode}\n '
+                              f'Status of mdx package {is_mdx.returncode}\n status of fs_extra {is_fs_extra.returncode}'
+                              f'\n '
+                              f'Status of commander {is_commander.returncode}')
+        except Exception as err:  # check if you can catch a more exact exception
+            if "No such file or directory: 'node': 'node'" in res.stderr:
+                print_warning(f'There is no node installed on the machine, Test Skipped, error {err}')
+            else:
+                print_error(f'Failed while verifying README.md, Path: {self.file_path}. Error Message is: {res.stderr}')
+                is_valid = False
+        return is_valid
--- demisto_sdk/commands/common/tests/readme_test.py
+++ demisto_sdk/commands/common/tests/readme_test.py
@@ -4,15 +4,14 @@ from demisto_sdk.commands.common.git_tools import git_path
 
 VALID_MD = f'{git_path()}/demisto_sdk/tests/test_files/README-valid.md'
 INVALID_MD = f'{git_path()}/demisto_sdk/tests/test_files/README-invalid.md'
-INVALID_MD_NODE = f'{git_path()}/demisto_sdk/tests/test_files/README-invalid.md'
 
 README_INPUTS = [
-    (VALID_MD, True, None),
-    (INVALID_MD, False, None),
+    (VALID_MD, True),
+    (INVALID_MD, False),
 ]
 
 
-@pytest.mark.parametrize("current, answer, NoNode", README_INPUTS)
-def test_is_file_valid(current, answer, NoNode):
+@pytest.mark.parametrize("current, answer", README_INPUTS)
+def test_is_file_valid(current, answer):
     readme_validator = ReadMeValidator(current)
-    assert readme_validator.is_file_valid() is answer or readme_validator.is_file_valid() is NoNode
+    assert readme_validator.is_file_valid() is answer

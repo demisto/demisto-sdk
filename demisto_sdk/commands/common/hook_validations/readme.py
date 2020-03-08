022186d70277c0bedd76cec790042cbf15035d24
yorhov <Orekhova97229!>
2020-03-08 09:30:08 +0200

    only pykwalify.core logs will not be printed

--- demisto_sdk/commands/common/hook_validations/readme.py
+++ demisto_sdk/commands/common/hook_validations/readme.py
@@ -6,8 +6,6 @@ from demisto_sdk.commands.common.tools import print_error, print_warning
 class ReadMeValidator:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.is_valid = True
-        self.ready = False
 
     def is_valid_file(self):
         """Check whether the readme file is valid or not
@@ -19,30 +17,49 @@ class ReadMeValidator:
 
     def is_mdx_file(self) -> bool:
         mdx_parse = f'{os.path.dirname(os.path.abspath(__file__))}/../mdx-parse.js'
-        self.are_modules_installed_for_verify()
-        if self.ready:
+        ready, is_valid = self.are_modules_installed_for_verify()
+        if ready:
             # run the java script mdx parse validator
             res = subprocess.run(['node', mdx_parse, '-f', self.file_path], text=True, timeout=10,
                                  capture_output=True)
             if res.returncode != 0:
                 print_error(f'Failed verifying README.md, Path: {self.file_path}. Error Message is: {res.stderr}')
-                self.is_valid = False
-        return self.is_valid
+                is_valid = False
+        return is_valid
 
     def are_modules_installed_for_verify(self):
+        is_valid = True
+        ready = False
         try:
             # check if requiring modules in node exist
             is_node = subprocess.run(['node', '-v'], text=True, timeout=10, capture_output=True)
+            print(f"return code node {is_node.stdout}")
             is_mdx = subprocess.run(['npm', 'ls', '@mdx-js/mdx'], text=True, timeout=10, capture_output=True)
+            print(f"return code node {is_mdx.stdout}")
             is_fs_extra = subprocess.run(['npm', 'ls', 'fs-extra'], text=True, timeout=10, capture_output=True)
+            print(f"return code node {is_fs_extra.stdout}")
             is_commander = subprocess.run(['npm', 'ls', 'commander'], text=True, timeout=10, capture_output=True)
+            print(f"return code node {is_commander.stdout}")
+
             if is_node.returncode == 0 and is_mdx.returncode == 0 and is_fs_extra.returncode == 0 and \
                     is_commander.returncode == 0:
-                self.ready = True
+                ready = True
+            else:
+                print_warning(f'There are some modules that are not installed on the machine, Test Skipped\n'
+                              f' error {is_mdx.stdout} \n'
+                              f' error {is_fs_extra.stdout}\n'
+                              f' error {is_commander}')
+
         except Exception as err:
             if "No such file or directory: 'node': 'node'" in str(err):
                 print_warning(f'There is no node installed on the machine, Test Skipped, error {err}')
+            if "Cannot find module 'fs-extra'" in str(err):
+                print_warning(f'There is no fs-extra module installed on the machine, Test Skipped, error {err}')
+            if "Cannot find module '@mdx-js/mdx'" in str(err):
+                print_warning(f'There is no @mdx-js/mdx module installed on the machine, Test Skipped, error {err}')
+            if "Cannot find module 'commander'" in str(err):
+                print_warning(f'There is no commander module installed on the machine, Test Skipped, error {err}')
             else:
                 print_error(f'Failed while verifying README.md, Path: {self.file_path}. Error Message is: {err}')
-                self.is_valid = False
-        return self.ready
+                is_valid = False
+        return ready, is_valid

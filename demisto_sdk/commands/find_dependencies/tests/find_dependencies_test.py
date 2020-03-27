cd0fe866c7356b97e3f9d30ba6abb809ddb508fa
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-03-28 02:19:31 +0300

    added filtering unit tests

--- demisto_sdk/commands/find_dependencies/tests/find_dependencies_test.py
+++ demisto_sdk/commands/find_dependencies/tests/find_dependencies_test.py
@@ -1,8 +1,7 @@
 import pytest
 import json
 import os
-# import networkx
-# from demisto_sdk.commands.find_dependencies.find_dependencies import PackDependencies
+from demisto_sdk.commands.find_dependencies.find_dependencies import PackDependencies
 
 
 @pytest.fixture(scope="module")
@@ -15,4 +14,76 @@ def id_set():
 
 
 class TestIdSetFilters:
-    pass
+    @pytest.mark.parametrize("item_section", ["scripts", "playbooks"])
+    def test_search_for_pack_item_with_no_result(self, item_section, id_set):
+        pack_id = "Non Existing Pack Name"
+        found_filtered_result = PackDependencies._search_for_pack_items(pack_id, id_set[item_section])
+
+        assert len(found_filtered_result) == 0
+
+    @pytest.mark.parametrize("pack_id", ["CalculateTimeDifference", "Expanse", "HelloWorld"])
+    def test_search_for_pack_script_item(self, pack_id, id_set):
+        found_filtered_result = PackDependencies._search_for_pack_items(pack_id, id_set['scripts'])
+
+        assert len(found_filtered_result) > 0
+
+    def test_search_for_specific_pack_script_item(self, id_set):
+        pack_id = "PrismaCloudCompute"
+        found_filtered_result = PackDependencies._search_for_pack_items(pack_id, id_set['scripts'])
+        expected_result = [
+            {
+                "PrismaCloudComputeParseAuditAlert": {
+                    "name": "PrismaCloudComputeParseAuditAlert",
+                    "file_path": "Packs/PrismaCloudCompute/Scripts/PrismaCloudComputeParseAuditAlert/PrismaCloudComputeParseAuditAlert.yml",
+                    "pack": "PrismaCloudCompute"
+                }
+            },
+            {
+                "PrismaCloudComputeParseCloudDiscoveryAlert": {
+                    "name": "PrismaCloudComputeParseCloudDiscoveryAlert",
+                    "file_path": "Packs/PrismaCloudCompute/Scripts/PrismaCloudComputeParseCloudDiscoveryAlert/PrismaCloudComputeParseCloudDiscoveryAlert.yml",
+                    "pack": "PrismaCloudCompute"
+                }
+            },
+            {
+                "PrismaCloudComputeParseComplianceAlert": {
+                    "name": "PrismaCloudComputeParseComplianceAlert",
+                    "file_path": "Packs/PrismaCloudCompute/Scripts/PrismaCloudComputeParseComplianceAlert/PrismaCloudComputeParseComplianceAlert.yml",
+                    "pack": "PrismaCloudCompute"
+                }
+            },
+            {
+                "PrismaCloudComputeParseVulnerabilityAlert": {
+                    "name": "PrismaCloudComputeParseVulnerabilityAlert",
+                    "file_path": "Packs/PrismaCloudCompute/Scripts/PrismaCloudComputeParseVulnerabilityAlert/PrismaCloudComputeParseVulnerabilityAlert.yml",
+                    "pack": "PrismaCloudCompute"
+                }
+            }
+        ]
+
+        assert found_filtered_result == expected_result
+
+    @pytest.mark.parametrize("pack_id", ["Claroty", "Code42", "Cymulate"])
+    def test_search_for_pack_playbook_item(self, pack_id, id_set):
+        found_filtered_result = PackDependencies._search_for_pack_items(pack_id, id_set['playbooks'])
+
+        assert len(found_filtered_result) > 0
+
+    def test_search_for_specific_pack_playbook_item(self, id_set):
+        pack_id = "Expanse"
+        found_filtered_result = PackDependencies._search_for_pack_items(pack_id, id_set['playbooks'])
+        expected_result = [
+            {
+                "ExpanseParseRawIncident": {
+                    "name": "Expanse Incident Playbook",
+                    "file_path": "Packs/Expanse/Playbooks/Expanse_Incident_Playbook.yml",
+                    "fromversion": "5.0.0",
+                    "implementing_scripts": [
+                        "ExpanseParseRawIncident"
+                    ],
+                    "pack": "Expanse"
+                }
+            }
+        ]
+
+        assert found_filtered_result == expected_result

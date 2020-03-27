51dbd5f63b2046464206a53d912d20555472e951
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-03-28 01:15:43 +0300

    added dummy id-set

--- demisto_sdk/commands/find_dependencies/tests/find_dependencies_test.py
+++ demisto_sdk/commands/find_dependencies/tests/find_dependencies_test.py
@@ -1,12 +1,17 @@
 import pytest
 import json
-import networkx
-from demisto_sdk.commands.find_dependencies.find_dependencies import PackDependencies
+import os
+# import networkx
+# from demisto_sdk.commands.find_dependencies.find_dependencies import PackDependencies
 
 
 @pytest.fixture(scope="module")
 def id_set():
-    pass
+    id_set_path = os.path.join('test_data', 'id_set.json')
+
+    with open(id_set_path, 'r') as id_set_file:
+        id_set = json.load(id_set_file)
+        yield id_set
 
 
 class TestIdSetFilters:

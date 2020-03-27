77cc3465b759fb2a4cb31a008ca993c28ebb0a31
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-03-28 00:56:12 +0300

    added dummy id-set

---
+++ demisto_sdk/commands/find_dependencies/tests/find_dependencies_test.py
@@ -0,0 +1,13 @@
+import pytest
+import json
+import networkx
+from demisto_sdk.commands.find_dependencies.find_dependencies import PackDependencies
+
+
+@pytest.fixture(scope="module")
+def id_set():
+    pass
+
+
+class TestIdSetFilters:
+    pass

c0cc33569c6a55560f607b25a0adc5afb37dabf5
gal <galrabin@gmail.com>
2020-03-29 18:21:37 +0300

    flake8 unit-tests

--- demisto_sdk/commands/lint/tests/test_linter/os_runner_test.py
+++ demisto_sdk/commands/lint/tests/test_linter/os_runner_test.py
@@ -40,18 +40,6 @@ class TestFlake8:
         assert exit_code == 0b1, "Exit code should be 1"
         assert output == expected_output, "Output should be empty"
 
-    def test_run_flake8_exception(self, linter_obj: Linter, lint_files: List[Path], mocker):
-        from demisto_sdk.commands.lint import helpers
-
-        mocker.patch.object(helpers, 'Popen')
-        expected_output = 'Boom'
-        helpers.Popen.side_effect = OSError(expected_output)
-
-        exit_code, output = linter_obj._run_flake8(lint_files=lint_files)
-
-        assert exit_code == 0b1, "Exit code should be 1"
-        assert output == expected_output, "Output should be empty"
-
 
 class TestBandit:
     def test_run_bandit_success(self, linter_obj: Linter, lint_files: List[Path], mocker):
@@ -83,18 +71,6 @@ class TestBandit:
         mocker.patch.object(linter, 'run_command_os')
         expected_output = 'Error code found'
         linter.run_command_os.return_value = ('not good', expected_output, 1)
-
-        exit_code, output = linter_obj._run_bandit(lint_files=lint_files)
-
-        assert exit_code == 0b1, "Exit code should be 1"
-        assert output == expected_output, "Output should be empty"
-
-    def test_run_bandit_exception(self, linter_obj: Linter, lint_files: List[Path], mocker):
-        from demisto_sdk.commands.lint import helpers
-
-        mocker.patch.object(helpers, 'Popen')
-        expected_output = 'Boom'
-        helpers.Popen.side_effect = OSError(expected_output)
 
         exit_code, output = linter_obj._run_bandit(lint_files=lint_files)
 
@@ -138,18 +114,6 @@ class TestMypy:
         assert exit_code == 0b1, "Exit code should be 1"
         assert output == expected_output, "Output should be empty"
 
-    def test_run_mypy_exception(self, linter_obj: Linter, lint_files: List[Path], mocker):
-        from demisto_sdk.commands.lint import helpers
-
-        mocker.patch.object(helpers, 'Popen')
-        expected_output = 'Boom'
-        helpers.Popen.side_effect = OSError(expected_output)
-
-        exit_code, output = linter_obj._run_mypy(lint_files=lint_files, py_num=3.7)
-
-        assert exit_code == 0b1, "Exit code should be 1"
-        assert output == expected_output, "Output should be empty"
-
 
 class TestVulture:
     def test_run_vulture_success(self, linter_obj: Linter, lint_files: List[Path], mocker):
@@ -181,18 +145,6 @@ class TestVulture:
         mocker.patch.object(linter, 'run_command_os')
         expected_output = 'Error code found'
         linter.run_command_os.return_value = ('not good', expected_output, 1)
-
-        exit_code, output = linter_obj._run_vulture(lint_files=lint_files, py_num=3.7)
-
-        assert exit_code == 0b1, "Exit code should be 1"
-        assert output == expected_output, "Output should be empty"
-
-    def test_run_vulture_exception(self, linter_obj: Linter, lint_files: List[Path], mocker):
-        from demisto_sdk.commands.lint import helpers
-
-        mocker.patch.object(helpers, 'Popen')
-        expected_output = 'Boom'
-        helpers.Popen.side_effect = OSError(expected_output)
 
         exit_code, output = linter_obj._run_vulture(lint_files=lint_files, py_num=3.7)
 

2987b294be26d9ab2bb828f60d8e76c1eb4876d7
yorhov <Orekhova97229!>
2020-04-07 19:19:57 +0300

    unit testing for addition

--- demisto_sdk/commands/common/tests/tools_test.py
+++ demisto_sdk/commands/common/tests/tools_test.py
@@ -90,14 +90,14 @@ class TestGenericFunctions:
 
 class TestGetRemoteFile:
     def test_get_remote_file_sanity(self):
-        gmail_yml = tools.get_remote_file('Integrations/Gmail/Gmail.yml')
-        assert gmail_yml
-        assert gmail_yml['commonfields']['id'] == 'Gmail'
+        hello_world_yml = tools.get_remote_file('Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.yml')
+        assert hello_world_yml
+        assert hello_world_yml['commonfields']['id'] == 'HelloWorld'
 
     def test_get_remote_file_origin(self):
-        gmail_yml = tools.get_remote_file('Integrations/Gmail/Gmail.yml', 'master')
-        assert gmail_yml
-        assert gmail_yml['commonfields']['id'] == 'Gmail'
+        hello_world_yml = tools.get_remote_file('Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.yml', 'master')
+        assert hello_world_yml
+        assert hello_world_yml['commonfields']['id'] == 'HelloWorld'
 
     def test_get_remote_file_tag(self):
         gmail_yml = tools.get_remote_file('Integrations/Gmail/Gmail.yml', '19.10.0')

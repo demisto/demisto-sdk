a2578fc03d59444f6f4d1ef1c5e0f5848e3ba33b
Andrew Shamah <andrew@demisto.com>
2020-04-12 14:23:05 +0300

    trying to squeak over the test coverage threshold

--- demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
+++ demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
@@ -15,3 +15,23 @@ def test_format_filename():
     filepath = 'sample_test.md'
     filename = update_rn.format_filename(filepath)
     assert expected_result == filename
+
+
+def test_ident_changed_file_type():
+    expected_result = ('HelloWorld test', 'Integration')
+    from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
+    update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
+    filepath = '/Integration/HelloWorld_test.md'
+    result = update_rn.ident_changed_file_type(filepath)
+    assert expected_result == result
+
+
+def test_return_release_notes_path():
+    expected_result = 'Packs/HelloWorld/ReleaseNotes/1_1_1.md'
+    from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
+    update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
+    input_version = '1.1.1'
+    result = update_rn.return_release_notes_path(input_version)
+    assert expected_result == result
+
+

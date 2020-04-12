a8ddfb226743dfe1ee2af43f9373aaa63f460a14
Andrew Shamah <andrew@demisto.com>
2020-04-12 14:11:32 +0300

    checking tests

--- demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
+++ demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
@@ -6,3 +6,11 @@ def test_build_rn_template():
     changed_items = {"test_integration": "Integration"}
     release_notes = update_rn.build_rn_template(changed_items)
     assert expected_result == release_notes
+
+def test_build_rn_template():
+    expected_result = "sample test"
+    from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
+    update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
+    filepath = 'sample_test.md'
+    filename = update_rn.format_filename(filepath)
+    assert expected_result == filename

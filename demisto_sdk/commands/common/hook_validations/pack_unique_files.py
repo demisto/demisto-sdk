1fc49e8131fc5b1cf3cc2ed39c3c102ef8b70ebd
sberman <sberman@paloaltonetworks.com>
2020-04-13 17:01:59 +0300

    Removed comment

--- demisto_sdk/commands/common/hook_validations/pack_unique_files.py
+++ demisto_sdk/commands/common/hook_validations/pack_unique_files.py
@@ -144,7 +144,6 @@ class PackUniqueFilesValidator:
             if not isinstance(dependencies_field, dict):
                 self._add_error('The dependencies field in the pack must be a dictionary.')
                 return False
-            # TODO: add it back after #23546 is ready.
             price_field = metadata.get(PACK_METADATA_PRICE)
             if price_field:
                 try:

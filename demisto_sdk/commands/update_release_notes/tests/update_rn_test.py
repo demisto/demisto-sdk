d4d3748793e71678eb9b9a97dad6defe7ac80641
Andrew Shamah <andrew@demisto.com>
2020-04-16 12:54:22 +0300

    Fix tests

--- demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
+++ demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
@@ -1,29 +1,20 @@
 
 def test_build_rn_template():
-    expected_result = "<details>\n<summary>HelloWorld</summary>\n\n#### Integrations\n- __test_integration__\n%%UPDATE_RN%%\n</details>"
+    expected_result = "\n#### Integrations\n- __Hello World__\n%%UPDATE_RN%%\n"
     from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
     update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
-    changed_items = {"test_integration": "Integration"}
+    changed_items = {"Hello World": "Integration"}
     release_notes = update_rn.build_rn_template(changed_items)
     assert expected_result == release_notes
 
 
-def test_format_filename():
-    expected_result = "sample test"
+def test_find_corresponding_yml():
+    expected_result = "Integration/HelloWorld.yml"
     from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
     update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
-    filepath = 'sample_test.md'
-    filename = update_rn.format_filename(filepath)
+    filepath = 'Integration/HelloWorld.py'
+    filename = update_rn.find_corresponding_yml(filepath)
     assert expected_result == filename
-
-
-def test_ident_changed_file_type():
-    expected_result = ('HelloWorld test', 'Integration')
-    from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
-    update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
-    filepath = '/Integration/HelloWorld_test.md'
-    result = update_rn.ident_changed_file_type(filepath)
-    assert expected_result == result
 
 
 def test_return_release_notes_path():

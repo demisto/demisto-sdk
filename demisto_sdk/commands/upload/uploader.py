7160e4644939c583666399920487b65d04ddd9a4
mgalitzki <mgalitzki@paloaltonetworks.com>
2020-04-19 16:56:58 +0300

    added support all files

--- demisto_sdk/commands/upload/uploader.py
+++ demisto_sdk/commands/upload/uploader.py
@@ -13,7 +13,7 @@ from demisto_sdk.commands.unify.unifier import Unifier
 class Uploader:
     """Upload a pack specified in self.infile to the remote Demisto instance.
         Attributes:
-            path (str): The path of a pack directory to upload.
+            path (str): The path of a pack / directory / file to upload.
             verbose (bool): Whether to output a detailed response.
             client (DefaultApi): Demisto-SDK client object.
         """
@@ -24,98 +24,111 @@ class Uploader:
         self.client = demisto_client.configure(verify_ssl=not insecure)
 
     def upload(self):
-        """Upload the integration specified in self.infile to the remote Demisto instance.
+        """Upload the pack / directory / file to the remote Demisto instance.
         """
         if os.path.isdir(self.path):
-            list_files_in_dir = get_child_files(self.path)
-            if f'{self.path}/pack_metadata.json' in list_files_in_dir:
-                self.peck_uploader()
+            if self.directory_uploader(self.path):
+                return 0
             else:
-                self.directory_uploader(self.path)
+                self.peck_uploader()
         else:
             file_type = find_type(self.path)
+
             if file_type == "integration":
                 self.integration_uploader(self.path)
-
             elif file_type == "script":
                 self.script_uploader(self.path)
-
             elif file_type == "playbook":
                 self.playbook_uploader(self.path)
-
+            elif file_type == "widget":
+                self.script_uploader(self.path)
+            elif file_type == "incidenttype":
+                self.playbook_uploader(self.path)
+            elif file_type == "classifier":
+                self.script_uploader(self.path)
+            elif file_type == "layout":
+                self.playbook_uploader(self.path)
+            elif file_type == "dashboard":
+                self.script_uploader(self.path)
+            elif file_type == "incidentfield":
+                self.playbook_uploader(self.path)
             else:
-                print_error(f'Error: Path input is not valid. Check the given input path: {self.path}.')
+                print_error(f'Error: Path input is not valid``. Check the given input path: {self.path}.')
         return 0
 
     def peck_uploader(self):
+        """Extracting the directories of the peck and upload them by directory_uploader
+        """
+        file_uploaded = False
         list_directories = get_child_directories(self.path)
         for directory in list_directories:
-            self.directory_uploader(directory)
+            file_uploaded = self.directory_uploader(directory)
+        if not file_uploaded:
+            print_error(f'Error: No files to upload``. Check the given input path: {self.path}.')
 
     def directory_uploader(self, path):
+        """Uploads directories by name
+        """
         if is_path_of_integration_directory(path):
             list_integrations = get_child_directories(path)
             for integration in list_integrations:
                 self.integration_uploader(integration)
+            return True
 
         elif is_path_of_script_directory(path):
             list_script = get_child_directories(path)
             for script in list_script:
                 self.script_uploader(script)
+            return True
 
-        elif is_path_of_playbook_directory(path):
+        elif is_path_of_playbook_directory(path) or is_path_of_test_playbook_directory(path):
             list_playbooks = get_child_files(path)
             for playbook in list_playbooks:
                 if playbook.endswith('.yml'):
                     self.playbook_uploader(playbook)
-
-        elif is_path_of_test_playbook_directory(path):
-            list_test_playbooks = get_child_files(path)
-            for test_playbook in list_test_playbooks:
-                if test_playbook.endswith('.yml'):
-                    self.test_playbook_uploader(test_playbook)
+            return True
 
         elif is_path_of_incident_field_directory(path):
             list_incident_fields = get_child_files(path)
             for incident_field in list_incident_fields:
                 if incident_field.endswith('.json'):
                     self.incident_field_uploader(incident_field)
+            return True
 
         elif is_path_of_widget_directory(path):
             list_widgets = get_child_files(path)
             for widget in list_widgets:
                 if widget.endswith('.json'):
                     self.widget_uploader(widget)
+            return True
 
         elif is_path_of_dashboard_directory(path):
             list_dashboards = get_child_files(path)
             for dashboard in list_dashboards:
                 if dashboard.endswith('.json'):
                     self.dashboard_uploader(dashboard)
+            return True
 
         elif is_path_of_layout_directory(path):
             list_layouts = get_child_files(path)
             for layout in list_layouts:
                 if layout.endswith('.json'):
                     self.layout_uploader(layout)
-
-        elif is_path_of_indicator_field_directory(path):
-            list_indicator_fields = get_child_files(path)
-            for indicator_field in list_indicator_fields:
-                if indicator_field.endswith('.json'):
-                    self.indicator_field_uploader(indicator_field)
+            return True
 
         elif is_path_of_incident_type_directory(path):
             list_incident_types = get_child_files(path)
             for incident_type in list_incident_types:
                 if incident_type.endswith('.json'):
                     self.incident_type_uploader(incident_type)
+            return True
 
         elif is_path_of_classifier_directory(path):
             list_classifiers = get_child_files(path)
             for classifiers in list_classifiers:
                 if classifiers.endswith('.json'):
                     self.classifiers_uploader(classifiers)
+            return True
 
     def integration_uploader(self, path):
         try:
@@ -200,22 +213,6 @@ class Uploader:
             return 1
         """
         print(f'{path} - playbook_uploader in construction')
-
-    def test_playbook_uploader(self, path):
-        """
-        try:
-            # Upload the file to Demisto
-            result = self.client.test_playbook_upload(file=path)
-
-            # Print results
-            print_v(f'Result:\n{result.to_str()}', self.log_verbose)
-            print_color(f'Uploaded \'{result.name}\' successfully', LOG_COLORS.GREEN)
-
-        except Exception as err:
-            print_error(str(err))
-            return 1
-        """
-        print(f'{path} - test_playbook_uploader in construction')
 
     def incident_field_uploader(self, path):
         """

bcd99fb5fccace70812971ec54d810c41a3a15c0
Andrew Shamah <andrew@demisto.com>
2020-04-19 11:12:46 +0300

    Testing gpg

--- demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
+++ demisto_sdk/commands/update_release_notes/tests/update_rn_test.py
@@ -17,6 +17,15 @@ def test_find_corresponding_yml():
     assert expected_result == filename
 
 
+def test_ident_changed_file_type():
+    expected_result = ('HelloWorld test', 'Integration')
+    from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN
+    update_rn = UpdateRN(pack="HelloWorld", update_type='minor')
+    filepath = './Integrations/HelloWorld/HelloWorld.py'
+    result = update_rn.ident_changed_file_type(filepath)
+    assert expected_result == result
+
+
 def test_return_release_notes_path():
     expected_result = 'Packs/HelloWorld/ReleaseNotes/1_1_1.md'
     from demisto_sdk.commands.update_release_notes.update_rn import UpdateRN

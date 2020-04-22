ef514171681c4f9a3d55f885f22618928badc8ea
yorhov <Orekhova97229!>
2020-04-22 15:06:31 +0300

    edited readme from path to file path

--- demisto_sdk/commands/format/format_module.py
+++ demisto_sdk/commands/format/format_module.py
@@ -52,7 +52,7 @@ def format_manager(input: str = None, output: str = None, from_version: str = No
         files = [file['name'] for file in
                  get_changed_files(filter_results=lambda _file: not _file.pop('status') == 'D')]
     if output and not output.endswith(('.yml', 'json')):
-        raise Exception("Given output path is not a file. Please specify an correct output file.")
+        raise Exception("Given output path is not a file. Please specify a correct output file.")
 
     log_list = []
     error_list = []

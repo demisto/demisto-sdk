caab07d6c3480698defb091791fcfe70a6bd0508
ohaim <ohaim@paloaltonetworks.com>
2020-05-04 11:45:43 +0300

    fix test

--- demisto_sdk/tests/integration_tests/validate_integration_test.py
+++ demisto_sdk/tests/integration_tests/validate_integration_test.py
@@ -73,8 +73,7 @@ class TestIntegration:
         assert "Starting validating files structure" in result.stdout
         assert f"Validating {pack_integration_path}" in result.stdout
         assert "The docker image tag is not the latest, please update it" in result.stdout
-        assert f"{pack_integration_path}: You're not using latest docker for the file, " \
-               "please update to latest version." in result.stdout
+        assert "You can check for the tags of demisto/python3 here:" in result.stdout
         assert result.stderr == ""
 
     def test_negative__hidden_param(self):

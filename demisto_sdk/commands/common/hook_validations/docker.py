1e21058e9720a3d150666d2e93e5561a8f4b4a2a
ohaim <ohaim@paloaltonetworks.com>
2020-05-07 11:09:15 +0300

    flag change

--- demisto_sdk/commands/common/hook_validations/docker.py
+++ demisto_sdk/commands/common/hook_validations/docker.py
@@ -57,7 +57,8 @@ class DockerImageValidator(object):
             self.is_latest_tag = False
             return self.is_latest_tag
 
-        if not self.is_latest_tag:
+        if self.docker_image_latest_tag != self.docker_image_tag:
+            # If docker image tag is not the most updated one that exists in docker-hub
             print_warning('The docker image tag is not the latest, please update it.\n'
                           'The docker image tag in the yml file is: {}\n'
                           'The latest docker image tag in docker hub is: {}\n'

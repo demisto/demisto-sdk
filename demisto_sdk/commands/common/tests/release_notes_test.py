694d6373711094a04021aa6ee7c61c3352f9fe4d
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-05-11 10:25:22 +0300

    Add coverage for RN Validator

--- demisto_sdk/commands/common/tests/release_notes_test.py
+++ demisto_sdk/commands/common/tests/release_notes_test.py
@@ -1,4 +1,7 @@
+import os
+
 import pytest
+from demisto_sdk.commands.common.git_tools import git_path
 from demisto_sdk.commands.common.hook_validations.release_notes import \
     ReleaseNotesValidator
 
@@ -11,6 +14,7 @@ def get_validator(file_path=''):
     return release_notes_validator
 
 
+FILES_PATH = os.path.normpath(os.path.join(__file__, f'{git_path()}/demisto_sdk/tests', 'test_files'))
 nothing_in_rn = ''
 rn_not_filled_out = '%%UPDATE_RN%%'
 rn_filled_out = 'This are sample release notes'
@@ -24,3 +28,11 @@ def test_rn_master_diff(release_notes, expected_result, mocker):
     mocker.patch.object(ReleaseNotesValidator, '__init__', lambda a, b: None)
     validator = get_validator(release_notes)
     assert validator.is_file_valid() == expected_result
+
+
+def test_init():
+    filepath = os.path.join(FILES_PATH, 'ReleaseNotes', '1_1_1.md')
+    release_notes_validator = ReleaseNotesValidator(filepath)
+    release_notes_validator.file_path = 'demisto_sdk/tests/test_files/ReleaseNotes/1_1_1.md'
+    assert release_notes_validator.release_notes_path == filepath
+    assert release_notes_validator.latest_release_notes == '### Test'

79c2906ccb5e958796843b56d8311bd31c768069
rsagi <rsagi@paloaltonetworks.com>
2020-05-25 12:39:53 +0300

    adding some file specific unit test

--- demisto_sdk/commands/common/tests/base_validator_test.py
+++ demisto_sdk/commands/common/tests/base_validator_test.py
@@ -3,7 +3,7 @@ from demisto_sdk.commands.common.hook_validations.base_validator import \
 
 
 def test_handle_error():
-    base_validator = BaseValidator(ignored_errors={'pack': ['SC101', 'SC103', "BA"]})
+    base_validator = BaseValidator(ignored_errors={'pack': ['SC101', 'SC103', "BA"], "file_name": {"IN100"}})
     formatted_error = base_validator.handle_error("Error-message", "SC102", "PATH")
     assert formatted_error == 'PATH: [SC102] - Error-message\n'
 
@@ -11,4 +11,10 @@ def test_handle_error():
     assert formatted_error is None
 
     formatted_error = base_validator.handle_error("ignore-this-as-well", "BA100", "PATH")
+    assert formatted_error is None
+
+    formatted_error = base_validator.handle_error("another-error-message", "IN101", "path/to/file_name")
+    assert formatted_error == 'path/to/file_name: [IN101] - another-error-message\n'
+
+    formatted_error = base_validator.handle_error("ignore-file-specific", "IN100", "path/to/file_name")
     assert formatted_error is None

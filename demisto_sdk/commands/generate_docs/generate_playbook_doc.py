c5783cdd824101c692491c8d7b54d905770ae25c
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2020-06-01 10:14:49 +0300

    Update generate_playbook_doc.py

--- demisto_sdk/commands/generate_docs/generate_playbook_doc.py
+++ demisto_sdk/commands/generate_docs/generate_playbook_doc.py
@@ -56,7 +56,7 @@ def generate_playbook_doc(input, output: str = None, permissions: str = None, li
         if limitations:
             doc.extend(generate_numbered_section('Known Limitations', limitations))
 
-        doc.append(f'![{_name}](Fill in the link to your image here please)')
+        doc.append(f'![{_name}](Insert the link to your image here)')
 
         doc_text = '\n'.join(doc)
 

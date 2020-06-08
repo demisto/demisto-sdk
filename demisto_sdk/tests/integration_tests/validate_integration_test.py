6e0ce33f7888e5737ed400e7e7d4512df0673721
rsagi <rsagi@paloaltonetworks.com>
2020-06-08 11:39:22 +0300

    Fixing test_negative__non_latest_docker_image

--- demisto_sdk/tests/integration_tests/validate_integration_test.py
+++ demisto_sdk/tests/integration_tests/validate_integration_test.py
@@ -138,13 +138,11 @@ class TestIntegration:
         - Running validation on it.
 
         Then
-        - Ensure validation fails.
-        - Ensure failure message on non-latest docker image.
+        - Ensure warning message on non-latest docker image.
         """
         pack_integration_path = join(AZURE_FEED_PACK_PATH, "Integrations/FeedAzure/FeedAzure.yml")
         runner = CliRunner(mix_stderr=False)
         result = runner.invoke(main, [VALIDATE_CMD, "-p", pack_integration_path, "--no-conf-json"])
-        assert result.exit_code == 1
         assert "Starting validating files structure" in result.stdout
         assert f"Validating {pack_integration_path}" in result.stdout
         assert "The docker image tag is not the latest, please update it" in result.stdout

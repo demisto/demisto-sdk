e10f802386a8862e34d44d3cec51dc81141ee5f1
sberman <sberman@paloaltonetworks.com>
2020-06-09 15:03:07 +0300

    -

--- demisto_sdk/commands/create_artifacts/content_creator.py
+++ demisto_sdk/commands/create_artifacts/content_creator.py
@@ -345,7 +345,7 @@ class ContentCreator:
                 if dir_name == 'TestPlaybooks':
                     self.copy_test_files(sub_dir_path)
                 elif dir_name == RELEASE_NOTES_DIR:
-                    continue
+                    self.copy_test_files(sub_dir_path)
                 else:
                     # handle one-level deep content
                     self.copy_dir_files(sub_dir_path, self.content_bundle)

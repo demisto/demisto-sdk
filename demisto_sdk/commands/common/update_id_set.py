4d81e09d8c8886a7eccb8819467ec901667ed8ce
teizenman <teizenman@paloaltonetworks.com>
2020-06-15 12:34:06 +0300

    minor fix

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -393,7 +393,7 @@ def get_incident_field_data(path, incidents_types_list):
         all_associated_types = all_associated_types.union(set(system_associated_types))
 
     if 'all' in all_associated_types:
-        all_associated_types = incidents_types_list
+        all_associated_types = [incident_type.keys()[0] for incident_type in incidents_types_list]
 
     scripts = json_data.get('script')
     if scripts:
@@ -783,8 +783,8 @@ def get_general_paths(path):
     return files
 
 
-def re_create_id_set(id_set_path: str = "./Tests/id_set.json", objects_to_create: list = None,
-                     print_logs: bool = True):  # noqa: C901
+def re_create_id_set(id_set_path: str = "./Tests/id_set.json", objects_to_create: list = None,  # noqa: C901
+                     print_logs: bool = True):
     if objects_to_create is None:
         objects_to_create = ['Integrations', 'Scripts', 'Playbooks', 'TestPlaybooks', 'Classifiers',
                              'Dashboards', 'IncidentFields', 'IncidentTypes', 'IndicatorFields', 'IndicatorTypes',

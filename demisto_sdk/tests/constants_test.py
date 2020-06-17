5789cf971eeb1192026db2d65ea879aef0bcd235
yorhov <Orekhova97229!>
2020-06-17 10:20:28 +0300

    removed test files

--- demisto_sdk/tests/constants_test.py
+++ demisto_sdk/tests/constants_test.py
@@ -131,8 +131,8 @@ VALID_BETA_INTEGRATION = f'{git_path()}/demisto_sdk/tests/test_files/valid-beta-
 INVALID_OUTPUT_PATH = f"{GIT_ROOT}/demisto_sdk/tests/test_files"
 CONF_JSON_MOCK_PATH = 'demisto_sdk/tests/test_files/conf.json'
 
-INTEGRATION_FEED_TAG_VALID = f"{GIT_ROOT}/demisto_sdk/tests/test_files/integration-feed-tags.yml"
-INTEGRATION_FEED_TAG_INVALID = f"{GIT_ROOT}/demisto_sdk/tests/test_files/integration-feed-no-tags.yml"
+SOURCE_FORMAT_INTEGRATION_VALID = f"{GIT_ROOT}/demisto_sdk/tests/test_files/integration-fetch-valid.yml"
+SOURCE_FORMAT_INTEGRATION_INVALID = f"{GIT_ROOT}/demisto_sdk/tests/test_files/integration-fetch-invalid.yml"
 
 DESTINATION_FORMAT_INTEGRATION = "Integrations/integration.yml"
 INTEGRATION_PATH = "Integrations"

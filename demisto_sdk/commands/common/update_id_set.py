45e3f231d0749fa825c45680b71d69a2be6476ca
teizenman <teizenman@paloaltonetworks.com>
2020-06-24 17:40:00 +0300

    minor fix

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -960,7 +960,7 @@ def process_indicator_fields(file_path: str, print_logs: bool) -> list:
     if checked_type(file_path, [PACKS_INDICATOR_FIELD_JSON_REGEX]):
         if print_logs:
             print("adding {} to id_set".format(file_path))
-        res.append(get_incident_field_data(file_path))
+        res.append(get_general_data(file_path))
     return res
 
 

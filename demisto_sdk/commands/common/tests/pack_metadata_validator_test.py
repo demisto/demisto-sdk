61a77a07479789209c712f5e6b7e031facca8bb0
syaakovi <syaakovi@paloaltonetworks.com>
2020-07-27 08:16:54 +0300

    allow empty support details for community packs

--- demisto_sdk/commands/common/tests/pack_metadata_validator_test.py
+++ demisto_sdk/commands/common/tests/pack_metadata_validator_test.py
@@ -12,11 +12,12 @@ from demisto_sdk.commands.common.hook_validations.pack_unique_files import \
 class TestPackMetadataValidator:
     FILES_PATH = os.path.normpath(os.path.join(__file__, f'{git_path()}/demisto_sdk/tests', 'test_files'))
 
-    def test_metadata_validator_valid(self, mocker):
+    @pytest.mark.parametrize('metadata', [os.path.join(FILES_PATH, 'pack_metadata__valid.json'),
+                                          os.path.join(FILES_PATH, 'pack_metadata__valid__community.json'),
+                                          ])
+    def test_metadata_validator_valid(self, mocker, metadata):
         mocker.patch.object(PackUniqueFilesValidator, '_read_file_content',
-                            return_value=TestPackMetadataValidator.
-                            read_file(os.path.join(TestPackMetadataValidator.FILES_PATH,
-                                                   'valid_pack_metadata.json')))
+                            return_value=TestPackMetadataValidator.read_file(metadata))
         mocker.patch.object(PackUniqueFilesValidator, '_is_pack_file_exists', return_value=True)
 
         validator = PackUniqueFilesValidator('fake')

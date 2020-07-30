28cfdaa90aceab387a70e15073b5f818d81345e1
grabin <grabin@paloaltonetworks.com>
2020-07-30 22:48:14 +0300

    reduce test files

--- demisto_sdk/commands/common/content/tests/pack/pack_test.py
+++ demisto_sdk/commands/common/content/tests/pack/pack_test.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import pytest
+
+from demisto_sdk.commands.common.constants import PACKS_DIR
 from demisto_sdk.commands.common.content.content import Pack
 from demisto_sdk.commands.common.content.content.objects.pack_objects import (Integration, Script, Playbook,
                                                                               IncidentField,
@@ -9,30 +11,35 @@ from demisto_sdk.commands.common.content.content.objects.pack_objects import (In
                                                                               Widget, ReleaseNote, PackMetaData,
                                                                               SecretIgnore, Readme, ChangeLog,
                                                                               PackIgnore, Tool, DocFile)
+from demisto_sdk.commands.common.tools import path_test_files
+
+TEST_DATA = path_test_files()
+TEST_CONTENT_REPO = TEST_DATA / 'content_slim'
+PACK = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01'
 
 
 @pytest.mark.parametrize(argnames="attribute, content_type, items",
                          argvalues=[
-                             ('integrations', (Integration,), 2),
-                             ('scripts', (Script,), 1),
-                             ('classifiers', (Classifier,), 1),
-                             ('playbooks', (Playbook,), 1),
-                             ('incident_fields', (IncidentField,), 1),
-                             ('incident_types', (IncidentType,), 1),
-                             ('connections', (Connection,), 1),
+                             ('integrations', (Integration,), 3),
+                             ('scripts', (Script,), 3),
+                             ('classifiers', (Classifier,), 3),
+                             ('playbooks', (Playbook,), 3),
+                             ('incident_fields', (IncidentField,), 3),
+                             ('incident_types', (IncidentType,), 3),
+                             ('connections', (Connection,), 3),
                              ('indicator_fields', (IndicatorField,), 1),
-                             ('indicator_types', (IndicatorType,), 1),
-                             ('reports', (Report,), 1),
-                             ('dashboards', (Dashboard,), 1),
-                             ('layouts', (Layout,), 1),
-                             ('widgets', (Widget,), 1),
-                             ('release_notes', (ReleaseNote,), 3),
+                             ('indicator_types', (IndicatorType,), 3),
+                             ('reports', (Report,), 3),
+                             ('dashboards', (Dashboard,), 3),
+                             ('layouts', (Layout,), 3),
+                             ('widgets', (Widget,), 3),
+                             ('release_notes', (ReleaseNote,), 1),
                              ('tools', (Tool,), 1),
                              ('doc_files', (DocFile,), 1),
-                             ('test_playbooks', (Script, Playbook), 2),
+                             ('test_playbooks', (Script, Playbook), 3),
                          ])
 def test_generators_detection(attribute: str, content_type: tuple, items: int):
-    pack = Pack(Path(__file__).parent / 'test_pack')
+    pack = Pack(PACK)
     generator_as_list = list(pack.__getattribute__(attribute))
     # Check detect all objects
     assert len(generator_as_list) == items
@@ -50,5 +57,5 @@ def test_generators_detection(attribute: str, content_type: tuple, items: int):
                              ('secrets_ignore', SecretIgnore),
                          ])
 def test_detection(attribute: str, content_type: object):
-    pack = Pack(Path(__file__).parent / 'test_pack')
+    pack = Pack(PACK)
     assert isinstance(pack.__getattribute__(attribute), content_type)

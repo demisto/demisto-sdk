b079cf7aea01f0ed6df3c8af30f724897c33780d
Bargenish <bgenish@paloaltonetworks.com>
2020-08-03 18:33:22 +0300

    Format epic (#631)
    Added support to format new layouts (layoutscontainer)
    Added Unittests
    
    * Override `update_id_to_equal_name` function in update_playbook.py to update id field to equal name only for new playbooks.
    
    * Removed `toversion` and `fromversion` fields of the classifiers  and mapper schemas
    Added support for formatting old classifier(5.9.9)
    Added support for formatting mappers
    Replaced json writing method with ujson writing in format_module.py
    Added remove_null_fields method in update_generic_json.py
    
    * Edited update_dashboard.py, update_incidentfields.py, update_incidenttype.py, update_indicatorfields.py and update_indicatortype.py according to changes in update_generic_json.py
    Added Unittests and test related constants.
    
    * Added support for widget
    
    * Update CHANGELOG.md
    
    * Added support for reports

--- demisto_sdk/commands/format/update_dashboard.py
+++ demisto_sdk/commands/format/update_dashboard.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 from demisto_sdk.commands.common.hook_validations.dashboard import \
     DashboardValidator
-from demisto_sdk.commands.common.tools import print_error
+from demisto_sdk.commands.common.tools import LOG_COLORS, print_color
 from demisto_sdk.commands.format.format_constants import (ERROR_RETURN_CODE,
                                                           SKIP_RETURN_CODE,
                                                           SUCCESS_RETURN_CODE)
@@ -14,27 +14,19 @@ class DashboardJSONFormat(BaseUpdateJSON):
 
        Attributes:
             input (str): the path to the file we are updating at the moment.
-            output (str): the desired file name to save the updated version of the YML to.
+            output (str): the desired file name to save the updated version of the JSON to.
     """
 
     def __init__(self, input: str = '', output: str = '', path: str = '', from_version: str = '', no_validate: bool = False):
         super().__init__(input, output, path, from_version, no_validate)
 
-    def update_id(self):
-        """Updates the id to be the same as name ."""
-
-        print('Updating ID')
-        if 'name' not in self.data:
-            print_error(f'Missing "name" field in file {self.source_file} - add this field manually')
-            raise Exception(f'Missing "name" field in file {self.source_file} - add this field manually')
-        self.data['id'] = self.data.get('name')
-
     def run_format(self) -> int:
         try:
+            print_color(f'\n======= Updating file: {self.source_file} =======', LOG_COLORS.WHITE)
             super().update_json()
             self.default_description()
             self.update_id()
-            super().save_json_to_destination_file()
+            self.save_json_to_destination_file()
             return SUCCESS_RETURN_CODE
         except Exception:
             return ERROR_RETURN_CODE
@@ -46,7 +38,7 @@ class DashboardJSONFormat(BaseUpdateJSON):
             self.data["isPredefined"] = True
 
     def format_file(self) -> Tuple[int, int]:
-        """Manager function for the integration YML updater."""
+        """Manager function for the Dashboard JSON updater."""
         format = self.run_format()
         if format:
             return format, SKIP_RETURN_CODE

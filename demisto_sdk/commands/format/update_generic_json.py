1646e5432bb2d5213a03fab032fcf4393b1c1a96
bgenish <bgenish@paloaltonetworks.com>
2020-08-12 09:49:00 +0300

    Fixed an issue where **format** removed keys with falsy values instead of null ones

--- demisto_sdk/commands/format/update_generic_json.py
+++ demisto_sdk/commands/format/update_generic_json.py
@@ -60,7 +60,7 @@ class BaseUpdateJSON(BaseUpdate):
         with open(self.schema_path, 'r') as file_obj:
             a = yaml.safe_load(file_obj)
         schema_fields = a.get('mapping').keys()
-        fields_to_remove = [field for field in schema_fields if (not self.data.get(field) and field in self.data)]
+        fields_to_remove = [field for field in schema_fields if (self.data.get(field) is None and field in self.data)]
         for field in fields_to_remove:
             self.data.pop(field, None)
 

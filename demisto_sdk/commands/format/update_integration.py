ffa137645179295b3e479c2f30b4607a04abe818
bgenish <bgenish@paloaltonetworks.com>
2020-08-13 19:34:44 +0300

    Added unittests

--- demisto_sdk/commands/format/update_integration.py
+++ demisto_sdk/commands/format/update_integration.py
@@ -87,7 +87,7 @@ class IntegrationYMLFormat(BaseUpdateYML):
             # Creates a deep copy of the feed integration configuration so the 'defaultvalue` field would not get
             # popped from the original configuration params.
             params = [dict(config) for config in self.data.get('configuration', [])]
-            for param in enumerate(params):
+            for param in params:
                 if 'defaultvalue' in param:
                     param.pop('defaultvalue')
             for param in FETCH_REQUIRED_PARAMS:

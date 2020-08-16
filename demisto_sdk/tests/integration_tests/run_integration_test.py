4c6968e15f2331a9dc6246afdac4149bfebad73d
olichter <olichter@paloaltonetworks.com>
2020-08-16 15:25:09 +0300

    another isort change

--- demisto_sdk/tests/integration_tests/run_integration_test.py
+++ demisto_sdk/tests/integration_tests/run_integration_test.py
@@ -1,7 +1,6 @@
 import pytest
 from click.testing import CliRunner
 from demisto_client.demisto_api import DefaultApi
-
 from demisto_sdk.__main__ import main
 from demisto_sdk.commands.common.git_tools import git_path
 from demisto_sdk.commands.run_cmd.runner import Runner

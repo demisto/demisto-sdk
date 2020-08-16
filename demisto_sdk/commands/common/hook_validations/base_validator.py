91500eb708aee5d5f3eeb8eca116d007c81157a4
cshayner <cshayner@paloaltonetworks.com>
2020-08-16 12:35:37 +0300

    fix pre-commit error

--- demisto_sdk/commands/common/hook_validations/base_validator.py
+++ demisto_sdk/commands/common/hook_validations/base_validator.py
@@ -7,12 +7,13 @@ from demisto_sdk.commands.common.constants import (PACK_METADATA_CERTIFICATION,
                                                    PACK_METADATA_SUPPORT,
                                                    PACKS_DIR,
                                                    PACKS_PACK_META_FILE_NAME, FileType)
+from demisto_sdk.commands.common.tools import get_pack_name, get_yaml, find_type
+
 from demisto_sdk.commands.common.errors import (ERROR_CODE,
                                                 FOUND_FILES_AND_ERRORS,
                                                 FOUND_FILES_AND_IGNORED_ERRORS,
                                                 PRESET_ERROR_TO_CHECK,
                                                 PRESET_ERROR_TO_IGNORE)
-from demisto_sdk.commands.common.tools import get_pack_name, get_yaml, find_type
 
 
 class BaseValidator:

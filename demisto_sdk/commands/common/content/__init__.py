6650e53efba33167d74ee0767fe8c7ad2a64ab0e
grabin <grabin@paloaltonetworks.com>
2020-08-20 10:04:06 +0300

    lgtm susspresion

--- demisto_sdk/commands/common/content/__init__.py
+++ demisto_sdk/commands/common/content/__init__.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import
 
 import inspect
 
-from .content import *  # noqa: E402 lgtm [py/polluting-import]
-from .errors import *  # noqa: E402 lgtm [py/polluting-import]
-from .objects.pack_objects import *  # noqa: E402 lgtm [py/polluting-import]
-from .objects_factory import *  # noqa: E402 lgtm [py/polluting-import]
+from .content import *  # lgtm [py/polluting-import]
+from .errors import *  # lgtm [py/polluting-import]
+from .objects.pack_objects import *  # lgtm [py/polluting-import]
+from .objects_factory import *  # lgtm [py/polluting-import]
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]
--- demisto_sdk/commands/common/content/objects/abstract_objects/__init__.py
+++ demisto_sdk/commands/common/content/objects/abstract_objects/__init__.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import
 
 import inspect
 
-from .json_object import *  # noqa: E402 lgtm [py/polluting-import]
-from .text_object import *  # noqa: E402 lgtm [py/polluting-import]
-from .yaml_object import *  # noqa: E402 lgtm [py/polluting-import]
+from .json_object import *  # lgtm [py/polluting-import]
+from .text_object import *  # lgtm [py/polluting-import]
+from .yaml_object import *  # lgtm [py/polluting-import]
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]

4b8c1359ceee2bbc4532042dea3eba3eebbc1f5e
grabin <grabin@paloaltonetworks.com>
2020-08-26 16:25:50 +0300

    Hot fix

--- demisto_sdk/commands/lint/linter.py
+++ demisto_sdk/commands/lint/linter.py
@@ -470,8 +470,9 @@ class Linter:
                         # If lint check perfrom and failed on reason related to enviorment will run twice,
                         # But it failing in second time it will count as test failure.
                         if (exit_code == RERUN and trial == 1) or exit_code == FAIL or exit_code == SUCCESS:
-                            self._pkg_lint_status["exit_code"] |= EXIT_CODES[check]
-                            status[f"{check}_errors"] = output
+                            if exit_code in [RERUN, FAIL]:
+                                self._pkg_lint_status["exit_code"] |= EXIT_CODES[check]
+                                status[f"{check}_errors"] = output
                             break
             else:
                 status["image_errors"] = str(errors)

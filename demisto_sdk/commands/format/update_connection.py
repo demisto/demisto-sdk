e7e479a576464bf0254cf92a5e299b6bf0a2487f
hod-alpert <haplert@paloaltonetworks.com>
2020-10-13 14:55:42 +0300

    Fixed CR comments

--- demisto_sdk/commands/format/update_connection.py
+++ demisto_sdk/commands/format/update_connection.py
@@ -13,9 +13,17 @@ from demisto_sdk.tests.constants_test import CONNECTION_SCHEMA_PATH
 class ConnectionJSONFormat(BaseUpdateJSON):
     """ConnectionJSONFormat class is designed to update connections JSON file according to Demisto's convention.
 
-       Attributes:
-            input (str): the path to the file we are updating at the moment.
-            output (str): the desired file name to save the updated version of the JSON to.
+        Attributes:
+            source_file (str): the path to the file we are updating at the moment.
+            output_file (str): the desired file name to save the updated version of the YML to.
+            relative_content_path (str): Relative content path of output path.
+            old_file (dict): Data of old file from content repo, if exist.
+            schema_path (str): Schema path of file.
+            from_version (str): Value of Wanted fromVersion key in file.
+            data (dict): Dictionary of loaded file.
+            file_type (str): Whether the file is yml or json.
+            from_version_key (str): The fromVersion key in file, different between yml and json files.
+            verbose (bool): Whether to print a verbose log
     """
 
     def __init__(self,
@@ -40,13 +48,15 @@ class ConnectionJSONFormat(BaseUpdateJSON):
             self.set_fromVersion(from_version=self.from_version)
             self.save_json_to_destination_file()
             return SUCCESS_RETURN_CODE
-        except Exception:
+        except Exception as err:
+            if self.verbose:
+                click.secho(f'\nFailed to update file {self.source_file}. Error: {err}', fg='white')
             return ERROR_RETURN_CODE
 
     def format_file(self) -> Tuple[int, int]:
         """Manager function for the indicator type JSON updater."""
-        format = self.run_format()
-        if format:
-            return format, SKIP_RETURN_CODE
+        format_ = self.run_format()
+        if format_:
+            return format_, SKIP_RETURN_CODE
         else:
-            return format, self.initiate_file_validator(ContentEntityValidator)
+            return format_, self.initiate_file_validator(ContentEntityValidator)

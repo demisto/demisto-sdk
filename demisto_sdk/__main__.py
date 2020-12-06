44d3696659e18c36c235158d353d0a5712b445ba
abaumgarten <abaumgarten@paloaltonetworks.com>
2020-12-06 10:39:36 +0200

    update

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -265,9 +265,9 @@ def unify(**kwargs):
 @click.option(
     '--skip-pack-dependencies', is_flag=True,
     help='Skip validation of pack dependencies.')
-@click.option(
-    '--skip-id-set-creation', is_flag=True,
-    help='Skip creation of id_set file.')
+# @click.option(
+#     '--skip-id-set-creation', is_flag=True,
+#     help='Skip creation of id_set file.')
 @pass_config
 def validate(config, **kwargs):
     sys.path.append(config.configuration.env_dir)
@@ -294,8 +294,8 @@ def validate(config, **kwargs):
             silence_init_prints=kwargs['silence_init_prints'],
             skip_dependencies=kwargs['skip_pack_dependencies'],
             id_set_path=kwargs.get('id_set_path'),
-            staged=kwargs['staged'],
-            skip_id_set_creation=kwargs['skip_id_set_creation']
+            staged=kwargs['staged']
+            # skip_id_set_creation=kwargs['skip_id_set_creation']
         )
         return validator.run_validation()
     except (git.InvalidGitRepositoryError, git.NoSuchPathError, FileNotFoundError) as e:

d24107fa6eb059a3bbf33ff1c01dea4feab2bda1
cshayner <cshayner@paloaltonetworks.com>
2020-12-30 16:30:58 +0200

    add comment

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -1451,6 +1451,8 @@ def has_duplicate(id_set_subset_list, id_to_check, object_type=None, print_logs=
             if dict1.get('kind', '') != dict2.get('kind', ''):
                 return False
 
+        # If they have the same pack name they actually the same entity.
+        # Added to support merge between two ID sets that contain the same pack.
         if dict1.get('pack') == dict2.get('pack'):
             return False
 

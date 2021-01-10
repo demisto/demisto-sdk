c455ff9ee1c4cc44522dae5a5f2507563a749630
rsagi <rsagi@paloaltonetworks.com>
2021-01-10 13:03:51 +0200

    moving unit tests part1

--- demisto_sdk/commands/common/content/tests/objects/pack_objects/classifier/classifier_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/classifier/classifier_test.py
@@ -1,40 +1,61 @@
-from demisto_sdk.commands.common.constants import CLASSIFIERS_DIR, PACKS_DIR
 from demisto_sdk.commands.common.content.objects.pack_objects import (
     Classifier, ClassifierMapper, OldClassifier)
 from demisto_sdk.commands.common.content.objects_factory import \
     path_to_pack_object
 from demisto_sdk.commands.common.tools import src_root
+from demisto_sdk.tests.test_files.validate_integration_test_valid_types import (
+    MAPPER, NEW_CLASSIFIER, OLD_CLASSIFIER)
 
 TEST_DATA = src_root() / 'tests' / 'test_files'
 TEST_CONTENT_REPO = TEST_DATA / 'content_slim'
-CLASSIFIER = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / CLASSIFIERS_DIR / 'classifier-sample_new.json'
+
+
+def mock_new_classifier(repo, classifier_data=None):
+    pack = repo.create_pack('Temp')
+    return pack.create_classifier(name='MyClassifier', content=classifier_data if classifier_data else NEW_CLASSIFIER)
+
+
+def mock_old_classifier(repo, classifier_data=None):
+    pack = repo.create_pack('Temp')
+    return pack.create_classifier(name='MyClassifier', content=classifier_data if classifier_data else OLD_CLASSIFIER)
+
+
+def mock_mapper(repo, mapper_data=None):
+    pack = repo.create_pack('Temp')
+    return pack.create_mapper(name='MyMapper', content=mapper_data if mapper_data else MAPPER)
 
 
 class TestClassifierType:
-    def test_objects_factory(self):
-        obj = path_to_pack_object(CLASSIFIER)
+    def test_objects_factory(self, repo):
+        classifier = mock_new_classifier(repo)
+        obj = path_to_pack_object(classifier.path)
         assert isinstance(obj, Classifier)
 
-    def test_prefix(self):
-        obj = Classifier(CLASSIFIER)
-        assert obj.normalize_file_name() == 'classifier-sample_new.json'
+    def test_prefix(self, repo):
+        classifier = mock_new_classifier(repo)
+        obj = Classifier(classifier.path)
+        assert obj.normalize_file_name() == 'classifier-MyClassifier.json'
 
 
 class TestOldClassifierType:
-    def test_objects_factory(self, datadir):
-        obj = path_to_pack_object(datadir['old_classifier.json'])
+    def test_objects_factory(self, repo):
+        classifier = mock_old_classifier(repo)
+        obj = path_to_pack_object(classifier.path)
         assert isinstance(obj, OldClassifier)
 
-    def test_prefix(self, datadir):
-        obj = OldClassifier(datadir['old_classifier.json'])
-        assert obj.normalize_file_name() == 'classifier-old_classifier.json'
+    def test_prefix(self, repo):
+        classifier = mock_old_classifier(repo)
+        obj = OldClassifier(classifier.path)
+        assert obj.normalize_file_name() == 'classifier-MyClassifier.json'
 
 
 class TestClassifierMapperType:
-    def test_objects_factory(self, datadir):
-        obj = path_to_pack_object(datadir['classifier_mapper.json'])
+    def test_objects_factory(self, repo):
+        mapper = mock_mapper(repo)
+        obj = path_to_pack_object(mapper.path)
         assert isinstance(obj, ClassifierMapper)
 
-    def test_prefix(self, datadir):
-        obj = ClassifierMapper(datadir['classifier_mapper.json'])
-        assert obj.normalize_file_name() == 'classifier-mapper-classifier_mapper.json'
+    def test_prefix(self, repo):
+        mapper = mock_mapper(repo)
+        obj = ClassifierMapper(mapper.path)
+        assert obj.normalize_file_name() == 'classifier-mapper-MyMapper.json'
--- demisto_sdk/commands/common/content/tests/objects/pack_objects/dashboard/dashboard_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/dashboard/dashboard_test.py
@@ -1,19 +1,103 @@
-from demisto_sdk.commands.common.constants import DASHBOARDS_DIR, PACKS_DIR
+import pytest
 from demisto_sdk.commands.common.content.objects.pack_objects import Dashboard
 from demisto_sdk.commands.common.content.objects_factory import \
     path_to_pack_object
+from demisto_sdk.commands.common.hook_validations.base_validator import \
+    BaseValidator
 from demisto_sdk.commands.common.tools import src_root
+from demisto_sdk.tests.test_files.validate_integration_test_valid_types import \
+    DASHBOARD
 
 TEST_DATA = src_root() / 'tests' / 'test_files'
 TEST_CONTENT_REPO = TEST_DATA / 'content_slim'
-DASHBOARD = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / DASHBOARDS_DIR / 'dashboard-sample_new.json'
+BASE_VALIDATOR = BaseValidator()
 
 
-def test_objects_factory():
-    obj = path_to_pack_object(DASHBOARD)
+def mock_valid_dashboard(repo, dashboard_data=None):
+    pack = repo.create_pack('Temp')
+    return pack.create_dashboard(name="myDashboard", content=dashboard_data if dashboard_data else DASHBOARD)
+
+
+def test_objects_factory(repo):
+    dashboard = mock_valid_dashboard(repo)
+    obj = path_to_pack_object(dashboard.path)
     assert isinstance(obj, Dashboard)
 
 
-def test_prefix():
-    obj = Dashboard(DASHBOARD)
-    assert obj.normalize_file_name() == DASHBOARD.name
+def test_prefix(repo):
+    dashboard = mock_valid_dashboard(repo)
+    obj = Dashboard(dashboard.path)
+    assert obj.normalize_file_name() == dashboard.name
+
+
+data_is_valid_version = [
+    (-1, True),
+    (0, False),
+    (1, False),
+]
+
+
+@pytest.mark.parametrize('version, is_valid', data_is_valid_version)
+def test_is_valid_version(version, is_valid, repo):
+    dashboard_data = DASHBOARD.copy()
+    dashboard_data['version'] = version
+    dashboard = mock_valid_dashboard(repo, dashboard_data)
+    dashboard_obj = Dashboard(dashboard.path, BASE_VALIDATOR)
+    assert dashboard_obj.is_valid_version() == is_valid, f'is_valid_version({version}) returns {not is_valid}.'
+
+
+data_is_id_equal_name = [
+    ('aa', 'aa', True),
+    ('aa', 'ab', False),
+    ('my-home-dashboard', 'My Dashboard', False)
+]
+
+
+@pytest.mark.parametrize('id_, name, is_valid', data_is_id_equal_name)
+def test_is_id_equal_name(id_, name, is_valid, repo):
+    dashboard_data = DASHBOARD.copy()
+    dashboard_data['id'] = id_
+    dashboard_data['name'] = name
+    dashboard = mock_valid_dashboard(repo, dashboard_data)
+    dashboard_obj = Dashboard(dashboard.path, BASE_VALIDATOR)
+    assert dashboard_obj.is_id_equals_name() == is_valid, f'is_id_equal_name returns {not is_valid}.'
+
+
+data_contains_forbidden_fields = [
+    ({"system": False}, False),
+    ({"isCommon": False}, False),
+    ({"shared": False}, False),
+    ({"owner": "Admin"}, False),
+    ({"layout": [{"widget": {"owner": "Admin"}}]}, False),
+    ({"layout": [{"widget": {"shared": "False"}}]}, False),
+    ({"layout": [{"widget": {"shared4": "False"}}]}, True)
+]
+
+
+@pytest.mark.parametrize('current_file, is_valid', data_contains_forbidden_fields)
+def test_contains_forbidden_fields(current_file, is_valid, repo):
+    dashboard_data = DASHBOARD.copy()
+    for key, val in current_file.items():
+        dashboard_data[key] = val
+    dashboard = mock_valid_dashboard(repo, dashboard_data)
+    dashboard_obj = Dashboard(dashboard.path, BASE_VALIDATOR)
+    assert dashboard_obj.contains_forbidden_fields() == is_valid, f'is_excluding_fields returns {not is_valid}.'
+
+
+data_is_including_fields = [
+    ({"fromDate": "1", "toDate": "2", "fromDateLicense": "3"}, True),
+    ({"fromDate": "1", "toDate": "2", "fromDateLicense": "3",
+      "layout": [{"widget": {"fromDate": "1", "toDate": "2", "fromDateLicense": "3"}}]}, True),
+    ({"fromDate": "1", "toDate": "2", "fromDateLicense": "3",
+      "layout": [{"widget": {"name": "bla", "fromDate": "1", "fromDateLicense": "3"}}]}, False)
+]
+
+
+@pytest.mark.parametrize('current_file, is_valid', data_is_including_fields)
+def test_is_including_fields(current_file, is_valid, repo):
+    dashboard_data = DASHBOARD.copy()
+    for key, val in current_file.items():
+        dashboard_data[key] = val
+    dashboard = mock_valid_dashboard(repo, dashboard_data)
+    dashboard_obj = Dashboard(dashboard.path, BASE_VALIDATOR)
+    assert dashboard_obj.is_including_fields() == is_valid, f'is_including_fields returns {not is_valid}.'

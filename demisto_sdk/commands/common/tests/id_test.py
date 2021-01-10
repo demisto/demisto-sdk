ecd62f52e058205349cc95314bd8454692925bc7
bar katzir <bkatzir@paloaltonetworks.com>
2021-01-10 11:27:50 +0200

    rm deprecated code

--- demisto_sdk/commands/common/tests/id_test.py
+++ demisto_sdk/commands/common/tests/id_test.py
@@ -4,65 +4,6 @@ from demisto_sdk.commands.common.hook_validations.id import IDSetValidator
 CONFIG = Configuration()
 
 
-def test_validness_in_set():
-    validator = IDSetValidator(is_circle=False, is_test_run=True, configuration=CONFIG)
-
-    obj_data = {
-        "test": {
-            "name": "test"
-        }
-    }
-    obj_set = [
-        obj_data,
-    ]
-
-    assert validator._is_valid_in_id_set(file_path="test", obj_data=obj_data, obj_set=obj_set), \
-        "The id validator couldn't find id as valid one"
-
-
-def test_obj_not_found_in_set():
-    validator = IDSetValidator(is_circle=False, is_test_run=True, configuration=CONFIG)
-
-    obj_data = {
-        "test": {
-            "name": "test"
-        }
-    }
-    actual_obj_set = {
-        "test": {
-            "name": "test",
-            "fromversion": "1.2.2"
-        }
-    }
-    obj_set = [
-        actual_obj_set,
-    ]
-
-    assert validator._is_valid_in_id_set(file_path="test", obj_data=obj_data, obj_set=obj_set) is False, \
-        "The id validator couldn't find id as valid one"
-
-
-def test_obj_data_mismatch_in_set():
-    validator = IDSetValidator(is_circle=False, is_test_run=True, configuration=CONFIG)
-
-    obj_data = {
-        "test": {
-            "name": "test"
-        }
-    }
-    actual_obj_set = {
-        "test": {
-            "name": "not test",
-        }
-    }
-    obj_set = [
-        actual_obj_set,
-    ]
-
-    assert validator._is_valid_in_id_set(file_path="test", obj_data=obj_data, obj_set=obj_set) is False, \
-        "The id validator couldn't find id as valid one"
-
-
 def test_is_incident_type_using_real_playbook__happy_flow():
     """
     Given

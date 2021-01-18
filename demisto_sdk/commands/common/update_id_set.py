6f681ae4ce2b76d1c98c77f9e46fcc5db7aa12f2
cshayner <cshayner@paloaltonetworks.com>
2021-01-18 13:37:06 +0200

    Add UT

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -695,6 +695,7 @@ def get_pack_metadata_data(path):
             "name": json_data.get('name', ''),
             "current_version": json_data.get('currentVersion', ''),
             "author": json_data.get('author', ''),
+            'certification': 'certified' if json_data.get('support', '').lower() in ['xsoar', 'partner'] else '',
             "tags": json_data.get('tags', ''),
             "usecases": json_data.get('usecases', ''),
             "categories": json_data.get('categories', '')

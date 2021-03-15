bdd342cf76184aa19b3af8fcdffdfb4007ab57bc
nmaimon <nmaimon@paloaltonetworks.com>
2021-03-15 16:03:40 +0200

    global

--- demisto_sdk/commands/lint/lint_manager.py
+++ demisto_sdk/commands/lint/lint_manager.py
@@ -88,7 +88,7 @@ class LintManager:
         Returns:
             dict: facts
         """
-        # global logger
+        global logger
         facts = {
             "content_repo": None,
             "requirements_3": None,

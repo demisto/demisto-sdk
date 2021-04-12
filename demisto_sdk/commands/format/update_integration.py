0d5e221fe428ca2324e839d61a3b5ad84a8fd937
tneeman <tneeman@paloaltonetworks.com>
2021-04-12 11:15:00 +0300

    reproduce bug

--- demisto_sdk/commands/format/update_integration.py
+++ demisto_sdk/commands/format/update_integration.py
@@ -120,6 +120,7 @@ class IntegrationYMLFormat(BaseUpdateYML):
                 if 'defaultvalue' in param and param.get('name') != 'feed':
                     params[counter].pop('defaultvalue')
             for param in FEED_REQUIRED_PARAMS:
+                param = dict(param.get('must_equal', dict()), **param('must_contain', dict()))
                 if param not in params:
                     self.data['configuration'].append(param)
 

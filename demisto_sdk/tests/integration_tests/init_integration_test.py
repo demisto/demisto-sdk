079317da7ed241cae9e5d1f2f0fcb883f633250e
dkoval <dkoval@paloaltonetworks.com>
2021-04-21 13:37:47 +0300

    fixing failed tests

--- demisto_sdk/tests/integration_tests/init_integration_test.py
+++ demisto_sdk/tests/integration_tests/init_integration_test.py
@@ -29,13 +29,14 @@ def test_integration_init_integration_positive(tmp_path):
     pack_author = "SuperMario"
     pack_url = "https://www.github.com/supermario"
     pack_email = "mario@super.com"
+    pack_dev_email = "mario@super.com"
     pack_tags = "SuperTag1,SuperTag2"
     pack_reviewers = "GithubUser1, GithubUser2"
     create_integration = 'Y'
     integration_name = "SuperIntegration"
     use_dir_name_as_id = 'Y'
     inputs = [fill_pack_metadata, pack_display_name, pack_desc, support_type, pack_category,
-              pack_author, pack_url, pack_email, pack_tags, pack_reviewers, create_integration, integration_name,
+              pack_author, pack_url, pack_email, pack_dev_email, pack_tags, pack_reviewers, create_integration, integration_name,
               use_dir_name_as_id]
 
     d = tmp_path / 'TestPacks'
@@ -64,6 +65,7 @@ def test_integration_init_integration_positive(tmp_path):
             "author": pack_author,
             "url": pack_url,
             "email": pack_email,
+            "devEmail": pack_dev_email,
             "categories": [
                        "Endpoint"
             ],

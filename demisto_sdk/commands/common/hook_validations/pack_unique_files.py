d468a0b1d03035c18826ac554db0b6917836ff62
rsagi <rsagi@paloaltonetworks.com>
2021-05-31 14:28:21 +0300

    adding only dependent on core packs print

--- demisto_sdk/commands/common/hook_validations/pack_unique_files.py
+++ demisto_sdk/commands/common/hook_validations/pack_unique_files.py
@@ -542,6 +542,7 @@ class PackUniqueFilesValidator(BaseValidator):
             for core_pack in core_pack_list:
                 first_level_dependencies.pop(core_pack, None)
             if not first_level_dependencies:
+                click.secho("Found first level dependencies only on core packs", fg="yellow")
                 return True
 
             dependency_result = json.dumps(first_level_dependencies, indent=4)

829e1ec0d1ffb2b2a4960c489e1dd821f2458877
tneeman <tneeman@paloaltonetworks.com>
2021-06-01 20:22:17 +0300

    pre commit fixes

--- demisto_sdk/commands/convert/convert_manager.py
+++ demisto_sdk/commands/convert/convert_manager.py
@@ -1,6 +1,6 @@
 import click
 from demisto_sdk.commands.common.content.objects.pack_objects.pack import Pack
-from demisto_sdk.commands.convert.dir_convert_managers import *
+from demisto_sdk.commands.convert.dir_convert_managers import *  # lgtm [py/polluting-import]
 
 
 class ConvertManager:

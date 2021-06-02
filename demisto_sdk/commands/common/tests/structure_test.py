acb838b72c1fcd2d86515ffdcff38aac6dde9a42
Bar Hochman <11165655+jochman@users.noreply.github.com>
2021-06-02 17:41:06 +0300

    Update structure_test.py

--- demisto_sdk/commands/common/tests/structure_test.py
+++ demisto_sdk/commands/common/tests/structure_test.py
@@ -131,7 +131,7 @@ class TestStructureValidator:
     def test_is_id_modified(self, current_file, old_file, answer, error):
         validator = StructureValidator(file_path=current_file)
         with open(old_file) as f:
-            validator.old_file = YAML(typ='unsafe', pure=True).load(f)
+            validator.old_file = YAML(typ='safe', pure=True).load(f)
             assert validator.is_id_modified() is answer, error
 
     POSITIVE_ERROR = "Didn't find a slash in the ID even though it contains a slash."

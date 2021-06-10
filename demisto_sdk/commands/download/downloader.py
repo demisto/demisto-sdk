4d54d5e64192bbb630052270377579880a7183fa
teizenman <teizenman@paloaltonetworks.com>
2021-06-10 12:42:58 +0300

    fail unsuccessful download

--- demisto_sdk/commands/download/downloader.py
+++ demisto_sdk/commands/download/downloader.py
@@ -99,12 +99,17 @@ class Downloader:
         self.handle_all_custom_content_flag()
         if not self.verify_output_pack_is_pack():
             return 1
+
         self.build_pack_content()
         self.build_custom_content()
         self.update_pack_hierarchy()
         self.merge_into_pack()
         self.log_files_downloaded()
         self.log_files_not_downloaded()
+
+        if self.files_not_downloaded:
+            return 1
+
         return 0
 
     def verify_flags(self) -> bool:

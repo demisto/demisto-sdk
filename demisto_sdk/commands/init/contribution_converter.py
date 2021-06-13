d3c58ce3440bc9de6d019e08c032c4ade11435c7
guyfreund <gfreund@paloaltonetworks.com>
2021-06-13 18:00:43 +0300

    cr fixes

--- demisto_sdk/commands/init/contribution_converter.py
+++ demisto_sdk/commands/init/contribution_converter.py
@@ -240,7 +240,9 @@ class ContributionConverter:
 
     def format_converted_pack(self) -> None:
         """Runs the demisto-sdk's format command on the pack converted from the contribution zipfile"""
-        click.echo('Executing \'format\' on the restructured-contribution-zip new/modified files')
+        click.echo(
+            f'Executing \'format\' on the restructured contribution zip new/modified files at {self.pack_dir_path}'
+        )
         from_version = '6.0.0' if self.create_new else ''
         format_manager(
             from_version=from_version, no_validate=True, update_docker=True, verbose=True, assume_yes=True

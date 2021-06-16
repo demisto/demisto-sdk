cd476a83223e25e9ef7507c428c4e4f290cd2cf9
meichlerpanw <meichler@paloaltonetworks.com>
2021-06-16 17:32:47 +0300

    Fix the find dependencies on send-notification command

--- demisto_sdk/commands/find_dependencies/find_dependencies.py
+++ demisto_sdk/commands/find_dependencies/find_dependencies.py
@@ -465,7 +465,8 @@ class PackDependencies:
 
             # searching for packs of implementing integrations
             implementing_commands_and_integrations = playbook_data.get('command_to_integration', {})
-
+            if implementing_commands_and_integrations.get('send-notification'):
+                del implementing_commands_and_integrations['send-notification']
             for command, integration_name in implementing_commands_and_integrations.items():
                 packs_found_from_integration = set()
                 if integration_name:

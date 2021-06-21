39409dbfd45816f34051a085fd144e159eb137db
Dean Arbel <darbel@paloaltonetworks.com>
2021-06-21 13:45:09 +0300

    add unit tests

--- demisto_sdk/commands/test_content/tests/test_context_test.py
+++ demisto_sdk/commands/test_content/tests/test_context_test.py
@@ -12,6 +12,10 @@ from demisto_sdk.commands.test_content.tests.DemistoClientMock import \
     DemistoClientMock
 from demisto_sdk.commands.test_content.tests.server_context_test import \
     generate_mocked_server_context
+from unittest.mock import patch
+import ast
+import json
+import demisto_client
 
 
 def test_is_runnable_on_this_instance(mocker):
@@ -49,6 +53,7 @@ def test_second_playback_enforcement(mocker, tmp_path):
         - Ensure that it exists in the failed_playbooks set
         - Ensure that it does not exists in the succeeded_playbooks list
     """
+
     class RunIncidentTestMock:
         call_count = 0
         count_response_mapping = {
@@ -129,3 +134,87 @@ def test_docker_thresholds_for_pwsh_integrations(mocker):
     memory_threshold, pid_threshold = test_context.get_threshold_values()
     assert memory_threshold == Docker.DEFAULT_PWSH_CONTAINER_MEMORY_USAGE
     assert pid_threshold == Docker.DEFAULT_PWSH_CONTAINER_PIDS_USAGE
+
+
+@patch('demisto_client.generic_request_func')
+class TestPrintContextToLog:
+
+    @staticmethod
+    def create_playbook_instance(mocker):
+        test_playbook_configuration = TestConfiguration(generate_test_configuration(
+            playbook_id='playbook_with_context',
+            integrations=['integration']
+        ),
+            default_test_timeout=30)
+        pb_instance = TestPlaybook(mocker.MagicMock(), test_playbook_configuration)
+        pb_instance.build_context.logging_module = mocker.MagicMock()
+        return pb_instance
+
+    def test_print_context_to_log__success(self, mocker):
+        """
+        Given:
+            - A test context with a context_print_dt value
+            - The context result is empty
+        When:
+            - Running 'print_context_to_log' method
+        Then:
+            - Ensure that an empty result is being printed to the context
+        """
+        dt_result = "{'foo': 'goo'}"
+        expected_result = json.dumps(ast.literal_eval(dt_result), indent=4)
+        playbook_instance = self.create_playbook_instance(mocker)
+        demisto_client.generic_request_func.return_value = (dt_result, 200)
+        playbook_instance.print_context_to_log(None, incident_id='1')
+        assert playbook_instance.build_context.logging_module.info.call_args.args[0] == expected_result
+
+    def test_print_context_to_log__empty(self, mocker):
+        """
+        Given:
+            - A test context with a context_print_dt value
+            - The context result is empty
+        When:
+            - Running 'print_context_to_log' method
+        Then:
+            - Ensure that an empty result is being printed to the context
+        """
+        expected_dt = '{}'
+        playbook_instance = self.create_playbook_instance(mocker)
+        demisto_client.generic_request_func.return_value = (expected_dt, 200)
+        playbook_instance.print_context_to_log(None, incident_id='1')
+        assert playbook_instance.build_context.logging_module.info.call_args.args[0] == expected_dt
+
+    def test_print_context_to_log__none(self, mocker):
+        """
+        Given:
+            - A test context with a context_print_dt value
+            - The context result is None
+        When:
+            - Running 'print_context_to_log' method
+        Then:
+            - Ensure that an exception is raised and handled via logging error
+        """
+        expected_dt = None
+        expected_error = 'received error: malformed node or string: None for None'
+        playbook_instance = self.create_playbook_instance(mocker)
+        demisto_client.generic_request_func.return_value = (expected_dt, 200)
+        playbook_instance.print_context_to_log(None, incident_id='1')
+        assert playbook_instance.build_context.logging_module.error.call_args.args[0] == expected_error
+
+    def test_print_context_to_log__error(self, mocker):
+        """
+        Given:
+            - A test context with a context_print_dt value
+            - The context return with an HTTP error code
+        When:
+            - Running 'print_context_to_log' method
+        Then:
+            - Ensure that an exception is raised and handled via logging error messages
+        """
+        expected_dt = 'No Permission'
+        expected_first_error = 'incident context fetch failed with Status code 403'
+        expected_second_error = f"('{expected_dt}', 403)"
+        playbook_instance = self.create_playbook_instance(mocker)
+        demisto_client.generic_request_func.return_value = (expected_dt, 403)
+        playbook_instance.print_context_to_log(None, incident_id='1')
+        assert playbook_instance.build_context.logging_module.error.call_args_list[0].args[0] == expected_first_error
+        assert playbook_instance.build_context.logging_module.error.call_args_list[1].args[0] == expected_second_error

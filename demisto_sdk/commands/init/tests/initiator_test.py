f748fc717e3475d9610018b464d5461ab8364c85
Yaakov Praisler <59408745+yaakovpraisler@users.noreply.github.com>
2021-06-24 10:09:31 +0300

    Update demisto_sdk/commands/init/tests/initiator_test.py
    Co-authored-by: roysagi <50295826+roysagi@users.noreply.github.com>

--- demisto_sdk/commands/init/tests/initiator_test.py
+++ demisto_sdk/commands/init/tests/initiator_test.py
@@ -270,7 +270,7 @@ def test_pack_init_without_filling_metadata(monkeypatch, mocker, initiator):
     When
         - Creating new pack without filling the metadata file.
     Then
-        - Ensure it's not fail.
+        - Ensure it does not fail.
     """
     monkeypatch.setattr('builtins.input', lambda _: 'n')
     mocker.patch.object(Initiator, 'create_new_directory', return_value=True)

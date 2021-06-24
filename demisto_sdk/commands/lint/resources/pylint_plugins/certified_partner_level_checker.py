85368648d7a91d611b218f67f34e14f5689b0d0b
yorhov <yorhov@paloaltonetworks.com>
2021-06-24 14:32:58 +0300

    added docs to partner and xsoar checkers

--- demisto_sdk/commands/lint/resources/pylint_plugins/certified_partner_level_checker.py
+++ demisto_sdk/commands/lint/resources/pylint_plugins/certified_partner_level_checker.py
@@ -2,6 +2,11 @@ import astroid
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 
+# You can find documentation about adding new checker here:
+# http://pylint.pycqa.org/en/latest/how_tos/custom_checkers.html#write-a-checker
+
+# -------------------------------------------- Messages ------------------------------------------------
+
 cert_partner_msg = {
     "E9001": ("Sys.exit use is found, Please use return instead.", "sys-exit-exists",
               "Ensure to not use sys.exit in the code.",),
@@ -37,6 +42,15 @@ class CertifiedPartnerChecker(BaseChecker):
         super(CertifiedPartnerChecker, self).__init__(linter)
         self.list_of_function_names = set()
 
+    # ------------------------------------- visit functions -------------------------------------------------
+    '''
+    `visit_<node_name>` is a function which will be activated while visiting the node_name in the ast of the
+    python code.
+    When adding a new check:
+    1. Add a new checker function to the validations section.
+    2. Add the function's activation under the relevant visit function.
+    '''
+
     def visit_call(self, node):
         self._sys_exit_checker(node)
         self._demisto_log_checker(node)
@@ -48,70 +62,150 @@ class CertifiedPartnerChecker(BaseChecker):
     def visit_functiondef(self, node):
         self.list_of_function_names.add(node.name)
 
+    # ------------------------------------- leave functions -------------------------------------------------
+    '''
+    `leave_<node_name>` is a function which will be activated while leaving the node_name in the ast of the
+    python code.
+    When adding a new check:
+    1. Add a new checker function to the validations section.
+    2. Add the function's activation under the relevant leave function.
+
+    * leave_module will be activated at the end of the file.
+    '''
+
     def leave_module(self, node):
         self._main_function(node)
 
-    # -------------------------------------------- Validations--------------------------------------------------
+# ---------------------------------------------------- Checkers  ------------------------------------------------------
+    '''
+    Checker functions are the functions that have the logic of our check and should be activated in one or more
+     visit/leave functions.
+    '''
+
+    # -------------------------------------------- Call Node ---------------------------------------------
 
     def _sys_exit_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if sys.exit() statement exists in the current node.
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.attrname == 'exit' and node.func.expr.name == 'sys' and node.args and node.args[0].value != 0:
                 self.add_message("sys-exit-exists", node=node)
+
         except Exception:
             pass
 
     def _demisto_log_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if demisto.log() statement exists in the current node.
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.attrname == 'log' and node.func.expr.name == 'demisto':
                 self.add_message("demisto-log-exists", node=node)
+
         except Exception:
             pass
 
-    def _main_function(self, node):
-        if 'main' not in self.list_of_function_names:
-            self.add_message("main-func-doesnt-exist", node=node)
-
     def _return_outputs_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if return_outputs() statement exists in the current node.
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.name == 'return_outputs':
                 self.add_message("return-outputs-exists", node=node)
+
         except Exception:
             pass
 
     def _demisto_results_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if demisto.results() statement exists in the current node.
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.attrname == 'results' and node.func.expr.name == 'demisto':
                 self.add_message("demisto-results-exists", node=node)
+
         except Exception:
             pass
 
     def _init_params_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if demisto.params() statement exists and if its parent node is main().
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.attrname == 'params' and node.func.expr.name == 'demisto':
                 check_param = True
                 parent = node.parent
+
+                # check if main function is one of the parent nodes of the current node that contains demisto.params()
                 while check_param and parent:
                     if isinstance(parent, astroid.FunctionDef) and parent.name == 'main':
                         check_param = False
                     parent = parent.parent
+
                 if check_param:
                     self.add_message("init-params-outside-main", node=node)
+
         except AttributeError:
             pass
 
     def _init_args_checker(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if demisto.args() statement exists and if its parent node is main().
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
         try:
             if node.func.attrname == 'args' and node.func.expr.name == 'demisto':
                 check_param = True
                 parent = node.parent
+
+                # check if main function is one of the parent nodes of the current node that contains demisto.params()
                 while check_param and parent:
                     if isinstance(parent, astroid.FunctionDef) and parent.name == 'main':
                         check_param = False
                     parent = parent.parent
+
                 if check_param:
                     self.add_message("init-args-outside-main", node=node)
+
         except AttributeError:
             pass
+
+    # -------------------------------------------- Module Node ---------------------------------------------
+
+    def _main_function(self, node):
+        """
+        Args: node which is a Call Node.
+        Check:
+        - if main() function exists in the code.
+
+        Adds the relevant error message using `add_message` function if one of the above exists.
+        """
+        if 'main' not in self.list_of_function_names:
+            self.add_message("main-func-doesnt-exist", node=node)
 
 
 def register(linter):

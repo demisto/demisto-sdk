5cc5b5a0489bce9c6623054a631df24a8e2d5a86
dkoval <dkoval@paloaltonetworks.com>
2021-07-05 14:09:22 +0300

    fixed tests

--- demisto_sdk/tests/integration_tests/validate_integration_test.py
+++ demisto_sdk/tests/integration_tests/validate_integration_test.py
@@ -530,9 +530,10 @@ class TestPackValidation:
         mocker.patch.object(ContentEntityValidator, '_load_conf_file', return_value=CONF_JSON_MOCK)
         mocker.patch.object(BaseValidator, 'check_file_flags', return_value='')
         mocker.patch.object(IntegrationValidator, 'is_there_separators_in_names', return_value=True)
-        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.get_current_usecases',
+        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.tools.get_current_usecases',
                      return_value=[])
-        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.get_current_tags', return_value=[])
+        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.tools.get_current_tags',
+                     return_value=[])
         runner = CliRunner(mix_stderr=False)
         result = runner.invoke(main, [VALIDATE_CMD, "-i", VALID_PACK_PATH, "--no-conf-json",
                                       "--allow-skipped"])
@@ -557,9 +558,9 @@ class TestPackValidation:
         """
         mocker.patch.object(ContentEntityValidator, '_load_conf_file', return_value=CONF_JSON_MOCK)
         mocker.patch.object(BaseValidator, 'check_file_flags', return_value='')
-        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.get_current_usecases',
+        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.tools.get_current_usecases',
                      return_value=[])
-        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.get_current_tags', return_value=[])
+        mocker.patch('demisto_sdk.commands.common.hook_validations.pack_unique_files.tools.get_current_tags', return_value=[])
         runner = CliRunner(mix_stderr=False)
         result = runner.invoke(main, [VALIDATE_CMD, "-i", AZURE_FEED_PACK_PATH, "--no-conf-json",
                                       "--allow-skipped"])

07a651d3024f5b67344141524e17a7709937370c
ypreisler <ypreisler@paloaltonetworks.com>
2021-07-06 14:07:20 +0300

    pre-commit fixes

--- demisto_sdk/commands/common/tests/tools_test.py
+++ demisto_sdk/commands/common/tests/tools_test.py
@@ -24,9 +24,10 @@ from demisto_sdk.commands.common.tools import (
     get_files_in_dir, get_ignore_pack_skipped_tests, get_last_release_version,
     get_last_remote_release_version, get_latest_release_notes_text,
     get_pack_metadata, get_relative_path_from_packs_dir,
-    get_release_notes_file_path, get_ryaml, get_to_version,
-    has_remote_configured, is_origin_content_repo, is_pack_path, is_uuid,
-    retrieve_file_ending, run_command_os, server_version_compare, get_release_note_entries)
+    get_release_note_entries, get_release_notes_file_path, get_ryaml,
+    get_to_version, has_remote_configured, is_origin_content_repo,
+    is_pack_path, is_uuid, retrieve_file_ending, run_command_os,
+    server_version_compare)
 from demisto_sdk.tests.constants_test import (IGNORED_PNG,
                                               INDICATORFIELD_EXTRA_FIELDS,
                                               SOURCE_FORMAT_INTEGRATION_COPY,
--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -1878,7 +1878,7 @@ def get_release_note_entries(version='') -> list:
     if f'# {version}' not in changelog_file_content:
         return []
 
-    result = changelog_file_content[changelog_file_content.index(f'# {version}')+1:]
+    result = changelog_file_content[changelog_file_content.index(f'# {version}') + 1:]
     result = result[:result.index('')]
 
     return result

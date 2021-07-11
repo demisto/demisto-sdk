07b78ee40a9657f83ee2f67a4acb2f1e76f4fc5c
Adi Daud <46249224+adi88d@users.noreply.github.com>
2021-07-11 13:53:55 +0300

    Update demisto_sdk/commands/common/tests/update_id_set_test.py
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- demisto_sdk/commands/common/tests/update_id_set_test.py
+++ demisto_sdk/commands/common/tests/update_id_set_test.py
@@ -502,7 +502,7 @@ class TestPlaybooks:
         "file_path": TESTS_DIR + "/test_files/DummyPack/Playbooks/DummyPlaybook.yml",
         "fromversion": "4.5.0",
         "filters": ["isEqualString"],
-        "transformers": ["'uniq'"],
+        "transformers": ["uniq"],
         "implementing_scripts": [
             "XDRSyncScript",
             "StopScheduledTask",

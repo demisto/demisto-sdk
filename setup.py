bfba94f3f794a554e49e5e7b4670b48f886360f3
tneeman <tneeman@paloaltonetworks.com>
2021-07-25 13:00:55 +0300

    wrapped setup.py with main func

--- setup.py
+++ setup.py
@@ -16,39 +16,40 @@ NAME = "demisto-sdk"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+if __name__ == "__main__":
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
 
-with open('README.md', 'r') as f:
-    readme = f.read()
+    with open('README.md', 'r') as f:
+        readme = f.read()
 
-setup(
-    use_scm_version={
-        'local_scheme': lambda a: ""
-    },
-    setup_requires=['setuptools_scm'],
-    name=NAME,
-    description="A Python library for the Demisto SDK",
-    author_email="",
-    url="https://github.com/demisto/demisto-sdk",
-    keywords=["Demisto"],
-    install_requires=requirements,
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        'console_scripts': ['demisto-sdk = demisto_sdk.__main__:main']
-    },
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython'
-    ],
-    python_requires=">=3.7",
-    author="Demisto"
-)
+    setup(
+        use_scm_version={
+            'local_scheme': lambda a: ""
+        },
+        setup_requires=['setuptools_scm'],
+        name=NAME,
+        description="A Python library for the Demisto SDK",
+        author_email="",
+        url="https://github.com/demisto/demisto-sdk",
+        keywords=["Demisto"],
+        install_requires=requirements,
+        packages=find_packages(),
+        include_package_data=True,
+        entry_points={
+            'console_scripts': ['demisto-sdk = demisto_sdk.__main__:main']
+        },
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        license='MIT',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: Implementation :: CPython'
+        ],
+        python_requires=">=3.7",
+        author="Demisto"
+    )

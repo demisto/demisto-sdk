37f74f0a001b05c5d93c4d2d31c48843e042f241
tneeman <tneeman@paloaltonetworks.com>
2021-07-25 12:40:15 +0300

    changed order of creating requirements file

--- demisto_sdk/utils/deploy.sh
+++ demisto_sdk/utils/deploy.sh
@@ -4,9 +4,9 @@
 set -e
 
 function deploy_to_pypi () {
+    pipfile2req > requirements.txt
     pip install twine
     python setup.py sdist
-    pipfile2req > requirements.txt
     TWINE_USERNAME=__token__ twine upload dist/*
 }
 

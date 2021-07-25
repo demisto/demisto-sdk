15db59db7ad937ed6ae3dacf0a115560d97ca9b1
tkatzir <tkatzir@paloaltonetworks.com>
2021-07-25 09:13:59 +0300

    Comment out an unused method

--- demisto_sdk/commands/common/hook_validations/pre_process_rules.py
+++ demisto_sdk/commands/common/hook_validations/pre_process_rules.py
@@ -66,52 +66,52 @@ class PreProcessRulesValidator(ContentEntityValidator):
                 return False
         return True
 
-    # TODO Needed! Check that scripts and incident_fields exists
-    # Needed incident_fields: description
-    # If the key "scriptName" is not empty - Check that the script exists
-    def is_incident_field_exist(self, id_set_file, is_circle) -> bool:
-        """Checks if incident field is valid - exist in the content.
+    # # TODO Needed! Check that scripts and incident_fields exists
+    # # Needed incident_fields: description
+    # # If the key "scriptName" is not empty - Check that the script exists
+    # def is_incident_field_exist(self, id_set_file, is_circle) -> bool:
+    #     """Checks if incident field is valid - exist in the content.
 
-        Returns:
-            bool. True if incident field is valid, else False.
-        """
-        if not is_circle:
-            return True
+    #     Returns:
+    #         bool. True if incident field is valid, else False.
+    #     """
+    #     if not is_circle:
+    #         return True
 
-        if not id_set_file:
-            click.secho("Skipping mapper incident field validation. Could not read id_set.json.", fg="yellow")
-            return True
+    #     if not id_set_file:
+    #         click.secho("Skipping mapper incident field validation. Could not read id_set.json.", fg="yellow")
+    #         return True
 
-        pre_process_rules_incident_fields = []
+    #     pre_process_rules_incident_fields = []
 
-        layout = self.current_file.get('layout', {})
-        pre_process_rules_sections = layout.get('sections', [])
-        for section in pre_process_rules_sections:
-            for field in section.get('fields', []):
-                inc_field = field.get('fieldId', '')
-                pre_process_rules_incident_fields.append(inc_field.replace('incident_', ''))
+    #     layout = self.current_file.get('layout', {})
+    #     pre_process_rules_sections = layout.get('sections', [])
+    #     for section in pre_process_rules_sections:
+    #         for field in section.get('fields', []):
+    #             inc_field = field.get('fieldId', '')
+    #             pre_process_rules_incident_fields.append(inc_field.replace('incident_', ''))
 
-        layout_tabs = layout.get('tabs', [])
-        for tab in layout_tabs:
-            pre_process_rules_sections = tab.get('sections', [])
+    #     layout_tabs = layout.get('tabs', [])
+    #     for tab in layout_tabs:
+    #         pre_process_rules_sections = tab.get('sections', [])
 
-            for section in pre_process_rules_sections:
-                if section and section.get('items'):
-                    for item in section.get('items', []):
-                        inc_field = item.get('fieldId', '')
-                        pre_process_rules_incident_fields.append(inc_field.replace('incident_', '').replace('indicator_', ''))
+    #         for section in pre_process_rules_sections:
+    #             if section and section.get('items'):
+    #                 for item in section.get('items', []):
+    #                     inc_field = item.get('fieldId', '')
+    #                     pre_process_rules_incident_fields.append(inc_field.replace('incident_', '').replace('indicator_', ''))
 
-        content_incident_fields = get_all_incident_and_indicator_fields_from_id_set(id_set_file, 'layout')
+    #     content_incident_fields = get_all_incident_and_indicator_fields_from_id_set(id_set_file, 'layout')
 
-        built_in_fields = [field.lower() for field in BUILT_IN_FIELDS] + PRE_PROCESS_RULES_BUILT_IN_FIELDS
+    #     built_in_fields = [field.lower() for field in BUILT_IN_FIELDS] + PRE_PROCESS_RULES_BUILT_IN_FIELDS
 
-        invalid_inc_fields_list = []
-        for inc_field in pre_process_rules_incident_fields:
-            if inc_field and inc_field.lower() not in built_in_fields and inc_field not in content_incident_fields:
-                invalid_inc_fields_list.append(inc_field) if inc_field not in invalid_inc_fields_list else None
+    #     invalid_inc_fields_list = []
+    #     for inc_field in pre_process_rules_incident_fields:
+    #         if inc_field and inc_field.lower() not in built_in_fields and inc_field not in content_incident_fields:
+    #             invalid_inc_fields_list.append(inc_field) if inc_field not in invalid_inc_fields_list else None
 
-        if invalid_inc_fields_list:
-            error_message, error_code = Errors.invalid_incident_field_in_pre_process_rules(invalid_inc_fields_list)
-            if self.handle_error(error_message, error_code, file_path=self.file_path):
-                return False
-        return True
+    #     if invalid_inc_fields_list:
+    #         error_message, error_code = Errors.invalid_incident_field_in_pre_process_rules(invalid_inc_fields_list)
+    #         if self.handle_error(error_message, error_code, file_path=self.file_path):
+    #             return False
+    #     return True

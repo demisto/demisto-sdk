df1e9a185ba7f14fd3e33f187893f3b80e59a008
tneeman <tneeman@paloaltonetworks.com>
2021-08-02 16:51:02 +0300

    yml checks

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     author_email="",
     url="https://github.com/demisto/demisto-sdk",
     keywords=["Demisto"],
-    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     entry_points={

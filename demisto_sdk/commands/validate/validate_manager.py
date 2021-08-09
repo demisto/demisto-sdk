36652962f9985439b267dbabb8a99ccddb86568d
rsagi <rsagi@paloaltonetworks.com>
2021-08-09 19:23:28 +0300

    adding author image validation when running on all files

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -321,8 +321,7 @@ class ValidateManager:
 
         author_image_path = os.path.join(pack_path, AUTHOR_IMAGE_FILE_NAME)
         if os.path.exists(author_image_path):
-            pack_entities_validation_results.add(self.run_validations_on_file(author_image_path,
-                                                                              pack_error_ignore_list))
+            pack_entities_validation_results.add(self.validate_author_image(author_image_path, pack_error_ignore_list))
 
         for content_dir in os.listdir(pack_path):
             content_entity_path = os.path.join(pack_path, content_dir)

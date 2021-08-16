3eec08e5cbe8f0771884b4fdd71bd0dd641b3cf7
ostolero <ostolero@paloaltonetworks.com>
2021-08-16 14:50:38 +0300

    add return statement (error) in get_definition_name

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -1994,6 +1994,8 @@ def get_definition_name(path: str, pack_path: str) -> str:
             for generic type and generic field return associated generic definition name folder
 
     """
+    exception = "Generic Definition name was not found - make sure the generic field/generic type files are valid and " \
+                "connected to a generic definition file "
     try:
         file_dictionary = get_json(path)
         definition_id = file_dictionary['definitionId']
@@ -2008,6 +2010,9 @@ def get_definition_name(path: str, pack_path: str) -> str:
                     return def_file_dictionary["name"]
 
     except FileNotFoundError or AttributeError:
-        raise Exception("Generic Definition name was not found - "
-                        "make sure the generic field/generic type files are valid and connected to a generic "
-                        "definition file")
+        raise Exception(exception)
+
+    # if we couldn't find the definition name throw exception
+    raise Exception(exception)
+
+

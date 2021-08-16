1d6fac47e4a7390b8b024218b57cf96ed27f1f53
tneeman <tneeman@paloaltonetworks.com>
2021-08-16 14:45:41 +0300

    changed from gitpath to relative path

--- demisto_sdk/commands/generate_docs/generate_integration_doc.py
+++ demisto_sdk/commands/generate_docs/generate_integration_doc.py
@@ -174,7 +174,7 @@ def generate_integration_doc(
 
 # Setup integration on Demisto
 
-with open(Path(git_path()) / 'demisto_sdk/commands/generate_docs/default_additional_information.json') as f:
+with open(os.path.normpath(os.path.join(__file__, '..', 'default_additional_information.json'))) as f:
     # Case insensitive to catch both `API key` and `API Key`, giving both the same value.
     default_additional_information: CaseInsensitiveDict = CaseInsensitiveDict(json.load(f))
 

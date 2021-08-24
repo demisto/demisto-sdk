13cdbdee06f02a2092b1b0ce89fc121d51b53743
Agam More <agmore@paloaltonetworks.com>
2021-08-24 19:27:00 +0300

    Fix coverage for the main function and the mock of the build examples

--- demisto_sdk/commands/generate_context/tests/generate_integration_context_test.py
+++ demisto_sdk/commands/generate_context/tests/generate_integration_context_test.py
@@ -1,14 +1,17 @@
+import json
 import os
 
 from demisto_sdk.commands.common.legacy_git_tools import git_path
-from demisto_sdk.commands.common.tools import get_json, get_yaml
+from demisto_sdk.commands.common.tools import get_json, get_yaml, write_yml
 
 FILES_PATH = os.path.normpath(
     os.path.join(__file__, git_path(), 'demisto_sdk', 'tests', 'test_files'))
-FAKE_INTEGRATION_YML = get_yaml(
-    os.path.join(FILES_PATH, 'fake_integration.yml'))
+
+FAKE_INTEGRATION_YML = get_yaml(os.path.join(FILES_PATH, 'fake_integration_empty_output.yml'))
 FAKE_OUTPUT_CONTEXTS = get_json(
     os.path.join(FILES_PATH, 'fake_outputs_with_contexts.json'))
+FAKE_OUTPUTS = get_json(
+    os.path.join(FILES_PATH, 'fake_outputs.json'))
 FAKE_EXAMPLES_FILE = os.path.join(FILES_PATH, 'fake_examples.txt')
 
 
@@ -93,15 +96,57 @@ def test_insert_outputs(mocker):
     # os.environ["DEMISTO_PASSWORD"] = "1"
     from demisto_sdk.commands.generate_context import \
         generate_integration_context
+    command_name = 'zoom-fetch-recording'
     mocker.patch.object(generate_integration_context,
                         'build_example_dict',
-                        return_value=(FAKE_OUTPUT_CONTEXTS, []))
+                        return_value=(
+                            {command_name: (None, None, json.dumps(FAKE_OUTPUTS))},
+                            []))
 
     yml_data = FAKE_INTEGRATION_YML
 
-    command = 'zoom-fetch-recording'
-    yml_data = generate_integration_context.insert_outputs(yml_data, command, FAKE_OUTPUT_CONTEXTS)
+    yml_data = generate_integration_context.insert_outputs(yml_data, command_name, FAKE_OUTPUT_CONTEXTS)
     for command in yml_data['script']['commands']:
-        if command.get('name') == command:
+        if command.get('name') == command_name:
+            assert command['outputs'] == FAKE_OUTPUT_CONTEXTS
+            break
+
+
+def test_generate_integration_context(mocker, tmpdir):
+    """
+    Given
+      - A yaml file and fake example file
+    When
+      - generating the yml ouputs from the examples
+    Then
+      - Ensure the outputs are inserted correctly
+    """
+    from demisto_sdk.commands.generate_context import \
+        generate_integration_context
+
+    command_name = 'zoom-fetch-recording'
+    mocker.patch.object(generate_integration_context,
+                        'build_example_dict',
+                        return_value=(
+                            {command_name: (None, None, json.dumps(FAKE_OUTPUTS))},
+                            []))
+
+    # Temp file to check
+    filename = os.path.join(tmpdir.strpath, 'fake_integration.yml')
+    write_yml(filename, FAKE_INTEGRATION_YML)
+
+    # Check there are no outputs
+    yml_data = get_yaml(filename)
+    for command in yml_data['script']['commands']:
+        if command.get('name') == command_name:
+            assert command['outputs'] == ''
+            break
+
+    generate_integration_context.generate_integration_context(filename, FAKE_EXAMPLES_FILE, insecure=True, verbose=False)
+
+    # Check we have new data
+    yml_data = get_yaml(filename)
+    for command in yml_data['script']['commands']:
+        if command.get('name') == command_name:
             assert command['outputs'] == FAKE_OUTPUT_CONTEXTS
             break

28aedfd52aee66945c19121f24cbdcb182c4eecd
dschwartz <dschwartz@paloaltonetworks.com>
2021-09-19 14:47:02 +0300

    more validator changes, scheme

--- demisto_sdk/commands/common/hook_validations/job.py
+++ demisto_sdk/commands/common/hook_validations/job.py
@@ -14,26 +14,8 @@ class JobValidator(ContentEntityValidator):
         super().__init__(structure_validator, ignored_errors, print_as_warnings, json_file_path=json_file_path,
                          **kwargs)
         self.from_version = self.current_file.get('fromServerVersion')
-        self.to_version = self.current_file.get('toServerVersion')
 
-    def is_valid_job(self):
-        validations: Tuple = (self.is_valid_version(),
-                              self.is_valid_from_server_version())
-        # todo id_set checks?
-        return all(validations)
-
-    def is_valid_version(self) -> bool:
-        """Checks if version field is valid. uses default method.
-        Returns:
-            bool. True if version is valid, else False.
-        """
-        return self._is_valid_version()
-
-    def is_valid_from_server_version(self) -> bool:
-        """Checks if from version field is valid.
-        Returns:
-            bool. True if from version field is valid, else False.
-        """
+    def is_valid_fromversion(self):
         if self.from_version:
             if LooseVersion(self.from_version) < MIN_FROM_VERSION:
                 error_message, error_code = Errors.invalid_from_server_version_in_job(self.from_version)

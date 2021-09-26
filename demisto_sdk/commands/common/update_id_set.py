2133c19f42efa609fc4137a1992d92588d238291
rweinstock <rweinstock@paloaltonetworks.com>
2021-09-26 18:15:53 +0300

    Fixed flake8

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -1893,8 +1893,8 @@ def has_duplicate(id_set_subset_list, id_to_check, object_type=None, print_logs=
             dict2_from_version < dict1_to_version <= dict2_to_version,  # will catch (C, B), (C, A)
         ]):
             print_warning('The following {} have the same ID ({}) and their versions overlap: '
-                          '1) "{}-{}", 2) "{}-{}".'.format(object_type, id_to_check, dict1_from_version, dict1_to_version,
-                                                         dict2_from_version, dict2_to_version))
+                          '1) "{}-{}", 2) "{}-{}".'.format(object_type, id_to_check, dict1_from_version,
+                                                           dict1_to_version, dict2_from_version, dict2_to_version))
             return True
 
         if print_logs and dict1.get('name') != dict2.get('name'):

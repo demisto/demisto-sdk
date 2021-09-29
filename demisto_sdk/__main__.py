788c11b3c553896c360ac7feeb5db4ef78bfd440
Agam More <agmore@paloaltonetworks.com>
2021-09-30 01:57:53 +0300

    Refactor utils for config __main__, add tests to the new generate outputs

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -4,7 +4,6 @@ import logging
 import os
 import sys
 import tempfile
-from configparser import ConfigParser, MissingSectionHeaderError
 from pathlib import Path
 from typing import IO
 
@@ -34,8 +33,6 @@ from demisto_sdk.commands.error_code_info.error_code_info import \
 from demisto_sdk.commands.find_dependencies.find_dependencies import \
     PackDependencies
 from demisto_sdk.commands.format.format_module import format_manager
-from demisto_sdk.commands.generate_context.generate_integration_context import \
-    generate_integration_context
 from demisto_sdk.commands.generate_docs.generate_integration_doc import \
     generate_integration_doc
 from demisto_sdk.commands.generate_docs.generate_playbook_doc import \
@@ -44,13 +41,13 @@ from demisto_sdk.commands.generate_docs.generate_script_doc import \
     generate_script_doc
 from demisto_sdk.commands.generate_integration.code_generator import \
     IntegrationGeneratorConfig
+from demisto_sdk.commands.generate_outputs.generate_outputs import \
+    run_generate_outputs
 from demisto_sdk.commands.generate_test_playbook.test_playbook_generator import \
     PlaybookTestsGenerator
 from demisto_sdk.commands.init.initiator import Initiator
 from demisto_sdk.commands.integration_diff.integration_diff_detector import \
     IntegrationDiffDetector
-from demisto_sdk.commands.json_to_outputs.json_to_outputs import \
-    json_to_outputs
 from demisto_sdk.commands.lint.lint_manager import LintManager
 from demisto_sdk.commands.openapi_codegen.openapi_codegen import \
     OpenAPIIntegration
@@ -73,6 +70,7 @@ from demisto_sdk.commands.upload.uploader import Uploader
 from demisto_sdk.commands.validate.validate_manager import ValidateManager
 from demisto_sdk.commands.zip_packs.packs_zipper import (EX_FAIL, EX_SUCCESS,
                                                          PacksZipper)
+from demisto_sdk.utils.config import check_configuration_file
 
 
 class PathsParamType(click.Path):
@@ -106,38 +104,6 @@ class DemistoSDK:
 
 
 pass_config = click.make_pass_decorator(DemistoSDK, ensure=True)
-
-
-def check_configuration_file(command, args):
-    config_file_path = '.demisto-sdk-conf'
-    true_synonyms = ['true', 'True', 't', '1']
-    if os.path.isfile(config_file_path):
-        try:
-            config = ConfigParser(allow_no_value=True)
-            config.read(config_file_path)
-
-            if command in config.sections():
-                for key in config[command]:
-                    if key in args:
-                        # if the key exists in the args we will run it over if it is either:
-                        # a - a flag currently not set and is defined in the conf file
-                        # b - not a flag but an arg that is currently None and there is a value for it in the conf file
-                        if args[key] is False and config[command][key] in true_synonyms:
-                            args[key] = True
-
-                        elif args[key] is None and config[command][key] is not None:
-                            args[key] = config[command][key]
-
-                    # if the key does not exist in the current args, add it
-                    else:
-                        if config[command][key] in true_synonyms:
-                            args[key] = True
-
-                        else:
-                            args[key] = config[command][key]
-
-        except MissingSectionHeaderError:
-            pass
 
 
 @click.group(invoke_without_command=True, no_args_is_help=True, context_settings=dict(max_content_width=100), )
@@ -963,54 +929,9 @@ def generate_outputs(**kwargs):
     """Demisto integrations/scripts have a YAML file that defines them.
     Creating the YAML file is a tedious and error-prone task of manually copying outputs from the API result to the
     file/UI/PyCharm. This script auto generates the YAML for a command from the JSON result of the relevant API call
+    In addition you can supply examples files and generate the context description directly in the YML from those examples.
     """
-
-    # Examples
-    # demisto-sdk generate-outputs -e <example> -o integration.yml - generating outputs from the example file
-    # demisto-sdk generate-outputs -e <example> -o script.yml -c some-command-here - specify the specific command to run
-    # demisto-sdk generate-outputs -j <json_input> -o integration.yml - generating outputs from the json input file (exactly like json-to-outputs)
-
-    # Do the same flow like previously in json-to-outputs
-    if kwargs.get('json', False):
-        if not kwargs.get('command'):
-            print_error(
-                'To use the json-to-outputs version of this command please include a `command` argument.')
-            return
-        if not kwargs.get('prefix'):
-            print_error(
-                'To use the json-to-outputs version of this command please include a `prefix` argument.')
-            return
-        check_configuration_file('json-to-outputs', kwargs)
-        args = [kwargs.get('command'), kwargs.get('json'), kwargs.get('prefix'),
-                kwargs.get('output'),
-                kwargs.get('verbose'), kwargs.get('interactive'),
-                kwargs.get('descriptions')]
-        json_to_outputs(*args)
-        return
-
-    input_path: str = kwargs.get('input', '')
-    examples: str = kwargs.get('examples', '')
-    insecure: bool = kwargs.get('insecure', False)
-
-    # validate inputs
-    if input_path and not os.path.isfile(input_path):
-        print_error(F'Input file {input_path} was not found.')
-        return 1
-
-    if not input_path.lower().endswith('.yml'):
-        print_error(F'Input {input_path} is not a valid yml file.')
-        return 1
-
-    file_type = find_type(kwargs.get('input', ''), ignore_sub_categories=True)
-    if file_type is not FileType.INTEGRATION:
-        print_error('File is not an Integration.')
-        return 1
-
-    if file_type == FileType.INTEGRATION:
-        generate_integration_context(input_path, examples, insecure)
-    else:
-        print_error(f'File type {file_type.value} is not supported.')
-        return 1
+    return run_generate_outputs(**kwargs)
 
 
 # ====================== generate-test-playbook ====================== #
@@ -1119,52 +1040,6 @@ def init(**kwargs):
     initiator = Initiator(**kwargs)
     initiator.init()
     return 0
-
-
-# ====================== generate-context ====================== #
-@main.command()
-@click.help_option(
-    '-h', '--help'
-)
-@click.option(
-    "-i", "--input", help="Path of the yml file (will change it in-place).",
-    required=True)
-@click.option(
-    "-e", "--examples",
-    help="Integrations: path for file containing command examples."
-         " Each command should be in a separate line.")
-@click.option(
-    "--insecure",
-    help="Skip certificate validation to run the commands in order to generate the docs.",
-    is_flag=True)
-@click.option(
-    "-v", "--verbose", is_flag=True,
-    help="Verbose output - mainly for debugging purposes.")
-def generate_context(**kwargs):
-    input_path: str = kwargs.get('input', '')
-    examples: str = kwargs.get('examples', '')
-    insecure: bool = kwargs.get('insecure', False)
-    verbose: bool = kwargs.get('verbose', False)
-
-    # validate inputs
-    if input_path and not os.path.isfile(input_path):
-        print_error(F'Input file {input_path} was not found.')
-        return 1
-
-    if not input_path.lower().endswith('.yml'):
-        print_error(F'Input {input_path} is not a valid yml file.')
-        return 1
-
-    file_type = find_type(kwargs.get('input', ''), ignore_sub_categories=True)
-
-    if not examples or not os.path.isfile(examples):
-        print_error(f'Command examples file was not found {examples}.')
-
-    if file_type == FileType.INTEGRATION:
-        return generate_integration_context(input_path, examples, insecure, verbose)
-    else:
-        print_error(f'File type {file_type.value} is not supported.')
-        return 1
 
 
 # ====================== generate-docs ====================== #

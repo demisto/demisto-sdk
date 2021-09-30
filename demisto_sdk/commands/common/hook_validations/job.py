a704cf0857958b37ca2d66b63af766006c5b9b8b
dschwartz <dschwartz@paloaltonetworks.com>
2021-09-30 11:14:18 +0300

    fromServer validation

--- demisto_sdk/commands/common/hook_validations/job.py
+++ demisto_sdk/commands/common/hook_validations/job.py
@@ -8,6 +8,10 @@ MIN_FROM_VERSION = LooseVersion("6.5.0")
 
 
 class JobValidator(ContentEntityValidator):
+    def is_valid_version(self):
+        # todo do we want to validate this?
+        pass
+
     def __init__(self, structure_validator=True, ignored_errors=False, print_as_warnings=False, json_file_path=None,
                  **kwargs):
         super().__init__(structure_validator, ignored_errors, print_as_warnings, json_file_path=json_file_path,
@@ -15,12 +19,11 @@ class JobValidator(ContentEntityValidator):
         self.from_version = self.current_file.get('fromServerVersion')
 
     def is_valid_fromversion(self):
-        if self.from_version:
-            if LooseVersion(self.from_version) < MIN_FROM_VERSION:
-                error_message, error_code = Errors.invalid_from_server_version_in_job(self.from_version)
-                if self.handle_error(error_message, error_code, file_path=self.file_path):
-                    return False
-        return True  # todo is missing from_version okay?
+        if not self.from_version or LooseVersion(self.from_version) < MIN_FROM_VERSION:
+            error_message, error_code = Errors.invalid_from_server_version_in_job(self.from_version)
+            if self.handle_error(error_message, error_code, file_path=self.file_path):
+                return False
+        return True
 
     def is_valid_feed_fields(self):
         is_feed = self.current_file.get('is_feed')

0892dc01470adc0b9f390aa95d1f682a81d27719
dschwartz <dschwartz@paloaltonetworks.com>
2021-10-03 18:37:28 +0300

    job_test

--- demisto_sdk/commands/common/content/tests/objects/pack_objects/job/job_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/job/job_test.py
@@ -5,19 +5,20 @@ from demisto_sdk.commands.common.content.objects.pack_objects.job.job import \
 from demisto_sdk.commands.common.content.objects_factory import \
     path_to_pack_object
 
-sample_file_path = Path('sample-job.json')
+sample_file_path = Path('Jobs/sample-job.json')
+sample_file_folder = Path("Jobs")
 
 
 class TestJob:
     def test_objects_factory(self, datadir):
-        obj = path_to_pack_object(datadir[sample_file_path.name])
+        obj = path_to_pack_object(datadir / sample_file_path)
         assert isinstance(obj, Job)
 
     def test_prefix(self, datadir):
-        obj = Job(datadir[sample_file_path.name])
+        obj = Job(datadir / sample_file_path)
         assert obj.normalize_file_name() == 'job-' + sample_file_path.name
 
     def test_files_detection(self, datadir):
-        obj = Job(datadir[sample_file_path.name])
-        assert obj.readme.path == Path(datadir[f"{sample_file_path.stem}_README.md"])
-        assert obj.changelog.path == Path(datadir[f"{sample_file_path.stem}_CHANGELOG.md"])
+        obj = Job(datadir / sample_file_path)
+        assert obj.readme.path == Path(datadir / sample_file_folder / f"{sample_file_path.stem}_README.md")
+        assert obj.changelog.path == Path(datadir / sample_file_folder / f"{sample_file_path.stem}_CHANGELOG.md")
--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -1186,7 +1186,7 @@ def find_type(path: str = '', _dict=None, file_type: Optional[str] = None, ignor
         if 'auditable' in _dict:
             return FileType.GENERIC_DEFINITION
 
-        if all(k in _dict for k in ('isAllFields', 'selectedFeeds', 'isFeeds')):  # todo
+        if all(k in _dict for k in ('isAllFields', 'selectedFeeds', 'isFeed')):  # todo
             return FileType.JOB
 
         # When using it for all files validation- sometimes 'id' can be integer

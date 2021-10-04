34b39f99417e501bf26775b477bcd4a5c1493500
dschwartz <dschwartz@paloaltonetworks.com>
2021-10-04 09:29:52 +0300

    test job

--- demisto_sdk/commands/common/content/tests/objects/pack_objects/job/job_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/job/job_test.py
@@ -5,20 +5,22 @@ from demisto_sdk.commands.common.content.objects.pack_objects.job.job import \
 from demisto_sdk.commands.common.content.objects_factory import \
     path_to_pack_object
 
-sample_file_path = Path('Jobs/sample-job.json')
-sample_file_folder = Path("Jobs")
+sample_file_path = "Jobs/sample-job.json"
 
 
 class TestJob:
     def test_objects_factory(self, datadir):
-        obj = path_to_pack_object(datadir / sample_file_path)
+        obj = path_to_pack_object(datadir[sample_file_path])
         assert isinstance(obj, Job)
 
     def test_prefix(self, datadir):
-        obj = Job(datadir / sample_file_path)
-        assert obj.normalize_file_name() == 'job-' + sample_file_path.name
+        """
+        Test that Jobs created from files whose name does not start with `job-` are normalized correctly.
+        """
+        obj = Job(datadir[sample_file_path])
+        assert obj.normalize_file_name() == 'job-' + sample_file_path
 
     def test_files_detection(self, datadir):
-        obj = Job(datadir / sample_file_path)
-        assert obj.readme.path == Path(datadir / sample_file_folder / f"{sample_file_path.stem}_README.md")
-        assert obj.changelog.path == Path(datadir / sample_file_folder / f"{sample_file_path.stem}_CHANGELOG.md")
+        obj = Job(datadir[sample_file_path])
+        assert obj.readme.path == Path(datadir[f"{sample_file_path}_README.md"])
+        assert obj.changelog.path == Path(datadir[f"{sample_file_path}_CHANGELOG.md"])

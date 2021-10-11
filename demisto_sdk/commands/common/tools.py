8f0be01153f1a87d66802ebb9ae8e91a493b0545
dschwartz <dschwartz@paloaltonetworks.com>
2021-10-11 14:58:21 +0300

    syntax

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -1203,7 +1203,7 @@ def find_type(path: str = '', _dict=None, file_type: Optional[str] = None, ignor
         if 'auditable' in _dict:
             return FileType.GENERIC_DEFINITION
 
-        if {'isAllFields', 'selectedFeeds', 'isFeed'}.issubset(_dict.keys()):
+        if isinstance(_dict, dict) and {'isAllFields', 'selectedFeeds', 'isFeed'}.issubset(_dict.keys()):
             return FileType.JOB
 
         # When using it for all files validation- sometimes 'id' can be integer

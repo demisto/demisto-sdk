3f5d638487358799664f2001b54443ebb9eddb06
cshayner <cshayner@paloaltonetworks.com>
2021-10-18 15:08:43 +0300

    CR Fixes

--- demisto_sdk/commands/upload/uploader.py
+++ demisto_sdk/commands/upload/uploader.py
@@ -389,20 +389,22 @@ def sort_directories_based_on_dependencies(dir_list: list) -> list:
 
 
 class ConfigFileParser:
+    """Parse configuration file to get a list of custom packs to upload to a remote Cortex XSOAR instance.
+        Attributes:
+            config_file_path (str): The path of the configuration file.
+        """
 
     def __init__(self, config_file_path: str):
         self.config_file_path = config_file_path
 
     def parse_file(self):
-        if not self.config_file_path:
-            return
         config_file_data = self.get_file_data()
         custom_packs_paths = self.get_custom_packs_paths(config_file_data)
         return custom_packs_paths
 
     def get_file_data(self):
-        with open(self.config_file_path) as cfp:
-            config_file_data = json.load(cfp)
+        with open(self.config_file_path) as config_file:
+            config_file_data = json.load(config_file)
         return config_file_data
 
     def get_custom_packs_paths(self, config_file_data):

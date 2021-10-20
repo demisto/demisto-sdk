bc1a3e4611007e38caef0b32ab0a25a92649f923
nmaimon <nmaimon@paloaltonetworks.com>
2021-10-20 12:39:41 +0300

    fix UT

--- demisto_sdk/commands/common/content/tests/objects/pack_objects/lists/lists_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/lists/lists_test.py
@@ -7,8 +7,8 @@ from demisto_sdk.commands.common.tools import src_root
 TEST_DATA = src_root() / 'tests' / 'test_files'
 TEST_CONTENT_REPO = TEST_DATA / 'content_slim'
 LIST_GOOD = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / LISTS_DIR / 'list-checked_integrations.json'
-LIST_BAD = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / LISTS_DIR / 'sample_bad.json'
-LIST_BAD_NORMALIZED = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / LISTS_DIR / 'list-sample_bad.json'
+LIST_BAD = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / LISTS_DIR / 'bad_name.json'
+LIST_BAD_NORMALIZED = TEST_CONTENT_REPO / PACKS_DIR / 'Sample01' / LISTS_DIR / 'list-bad_name.json'
 
 
 def test_objects_factory():

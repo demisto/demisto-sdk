31313868cc454700ffa40e2c79106147349a8dcf
Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
2021-11-17 11:57:49 +0200

    Update __main__.py (#1714)

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -1864,7 +1864,6 @@ def convert(config, **kwargs):
 @main.command(
     name='error-code',
     help='Quickly find relevant information regarding an error code.',
-    hidden=True,
 )
 @click.help_option(
     '-h', '--help'

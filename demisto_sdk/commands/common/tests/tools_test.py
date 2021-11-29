39b95b9e211f427fc2ec805cda828b8d99ca6782
BEAdi <adi.bamberger@gmail.com>
2021-11-29 11:10:06 +0200

    added UT to indicate it fails

--- demisto_sdk/commands/common/tests/tools_test.py
+++ demisto_sdk/commands/common/tests/tools_test.py
@@ -26,7 +26,7 @@ from demisto_sdk.commands.common.tools import (
     get_release_note_entries, get_release_notes_file_path, get_ryaml,
     get_test_playbook_id, get_to_version, has_remote_configured,
     is_origin_content_repo, is_pack_path, is_uuid, retrieve_file_ending,
-    run_command_os, server_version_compare)
+    run_command_os, server_version_compare, to_kebab_case)
 from demisto_sdk.tests.constants_test import (IGNORED_PNG,
                                               INDICATORFIELD_EXTRA_FIELDS,
                                               SOURCE_FORMAT_INTEGRATION_COPY,
@@ -1473,3 +1473,15 @@ def test_get_test_playbook_id():
     test_playbook_name, test_playbook_pack = get_test_playbook_id(test_playbook_id_set, test_name)
     assert test_playbook_name == 'HelloWorld-Test'
     assert test_playbook_pack == 'HelloWorld'
+
+
+KEBAB_CASES = [('Scan File', 'scan-file'),
+               ('Scan File-', 'scan-file'),
+               ('*scan,file', 'scan-file'),
+               ('Scan     File', 'scan-file'),
+               ('Scan - File', 'scan-file')]
+
+
+@pytest.mark.parametrize('input_str, output_str', KEBAB_CASES)
+def test_to_kebab_case(input_str, output_str):
+    assert to_kebab_case(input_str) == output_str

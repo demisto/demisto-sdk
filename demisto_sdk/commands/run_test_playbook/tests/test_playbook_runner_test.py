4c816672d35fe713e6052b32e1aca4c5b24c0a08
cshayner <cshayner@paloaltonetworks.com>
2021-12-05 23:19:07 +0200

    More UTs

--- demisto_sdk/commands/run_test_playbook/tests/test_playbook_runner_test.py
+++ demisto_sdk/commands/run_test_playbook/tests/test_playbook_runner_test.py
@@ -10,6 +10,11 @@ from demisto_sdk.tests.constants_test import (CONTENT_REPO_EXAMPLE_ROOT,
                                               TEST_PLAYBOOK, VALID_PACK)
 from TestSuite.test_tools import ChangeCWD
 
+WAITING_MASSAGE = "Waiting for the test playbook to finish running.."
+LINK_MASSAGE = 'To see the test playbook run in real-time please go to : http://localhost:8080/#/WorkPlan/1234'
+SUCCESS_MASSAGE = 'The test playbook has completed its run successfully'
+FAILED_MASSAGE = 'The test playbook finished running with status: FAILED'
+
 
 class TestTestPlaybookRunner:
 
@@ -30,9 +35,9 @@ class TestTestPlaybookRunner:
         result = click.Context(command=run_test_playbook).invoke(run_test_playbook, input=TEST_PLAYBOOK)
         assert result == res
 
-    @pytest.mark.parametrize(argnames='tpb_result, res', argvalues=[('failed', 1),
-                                                                    ('success', 0)])
-    def test_run_pack_test_playbooks(self, mocker, tpb_result, res):
+    @pytest.mark.parametrize(argnames='tpb_result, res, massage', argvalues=[('failed', 1, FAILED_MASSAGE),
+                                                                             ('success', 0, SUCCESS_MASSAGE)])
+    def test_run_pack_test_playbooks(self, mocker, tpb_result, res, massage, capsys):
         """
         Given:
             - run all pack test playbooks with result as True or False
@@ -40,6 +45,7 @@ class TestTestPlaybookRunner:
             - run the run_test_playbook command
         Then:
             - validate the results is aas expected
+            - validate the num of tpb is as expected (4 tpb in Azure Pack)
         """
         mocker.patch.object(demisto_client, 'configure', return_value=DefaultApi())
         mocker.patch.object(TestPlaybookRunner, 'create_incident_with_test_playbook', return_value='1234')
@@ -47,9 +53,12 @@ class TestTestPlaybookRunner:
         result = click.Context(command=run_test_playbook).invoke(run_test_playbook, input=VALID_PACK)
         assert result == res
 
-    @pytest.mark.parametrize(argnames='tpb_result, res', argvalues=[('failed', 1),
-                                                                    ('success', 0)])
-    def test_run_repo_test_playbooks(self, mocker, tpb_result, res):
+        stdout, _ = capsys.readouterr()
+        assert stdout.count(massage) == 4
+
+    @pytest.mark.parametrize(argnames='tpb_result, res, massage', argvalues=[('failed', 1, FAILED_MASSAGE),
+                                                                             ('success', 0, SUCCESS_MASSAGE)])
+    def test_run_repo_test_playbooks(self, mocker, tpb_result, res, massage, capsys):
         """
         Given:
             - run all repo test playbook with result as True or False
@@ -57,6 +66,7 @@ class TestTestPlaybookRunner:
             - run the run_test_playbook command
         Then:
             - validate the results is aas expected
+            - validate the num of tpb is as expected (7 tpb in CONTENT_REPO_EXAMPLE_ROOT)
         """
         with ChangeCWD(CONTENT_REPO_EXAMPLE_ROOT):
             mocker.patch.object(demisto_client, 'configure', return_value=DefaultApi())
@@ -65,17 +75,18 @@ class TestTestPlaybookRunner:
             result = click.Context(command=run_test_playbook).invoke(run_test_playbook, all=True, input='')
             assert result == res
 
+            stdout, _ = capsys.readouterr()
+            assert stdout.count(massage) == 7
+
     @pytest.mark.parametrize(argnames='input_tpb, exit_code, err',
-                             argvalues=[('', 1, "Error: Missing option '-i' / '--input'."),
-                                        ('BlaBla', 1, 'Error: Given input path: BlaBla does not exist'),
-                                        (VALID_PACK, 0, ''),
+                             argvalues=[(VALID_PACK, 0, ''),
                                         (TEST_PLAYBOOK, 0, '')])
-    def test_update_config_file_manager(self, mocker, input_tpb, exit_code, err, capsys):
+    def test_run_test_playbook_manager(self, mocker, input_tpb, exit_code, err, capsys):
         """
         Given:
-            - arguments to the xsoar-configuration-file
+            - arguments to the run-test-playbook
         When:
-            - check that the update_config_file_manager works as expected
+            - check that the run-test-playbook works as expected
         Then:
             - validate the error code is as expected.
             - validate the Error massage when the argument is missing
@@ -93,6 +104,85 @@ class TestTestPlaybookRunner:
         if err:
             assert err in stdout
 
-    # def test_run_test_playbook_by_id(self, mocker, playbook_id):
-    #     mocker.patch.object(TestPlaybookRunner, 'create_incident_with_test_playbook', return_value='1234')
-    #     mocker.patch.object(TestPlaybookRunner, 'get_test_playbook_results_dict', return_value={'state': 'success'})
+    @pytest.mark.parametrize(argnames='input_tpb, exit_code, err',
+                             argvalues=[('', 1, "Error: Missing option '-i' / '--input'."),
+                                        ('BlaBla', 1, 'Error: Given input path: BlaBla does not exist')])
+    def test_failed_run_test_playbook_manager(self, mocker, input_tpb, exit_code, err, capsys):
+        """
+        Given:
+            - arguments to the run-test-playbook
+        When:
+            - check that the run-test-playbook works as expected
+        Then:
+            - validate the error code is as expected.
+            - validate the Error massage when the argument is missing
+        """
+        mocker.patch.object(demisto_client, 'configure', return_value=DefaultApi())
+        mocker.patch.object(TestPlaybookRunner, 'create_incident_with_test_playbook', return_value='1234')
+        mocker.patch.object(TestPlaybookRunner, 'get_test_playbook_results_dict', return_value={'state': 'success'})
+
+        self.test_playbook_input = input_tpb
+        test_playbook = TestPlaybookRunner(input=self.test_playbook_input)
+        error_code = test_playbook.run_test_playbooks_manager()
+        assert error_code == exit_code
+
+        stdout, _ = capsys.readouterr()
+        if err:
+            assert err in stdout
+
+    @pytest.mark.parametrize(argnames='playbook_id, tpb_results, exit_code',
+                             argvalues=[(VALID_PACK, "success", 0),
+                                        (TEST_PLAYBOOK, "success", 0)])
+    def test_run_test_playbook_by_id(self, mocker, playbook_id, tpb_results, exit_code, capsys):
+        """
+        Given:
+            - arguments to the xsoar-configuration-file
+        When:
+            - check that the run_test_playbook_by_id works as expected
+        Then:
+            - validate the error code is as expected.
+            - validate all the massage is as expected.
+        """
+        mocker.patch.object(demisto_client, 'configure', return_value=DefaultApi())
+        mocker.patch.object(TestPlaybookRunner, 'create_incident_with_test_playbook', return_value='1234')
+        mocker.patch.object(TestPlaybookRunner, 'get_test_playbook_results_dict', return_value={'state': tpb_results})
+
+        self.test_playbook_input = TEST_PLAYBOOK
+        test_playbook_runner = TestPlaybookRunner(input=self.test_playbook_input)
+        res = test_playbook_runner.run_test_playbook_by_id(playbook_id)
+
+        assert res == exit_code
+
+        stdout, _ = capsys.readouterr()
+        assert WAITING_MASSAGE in stdout
+        assert LINK_MASSAGE in stdout
+        assert SUCCESS_MASSAGE in stdout
+
+    @pytest.mark.parametrize(argnames='playbook_id, tpb_results, exit_code',
+                             argvalues=[('VALID_PACK', "failed", 1),
+                                        ('TEST_PLAYBOOK', "failed", 1)])
+    def test_failed_run_test_playbook_by_id(self, mocker, playbook_id, tpb_results, exit_code, capsys):
+        """
+        Given:
+            - arguments to the xsoar-configuration-file
+        When:
+            - check that the run_test_playbook_by_id works as expected
+        Then:
+            - validate the error code is as expected.
+            - validate the all the massages is as expected.
+        """
+        mocker.patch.object(demisto_client, 'configure', return_value=DefaultApi())
+        mocker.patch.object(TestPlaybookRunner, 'create_incident_with_test_playbook', return_value='1234')
+        mocker.patch.object(TestPlaybookRunner, 'get_test_playbook_results_dict', return_value={'state': tpb_results})
+
+        self.test_playbook_input = TEST_PLAYBOOK
+        test_playbook_runner = TestPlaybookRunner(input=self.test_playbook_input)
+        res = test_playbook_runner.run_test_playbook_by_id(playbook_id)
+
+        assert res == exit_code
+
+        stdout, _ = capsys.readouterr()
+        assert WAITING_MASSAGE in stdout
+        assert LINK_MASSAGE in stdout
+        assert FAILED_MASSAGE in stdout
+

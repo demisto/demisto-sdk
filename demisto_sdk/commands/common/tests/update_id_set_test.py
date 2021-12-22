5f88fd6198b7392d2c952aeffc5656fcf0c694cb
Noy-Maimon <72340690+Noy-Maimon@users.noreply.github.com>
2021-12-22 22:33:41 +0200

    Update demisto_sdk/commands/common/tests/update_id_set_test.py
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- demisto_sdk/commands/common/tests/update_id_set_test.py
+++ demisto_sdk/commands/common/tests/update_id_set_test.py
@@ -49,7 +49,7 @@ class TestPacksMetadata:
         'tags': ['Alerts'],
         'useCases': ['Case Management'],
         'categories': ['Endpoint'],
-        'marketplaces': ['xsoar', 'marketplacev2']
+        'marketplaces': ['xsoar', 'marketplacev2'],
     }
 
     METADATA_WITH_COMMUNITY_SUPPORT = {

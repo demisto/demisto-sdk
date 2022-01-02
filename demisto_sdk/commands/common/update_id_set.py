b2e7b89a80313a932580f03fd80d06382645edb8
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-02 13:50:44 +0200

    deleted redundant flag

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -1576,7 +1576,7 @@ def merge_id_sets_from_files(first_id_set_path, second_id_set_path, output_id_se
     with open(second_id_set_path, mode='r') as f2:
         second_id_set = json.load(f2)
 
-    unified_id_set, duplicates = merge_id_sets(first_id_set, second_id_set, print_logs, is_new=False)
+    unified_id_set, duplicates = merge_id_sets(first_id_set, second_id_set, print_logs)
 
     if unified_id_set:
         with open(output_id_set_path, mode='w', encoding='utf-8') as f:

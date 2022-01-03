3f0499d33f27357f591faf2e4159b69d215b4372
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-03 19:00:14 +0200

    change to demisto/contrib

--- demisto_sdk/commands/init/contribution_converter.py
+++ demisto_sdk/commands/init/contribution_converter.py
@@ -264,7 +264,7 @@ class ContributionConverter:
             update_docker=True,
             verbose=True,
             assume_yes=True,
-            prev_ver='master',  # default is demisto/master
+            prev_ver='demisto/contrib',  # default is demisto/master
         )
 
     def generate_readme_for_pack_content_item(self, yml_path: str) -> None:

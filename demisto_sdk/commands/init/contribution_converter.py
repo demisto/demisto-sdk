ee56303be5dca1baad925f6df9243574a8e7e02a
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-04 18:08:59 +0200

    with input

--- demisto_sdk/commands/init/contribution_converter.py
+++ demisto_sdk/commands/init/contribution_converter.py
@@ -258,6 +258,7 @@ class ContributionConverter:
         )
         from_version = '6.0.0' if self.create_new else ''
         format_manager(
+            input=self.pack_dir_path,
             from_version=from_version,
             no_validate=True,
             update_docker=True,

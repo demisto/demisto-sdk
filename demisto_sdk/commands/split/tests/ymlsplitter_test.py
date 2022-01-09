6cbb914dc2b9938655f65a0dfd362fcbdaff985c
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-09 16:27:04 +0200

    pathlib

--- demisto_sdk/commands/split/tests/ymlsplitter_test.py
+++ demisto_sdk/commands/split/tests/ymlsplitter_test.py
@@ -116,7 +116,7 @@ def test_extract_code_pwsh(tmpdir):
 
     extractor.extract_code(extractor.output)
     # notice that we passed without an extension. Extractor should be adding .ps1
-    with open(extractor.output + '.ps1', 'r', encoding='utf-8') as temp_code:
+    with open(extractor.output.with_suffix('.ps1'), 'r', encoding='utf-8') as temp_code:
         file_data = temp_code.read()
         assert '. $PSScriptRoot\\CommonServerPowerShell.ps1\n' in file_data
         assert file_data[-1] == '\n'

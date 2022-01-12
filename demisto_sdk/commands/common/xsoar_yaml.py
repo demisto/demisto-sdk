469a7073f07bbee3fa45919ec626b81aa01c2a57
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-12 15:48:59 +0200

    make ryaml for _

--- demisto_sdk/commands/common/xsoar_yaml.py
+++ demisto_sdk/commands/common/xsoar_yaml.py
@@ -5,12 +5,12 @@ from ruamel.yaml import YAML
 
 class XSOAR_YAML:
     def __init__(self, preserve_quotes=True, allow_duplicate_keys=True, default_flow_style=None, width=None, typ=None):
-        self.ryaml = YAML()
-        self.ryaml.preserve_quotes = preserve_quotes
-        self.ryaml.allow_duplicate_keys = allow_duplicate_keys
-        self.ryaml.default_flow_style = default_flow_style
-        self.ryaml.width = width
-        self.ryaml.typ = typ
+        self._ryaml = YAML()
+        self._ryaml.preserve_quotes = preserve_quotes
+        self._ryaml.allow_duplicate_keys = allow_duplicate_keys
+        self._ryaml.default_flow_style = default_flow_style
+        self._ryaml.width = width
+        self._ryaml.typ = typ
 
     @staticmethod
     def _order_dict(data):
@@ -18,15 +18,15 @@ class XSOAR_YAML:
                 for k, v in sorted(data.items())}
 
     def load(self, stream):
-        return self.ryaml.load(stream)
+        return self._ryaml.load(stream)
 
     def dump(self, data, stream=None, sort_keys=False, **kwargs):
         if stream is None:
             string_stream = StringIO()
-            self.ryaml.dump(data, string_stream)
+            self._ryaml.dump(data, string_stream)
             output_str = string_stream.getvalue()
             string_stream.close()
             return output_str
         if sort_keys:
             data = XSOAR_YAML._order_dict(data)
-        return self.ryaml.dump(data, stream, **kwargs)
+        return self._ryaml.dump(data, stream, **kwargs)

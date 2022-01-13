41421c2330d1a63919123fe47468177bdf840385
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-13 15:07:13 +0200

    precommit

--- TestSuite/utils.py
+++ TestSuite/utils.py
@@ -3,7 +3,6 @@ class IsEqualFunctions:
     def is_dicts_equal(dict1: dict, dict2: dict):
         return dict(dict1) == dict(dict2)
 
-
     @staticmethod
     def is_lists_equal(list1: list, list2: list):
         return list(list1) == list(list2)

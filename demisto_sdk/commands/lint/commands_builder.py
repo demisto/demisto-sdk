dbbdd1f32f240d52947f7a3fd4fa576a98f3b2d8
ilappe <ilappe@paloaltonetworks.com>
2022-01-14 10:09:54 +0200

    mypy_cache

--- demisto_sdk/commands/lint/commands_builder.py
+++ demisto_sdk/commands/lint/commands_builder.py
@@ -151,7 +151,7 @@ def build_xsoar_linter_command(files: List[Path], py_num: float, support_level: 
     return command
 
 
-def build_mypy_command(files: List[Path], version: float) -> str:
+def build_mypy_command(files: List[Path], version: float, content_repo: Path = None) -> str:
     """ Build command to execute with mypy module
         https://mypy.readthedocs.io/en/stable/command_line.html
     Args:
@@ -181,7 +181,8 @@ def build_mypy_command(files: List[Path], version: float) -> str:
     # Get the full path to the file.
     command += " --show-absolute-path"
     # Disable cache creation
-    command += " --cache-dir=/dev/null"
+    # command += " --cache-dir=/dev/null"
+    command += f" --cache-dir={str(content_repo/'.mypy_cache') if content_repo else '/dev/null'}"
     # Generating path patterns - file1 file2 file3,..
     files_list = [str(item) for item in files]
     command += " " + " ".join(files_list)

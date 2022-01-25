1a550d8cc50469e1e1638f8651dec2f5f71164b0
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-25 11:55:47 +0200

    precommit

--- demisto_sdk/commands/common/tests/tools_test.py
+++ demisto_sdk/commands/common/tests/tools_test.py
@@ -9,13 +9,13 @@ import pytest
 import requests
 
 from demisto_sdk.commands.common import tools
-from demisto_sdk.commands.common.GitContentConfig import GitCredentials
 from demisto_sdk.commands.common.constants import (
     DEFAULT_CONTENT_ITEM_TO_VERSION, INTEGRATIONS_DIR, LAYOUTS_DIR, PACKS_DIR,
     PACKS_PACK_IGNORE_FILE_NAME, PLAYBOOKS_DIR, SCRIPTS_DIR,
     TEST_PLAYBOOKS_DIR, FileType)
 from demisto_sdk.commands.common.content import Content
 from demisto_sdk.commands.common.git_util import GitUtil
+from demisto_sdk.commands.common.GitContentConfig import GitCredentials
 from demisto_sdk.commands.common.legacy_git_tools import git_path
 from demisto_sdk.commands.common.tools import (
     LOG_COLORS, arg_to_list, compare_context_path_in_yml_and_readme,

41df322ab3b98e120b0beee7bb9fc65424153a8b
cshayner <cshayner@paloaltonetworks.com>
2022-01-30 19:15:16 +0200

    Remove mda start from the processes

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -321,10 +321,10 @@ class ValidateManager:
 
         click.secho('\n================= Validating all files =================', fg="bright_cyan")
 
-        readme_validator = ReadMeValidator(file_path='', ignored_errors='',
-                                           print_as_warnings=self.print_ignored_errors,
-                                           json_file_path=self.json_file_path)
-        readme_validator.start_mdx_server()
+        # readme_validator = ReadMeValidator(file_path='', ignored_errors='',
+        #                                    print_as_warnings=self.print_ignored_errors,
+        #                                    json_file_path=self.json_file_path)
+        # readme_validator.start_mdx_server()
 
         all_packs_valid = set()
 

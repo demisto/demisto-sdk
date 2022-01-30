3823f165bd2a8fdcfc2939386ef744c63f2bc750
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-30 16:04:52 +0200

    change ut to requests_mock

--- demisto_sdk/commands/test_content/tests/execute_test_content_test.py
+++ demisto_sdk/commands/test_content/tests/execute_test_content_test.py
@@ -3,7 +3,7 @@ import os
 from unittest import mock
 
 import pytest
-import responses
+import requests_mock
 
 from demisto_sdk.commands.test_content.execute_test_content import (
     COVERAGE_REPORT_COMMENT, SKIPPED_CONTENT_COMMENT, _add_pr_comment)
@@ -16,16 +16,21 @@ MOCK_ENV_VARIABLES = {
 }
 
 
-# https://github.com/getsentry/responses
-@pytest.mark.parametrize('comment', [
-    'The following integrations/tests were collected by the CI build but are currently skipped. '
-    'The collected tests are related to this pull request and might be critical:\n- demo integration',
-
-    'Link to the coverage report of the integration:\n '
-    'https://xsoar.docs.pan.run/-/content/-/jobs/123456/artifacts/artifacts/coverage_report/html/index.html'
+@pytest.mark.parametrize('comment, skipped_flow, coverage_flow', [
+    (
+        'The following integrations/tests were collected by the CI build but are currently skipped. '
+        'The collected tests are related to this pull request and might be critical:\n- demo integration',
+        True,
+        False,
+    ),
+    (
+        'Link to the coverage report of the integration:\n '
+        'https://xsoar.docs.pan.run/-/content/-/jobs/123456/artifacts/artifacts/coverage_report/html/index.html',
+        False,
+        True
+    )
 ])
-@responses.activate
-def test_add_pr_comment(mocker, comment):
+def test_add_pr_comment(mocker, comment, skipped_flow, coverage_flow):
     """
     When:
         - A job in content pipeline is running.
@@ -38,16 +43,8 @@ def test_add_pr_comment(mocker, comment):
         - Verify that comment was added as expected (and remove if needed)
     """
 
-    results = {}
-
     def mock_handle_github_response(response, logging_module):
-        return response.json()
-
-    def mock_post_response(request):
-        body = json.loads(request.body)
-        headers = {'request-id': '123456789'}
-        results.update(body)
-        return 200, headers, json.dumps(body)
+        return response.json() if response.text else ''
 
     mocker.patch('demisto_sdk.commands.test_content.execute_test_content._handle_github_response',
                  side_effect=mock_handle_github_response)
@@ -55,16 +52,19 @@ def test_add_pr_comment(mocker, comment):
     query = '?q=1234567890abcdef+repo:demisto/content+org:demisto+is:pr+is:open+head:mock_branch+is:open'
 
     with mock.patch.dict(os.environ, MOCK_ENV_VARIABLES, clear=True):
-        responses.add(responses.GET, url + query,
-                      json={'total_count': 1, 'items': [{'comments_url': 'https://api.github.com/search/issues/1'}]})
-        responses.add(responses.GET, 'https://api.github.com/search/issues/1', json=[
-            {'body': SKIPPED_CONTENT_COMMENT, 'url': 'https://github.com/comment_123456'},
-            {'body': COVERAGE_REPORT_COMMENT, 'url': 'https://github.com/comment_123456'}
-        ])
-        responses.add(responses.DELETE, 'https://github.com/comment_123456')
-        responses.add_callback(responses.POST, 'https://api.github.com/search/issues/1',
-                               callback=mock_post_response, content_type='application/json')
+        with requests_mock.Mocker() as m:
+            m.get(url + query, json={'total_count': 1,
+                                     'items': [{'comments_url': 'https://api.github.com/search/issues/1'}]})
+            m.get('https://api.github.com/search/issues/1', json=[
+                {'body': SKIPPED_CONTENT_COMMENT, 'url': 'https://github.com/comment_12345'},
+                {'body': COVERAGE_REPORT_COMMENT, 'url': 'https://github.com/comment_67890'}
+            ])
+            skipped_deleted = m.delete('https://github.com/comment_12345')
+            coverage_deleted = m.delete('https://github.com/comment_67890')
+            result = m.post('https://api.github.com/search/issues/1')
 
-        _add_pr_comment(comment, "")
+            _add_pr_comment(comment, "")
 
-    assert comment == results.get('body')
+    assert json.loads(result.last_request.text).get('body') == comment
+    assert skipped_deleted.called == skipped_flow
+    assert coverage_deleted.called == coverage_flow

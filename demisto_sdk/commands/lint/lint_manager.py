ea79bf964a622fa24ed5594e73d84d8c61ff0a57
dschwartz <81086590+dorschw@users.noreply.github.com>
2022-02-01 10:51:33 +0200

    lint dependency message

--- demisto_sdk/commands/lint/lint_manager.py
+++ demisto_sdk/commands/lint/lint_manager.py
@@ -85,9 +85,11 @@ class LintManager:
             dependent_on_api_module = self._get_packages(content_repo=self._facts["content_repo"],
                                                          input=dependent_on_api_module)
             self._pkgs = list(set(self._pkgs + dependent_on_api_module))
-            print(f"Found {Colors.Fg.cyan}{len(dependent_on_api_module)}{Colors.reset} dependent packages."
-                  f" Executing lint and test on dependent packages as well.")
-
+            if dependent_on_api_module:
+                print(f"Found {Colors.Fg.cyan}{len(dependent_on_api_module)}{Colors.reset} dependent packages."
+                      f" Executing lint and test on dependent packages as well.")
+            else:
+                print(f"No dependent packages found.")
         if json_file_path:
             if os.path.isdir(json_file_path):
                 json_file_path = os.path.join(json_file_path, 'lint_outputs.json')

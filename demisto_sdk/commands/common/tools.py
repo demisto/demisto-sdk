bf2aed5c490764b4aaec7d4835612e1b37273b16
Noy-Maimon <72340690+Noy-Maimon@users.noreply.github.com>
2022-02-06 15:24:40 +0200

    Update demisto_sdk/commands/common/tools.py
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -2412,7 +2412,7 @@ def alternate_item_fields(content_item):
     as_dict_types = (dict, CommentedMap)
     as_list_types = (list, CommentedSeq)
     current_dict = content_item.to_dict() if type(content_item) not in as_dict_types else content_item
-    copy_dict = current_dict.copy()  # for modifyting dict while iterating
+    copy_dict = current_dict.copy()  # for modifying dict while iterating
     for field, value in copy_dict.items():
         if field.endswith('_x2'):
             current_dict[field[:-3]] = value

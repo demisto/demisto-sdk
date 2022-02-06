7cba2e927e332918a1cab3b79754d61d43faad5f
cshayner <cshayner@paloaltonetworks.com>
2022-02-06 13:56:35 +0200

    add retry to 4

--- demisto_sdk/commands/common/hook_validations/readme.py
+++ demisto_sdk/commands/common/hook_validations/readme.py
@@ -118,7 +118,7 @@ class ReadMeValidator(BaseValidator):
             if not server_started:
                 return False
         readme_content = self.fix_mdx()
-        retry = Retry(total=2)
+        retry = Retry(total=4)
         adapter = HTTPAdapter(max_retries=retry)
         session = requests.Session()
         session.mount('http://', adapter)

ed185390ec5a0137e77e02a0ddedab95cd545596
ilan <ierukhimovic@paloaltonetworks.com>
2022-02-08 16:49:05 +0200

    fix tests

--- demisto_sdk/commands/generate_test_playbook/tests/test_playbook_generator_test.py
+++ demisto_sdk/commands/generate_test_playbook/tests/test_playbook_generator_test.py
@@ -44,9 +44,9 @@ class TestGenerateTestPlaybook:
         )
 
         generator.run()
-        expected_test_playbook_yml = (TestGenerateTestPlaybook.TEST_FILE_PATH / expected_yml).read_text()
-        actual_test_playbook_yml = (Path(TestGenerateTestPlaybook.TEMP_DIR) /
-                                    'playbook-TestPlaybook_Test.yml').read_text()
+        expected_test_playbook_yml = get_yaml(str(TestGenerateTestPlaybook.TEST_FILE_PATH / expected_yml))
+        actual_test_playbook_yml = get_yaml(str(Path(TestGenerateTestPlaybook.TEMP_DIR) /
+                                            'playbook-TestPlaybook_Test.yml'))
 
         assert expected_test_playbook_yml == actual_test_playbook_yml
 

2d44307a9a467b4bb4ab827bfdfa62fecdeb0e94
ilan <ierukhimovic@paloaltonetworks.com>
2022-02-09 15:04:14 +0200

    fix imports

--- demisto_sdk/commands/common/handlers/__init__.py
+++ demisto_sdk/commands/common/handlers/__init__.py
@@ -2,5 +2,4 @@
 This is for source of truth of handlers
 """
 
-from demisto_sdk.commands.common.handlers.ruamel_handler import \
-    RUAMEL_Handler as YAML_Handler  # noqa: F401
+from .ruamel_handler import RUAMEL_Handler as YAML_Handler  # noqa: F401

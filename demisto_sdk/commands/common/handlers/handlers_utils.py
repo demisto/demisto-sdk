c103c25d31efb2d75d6866a23daf41f4ece9374f
ilan <ierukhimovic@paloaltonetworks.com>
2022-02-09 16:47:43 +0200

    dict()

--- demisto_sdk/commands/common/handlers/handlers_utils.py
+++ demisto_sdk/commands/common/handlers/handlers_utils.py
@@ -1,9 +1,6 @@
-from collections import OrderedDict
-
-
 def order_dict(data):
     """
     Order dict by default order
     """
-    return OrderedDict({k: order_dict(v) if isinstance(v, dict) else v
-                        for k, v in sorted(data.items())})
+    return {k: order_dict(v) if isinstance(v, dict) else v
+            for k, v in sorted(data.items())}

11476a56e11693fe0a91218593d592b50906c8da
ilaner <88267954+ilaner@users.noreply.github.com>
2022-02-14 12:08:09 +0200

    Loading and dumping  all YAML files with XSOAR_YAML, which uses ruamel.yaml (#1820)

--- demisto_sdk/commands/common/tests/description_test.py
+++ demisto_sdk/commands/common/tests/description_test.py
@@ -2,11 +2,13 @@ import glob
 import os
 
 import pytest
-from ruamel.yaml import YAML
 
+from demisto_sdk.commands.common.handlers import YAML_Handler
 from demisto_sdk.commands.common.hook_validations.description import \
     DescriptionValidator
 from TestSuite.test_tools import ChangeCWD
+
+yaml = YAML_Handler()
 
 
 @pytest.mark.parametrize('integration_obj', [
@@ -32,7 +34,7 @@ def test_is_duplicate_description_unified_deprecated_integration(mocker, tmp_pat
     integration_dir = tmp_path / 'Packs' / 'SomePack' / 'Integrations' / 'SomeIntegration'
     integration_dir.mkdir(parents=True)
     unified_integration_yml = integration_dir / 'SomeIntegration.yml'
-    YAML(typ='safe', pure=True).dump(integration_obj, unified_integration_yml.open('w'))
+    yaml.dump(integration_obj, unified_integration_yml.open('w'))
     description_validator = DescriptionValidator(str(unified_integration_yml))
     assert description_validator.is_duplicate_description()
     assert not DescriptionValidator.handle_error.called

4ce7a37f890c37885743a72b468f0d60addb761b
YuvHayun <yhayun@paloaltonetworks.com>
2022-02-22 09:49:12 +0200

    added comments on code and changelog

--- demisto_sdk/commands/common/hook_validations/integration.py
+++ demisto_sdk/commands/common/hook_validations/integration.py
@@ -912,6 +912,8 @@ class IntegrationValidator(ContentEntityValidator):
                 if 'defaultvalue' in param:
                     param.pop('defaultvalue')
             for fetch_required_param in fetch_required_params:
+                # If this condition returns true, we'll go over the params dict and we'll check if there's a param that match the fetch_required_param name.
+                # If there is one, we know that in the params dict there is a matching param to the fetch_required_param but it has a malformed structure.
                 if fetch_required_param not in params:
                     error_message = ''
                     error_code = ''

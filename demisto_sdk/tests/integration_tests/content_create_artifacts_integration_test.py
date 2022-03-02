60f7889990e0e7c0297b0c269d9766b5c0ece3ff
YuvHayun <yhayun@paloaltonetworks.com>
2022-03-02 10:17:57 +0200

    tests fixes

--- demisto_sdk/tests/integration_tests/content_create_artifacts_integration_test.py
+++ demisto_sdk/tests/integration_tests/content_create_artifacts_integration_test.py
@@ -28,14 +28,14 @@ def mock_git(mocker):
 
 
 def test_integration_create_content_artifacts_no_zip(repo):
-    expected_artifacts_path = ARTIFACTS_EXPEXTED_RESULTS / 'content'
+    expected_artifacts_path = ARTIFACTS_EXPEXTED_RESULTS / 'integration_test'
 
     with ChangeCWD(repo.path):
         dir_path = repo.make_dir()
         runner = CliRunner()
         result = runner.invoke(main, [ARTIFACTS_CMD, '-a', dir_path, '--no-zip'])
 
-        assert not same_folders(dir_path, expected_artifacts_path)
+        assert same_folders(dir_path, expected_artifacts_path)
         assert result.exit_code == 0
 
 

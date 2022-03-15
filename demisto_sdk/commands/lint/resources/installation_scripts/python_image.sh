828b8074f6e4763441411fe9e6f00716012b0f7f
esharf <esahrf@paloaltonetworks.com>
2022-03-16 01:08:35 +0200

    reformat the python installation script

--- demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
+++ demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
@@ -3,11 +3,19 @@ mkdir -p /devwork/
 cd /devwork
 chown -R :4000 /devwork/
 chmod -R 775 /devwork
-OS_RELEASE=$(cat /etc/os-release); if echo "$OS_RELEASE" | grep -q "alpine"; \
-then apk add --no-cache --virtual .build-deps python3-dev gcc build-base; \
-elif echo "$OS_RELEASE" | grep -qi "Debian"; \
-then apt-get update && apt-get install -y --no-install-recommends gcc python3-dev;fi; \
-pip install --no-cache-dir -r /test-requirements.txt; \
-if echo "$OS_RELEASE" | grep -q "alpine"; then apk del .build-deps; \
-elif echo "$OS_RELEASE" | grep -qi "Debian"; \
-then apt-get purge -y --auto-remove gcc python3-dev; fi;
+source /etc/os-release
+if [[ $ID == "alpine" ]]
+then
+    apk add --no-cache --virtual .build-deps python3-dev gcc build-base;
+elif [[ $ID == "debian" ]]
+then
+    apt-get update && apt-get install -y --no-install-recommends gcc python3-dev
+fi
+pip install --no-cache-dir -r /test-requirements.txt
+if [[ $ID == "alpine" ]]
+then
+    apk del .build-deps
+elif [[ $ID == "debian" ]]
+then
+    apt-get purge -y --auto-remove gcc python3-dev
+fi

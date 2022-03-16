29dd2b5676fcd11364b88e6bf44f4d8f81ff42e3
yaakovpraisler <ypreisler@paloaltonetworks.com>
2022-03-16 10:28:19 +0200

    imports fix

--- demisto_sdk/commands/common/content/objects/pack_objects/__init__.py
+++ demisto_sdk/commands/common/content/objects/pack_objects/__init__.py
@@ -3,46 +3,71 @@ from __future__ import absolute_import
 
 import inspect
 
-from .abstract_pack_objects.json_content_object import *  # lgtm [py/polluting-import]
-from .abstract_pack_objects.yaml_content_object import *  # lgtm [py/polluting-import]
-from .abstract_pack_objects.yaml_unify_content_object import *  # lgtm [py/polluting-import]
-from .author_image.author_image import *  # lgtm [py/polluting-import]
-from .change_log.change_log import *  # lgtm [py/polluting-import]
-from .classifier.classifier import *  # lgtm [py/polluting-import]
-from .connection.connection import *  # lgtm [py/polluting-import]
-from .contributors.contributors import *  # lgtm [py/polluting-import]
-from .corrlation_rule.correlation_rule import *  # lgtm [py/polluting-import]
-from .dashboard.dashboard import *  # lgtm [py/polluting-import]
-from .doc_file.doc_file import *  # lgtm [py/polluting-import]
-from .generic_definition.generic_definition import *  # lgtm [py/polluting-import]
-from .generic_field.generic_field import *  # lgtm [py/polluting-import]
-from .generic_module.generic_module import *  # lgtm [py/polluting-import]
-from .generic_type.generic_type import *  # lgtm [py/polluting-import]
-from .incident_field.incident_field import *  # lgtm [py/polluting-import]
-from .incident_type.incident_type import *  # lgtm [py/polluting-import]
-from .indicator_field.indicator_field import *  # lgtm [py/polluting-import]
-from .indicator_type.indicator_type import *  # lgtm [py/polluting-import]
-from .integration.integration import *  # lgtm [py/polluting-import]
-from .job.job import *  # lgtm [py/polluting-import]
-from .layout.layout import *  # lgtm [py/polluting-import]
-from .lists.lists import *  # lgtm [py/polluting-import]
-from .modeling_rule.modeling_rule import *  # lgtm [py/polluting-import]
-from .pack_ignore.pack_ignore import *  # lgtm [py/polluting-import]
-from .pack_metadata.pack_metadata import *  # lgtm [py/polluting-import]
-from .parsing_rule.parsing_rule import *  # lgtm [py/polluting-import]
-from .playbook.playbook import *  # lgtm [py/polluting-import]
-from .pre_process_rule.pre_process_rule import *  # lgtm [py/polluting-import]
-from .readme.readme import *  # lgtm [py/polluting-import]
-from .release_note.release_note import *  # lgtm [py/polluting-import]
-from .release_note_config.release_note_config import *  # lgtm [py/polluting-import]
-from .report.report import *  # lgtm [py/polluting-import]
-from .script.script import *  # lgtm [py/polluting-import]
-from .secret_ignore.secret_ignore import *  # lgtm [py/polluting-import]
-from .tool.agent_tool import *  # lgtm [py/polluting-import]
-from .trigger.trigger import *  # lgtm [py/polluting-import]
-from .widget.widget import *  # lgtm [py/polluting-import]
-from .xsiam_dashboard.xsiam_dashboard import *  # lgtm [py/polluting-import]
-from .xsiam_report.xsiam_report import *  # lgtm [py/polluting-import]
+from .abstract_pack_objects.json_content_object import \
+    JSONContentObject  # lgtm [py/polluting-import]
+from .abstract_pack_objects.yaml_content_object import \
+    YAMLContentObject  # lgtm [py/polluting-import]
+from .abstract_pack_objects.yaml_unify_content_object import \
+    YAMLContentUnifiedObject  # lgtm [py/polluting-import]
+from .author_image.author_image import \
+    AuthorImage  # lgtm [py/polluting-import]
+from .change_log.change_log import ChangeLog  # lgtm [py/polluting-import]
+from .classifier.classifier import Classifier  # lgtm [py/polluting-import]
+from .classifier.classifier import ClassifierMapper, OldClassifier
+from .connection.connection import Connection  # lgtm [py/polluting-import]
+from .contributors.contributors import \
+    Contributors  # lgtm [py/polluting-import]
+from .corrlation_rule.correlation_rule import \
+    CorrelationRule  # lgtm [py/polluting-import]
+from .dashboard.dashboard import Dashboard  # lgtm [py/polluting-import]
+from .doc_file.doc_file import DocFile  # lgtm [py/polluting-import]
+from .generic_definition.generic_definition import \
+    GenericDefinition  # lgtm [py/polluting-import]
+from .generic_field.generic_field import \
+    GenericField  # lgtm [py/polluting-import]
+from .generic_module.generic_module import \
+    GenericModule  # lgtm [py/polluting-import]
+from .generic_type.generic_type import \
+    GenericType  # lgtm [py/polluting-import]
+from .incident_field.incident_field import \
+    IncidentField  # lgtm [py/polluting-import]
+from .incident_type.incident_type import \
+    IncidentType  # lgtm [py/polluting-import]
+from .indicator_field.indicator_field import \
+    IndicatorField  # lgtm [py/polluting-import]
+from .indicator_type.indicator_type import (  # lgtm [py/polluting-import]
+    IndicatorType, OldIndicatorType)
+from .integration.integration import Integration  # lgtm [py/polluting-import]
+from .job.job import Job  # lgtm [py/polluting-import]
+from .layout.layout import (Layout, LayoutObject,  # lgtm [py/polluting-import]
+                            LayoutsContainer)
+from .lists.lists import Lists  # lgtm [py/polluting-import]
+from .modeling_rule.modeling_rule import \
+    ModelingRule  # lgtm [py/polluting-import]
+from .pack_ignore.pack_ignore import PackIgnore  # lgtm [py/polluting-import]
+from .pack_metadata.pack_metadata import \
+    PackMetaData  # lgtm [py/polluting-import]
+from .parsing_rule.parsing_rule import \
+    ParsingRule  # lgtm [py/polluting-import]
+from .playbook.playbook import Playbook  # lgtm [py/polluting-import]
+from .pre_process_rule.pre_process_rule import \
+    PreProcessRule  # lgtm [py/polluting-import]
+from .readme.readme import Readme  # lgtm [py/polluting-import]
+from .release_note.release_note import \
+    ReleaseNote  # lgtm [py/polluting-import]
+from .release_note_config.release_note_config import \
+    ReleaseNoteConfig  # lgtm [py/polluting-import]
+from .report.report import Report  # lgtm [py/polluting-import]
+from .script.script import Script  # lgtm [py/polluting-import]
+from .secret_ignore.secret_ignore import \
+    SecretIgnore  # lgtm [py/polluting-import]
+from .tool.agent_tool import AgentTool  # lgtm [py/polluting-import]
+from .trigger.trigger import Trigger  # lgtm [py/polluting-import]
+from .widget.widget import Widget  # lgtm [py/polluting-import]
+from .xsiam_dashboard.xsiam_dashboard import \
+    XSIAMDashboard  # lgtm [py/polluting-import]
+from .xsiam_report.xsiam_report import \
+    XSIAMReport  # lgtm [py/polluting-import]
 
 __all__ = [name for name, obj in locals().items()
            if not (name.startswith('_') or inspect.ismodule(obj))]

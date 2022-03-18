d2145da82b172afa55c1d1106ea56ba9ba3cd1f5
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-18 17:02:58 +0200

    ujson instead orjson

---
+++ demisto_sdk/commands/common/handlers/json/ujson_handler.py
@@ -0,0 +1,25 @@
+from typing import TextIO
+
+import ujson
+
+from demisto_sdk.commands.common.handlers.xsoar_handler import XSOAR_Handler
+
+
+class OrJSON_Handler(XSOAR_Handler):
+    """
+    XSOAR wrapper to orjso
+    Use only this wrapper for yaml handling.
+    """
+
+    def load(self, stream: TextIO):
+        return ujson.load(stream)
+
+    def dump(self, data, stream: TextIO, sort_keys=False, indent=None):
+        ujson.dump(data, stream, sort_keys=sort_keys, indent=indent)
+
+    def dumps(self, data, sort_keys=False, indent=None):
+        return ujson.dumps(
+            data,
+            sort_keys=sort_keys,
+            indent=indent
+        )

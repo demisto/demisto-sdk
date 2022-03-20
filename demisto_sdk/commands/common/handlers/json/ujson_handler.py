bf10118a1dad21632314f8b02b863b9c61540619
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-20 08:56:59 +0200

    fix buggy bug

--- demisto_sdk/commands/common/handlers/json/ujson_handler.py
+++ demisto_sdk/commands/common/handlers/json/ujson_handler.py
@@ -5,7 +5,7 @@ import ujson
 from demisto_sdk.commands.common.handlers.xsoar_handler import XSOAR_Handler
 
 
-class OrJSON_Handler(XSOAR_Handler):
+class UJSON_Handler(XSOAR_Handler):
     """
     XSOAR wrapper to orjso
     Use only this wrapper for yaml handling.

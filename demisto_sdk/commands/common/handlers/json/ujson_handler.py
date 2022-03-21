10b7e74f73f63585ec7d09fa092ad5747f7a7f1f
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-21 17:55:25 +0200

    fix: JSONDecodeError on JSON Handler

--- demisto_sdk/commands/common/handlers/json/ujson_handler.py
+++ demisto_sdk/commands/common/handlers/json/ujson_handler.py
@@ -5,24 +5,44 @@ import ujson
 from demisto_sdk.commands.common.handlers.xsoar_handler import XSOAR_Handler
 
 
+class JSONDecodeError(ValueError):
+    pass
+
+
 class UJSON_Handler(XSOAR_Handler):
     """
-    XSOAR wrapper to orjso
+    XSOAR wrapper to UJSON
     Use only this wrapper for yaml handling.
     """
+    JSONDecodeError = JSONDecodeError
+
+    def __init__(self):
+        self.json = ujson
 
     def loads(self, s: AnyStr):
-        return ujson.loads(s)
+        try:
+            return ujson.loads(s)
+        except ValueError as e:
+            raise JSONDecodeError(e)
 
     def load(self, stream: TextIO):
-        return ujson.load(stream)
+        try:
+            return ujson.load(stream)
+        except ValueError as e:
+            raise JSONDecodeError(e)
 
-    def dump(self, data, stream: TextIO, sort_keys=False, indent=None):
-        ujson.dump(data, stream, sort_keys=sort_keys, indent=indent)
+    def dump(self, data, stream: TextIO, sort_keys=False, indent=0):
+        try:
+            ujson.dump(data, stream, sort_keys=sort_keys, indent=indent)
+        except ValueError as e:
+            raise JSONDecodeError(e)
 
-    def dumps(self, data, sort_keys=False, indent=None):
-        return ujson.dumps(
-            data,
-            sort_keys=sort_keys,
-            indent=indent
-        )
+    def dumps(self, data, sort_keys=False, indent=0):
+        try:
+            return ujson.dumps(
+                data,
+                sort_keys=sort_keys,
+                indent=indent
+            )
+        except ValueError as e:
+            raise JSONDecodeError(e)

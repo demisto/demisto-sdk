30c13e644112abe6e15f9dabb6713066bba3ba06
Itay Keren <ikeren@paloaltonetworks.com>
2022-03-22 11:53:18 +0200

    Unify - YAML Unifier Interface (#1964)
    * refactor(unify): add yaml unifier interface
    
    * refactor(unify): ignore mypy errors
    
    * test(unify): fix mock path according to new file name
    
    * Update demisto_sdk/commands/unify/yaml_unifier.py
    
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
    
    * Update demisto_sdk/commands/unify/yaml_unifier.py
    
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
    
    * test(unify): fix invalid input test output check
    
    * refactor(unify): change to always use abs path for input
    
    * test(find_dependencies): adjust expected script paths to be abs
    
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- demisto_sdk/commands/common/hook_validations/id.py
+++ demisto_sdk/commands/common/hook_validations/id.py
@@ -18,7 +18,8 @@ from demisto_sdk.commands.common.update_id_set import (
     get_integration_data, get_layout_data, get_layouts_scripts_ids,
     get_layoutscontainer_data, get_mapper_data, get_pack_metadata_data,
     get_playbook_data, get_script_data)
-from demisto_sdk.commands.unify.yml_unifier import YmlUnifier
+from demisto_sdk.commands.unify.integration_script_unifier import \
+    IntegrationScriptUnifier
 
 
 class IDSetValidations(BaseValidator):
@@ -637,7 +638,7 @@ class IDSetValidations(BaseValidator):
             click.echo(f"id set validations for: {file_path}")
 
             if re.match(constants.PACKS_SCRIPT_YML_REGEX, file_path, re.IGNORECASE):
-                unifier = YmlUnifier(os.path.dirname(file_path))
+                unifier = IntegrationScriptUnifier(os.path.dirname(file_path))
                 yml_path, code = unifier.get_script_or_integration_package_data()
                 script_data = get_script_data(yml_path, script_code=code)
                 is_valid = self._is_non_real_command_found(script_data)

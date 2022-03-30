fe6d41729fc0d1cd00f0be2fa1a6eba873bf3fb3
yaakovpraisler <ypreisler@paloaltonetworks.com>
2022-03-30 14:33:25 +0300

    test fix

--- demisto_sdk/commands/common/content/tests/objects/pack_objects/parsing_rule/parsing_rule_test.py
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/parsing_rule/parsing_rule_test.py
@@ -5,22 +5,31 @@ from demisto_sdk.commands.common.content.objects_factory import \
 
 
 def get_parsing_rule(pack, name):
-    return pack.create_parsing_rule(name, {"id": "parsing_rule_id", "rules": "", "name": "parsing_rule_name"})
+    return pack.create_parsing_rule(name)
 
 
-def test_objects_factory(pack):
-    parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
-    obj = path_to_pack_object(parsing_rule.parsing_rule_tmp_path)
-    assert isinstance(obj, ParsingRule)
+class TestNotUnifiedParsingRule:
+    def test_objects_factory(self, pack):
+        parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
+        obj = path_to_pack_object(parsing_rule.yml._tmp_path)
+        assert isinstance(obj, ParsingRule)
 
+    def test_prefix(self, pack):
+        parsing_rule = get_parsing_rule(pack, 'parsingrule-parsing_rule_name')
+        obj = ParsingRule(parsing_rule._tmpdir_rule_path)
+        assert obj.normalize_file_name() == parsing_rule.yml._tmp_path.name
 
-def test_prefix(pack):
-    parsing_rule = get_parsing_rule(pack, 'parsingrule-parsing_rule_name')
+        parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
+        obj = ParsingRule(parsing_rule._tmpdir_rule_path)
+        assert obj.normalize_file_name() == f"parsingrule-{parsing_rule.yml._tmp_path.name}"
 
-    obj = ParsingRule(parsing_rule.parsing_rule_tmp_path)
-    assert obj.normalize_file_name() == parsing_rule.parsing_rule_tmp_path.name
+    def test_files_detection(self, pack):
+        parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
+        obj = ParsingRule(parsing_rule._tmpdir_rule_path)
+        # assert obj.yml._tmp_path == Path(datadir["README.md"])
+        assert obj.rules_path == parsing_rule.rules._tmp_path
 
-    parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
-
-    obj = ParsingRule(parsing_rule.parsing_rule_tmp_path)
-    assert obj.normalize_file_name() == f"parsingrule-{parsing_rule.parsing_rule_tmp_path.name}"
+    def test_is_unify(self, pack):
+        parsing_rule = get_parsing_rule(pack, 'parsing_rule_name')
+        obj = ParsingRule(parsing_rule._tmpdir_rule_path)
+        assert not obj.is_unify()

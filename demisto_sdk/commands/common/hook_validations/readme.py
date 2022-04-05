c29c9dbdaaa5b6ba6e00a335aa017e54e7c1a3c9
israelpolishook <ipolishuk@paloaltonetworks.com>
2022-04-05 08:43:50 +0300

    commit

--- demisto_sdk/commands/common/hook_validations/readme.py
+++ demisto_sdk/commands/common/hook_validations/readme.py
@@ -543,14 +543,8 @@ class ReadMeValidator(BaseValidator):
         with ReadMeValidator._MDX_SERVER_LOCK:
             if not ReadMeValidator._MDX_SERVER_PROCESS:
                 mdx_parse_server = Path(__file__).parent.parent / 'mdx-parse-server.js'
-                try:
-                    ReadMeValidator._MDX_SERVER_PROCESS = subprocess.Popen(['node', str(mdx_parse_server)],
-                                                                            stdout=subprocess.PIPE, text=True)
-                except FileNotFoundError:
-                    error_message, error_code = Errors.error_uninstall_node()
-                    if handle_error(error_message, error_code,file_path=file_path):
-                        return False
-
+                ReadMeValidator._MDX_SERVER_PROCESS = subprocess.Popen(['node', str(mdx_parse_server)],
+                                                                       stdout=subprocess.PIPE, text=True)
                 line = ReadMeValidator._MDX_SERVER_PROCESS.stdout.readline()  # type: ignore
                 if 'MDX server is listening on port' not in line:
                     ReadMeValidator.stop_mdx_server()

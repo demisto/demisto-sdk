967f117ded1d0825f25f01697e9da237489fa6e0
rshunim <rshunim@paloaltonetworks.com>
2022-04-14 11:54:28 +0300

    ''

--- demisto_sdk/commands/common/hook_validations/integration.py
+++ demisto_sdk/commands/common/hook_validations/integration.py
@@ -1037,7 +1037,7 @@ class IntegrationValidator(ContentEntityValidator):
         is_siem = self.current_file.get('script', {}).get('isFetchEvents')
 
         if is_siem:
-            display_name = self.current_file.get('display')
+            display_name = self.current_file.get('display', '')
             if not display_name.endswith('Event Collector'):
                 error_message, error_code = Errors.invalid_siem_integration_name(display_name)
                 if self.handle_error(error_message, error_code, file_path=self.file_path):

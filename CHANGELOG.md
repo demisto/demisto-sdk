4c33da49bcc9443d196900502a637ad06dec12d6
DinaMeylakh <72339665+DinaMeylakh@users.noreply.github.com>
2022-04-26 17:51:03 +0300

    Dont change old ids in format (#2040)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 
 ## Unreleased
-
+* Fixed an issue in the **format** command where the `id` field was overwritten for existing JSON files.
 * Fixed an issue where the **doc-review** command was successful even when the release-note is malformed.
 * Added timestamps to the `demisto-sdk` logger.
 * Added time measurements to **lint**.
@@ -41,6 +41,7 @@ the same as the last release note version.
 * Fixed an issue in the **validate** and **format** commands where *None* arguments in integration commands caused the commands to fail unexpectedly.
 * Added support for running tests on XSIAM machines in the **test-content** command.
 * Fixed an issue where the **validate** command did not work properly when deleting non-content items.
+* Added the flag '-d', '--dependency' to **find-dependencies** command to get the content items that cause the dependencies between two packs.
 
 ## 1.6.3
 
--- demisto_sdk/commands/format/update_generic_json.py
+++ demisto_sdk/commands/format/update_generic_json.py
@@ -93,18 +93,26 @@ class BaseUpdateJSON(BaseUpdate):
     def update_id(self, field='name') -> None:
         """Updates the id to be the same as the provided field ."""
         updated_integration_id_dict = {}
-
-        if self.verbose:
-            click.echo('Updating ID')
-        if field not in self.data:
-            print_error(f'Missing {field} field in file {self.source_file} - add this field manually')
-            return None
-        if 'id' in self.data and is_uuid(self.data['id']):  # only happens if id had been defined
-            updated_integration_id_dict[self.data['id']] = self.data[field]
-        self.data['id'] = self.data[field]
-
-        if updated_integration_id_dict:
-            self.updated_ids.update(updated_integration_id_dict)
+        if self.old_file:
+            current_id = self.data.get('id')
+            old_id = self.old_file.get('id')
+            if current_id != old_id:
+                click.secho(
+                    f'The modified JSON file corresponding to the path: {self.relative_content_path} contains an '
+                    f'ID which does not match the ID in remote file. Changing the ID from {current_id} back '
+                    f'to {old_id}.', fg="yellow")
+                self.data['id'] = old_id
+        else:
+            if self.verbose:
+                click.echo('Updating ID to be the same as JSON name')
+            if field not in self.data:
+                print_error(f'Missing {field} field in file {self.source_file} - add this field manually')
+                return None
+            if 'id' in self.data and is_uuid(self.data['id']):  # only happens if id had been defined
+                updated_integration_id_dict[self.data['id']] = self.data[field]
+            self.data['id'] = self.data[field]
+            if updated_integration_id_dict:
+                self.updated_ids.update(updated_integration_id_dict)
 
     def remove_spaces_end_of_id_and_name(self):
         """Updates the id and name of the json to have no spaces on its end

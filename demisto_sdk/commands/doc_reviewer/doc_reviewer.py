71682fae485e9b8c73b98f6cddf990e935796579
abaumgarten <abaumgarten@paloaltonetworks.com>
2022-04-27 11:30:07 +0300

    debugging

--- demisto_sdk/commands/doc_reviewer/doc_reviewer.py
+++ demisto_sdk/commands/doc_reviewer/doc_reviewer.py
@@ -284,7 +284,7 @@ class DocReviewer:
                     # Add the new known_words packs file
                     self.spellchecker.word_frequency.load_words(known_words)
                     restarted_spellchecker = True
-                    click.secho('HERE 4')
+                    click.secho(f'HERE 4: {self.spellchecker.word_frequency._dictionary}')
 
         return restarted_spellchecker
 

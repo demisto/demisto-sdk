5f5136cc6fe1ca01f4a0de02afa13b326ffff19a
aradcarmi <aradcarmi220@gmail.com>
2022-05-23 18:08:34 +0300

    Fixed CR note

--- demisto_sdk/commands/common/errors.py
+++ demisto_sdk/commands/common/errors.py
@@ -1255,7 +1255,8 @@ class Errors:
     @staticmethod
     @error_code_decorator
     def release_notes_bc_json_file_missing(json_path: str):
-        return f'A new release notes file contains the phrase \"breaking changes\" without a proper JSON file. ' \
+        return f'A new release notes file contains the phrase \"breaking changes\" ' \
+               'without a matching JSON file (with the same name as the release note file, e.g. 1_2_3.md). ' \
                f'Please run \"demisto-sdk update-release-notes -i {json_path[:-4]}md -bc\". ' \
                'For more information, refer to the following documentation: https://xsoar.pan.dev/docs/documentation/release-notes'
 

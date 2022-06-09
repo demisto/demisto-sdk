5d25fdb987cbe7891938e4abc1fb69885e65f665
ostolero <ostolero@paloaltonetworks.com>
2022-06-09 12:27:16 +0300

    lint fix

--- demisto_sdk/commands/common/tests/readme_test.py
+++ demisto_sdk/commands/common/tests/readme_test.py
@@ -89,7 +89,8 @@ def test_relative_url_not_valid():
     """
     captured_output = io.StringIO()
     sys.stdout = captured_output  # redirect stdout.
-    absolute_urls = ["https://www.good.co.il", "https://example.com","https://github.com/demisto/content/blob/123/Packs/FeedOffice365/doc_files/test.png"]
+    absolute_urls = ["https://www.good.co.il", "https://example.com", "https://github.com/demisto/content/blob/123"
+                                                                      "/Packs/FeedOffice365/doc_files/test.png"]
     relative_urls = ["relative1.com", "www.relative2.com", "hreftesting.com"]
     readme_validator = ReadMeValidator(INVALID_MD)
     result = readme_validator.verify_readme_relative_urls()

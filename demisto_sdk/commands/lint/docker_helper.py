c16924a950d606c995a864a1c88dc902205fa3ef
ilan <ierukhimovic@paloaltonetworks.com>
2022-06-12 16:29:55 +0300

    gitlab ci

--- demisto_sdk/commands/lint/docker_helper.py
+++ demisto_sdk/commands/lint/docker_helper.py
@@ -15,12 +15,11 @@ DOCKER_CLIENT = None
 logger = logging.getLogger('demisto-sdk')
 FILES_SRC_TARGET = List[Tuple[os.PathLike, str]]
 # this will be used to determine if the system supports mounts
-CAN_MOUNT_FILES = (not os.getenv('CIRCLECI', False)) and (
+CAN_MOUNT_FILES = os.getenv('GITLAB_CI') or ((not os.getenv('CIRCLECI', False)) and (
 
     (not os.getenv('DOCKER_HOST')) or
-    os.getenv('DOCKER_HOST', "").lower().startswith("unix:") or
-    os.getenv('DOCKER_HOST', "").lower().startswith("tcp:")
-
+    os.getenv('DOCKER_HOST', "").lower().startswith("unix:")
+)
 )
 
 

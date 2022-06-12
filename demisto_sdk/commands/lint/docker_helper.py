d348c45233186614418ea7d03e6f36093973b58a
ilan <ierukhimovic@paloaltonetworks.com>
2022-06-12 15:40:53 +0300

    rm comment

--- demisto_sdk/commands/lint/docker_helper.py
+++ demisto_sdk/commands/lint/docker_helper.py
@@ -15,11 +15,6 @@ DOCKER_CLIENT = None
 logger = logging.getLogger('demisto-sdk')
 FILES_SRC_TARGET = List[Tuple[os.PathLike, str]]
 # this will be used to determine if the system supports mounts
-# CAN_MOUNT_FILES = (not os.getenv('CIRCLECI', False)) and \
-#                   ((not os.getenv('DOCKER_HOST')) or
-#                    os.getenv('DOCKER_HOST', "").lower().startswith("unix:") or
-#                    os.getenv('DOCKER_HOST', "").lower().startswith("tcp:"))
-
 CAN_MOUNT_FILES = (not os.getenv('CIRCLECI', False)) and (
     
     (not os.getenv('DOCKER_HOST')) or

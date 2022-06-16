0c9da613bc1535138433f5858f07e89f00c0ee13
ilan <ierukhimovic@paloaltonetworks.com>
2022-06-16 10:12:48 +0300

    fix

--- demisto_sdk/commands/lint/linter.py
+++ demisto_sdk/commands/lint/linter.py
@@ -782,7 +782,8 @@ class Linter:
         test_json = {}
         try:
             # Running pytest container
-            cov = '' if no_coverage else self._pack_abs_dir.stem
+            cov_file_path = Path.joinpath(self._pack_abs_dir, '.coverage')
+            cov = self._pack_abs_dir.stem if not no_coverage and cov_file_path.exists() else ''
             uid = os.getuid() or 4000
             logger.debug(f'{log_prompt} - user uid for running lint/test: {uid}')  # lgtm[py/clear-text-logging-sensitive-data]
             container: docker.models.containers.Container = Docker.create_container(
@@ -816,7 +817,6 @@ class Linter:
                         f.write(test_data_xml)  # type: ignore
 
                 if not no_coverage:
-                    cov_file_path = os.path.join(self._pack_abs_dir, '.coverage')
                     cov_data = get_file_from_container(container_obj=container,
                                                        container_path="/devwork/.coverage")
                     cov_data = cov_data if isinstance(cov_data, bytes) else cov_data.encode()

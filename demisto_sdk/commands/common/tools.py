b2388869b81d52c3128c3dcdde08e1ee6b6714b8
GuyAfik <guyafik11@gmail.com>
2022-07-14 14:41:47 +0300

    remove indicatortype conversion

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -2022,7 +2022,6 @@ def get_all_incident_and_indicator_fields_from_id_set(id_set_file, entity_type):
 def item_type_to_content_items_header(item_type):
     converter = {
         "incidenttype": "incidentType",
-        "indicatortype": "indicatorType",
         "reputation": "indicatorType",
         "indicatorfield": "indicatorField",
         "incidentfield": "incidentField",

2a687ad54021ac8f0437840f521c446a5e2d6d2a
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-01 17:27:08 +0300

    fix types

--- demisto_sdk/tests/integration_tests/validate_integration_test.py
+++ demisto_sdk/tests/integration_tests/validate_integration_test.py
@@ -1202,7 +1202,7 @@ class TestClassifierValidation:
         with ChangeCWD(pack.repo_path):
             runner = CliRunner(mix_stderr=False)
             result = runner.invoke(main, [VALIDATE_CMD, '-i', classifier.path], catch_exceptions=False)
-        assert 'The file type is not supported in the validate command.' in result.stdout
+        assert 'File type None is not supported in the validate command.' in result.stdout
         assert result.exit_code == 1
 
     def test_valid_old_classifier(self, mocker, repo):
@@ -1455,7 +1455,7 @@ class TestMapperValidation:
         with ChangeCWD(pack.repo_path):
             runner = CliRunner(mix_stderr=False)
             result = runner.invoke(main, [VALIDATE_CMD, '-i', mapper.path], catch_exceptions=False)
-        assert 'The file type is not supported in the validate command.' in result.stdout
+        assert 'File type None is not supported in the validate command.' in result.stdout
         assert result.exit_code == 1
 
 

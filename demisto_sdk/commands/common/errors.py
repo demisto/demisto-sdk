ed7ca103121ac1bfb3e8d88be08b46f2d85e8310
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-02 09:40:52 +0300

    add file type to error message when it is not supported (#2272)

--- demisto_sdk/commands/common/errors.py
+++ demisto_sdk/commands/common/errors.py
@@ -1,6 +1,6 @@
 from distutils.version import LooseVersion
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import decorator
 from requests import Response
@@ -532,8 +532,10 @@ class Errors:
 
     @staticmethod
     @error_code_decorator
-    def file_type_not_supported():
-        return "The file type is not supported in the validate command.\n" \
+    def file_type_not_supported(file_type: Optional[FileType], file_path: Union[str, Path]):
+        joined_path = '/'.join(Path(file_path).parts[-3:])
+        file_type_str = f'File type {file_type}' if file_type else f'File {joined_path}'
+        return f"{file_type_str} is not supported in the validate command.\n" \
                "The validate command supports: Integrations, Scripts, Playbooks, " \
                "Incident fields, Incident types, Indicator fields, Indicator types, Objects fields, Object types," \
                " Object modules, Images, Release notes, Layouts, Jobs, Wizards, Descriptions And Modeling Rules."

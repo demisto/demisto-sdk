12b700fd544ae7e53fd19906d6dd39363c0f7e07
ilan <ierukhimovic@paloaltonetworks.com>
2022-08-08 10:18:19 +0300

    precommit

--- .devcontainer/create_certs.sh
+++ .devcontainer/create_certs.sh
@@ -4,7 +4,7 @@
 # This script creates a local certificate file.
 # It tries to connect to a server (for instance, github), show the local certificates and save it to a file.
 
-# We connect to a random server and not paloaltonetworks.com to get external certificates. 
+# We connect to a random server and not paloaltonetworks.com to get external certificates.
 CONNECT_SERVER="github.com:443"
 
 FILE=$1
@@ -20,4 +20,3 @@ if [ ! -f "$FILE" ]; then
     echo "Failed getting the certificates, no output file was created."
     exit
 fi
-

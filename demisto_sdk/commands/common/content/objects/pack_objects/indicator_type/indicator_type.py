6aa099c35e78d37059a59c59eea323a0f30bac93
ashamah <ashamah@paloaltonetworks.com>
2022-08-10 13:51:58 +0300

    Add Reputations to Upload Command

--- demisto_sdk/commands/common/content/objects/pack_objects/indicator_type/indicator_type.py
+++ demisto_sdk/commands/common/content/objects/pack_objects/indicator_type/indicator_type.py
@@ -1,5 +1,7 @@
+from tempfile import NamedTemporaryFile
 from typing import Union
 
+import demisto_client
 from wcmatch.pathlib import Path
 
 from demisto_sdk.commands.common.constants import (INDICATOR_TYPE,
@@ -8,10 +10,33 @@ from demisto_sdk.commands.common.constants import (INDICATOR_TYPE,
 from demisto_sdk.commands.common.content.objects.pack_objects.abstract_pack_objects.json_content_object import \
     JSONContentObject
 
+from demisto_sdk.commands.common.handlers import JSON_Handler
+
+json = JSON_Handler()
+
 
 class IndicatorType(JSONContentObject):
     def __init__(self, path: Union[Path, str]):
         super().__init__(path, INDICATOR_TYPE)
+
+    def upload(self, client: demisto_client):
+        """
+        Upload the incident type Container to demisto_client
+        Args:
+            client: The demisto_client object of the desired XSOAR machine to upload to.
+
+        Returns:
+            The result of the upload command from demisto_client
+        """
+        if isinstance(self._as_dict, dict):
+            indicator_type_unified_data = [self._as_dict]
+        else:
+            indicator_type_unified_data = self._as_dict
+
+        with NamedTemporaryFile(suffix='.json') as indicator_type_unified_file:
+            indicator_type_unified_file.write(bytes(json.dumps(indicator_type_unified_data), 'utf-8'))
+            indicator_type_unified_file.seek(0)
+            return client.import_reputation_handler(file=indicator_type_unified_file.name)
 
     def type(self):
         return FileType.REPUTATION

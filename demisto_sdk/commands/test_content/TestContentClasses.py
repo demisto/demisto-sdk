43b499e53d6ba9a27e5c12328aba3679d030e5b7
Darya Koval <dkoval@paloaltonetworks.com>
2022-08-18 19:04:00 +0300

    change how to open json file

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -515,7 +515,8 @@ class BuildContext:
         self.xsiam_servers_path = kwargs.get('xsiam_servers_path')
         self.conf, self.secret_conf = self._load_conf_files(kwargs['conf'], kwargs['secret'])
         if self.is_xsiam:
-            xsiam_servers_api_keys = get_json(kwargs.get('xsiam_servers_api_keys_path'))
+            with open(kwargs.get('xsiam_servers_api_keys_path'), 'r') as json_file:
+                xsiam_servers_api_keys = json.loads(json_file.read())
             self.logging_module.info(f'{kwargs.get("xsiam_servers_api_keys_path")=}, {xsiam_servers_api_keys=}, ')
             self.xsiam_conf = self._load_xsiam_file(self.xsiam_servers_path)
             self.env_json = [self.xsiam_conf.get(self.xsiam_machine, {})]

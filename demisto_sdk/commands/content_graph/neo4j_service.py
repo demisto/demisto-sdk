1d230abb95e60b301a48eb399575876230ade2e4
ilan <ierukhimovic@paloaltonetworks.com>
2022-08-24 18:04:58 +0300

    neo4j service refactor

--- demisto_sdk/commands/content_graph/neo4j_service.py
+++ demisto_sdk/commands/content_graph/neo4j_service.py
@@ -7,6 +7,10 @@ from requests.adapters import HTTPAdapter, Retry
 
 from demisto_sdk.commands.content_graph.common import NEO4J_PASSWORD, REPO_PATH
 
+DOCKER_TIMEOUT = 10
+NEO4J_SERVICE_IMAGE = 'neo4j:4.4.9'
+NEO4J_ADMIN_IMAGE = 'neo4j/neo4j-admin:4.4.9'
+
 logger = logging.getLogger('demisto-sdk')
 
 try:
@@ -20,30 +24,23 @@ class Neo4jServiceException(Exception):
     pass
 
 
-def start_neo4j_service(use_docker: bool = True):
-    if not use_docker:
-        neo4j_admin_command('set-initial-password', f'neo4j - admin set - initial - password {NEO4J_PASSWORD}')
-        run_command('neo4j start', cwd=REPO_PATH / 'neo4j', is_silenced=False)
+def _get_docker_client() -> docker.DockerClient:
+    try:
+        docker_client = docker.from_env(timeout=DOCKER_TIMEOUT)
+    except docker.errors.DockerException:
+        msg = 'Could not connect to docker daemon. Please make sure docker is running.'
+        raise Neo4jServiceException(msg)
+    return docker_client
 
-    else:
-        try:
-            docker_client = docker.from_env(timeout=10)
-        except docker.errors.DockerException:
-            msg = 'Could not connect to docker daemon. Please make sure docker is running.'
-            raise Neo4jServiceException(msg)
-        try:
-            docker_client.containers.get('neo4j-content').remove(force=True)
-        except Exception as e:
-            logger.info(f'Could not remove neo4j container: {e}')
-        docker_client.containers.run(
-            image='neo4j:4.4.9',
-            name='neo4j-content',
-            ports={'7474/tcp': 7474, '7687/tcp': 7687},
-            volumes=[f'{REPO_PATH / "neo4j" / "data"}:/data'],
-            detach=True,
-            environment={'NEO4J_AUTH': f'neo4j/{NEO4J_PASSWORD}'},
-        )
-    # health check to make sure that neo4j is up
+
+def _stop_neo4j_service_docker(docker_client: docker.DockerClient):
+    try:
+        docker_client.containers.get('neo4j-content').remove(force=True)
+    except Exception as e:
+        logger.info(f'Could not remove neo4j container: {e}')
+
+
+def _wait_until_service_is_up():
     s = requests.Session()
 
     retries = Retry(
@@ -53,14 +50,34 @@ def start_neo4j_service(use_docker: bool = True):
 
     s.mount('http://localhost', HTTPAdapter(max_retries=retries))
     s.get('http://localhost:7474')
-    logger.info('Connected to neo4j service')
+
+
+def start_neo4j_service(use_docker: bool = True):
+    if not use_docker:
+        neo4j_admin_command('set-initial-password', f'neo4j - admin set - initial - password {NEO4J_PASSWORD}')
+        run_command('neo4j start', cwd=REPO_PATH / 'neo4j', is_silenced=False)
+
+    else:
+        docker_client = _get_docker_client()
+        _stop_neo4j_service_docker(docker_client)
+        docker_client.containers.run(
+            image=NEO4J_SERVICE_IMAGE,
+            name='neo4j-content',
+            ports={'7474/tcp': 7474, '7687/tcp': 7687},
+            volumes=[f'{REPO_PATH / "neo4j" / "data"}:/data'],
+            detach=True,
+            environment={'NEO4J_AUTH': f'neo4j/{NEO4J_PASSWORD}'},
+        )
+    # health check to make sure that neo4j is up
+    _wait_until_service_is_up()
 
 
 def stop_neo4j_service(use_docker: bool):
     if not use_docker:
         run_command('neo4j stop', cwd=REPO_PATH / 'neo4j', is_silenced=False)
     else:
-        run_command('docker-compose down', cwd=REPO_PATH / 'neo4j', is_silenced=False)
+        docker_client = _get_docker_client()
+        _stop_neo4j_service_docker(docker_client)
 
 
 def neo4j_admin_command(name: str, command: str):
@@ -72,7 +89,7 @@ def neo4j_admin_command(name: str, command: str):
             docker_client.containers.get(f'neo4j-{name}').remove(force=True)
         except Exception as e:
             logger.info(f'Could not remove neo4j container: {e}')
-        docker_client.containers.run(image='neo4j/neo4j-admin:4.4.9',
+        docker_client.containers.run(image=NEO4J_ADMIN_IMAGE,
                                      name=f'neo4j-{name}',
                                      remove=True,
                                      volumes=[f'{REPO_PATH}/neo4j/data:/data', f'{REPO_PATH}/neo4j/backups:/backups'],

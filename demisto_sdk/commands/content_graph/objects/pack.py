0965eed65189a3d0c1d3187788b9956f54ffac87
Dan Tavori <dtavori@paloaltonetworks.com>
2022-08-26 18:06:42 +0300

    added readme

--- demisto_sdk/commands/content_graph/objects/pack.py
+++ demisto_sdk/commands/content_graph/objects/pack.py
@@ -103,9 +103,6 @@ class PackMetadata(BaseModel):
 
 class Pack(BaseContent, PackMetadata):
     path: Path
-    object_id: str
-    content_type: ContentTypes
-    node_id: str
     content_items: PackContentItems = Field(alias='contentItems', exclude=True)
     relationships: Relationships = Field(Relationships(), exclude=True)
 

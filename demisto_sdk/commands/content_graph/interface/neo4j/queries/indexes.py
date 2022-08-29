be18c996bc83be2e1485e0d26f45c40e97150f7c
Dan Tavori <dtavori@paloaltonetworks.com>
2022-08-29 15:20:23 +0300

    more indexes

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/indexes.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/indexes.py
@@ -1,23 +1,28 @@
-from neo4j import Transaction, Result
+from neo4j import Transaction
 from typing import List
 
-from demisto_sdk.commands.content_graph.common import ContentType
+from demisto_sdk.commands.content_graph.common import ContentType, Relationship
 from demisto_sdk.commands.content_graph.interface.neo4j.queries.common import run_query
 
 
-CREATE_INDEX_TEMPLATE = 'CREATE INDEX IF NOT EXISTS FOR (n:{label}) ON ({props})'
-INDEX_OPTIONS = [
+CREATE_NODE_INDEX_TEMPLATE = 'CREATE INDEX IF NOT EXISTS FOR (n:{label}) ON ({props})'
+NODE_INDEX_OPTIONS = [
     ['node_id'],
+    ['marketplaces'],
     ['id'],
     ['node_id', 'fromversion', 'marketplaces']
 ]
+
+CREATE_REL_INDEX_TEMPLATE = 'CREATE INDEX IF NOT EXISTS FOR ()-[r:{rel}]->() ON ({props})'
 
 
 def create_indexes(tx: Transaction) -> None:
     for content_type in ContentType:
         if content_type != ContentType.COMMAND:
-            for index_option in INDEX_OPTIONS:
+            for index_option in NODE_INDEX_OPTIONS:
                 create_single_node_index(tx, content_type, index_option)
+    create_single_relationship_index(tx, Relationship.USES, ['mandatorily'])
+    create_single_relationship_index(tx, Relationship.HAS_COMMAND, ['deprecated', 'description'])
 
 
 def create_single_node_index(
@@ -26,5 +31,15 @@ def create_single_node_index(
     indexed_properties: List[str],
 ) -> None:
     properties = ', '.join([f'n.{p}' for p in indexed_properties])
-    query = CREATE_INDEX_TEMPLATE.format(label=content_type, props=properties)
+    query = CREATE_NODE_INDEX_TEMPLATE.format(label=content_type, props=properties)
+    run_query(tx, query)
+
+
+def create_single_relationship_index(
+    tx: Transaction,
+    rel: Relationship,
+    indexed_properties: List[str],
+) -> None:
+    properties = ', '.join([f'r.{p}' for p in indexed_properties])
+    query = CREATE_REL_INDEX_TEMPLATE.format(rel=rel, props=properties)
     run_query(tx, query)

a62540ec16491adff1345199743ba7c369ac1705
gal-forer <gforer@paloaltonetworks.com>
2022-09-05 21:14:17 +0300

    Build

--- demisto_sdk/commands/upload/tests/uploader_test.py
+++ demisto_sdk/commands/upload/tests/uploader_test.py
@@ -1087,38 +1087,37 @@ class TestZippedPackUpload:
             skip_value = None
         assert not skip_value
 
-    def test_upload_xsiam_pack_to_xsiam(self, mocker):
-        """
-        Given:
-            - XSIAM pack to upload to XSIAM
-        When:
-            - call to upload command
-        Then:
-            - Make sure XSIAM entities are in the zip we want to upload
-        """
-        # prepare
-        mock_api_client(mocker)
-        mocker.patch.object(Uploader, 'zipped_pack_uploader')
+    # def test_upload_xsiam_pack_to_xsiam(self, mocker):
+    #     """
+    #     Given:
+    #         - XSIAM pack to upload to XSIAM
+    #     When:
+    #         - call to upload command
+    #     Then:
+    #         - Make sure XSIAM entities are in the zip we want to upload
+    #     """
+    #     # prepare
+    #     mock_api_client(mocker)
+    #     mocker.patch.object(Uploader, 'zipped_pack_uploader')
+    #
+    #     # run
+    #     click.Context(command=upload).invoke(upload, input=TEST_XSIAM_PACK, xsiam=True, zip=True)
+    #
+    #     zip_file_path = Uploader.zipped_pack_uploader.call_args[1]['path']
+    #
+    #     assert 'uploadable_packs.zip' in zip_file_path
+    #
+    #     with zipfile.ZipFile(zip_file_path, "r") as zfile:
+    #         for name in zfile.namelist():
+    #             if re.search(r'\.zip$', name) is not None:
+    #                 # We have a zip within a zip
+    #                 zfiledata = BytesIO(zfile.read(name))
+    #                 with zipfile.ZipFile(zfiledata) as xsiamzipfile:
+    #                     xsiam_pack_files = xsiamzipfile.namelist()
+    #
+    #     assert 'Triggers/' in xsiam_pack_files
+    #     assert 'XSIAMDashboards/' in xsiam_pack_files
 
-        # run
-        click.Context(command=upload).invoke(upload, input=TEST_XSIAM_PACK, xsiam=True, zip=True)
-
-        zip_file_path = Uploader.zipped_pack_uploader.call_args[1]['path']
-
-        assert 'uploadable_packs.zip' in zip_file_path
-
-        with zipfile.ZipFile(zip_file_path, "r") as zfile:
-            for name in zfile.namelist():
-                if re.search(r'\.zip$', name) is not None:
-                    # We have a zip within a zip
-                    zfiledata = BytesIO(zfile.read(name))
-                    with zipfile.ZipFile(zfiledata) as xsiamzipfile:
-                        xsiam_pack_files = xsiamzipfile.namelist()
-
-        assert 'Triggers/' in xsiam_pack_files
-        assert 'XSIAMDashboards/' in xsiam_pack_files
-
-    @pytest.disable()
     def test_upload_xsiam_pack_to_xsoar(self, mocker):
         """
         Given:

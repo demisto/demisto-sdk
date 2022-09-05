ad3b17bbb155c68dd96c8ed53e0d31840b7a179a
gal-forer <gforer@paloaltonetworks.com>
2022-09-05 20:08:47 +0300

    Added triggers to XSIAM files for external

--- demisto_sdk/commands/common/content/objects/pack_objects/trigger/trigger.py
+++ demisto_sdk/commands/common/content/objects/pack_objects/trigger/trigger.py
@@ -1,4 +1,5 @@
-from typing import Union
+import shutil
+from typing import Union, Optional, List
 
 import demisto_client
 from wcmatch.pathlib import Path
@@ -11,6 +12,9 @@ from demisto_sdk.commands.common.content.objects.pack_objects.abstract_pack_obje
 class Trigger(JSONContentObject):
     def __init__(self, path: Union[Path, str]):
         super().__init__(path, TRIGGER)
+
+    def normalize_file_name(self) -> str:
+        return generate_xsiam_normalized_name(self._path.name, TRIGGER)
 
     def upload(self, client: demisto_client):
         """
@@ -26,3 +30,10 @@ class Trigger(JSONContentObject):
 
     def type(self):
         return FileType.TRIGGER
+
+    def dump(self, dest_dir: Optional[Union[Path, str]] = None) -> List[Path]:
+        created_files: List[Path] = []
+        created_files.extend(super().dump(dest_dir=dest_dir))
+        new_file_path = created_files[0]
+        shutil.copyfile(new_file_path, new_file_path.parent / self.normalize_file_name())
+        return created_files

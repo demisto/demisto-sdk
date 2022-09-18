74aa9fb1840eadbfcbb72cdcf5d347172ad3b1ec
merit-maita <49760643+merit-maita@users.noreply.github.com>
2022-09-18 17:25:44 +0300

    Update demisto_sdk/commands/download/downloader.py
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- demisto_sdk/commands/download/downloader.py
+++ demisto_sdk/commands/download/downloader.py
@@ -605,7 +605,7 @@ class Downloader:
         if file_type and file_type == 'playbook':
             name: str = get_entity_name_by_entity_type(file_data, PLAYBOOKS_DIR)
             if name.endswith(("Test", "_test", "_Test", "-test", "-Test")) \
-                    or name.startswith(("Test", "Test_", "test_", "Test-", "test-")):
+                    or name.lower().startswith('test'):
                 return TEST_PLAYBOOKS_DIR
         return ENTITY_TYPE_TO_DIR.get(file_type, '')
 

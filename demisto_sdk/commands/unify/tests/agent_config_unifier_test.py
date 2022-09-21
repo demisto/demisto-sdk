ea384090f7098613db70f5238e03696d3ee432e9
bziser <bziser@paloaltonetworks.com>
2022-09-22 00:33:58 +0300

    test_unify_agent_config

--- demisto_sdk/commands/unify/tests/agent_config_unifier_test.py
+++ demisto_sdk/commands/unify/tests/agent_config_unifier_test.py
@@ -1,6 +1,5 @@
+import json
 import os
-import shutil
-import pytest
 
 from demisto_sdk.commands.unify.agent_config_unifier import AgentConfigUnifier
 from demisto_sdk.commands.common.legacy_git_tools import git_path
@@ -28,8 +27,12 @@ def test_unify_agent_config():
     expected_json_path = TESTS_DIR + '/test_files/Packs/DummyPack/AgentConfigs/DummyAgentConfig/agentconfig-DummyAgentConfig.json'
     assert export_json_path == expected_json_path
 
-    expected_json_file_path = TESTS_DIR + '/test_files/Packs/DummyPack/AgentConfigs/DummyAgentConfig/expected-agentconfig-DummyAgentConfig.json'
-    with open(expected_json_file_path, 'r') as expected_file, open(expected_json_path, 'r') as real_file:
-        assert expected_file.read() == real_file.read()
+    expected_json_file = {'content_global_id': '1',
+                          'name': 'Dummmy',
+                          'os_type': 'AGENT_OS_LINUX',
+                          'profile_type': 'STANDARD',
+                          'yaml_template': 'dGVzdDogZHVtbXlfdGVzdA=='}
+    with open(expected_json_path, 'r') as real_file:
+        assert expected_json_file == json.load(real_file)
 
     os.remove(expected_json_path)

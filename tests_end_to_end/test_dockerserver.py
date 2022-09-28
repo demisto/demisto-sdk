a7c5d1959e7e54397f931efa42d19944e9a3669c
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-09-28 14:19:46 +0300

    test dont rely on file

--- tests_end_to_end/test_dockerserver.py
+++ tests_end_to_end/test_dockerserver.py
@@ -1,10 +1,13 @@
 import pytest
+import requests
+from requests.sessions import HTTPAdapter
+from urllib3 import Retry
 
 from demisto_sdk.commands.common.hook_validations.readme import ReadMeValidator
 from demisto_sdk.commands.common.MDXServer import (DEMISTO_DEPS_DOCKER_NAME,
                                                    start_docker_MDX_server)
-from demisto_sdk.commands.common.tests.readme_test import (
-    README_INPUTS, assert_successful_mdx_call)
+from demisto_sdk.commands.common.tests.readme_test import \
+    assert_successful_mdx_call
 from demisto_sdk.commands.lint.docker_helper import init_global_docker_client
 
 
@@ -21,12 +24,26 @@ def test_docker_server_up_and_down():
     assert not container_is_up()
 
 
-@pytest.mark.parametrize("current, answer", README_INPUTS)
-def test_is_file_valid_docker_mdx_server(mocker, current, answer):
-    with start_docker_MDX_server():
-        readme_validator = ReadMeValidator(current)
-        mocker.patch.object(ReadMeValidator, 'are_modules_installed_for_verify', return_value=False)
-        assert readme_validator.is_valid_file() is answer
+def assert_not_successful_mdx_call():
+    session = requests.Session()
+    retry = Retry(total=2)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    response = session.request(
+        'POST',
+        'http://localhost:6161',
+        data='<div> Hello',
+        timeout=20
+    )
+    assert response.status_code == 500
+
+
+def test_is_file_not_valid():
+    with start_docker_MDX_server() as boolean:
+        assert boolean
+        assert container_is_up()
+        assert_not_successful_mdx_call()
+    assert not container_is_up()
 
 
 def test_docker_server_reentrant():

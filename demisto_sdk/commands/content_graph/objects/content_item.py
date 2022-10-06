de105109d23603993f188f99127aac8e20d384f5
ilan <ierukhimovic@paloaltonetworks.com>
2022-10-06 14:47:00 +0300

    content item

--- demisto_sdk/commands/content_graph/objects/content_item.py
+++ demisto_sdk/commands/content_graph/objects/content_item.py
@@ -1,13 +1,14 @@
 import shutil
 from pathlib import Path
-from typing import Dict, List, Optional, Set, Type, cast
+from typing import Dict, List, Optional, Set, Type
 
 from pydantic import DirectoryPath
-from pydantic.main import ModelMetaclass
+from demisto_sdk.commands.common.tools import get_pack_name
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects.base_content import BaseContent
+
 
 content_type_to_model: Dict[ContentType, Type['ContentItem']] = {}
 
@@ -37,3 +38,9 @@ class ContentItem(BaseContent):
     def dump(self, dir: DirectoryPath, _: MarketplaceVersions) -> None:
         dir.mkdir(exist_ok=True, parents=True)
         shutil.copy(self.path, dir / self.normalize_file_name(self.path.name))
+
+    def to_id_set_entity(self) -> dict:
+        id_set_entity = self.dict()
+        id_set_entity['file_path'] = str(self.path)
+        id_set_entity['pack'] = get_pack_name(self.path)
+        return id_set_entity

889f73128a6d18a6202958535633344c510575bd
yaakovi <syaakovi@paloaltonetworks.com>
2022-10-12 08:12:53 +0300

    add unit test

--- demisto_sdk/commands/error_code_info/error_code_info.py
+++ demisto_sdk/commands/error_code_info/error_code_info.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, Optional, Union, get_args, get_origin
+from typing import Any, Dict, Optional, Union, get_args, get_origin
 
 import click
 from colorama import Fore
@@ -19,31 +19,41 @@ TYPE_FILLER_MAPPING = {
     int: 1337,
     bool: True,
     dict: {'key1': 'value1', 'key2': 'value2'},
+    Dict: {'key1': 'value1', 'key2': 'value2'},
     list: ['element1', 'element2'],
     constants.FileType: constants.FileType.INTEGRATION,
 }
 
 
-def print_error_information(func_name, error_data, func, sig: inspect.Signature):
+def parse_function_parameters(sig: inspect.Signature):
     parameters = {}
     for param in sig.parameters.values():
         value: Any = f'<{param.name}>'
         if param.default is not inspect.Parameter.empty:
             value = param.default
+
         if param.annotation is not inspect.Parameter.empty:
             param_type = param.annotation
             if get_origin(param.annotation) in [Union, Optional]:
                 param_type = get_args(param.annotation)[0]
+
             if param_type is not str:
                 value = TYPE_FILLER_MAPPING.get(param_type) or param_type()
+
         parameters[param.name] = value
+
+    return parameters
+
+
+def print_error_information(func_name, error_data, func, sig: inspect.Signature):
+    parameters = parse_function_parameters(sig)
 
     click.secho(f'{Fore.GREEN}## Error Code Info ##{Fore.RESET}')
     click.secho(TEMPLATE.format(
         code=error_data['code'],
         func=f'{func_name}{sig}',
         ignorable=error_data['code'] in errors.ALLOWED_IGNORE_ERRORS,
-        msg=func(**{a: f'<{a}>' for a in list(sig.parameters)})[0],
+        msg=func(**parameters)[0],
     ))
 
 

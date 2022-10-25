c5cd9a0316695ac34d9bbac4b67b9c3d97fc4a99
ilan <ierukhimovic@paloaltonetworks.com>
2022-10-25 07:02:24 +0000

    fix

--- demisto_sdk/commands/content_graph/content_graph_commands.py
+++ demisto_sdk/commands/content_graph/content_graph_commands.py
@@ -1,12 +1,9 @@
 import logging
 
 import demisto_sdk.commands.content_graph.neo4j_service as neo4j_service
-from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import REPO_PATH
 from demisto_sdk.commands.content_graph.content_graph_builder import ContentGraphBuilder
-from demisto_sdk.commands.content_graph.content_graph_loader import ContentGraphLoader
 from demisto_sdk.commands.content_graph.interface.graph import ContentGraphInterface
-from demisto_sdk.commands.content_graph.objects.repository import Repository
 
 logger = logging.getLogger("demisto-sdk")
 

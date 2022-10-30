d875352fee7713160a87e1956ab9487151752ac6
Binat Ziser <89336697+bziser@users.noreply.github.com>
2022-10-30 10:56:54 +0200

    mypy ignore

--- demisto_sdk/commands/common/constants.py
+++ demisto_sdk/commands/common/constants.py
@@ -45,7 +45,7 @@ XSIAM_DASHBOARDS_DIR = 'XSIAMDashboards'
 XSIAM_REPORTS_DIR = 'XSIAMReports'
 TRIGGER_DIR = 'Triggers'
 WIZARDS_DIR = 'Wizards'
-AGENT_CONFIG_DIR = 'AgentConfigs'
+XDRC_TEMPLATE_DIR = 'XDRCTemplates'
 
 # NAMES OF ENTITIES
 
@@ -88,7 +88,7 @@ XSIAM_DASHBOARD = 'xsiamdashboard'
 XSIAM_REPORT = 'xsiamreport'
 TRIGGER = 'trigger'
 WIZARD = 'wizard'
-AGENT_CONFIG = 'agentconfig'
+XDRC_TEMPLATE = 'xdrctemplate'
 
 MARKETPLACE_KEY_PACK_METADATA = 'marketplaces'
 
@@ -157,8 +157,8 @@ class FileType(str, Enum):
     PACK_IGNORE = '.pack-ignore'
     SECRET_IGNORE = '.secrets-ignore'
     DOC_FILE = 'doc_files'
-    AGENT_CONFIG = 'agentconfig'
-    AGENT_CONFIG_YML = 'agentconfigyml'
+    XDRC_TEMPLATE = 'xdrctemplate'
+    XDRC_TEMPLATE_YML = 'xdrctemplateyml'
 
 
 RN_HEADER_BY_FILE_TYPE = {
@@ -223,7 +223,7 @@ ENTITY_TYPE_TO_DIR = {
     FileType.PARSING_RULE.value: PARSING_RULES_DIR,
     FileType.MODELING_RULE.value: MODELING_RULES_DIR,
     FileType.WIZARD.value: WIZARDS_DIR,
-    FileType.AGENT_CONFIG.value: AGENT_CONFIG_DIR,
+    FileType.XDRC_TEMPLATE.value: XDRC_TEMPLATE_DIR,
 }
 
 SIEM_ONLY_ENTITIES = [
@@ -233,7 +233,7 @@ SIEM_ONLY_ENTITIES = [
     FileType.XSIAM_DASHBOARD.value,
     FileType.XSIAM_REPORT.value,
     FileType.TRIGGER.value,
-    FileType.AGENT_CONFIG.value,
+    FileType.XDRC_TEMPLATE.value,
 ]
 
 CONTENT_FILE_ENDINGS = ['py', 'yml', 'png', 'json', 'md']
@@ -267,7 +267,7 @@ CONTENT_ENTITIES_DIRS = [
     JOBS_DIR,
     WIZARDS_DIR,
     MODELING_RULES_DIR,
-    AGENT_CONFIG_DIR,
+    XDRC_TEMPLATE_DIR,
 ]
 
 CONTENT_ENTITY_UPLOAD_ORDER = [
@@ -650,7 +650,7 @@ INTEGRATION_PREFIX = 'integration'
 SCRIPT_PREFIX = 'script'
 PARSING_RULE_PREFIX = 'parsingrule'
 MODELING_RULE_PREFIX = 'modelingrule'
-AGENT_CONFIG_PREFIX = 'agentconfig'
+XDRC_TEMPLATE_PREFIX = 'xdrctemplate'
 
 # Pack Unique Files
 PACKS_WHITELIST_FILE_NAME = '.secrets-ignore'
@@ -1116,7 +1116,7 @@ DIR_TO_PREFIX = {
     'Scripts': SCRIPT_PREFIX,
     'ModelingRules': MODELING_RULE_PREFIX,
     'ParsingRules': PARSING_RULE_PREFIX,
-    'AgentConfigs': AGENT_CONFIG_PREFIX,
+    'XDRCTemplates': XDRC_TEMPLATE_PREFIX,
 }
 
 ENTITY_NAME_SEPARATORS = [' ', '_', '-']
@@ -1450,7 +1450,7 @@ class ContentItems(Enum):
     XSIAM_REPORTS = 'xsiamreport'
     TRIGGERS = 'trigger',
     WIZARDS = 'wizard',
-    AGENT_CONFIG = 'agentconfig'
+    XDRC_TEMPLATE = 'xdrctemplate'
 
 
 CONTENT_ITEMS_DISPLAY_FOLDERS = {
@@ -1476,7 +1476,7 @@ CONTENT_ITEMS_DISPLAY_FOLDERS = {
     XSIAM_REPORTS_DIR,
     TRIGGER_DIR,
     WIZARDS_DIR,
-    AGENT_CONFIG_DIR,
+    XDRC_TEMPLATE_DIR,
 }
 
 
---
+++ demisto_sdk/commands/common/content/tests/objects/pack_objects/xdrctemplate/xdrc_template_test.py
@@ -0,0 +1,25 @@
+from demisto_sdk.commands.common.content.objects.pack_objects import \
+    XDRCTemplate
+from demisto_sdk.commands.common.content.objects_factory import \
+    path_to_pack_object
+
+
+class TestXDRCTemplate:
+    def test_objects_factory(self, pack):
+        xdrc_template = pack.create_xdrc_template('xdrc_template_name')
+        obj = path_to_pack_object(xdrc_template.xdrc_template_tmp_path)
+        assert isinstance(obj, XDRCTemplate)
+
+    def test_prefix(self, pack):
+        xdrc_template = pack.create_xdrc_template('external-xdrctemplate-xdrc_template_name')
+        obj = XDRCTemplate(xdrc_template.xdrc_template_tmp_path)
+        assert obj.normalize_file_name() == xdrc_template.xdrc_template_tmp_path.name
+
+        xdrc_template = pack.create_xdrc_template('xdrc_template_name')
+        obj = XDRCTemplate(xdrc_template.xdrc_template_tmp_path)
+        assert obj.normalize_file_name() == f"external-xdrctemplate-{xdrc_template.xdrc_template_tmp_path.name}"
+
+    def test_files_detection(self, pack):
+        xdrc_template = pack.create_xdrc_template('xdrc_template_name')
+        obj = XDRCTemplate(xdrc_template.xdrc_template_tmp_path)
+        assert obj.path == xdrc_template.xdrc_template_tmp_path

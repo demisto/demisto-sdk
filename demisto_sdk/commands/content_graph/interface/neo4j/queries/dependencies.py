204ad3261f3be1d212d82982fa1925a0bafc6a6d
ilan <ierukhimovic@paloaltonetworks.com>
2022-11-03 17:00:20 +0200

    fix query

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/dependencies.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/dependencies.py
@@ -117,11 +117,10 @@ def update_marketplaces_property(tx: Transaction, marketplace: str) -> None:
 def update_uses_for_integration_commands(tx: Transaction) -> None:
     query = f"""
     MATCH (content_item:{ContentType.BASE_CONTENT})-[r:{RelationshipType.USES}]->(command:{ContentType.COMMAND})
-    OPTIONAL MATCH (command)<-[rcmd:HAS_COMMAND]-(integration:{ContentType.INTEGRATION})
-    // AND rcmd.deprecated = false  # TODO check if we want to include deprecated commands
+    MATCH (command)<-[rcmd:{RelationshipType.HAS_COMMAND}]-(integration:{ContentType.INTEGRATION})
     WHERE NOT command.object_id IN {list(GENERIC_COMMANDS_NAMES)}
-    MERGE (content_item)-[u:{RelationshipType.USES}]->(integration)
-    SET u.mandatorily = r.mandatorily OR u.mandatorily
+    MERGE (content_item)-[u:USES]->(integration)
+    SET u.mandatorily = r.mandatorily
     RETURN count(u) as uses_relationships
     """
     result = run_query(tx, query).single()

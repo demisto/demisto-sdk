f4feb1dad9353e1f675efaf78027efd7121ea783
ilan <ierukhimovic@paloaltonetworks.com>
2022-11-07 17:26:34 +0200

    precommit

--- demisto_sdk/commands/content_graph/objects/pack.py
+++ demisto_sdk/commands/content_graph/objects/pack.py
@@ -1,4 +1,4 @@
-from packaging.version import Version, parse
+from packaging.version import parse
 import logging
 import shutil
 from collections import defaultdict
@@ -191,7 +191,7 @@ class Pack(BaseContent, PackMetadata, content_type=ContentType.PACK):  # type: i
         content_item_dct = defaultdict(list)
         for c in content_items:
             content_item_dct[c.content_type].append(c)
-        
+
         # If there is no server_min_version, set it to the maximum of its content items fromversion
         self.server_min_version = self.server_min_version or str(max(parse(content_item.fromversion) for content_item in content_items))
         self.content_items = PackContentItems.parse_obj(content_item_dct)

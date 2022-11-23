44575685b0462fb4957dd3de17ec51e5651a2e28
Dan Tavori <dtavori@paloaltonetworks.com>
2022-11-23 20:21:30 +0200

    import fixes

--- demisto_sdk/commands/content_graph/neo4j_service.py
+++ demisto_sdk/commands/content_graph/neo4j_service.py
@@ -7,7 +7,7 @@ import requests
 from requests.adapters import HTTPAdapter, Retry
 
 from demisto_sdk.commands.common.tools import get_content_path, run_command
-from demisto_sdk.commands.content_graph.common import NEO4J_DATABASE_HTTP, NEO4J_FOLDER, NEO4J_PASSWORD, REPO_PATH
+from demisto_sdk.commands.content_graph.common import NEO4J_DATABASE_HTTP, NEO4J_FOLDER, NEO4J_PASSWORD
 
 REPO_PATH = Path(get_content_path())  # type: ignore
 

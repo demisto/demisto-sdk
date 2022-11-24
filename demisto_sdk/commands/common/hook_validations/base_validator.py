ac0e07b7f9e1a27c656902edd0fe655144bf33ff
epintzov <epintzov@paloaltonetworks.com>
2022-11-24 17:23:35 +0200

    bug fix check deprecated

--- demisto_sdk/commands/common/hook_validations/base_validator.py
+++ demisto_sdk/commands/common/hook_validations/base_validator.py
@@ -217,7 +217,7 @@ class BaseValidator:
     def check_deprecated(self, file_path):
         if file_path.endswith('.yml'):
             yml_dict = get_yaml(file_path)
-            if hasattr(yml_dict, 'deprecated') and yml_dict.get('deprecated'):
+            if 'deprecated' in yml_dict and yml_dict.get('deprecated'):
                 self.add_flag_to_ignore_list(file_path, 'deprecated')
 
     @staticmethod

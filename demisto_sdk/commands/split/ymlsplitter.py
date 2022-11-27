f863611ed3caf0a2f33bc17edb9af2bf3b9aee77
epintzov <epintzov@paloaltonetworks.com>
2022-11-27 16:54:45 +0200

    imports optimized

--- demisto_sdk/commands/split/ymlsplitter.py
+++ demisto_sdk/commands/split/ymlsplitter.py
@@ -2,22 +2,18 @@ import base64
 import os
 import re
 import shutil
-import subprocess
-import tempfile
-import autopep8
-import isort
-
 from io import open
 from pathlib import Path
 from typing import Optional
 
+import autopep8
+import isort
 from ruamel.yaml.scalarstring import PlainScalarString, SingleQuotedScalarString
 
 from demisto_sdk.commands.common.configuration import Configuration
 from demisto_sdk.commands.common.constants import TYPE_PWSH, TYPE_PYTHON, TYPE_TO_EXTENSION
 from demisto_sdk.commands.common.handlers import YAML_Handler
-from demisto_sdk.commands.common.tools import (LOG_COLORS, get_all_docker_images, get_pipenv_dir, get_python_version,
-                                               pascal_case, print_color, print_error)
+from demisto_sdk.commands.common.tools import (LOG_COLORS, pascal_case, print_color, print_error)
 from demisto_sdk.commands.unify.integration_script_unifier import IntegrationScriptUnifier
 
 yaml = YAML_Handler()

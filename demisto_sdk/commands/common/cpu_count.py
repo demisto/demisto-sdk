9072a8f8c79996041d6adb5c44f2b9e1120e2942
esharf <57587340+esharf@users.noreply.github.com>
2022-11-29 19:27:17 +0200

    fixed env var name (CR)

--- demisto_sdk/commands/common/cpu_count.py
+++ demisto_sdk/commands/common/cpu_count.py
@@ -1,12 +1,12 @@
 import os
 
-MAX_DEMISTO_SDK_THREADS = 'MAX_DEMISTO_SDK_THREADS'
+DEMISTO_SDK_MAX_THREADS = 'DEMISTO_SDK_MAX_THREADS'
 
 
 def cpu_count():
     max_available_cpus = os.cpu_count() or 1
     try:
-        max_allowed_cpus = int(os.environ[MAX_DEMISTO_SDK_THREADS])
+        max_allowed_cpus = int(os.environ[DEMISTO_SDK_MAX_THREADS])
         requsted_cpus = min(max_allowed_cpus, max_available_cpus)
         return max(1, requsted_cpus)
     except Exception:
--- demisto_sdk/commands/common/tests/cpu_count_test.py
+++ demisto_sdk/commands/common/tests/cpu_count_test.py
@@ -1,13 +1,13 @@
-from demisto_sdk.commands.common.cpu_count import MAX_DEMISTO_SDK_THREADS, cpu_count, os
+from demisto_sdk.commands.common.cpu_count import DEMISTO_SDK_MAX_THREADS, cpu_count, os
 
 
 def test_with_env_set(mocker):
-    mocker.patch.dict(os.environ, {MAX_DEMISTO_SDK_THREADS: "1"})
+    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_THREADS: "1"})
     assert cpu_count() == 1
 
 
 def test_with_invalid_env_set(mocker):
-    mocker.patch.dict(os.environ, {MAX_DEMISTO_SDK_THREADS: ""})
+    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_THREADS: ""})
     assert cpu_count() == os.cpu_count()
 
 

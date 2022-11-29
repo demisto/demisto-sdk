978b02ad362af1a0621d21e0b0553d87d81eeeb8
esharf <57587340+esharf@users.noreply.github.com>
2022-11-29 19:40:00 +0200

    fixup! fixed env var name (CR)

--- demisto_sdk/commands/common/tests/cpu_count_test.py
+++ demisto_sdk/commands/common/tests/cpu_count_test.py
@@ -1,13 +1,13 @@
-from demisto_sdk.commands.common.cpu_count import DEMISTO_SDK_MAX_THREADS, cpu_count, os
+from demisto_sdk.commands.common.cpu_count import DEMISTO_SDK_MAX_CPU_CORES, cpu_count, os
 
 
 def test_with_env_set(mocker):
-    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_THREADS: "1"})
+    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_CPU_CORES: "1"})
     assert cpu_count() == 1
 
 
 def test_with_invalid_env_set(mocker):
-    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_THREADS: ""})
+    mocker.patch.dict(os.environ, {DEMISTO_SDK_MAX_CPU_CORES: ""})
     assert cpu_count() == os.cpu_count()
 
 

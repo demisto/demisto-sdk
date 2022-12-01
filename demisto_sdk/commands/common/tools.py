17518aad392d63e46fb6fed683112f60efd9e374
Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
2022-12-01 11:25:58 +0200

    hotfix for categories validation for private repo (#2525)
    * hotfix
    
    * fixes
    
    * empty

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -2431,10 +2431,8 @@ def get_current_categories() -> list:
     Returns:
         List of approved categories from current branch
     """
-    if not is_external_repository():
-        approved_categories_json, _ = get_dict_from_file('Tests/Marketplace/approved_categories.json')
-        return approved_categories_json.get('approved_list', [])
-    return []
+    approved_categories_json, _ = get_dict_from_file('Tests/Marketplace/approved_categories.json')
+    return approved_categories_json.get('approved_list', [])
 
 
 @contextmanager

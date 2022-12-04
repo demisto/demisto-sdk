fed2cd1a580e5ed0bcf2ad6690b2dc3da915eb9d
Dor <81086590+dorschw@users.noreply.github.com>
2022-12-04 15:25:47 +0000

    improve auth error message on download

--- demisto_sdk/commands/download/downloader.py
+++ demisto_sdk/commands/download/downloader.py
@@ -201,7 +201,7 @@ class Downloader:
 
     def handle_api_exception(self, e):
         if e.status == 401:
-            print_color("\nError authenticating with XSOAR. Please verify that the appropriate environment variables "
+            print_color("\nAuthentication error: please verify that the appropriate environment variables "
                         "(either DEMISTO_USERNAME and DEMISTO_PASSWORD, or just DEMISTO_API_KEY) are properly configured.\n",
                         LOG_COLORS.RED)
         print_color(f'Exception raised when fetching custom content:\nStatus: {e}', LOG_COLORS.NATIVE)

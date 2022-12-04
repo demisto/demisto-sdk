8d0ded54a339ac7310fbeaee24355cfb1e704577
avidan-H <>
2022-12-04 16:30:08 +0200

    fix(ModelingRuleValidator): properly split testdata filenames when checking is_valid_rule_names

--- demisto_sdk/commands/common/hook_validations/modeling_rule.py
+++ demisto_sdk/commands/common/hook_validations/modeling_rule.py
@@ -84,8 +84,9 @@ class ModelingRuleValidator(ContentEntityValidator):
 
         for file_path in files_to_check:
             file_name = os.path.basename(file_path)
-            # The schema has _schema.json suffix whereas the integration only has the .suffix
-            splitter = '_' if file_name.endswith('_schema.json') else '.'
+            # The schema has _schema.json suffix and the testdata file has _testdata.json suffix
+            # whereas the other content entity component files only has the .suffix
+            splitter = '_' if file_name.endswith('_schema.json') or file_name.endswith('_testdata.json') else '.'
             base_name = file_name.rsplit(splitter, 1)[0]
 
             if integrations_folder != base_name:

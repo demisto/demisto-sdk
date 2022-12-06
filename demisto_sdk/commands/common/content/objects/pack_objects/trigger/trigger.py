36bbe29cefae5bb67b102c53f8896844f12bf9b6
yaakovpraisler <ypreisler@paloaltonetworks.com>
2022-12-06 17:28:28 +0200

    capitalize Version for json objects

--- demisto_sdk/commands/common/content/objects/pack_objects/trigger/trigger.py
+++ demisto_sdk/commands/common/content/objects/pack_objects/trigger/trigger.py
@@ -41,7 +41,7 @@ class Trigger(JSONContentObject):
         created_files.extend(super().dump(dest_dir=dest_dir))
         new_file_path = created_files[0]
 
-        if Version(self.get('fromversion', '0.0.0')) >= Version('6.10.0'):
+        if Version(self.get('fromVersion', '0.0.0')) >= Version('6.10.0'):
             # export XSIAM 1.3 items only with the external prefix
             if not new_file_path.name.startswith('external-'):
                 move_to_path = new_file_path.parent / self.normalize_file_name()
@@ -49,7 +49,7 @@ class Trigger(JSONContentObject):
                 created_files.remove(new_file_path)
                 created_files.append(move_to_path)
 
-        elif Version(self.get('toversion', '99.99.99')) < Version('6.10.0'):
+        elif Version(self.get('toVersion', '99.99.99')) < Version('6.10.0'):
             # export XSIAM 1.2 items only without the external prefix
             if new_file_path.name.startswith('external-'):
                 move_to_path = Path(str(new_file_path).replace('external-', ''))

43c5824a7f92092aeb3ffd1014c5e53cef7b3896
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-06 14:58:18 +0200

    fix dumps

--- demisto_sdk/commands/content_graph/objects/correlation_rule.py
+++ demisto_sdk/commands/content_graph/objects/correlation_rule.py
@@ -1,10 +1,10 @@
 from typing import Set
 
 from demisto_sdk.commands.content_graph.common import ContentType
-from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
+from demisto_sdk.commands.content_graph.objects.content_item_x2 import ContentItemX2
 
 
-class CorrelationRule(ContentItem, content_type=ContentType.CORRELATION_RULE):  # type: ignore[call-arg]
+class CorrelationRule(ContentItemX2, content_type=ContentType.CORRELATION_RULE):  # type: ignore[call-arg]
 
     def metadata_fields(self) -> Set[str]:
         return {"name", "description"}
--- demisto_sdk/commands/content_graph/objects/modeling_rule.py
+++ demisto_sdk/commands/content_graph/objects/modeling_rule.py
@@ -2,11 +2,11 @@ from typing import Set
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
-from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
+from demisto_sdk.commands.content_graph.objects.content_item_x2 import ContentItemX2
 from demisto_sdk.commands.unify.rule_unifier import RuleUnifier
 
 
-class ModelingRule(ContentItem, content_type=ContentType.MODELING_RULE):  # type: ignore[call-arg]
+class ModelingRule(ContentItemX2, content_type=ContentType.MODELING_RULE):  # type: ignore[call-arg]
 
     def metadata_fields(self) -> Set[str]:
         return {"name", "description"}

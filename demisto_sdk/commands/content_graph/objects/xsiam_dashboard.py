0e89e27d5f102b9c2fa4d5845c6a7de3b35203f3
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-07 11:12:55 +0200

    merge

--- demisto_sdk/commands/content_graph/objects/xsiam_dashboard.py
+++ demisto_sdk/commands/content_graph/objects/xsiam_dashboard.py
@@ -1,7 +1,7 @@
 from typing import Set
 
 from demisto_sdk.commands.content_graph.common import ContentType
-from demisto_sdk.commands.content_graph.objects.content_item_x2 import ContentItemXSIAM
+from demisto_sdk.commands.content_graph.objects.content_item_xsiam import ContentItemXSIAM
 
 
 class XSIAMDashboard(ContentItemXSIAM, content_type=ContentType.XSIAM_DASHBOARD):  # type: ignore[call-arg]

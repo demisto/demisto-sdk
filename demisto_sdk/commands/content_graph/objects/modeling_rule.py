7efde38907faeb2b02e8e0e274c1c3c6bc87a408
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-08 13:12:25 +0200

    fixes

--- demisto_sdk/commands/content_graph/objects/modeling_rule.py
+++ demisto_sdk/commands/content_graph/objects/modeling_rule.py
@@ -2,11 +2,11 @@ from typing import Set
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
-from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
+from demisto_sdk.commands.content_graph.objects.content_item_xsiam import ContentItemXSIAM
 from demisto_sdk.commands.prepare_content.rule_unifier import RuleUnifier
 
 
-class ModelingRule(ContentItem, content_type=ContentType.MODELING_RULE):  # type: ignore[call-arg]
+class ModelingRule(ContentItemXSIAM, content_type=ContentType.MODELING_RULE):  # type: ignore[call-arg]
 
     def metadata_fields(self) -> Set[str]:
         return {"name", "description"}

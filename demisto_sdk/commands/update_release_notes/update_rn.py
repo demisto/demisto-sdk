4ea393a6105970ad2b23b29103c77ac37fd9a321
Arad Carmi <62752352+AradCarmi@users.noreply.github.com>
2022-12-08 16:58:27 +0200

    Update demisto_sdk/commands/update_release_notes/update_rn.py

--- demisto_sdk/commands/update_release_notes/update_rn.py
+++ demisto_sdk/commands/update_release_notes/update_rn.py
@@ -793,7 +793,7 @@ def get_file_description(path, file_type) -> str:
         print_warning(f'Cannot get file description: "{path}" file does not exist')
         return ''
 
-    elif file_type in (FileType.PLAYBOOK, FileType.INTEGRATION, FileType.CORRELATION_RULE):
+    elif file_type in (FileType.PLAYBOOK, FileType.INTEGRATION, FileType.CORRELATION_RULE, FileType.MODELING_RULE):
         yml_file = get_yaml(path)
         return yml_file.get('description', '')
 

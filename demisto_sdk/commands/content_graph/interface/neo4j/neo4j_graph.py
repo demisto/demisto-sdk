05f0ea7fd1d40113efe056f73c11d86cc919fe3b
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-19 22:30:31 +0200

    fix

--- demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
@@ -112,7 +112,7 @@ class Neo4jContentGraphInterface(ContentGraphInterface):
         for id, res in result.items():
             obj = Neo4jContentGraphInterface._id_to_obj[id]
             self._add_relationships(obj, res.relationships, res.nodes_to)
-            if isinstance(obj, Pack) and not list(obj.content_items):
+            if isinstance(obj, Pack) and not obj.content_items:
                 obj.set_content_items()  # type: ignore[union-attr]
                 content_item_nodes.update(
                     content_item.database_id for content_item in obj.content_items if content_item.database_id

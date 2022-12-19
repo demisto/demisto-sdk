1adc3c108b4e1094e172dd875d5772697085cacc
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-19 21:46:42 +0200

    without filter

--- demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
@@ -189,7 +189,8 @@ class Neo4jContentGraphInterface(ContentGraphInterface):
         Args:
             nodes (List[graph.Node]): list of nodes to add
         """
-        nodes = filter(lambda node: node.id not in self._id_to_obj, nodes)
+        nodes = [node for node in nodes if node.id not in self._id_to_obj]
+        # nodes = filter(lambda node: node.id not in self._id_to_obj, nodes)
         if not nodes:
             logger.debug("No nodes to parse packs because all of them in mapping", self._id_to_obj)
             return

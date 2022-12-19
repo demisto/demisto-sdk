291def68beca50f5e9cd3f8ebd610aa1411fe170
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-19 21:24:33 +0200

    fix nasty bug

--- demisto_sdk/commands/content_graph/objects/pack.py
+++ demisto_sdk/commands/content_graph/objects/pack.py
@@ -185,12 +185,12 @@ class Pack(BaseContent, PackMetadata, content_type=ContentType.PACK):  # type: i
         ]
         content_item_dct = defaultdict(list)
         for c in content_items:
-            content_item_dct[c.content_type].append(c)
+            content_item_dct[c.content_type.value].append(c)
 
         # If there is no server_min_version, set it to the maximum of its content items fromversion
         self.server_min_version = self.server_min_version or str(max(parse(content_item.fromversion) for content_item in content_items))
-        self.content_items = PackContentItems.parse_obj(content_item_dct)
-
+        self.content_items = PackContentItems(**content_item_dct)
+        
     def dump_metadata(self, path: Path, marketplace: MarketplaceVersions) -> None:
         metadata = self.dict(exclude={"path", "node_id", "content_type"})
         metadata["contentItems"] = {}

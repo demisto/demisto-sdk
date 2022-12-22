58c2a3e90b07d8e5ca0ad01c421044db4a4f666c
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-22 15:37:50 +0200

    fix

--- demisto_sdk/commands/content_graph/objects/generic_type.py
+++ demisto_sdk/commands/content_graph/objects/generic_type.py
@@ -1,8 +1,7 @@
 from typing import Optional, Set
 
-from pydantic import DirectoryPath, Field
+from pydantic import Field
 
-from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
 
@@ -12,7 +11,3 @@ class GenericType(ContentItem, content_type=ContentType.GENERIC_TYPE):  # type: 
 
     def metadata_fields(self) -> Set[str]:
         return {"name", "description"}
-
-    def dump(self, dir: DirectoryPath, marketplace: MarketplaceVersions) -> None:
-        dir = dir / self.path.parent.name
-        super().dump(dir, marketplace)

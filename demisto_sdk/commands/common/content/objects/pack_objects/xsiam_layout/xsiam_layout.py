a6dd990847e23b338ae9da785515226e0f9d448d
epintzov <epintzov@paloaltonetworks.com>
2022-12-22 11:51:09 +0200

    updated dump of xsiam layout

--- demisto_sdk/commands/common/content/objects/pack_objects/xsiam_layout/xsiam_layout.py
+++ demisto_sdk/commands/common/content/objects/pack_objects/xsiam_layout/xsiam_layout.py
@@ -47,29 +47,11 @@ class XSIAMLayout(JSONContentObject):
 
         new_file_path = created_files[0]
 
-        if Version(self.get('fromVersion', '0.0.0')) >= Version('6.10.0'):
-            # export XSIAM 1.3 items only with the external prefix
-            if not new_file_path.name.startswith('external-'):
-                move_to_path = new_file_path.parent / self.normalize_file_name()
-                shutil.move(new_file_path.as_posix(), move_to_path)
-                created_files.remove(new_file_path)
-                created_files.append(move_to_path)
-
-        elif Version(self.get('toVersion', '99.99.99')) < Version('6.10.0'):
-            # export XSIAM 1.2 items only without the external prefix
-            if new_file_path.name.startswith('external-'):
-                move_to_path = Path(str(new_file_path).replace('external-', ''))
-                shutil.move(new_file_path.as_posix(), move_to_path)
-                created_files.remove(new_file_path)
-                created_files.append(move_to_path)
-
-        else:
-            # export 2 versions of the file, with/without the external prefix.
-            if new_file_path.name.startswith('external-'):
-                copy_to_path = str(new_file_path).replace('external-', '')
-            else:
-                copy_to_path = f'{new_file_path.parent}/{self.normalize_file_name()}'
-
-            shutil.copyfile(new_file_path, copy_to_path)
+        # export XSIAM 1.3 items only with the external prefix
+        if not new_file_path.name.startswith('external-'):
+            move_to_path = new_file_path.parent / self.normalize_file_name()
+            shutil.move(new_file_path.as_posix(), move_to_path)
+            created_files.remove(new_file_path)
+            created_files.append(move_to_path)
 
         return created_files

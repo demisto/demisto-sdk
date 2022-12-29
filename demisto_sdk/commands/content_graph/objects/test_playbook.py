f957f5cda42bdbf2c0cee5c0aac2edd95d2ab2f5
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-29 10:17:04 +0200

    testplaybooks uploaded also

--- demisto_sdk/commands/content_graph/objects/test_playbook.py
+++ demisto_sdk/commands/content_graph/objects/test_playbook.py
@@ -6,6 +6,3 @@ from demisto_sdk.commands.content_graph.objects.playbook import Playbook
 
 class TestPlaybook(Playbook, content_type=ContentType.TEST_PLAYBOOK):  # type: ignore[call-arg]
     is_test: bool = True
-
-    def metadata_fields(self) -> Set[str]:
-        raise NotImplementedError("TestPlaybooks not included in metadata")

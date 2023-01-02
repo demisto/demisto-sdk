5587de796b5acde85a91f46d373e688c9ecd2b1d
epintzov <epintzov@paloaltonetworks.com>
2023-01-02 14:01:40 +0200

    fix

--- demisto_sdk/commands/content_graph/objects/layout_rule.py
+++ demisto_sdk/commands/content_graph/objects/layout_rule.py
@@ -8,4 +8,4 @@ from demisto_sdk.commands.content_graph.objects.content_item_xsiam import (
 
 class LayoutRule(ContentItemXSIAM, content_type=ContentType.LAYOUT_RULE):  # type: ignore[call-arg]
     def metadata_fields(self) -> Set[str]:
-        return {"name", "description"}
+        return {"layout_rule_name", "description"}

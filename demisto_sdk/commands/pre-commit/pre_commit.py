756ed56d9ad52b1fb2e5830910cd6491bb471dc0
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-08 15:27:37 +0200

    add template

--- demisto_sdk/commands/pre-commit/pre_commit.py
+++ demisto_sdk/commands/pre-commit/pre_commit.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Union
+from typing import List, Set, Union
 from demisto_sdk.commands.common.constants import INTEGRATIONS_DIR, SCRIPTS_DIR
 from demisto_sdk.commands.common.git_util import GitUtil
 from demisto_sdk.commands.content_graph.objects.base_content import BaseContent
@@ -8,16 +8,12 @@ from demisto_sdk.commands.content_graph.objects.integration_script import Integr
 
 @dataclass
 class PreCommit:
-    integrations_scripts_new: List[Path]
-    integrations_scripts_modified: List[Path]
-    other_files: List[Path]
-    
-    def __post_init__(self):
-        self.integrations_scripts_new = []
-        self.integrations_scripts_modified = []
-        self.other_files = []
-    
+    integrations_scripts_new: Set[Path]
+    integrations_scripts_modified: Set[Path]
+    other_files: Set[Path]
+        
     def run():
+        pass
 
 def main():
     # Get all changed integrations and scripts
@@ -25,11 +21,8 @@ def main():
     prev_ver = 'origin/master'
     added_files = git_util.added_files(prev_ver=prev_ver)
     modified_files = git_util.modified_files(prev_ver=prev_ver)
-    integrations_scripts_new = {file for file in added_files if INTEGRATIONS_DIR in file.parts or SCRIPTS_DIR in file.parts}
+    integrations_scripts_added = {file for file in added_files if INTEGRATIONS_DIR in file.parts or SCRIPTS_DIR in file.parts}
     integrations_scripts_modified = {file for file in modified_files if INTEGRATIONS_DIR in file.parts or SCRIPTS_DIR in file.parts}
-    other_files = [file for file in added_files + modified_files if file not in integrations_scripts_new | integrations_scripts_modified]
-    changed_file_paths = GitUtil()
-    content_items: List[IntegrationScript] = [BaseContent.from_path(path) for path in changed_file_paths]  # type: ignore[misc]
-    for content_item in content_items:
-        docker_image = content_item.docker_image
+    other_files = [file for file in added_files + modified_files if file not in integrations_scripts_added | integrations_scripts_modified]
+    PreCommit(integrations_scripts_added, integrations_scripts_modified, other_files).run()
         

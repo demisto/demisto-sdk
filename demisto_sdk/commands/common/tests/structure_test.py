bdf6661c769bff33459e82cc392cb86522d73d96
Dan Tavori <dtavori@paloaltonetworks.com>
2023-01-10 12:18:14 +0200

    wip

--- demisto_sdk/commands/common/tests/structure_test.py
+++ demisto_sdk/commands/common/tests/structure_test.py
@@ -701,13 +701,9 @@ def _get_dummy_xsiam_pack_items(valid: bool = True) -> List[str]:
 class TestXSIAMStructureValidator(TestStructureValidator):
     @staticmethod
     def _get_dummy_xsiam_pack_items(valid: bool = True) -> List[str]:
-        # patterns = [
-        #     f"{DUMMY_XSIAM_PACK_PATH}{'/*/valid_*' if valid else '/*/invalid_*'}",
-        #     f"{DUMMY_XSIAM_PACK_PATH}{'/*/valid_*/*' if valid else '/*/invalid_*/*'}",
-        # ]
-        # files = glob(patterns[0]) + glob(patterns[1])
         files = glob(
-            f"{DUMMY_XSIAM_PACK_PATH}{'/*/valid_*' if valid else '/*/invalid_*'}"
+            f"{DUMMY_XSIAM_PACK_PATH}/**/{'valid' if valid else 'invalid'}_*",
+            recursive=True,
         )
         res = [f for f in files if not os.path.isdir(f)]
         assert res  # make sure files exist

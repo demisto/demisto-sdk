fbf8c44c65f32a81c4ad1397ece6e7a624a14440
dorschw <81086590+dorschw@users.noreply.github.com>
2023-01-10 13:28:00 +0000

    black

--- lint2/parse_linter.py
+++ lint2/parse_linter.py
@@ -43,13 +43,13 @@ class RuffParser(BaseParser):
             raise ValueError(f"must be a dictionary, got {raw}")
 
         return ParseResult(
-            error_code=raw['code'],
-            row_start=raw['location']['row'],
-            col_start=raw['location']['column'],
-            row_end=raw['end_location']['row'],
-            col_end=raw['end_location']['column'],
-            path=Path(raw['filename']),
-            error_message=raw['message'],
+            error_code=raw["code"],
+            row_start=raw["location"]["row"],
+            col_start=raw["location"]["column"],
+            row_end=raw["end_location"]["row"],
+            col_end=raw["end_location"]["column"],
+            path=Path(raw["filename"]),
+            error_message=raw["message"],
         )
 
 
@@ -61,22 +61,22 @@ class Flake8Parser(BaseParser):
         if not isinstance(raw, dict):
             raise ValueError(f"must be a dictionary, got {raw}")
         return ParseResult(
-            error_code=raw['code'],
-            row_start=raw['line_number'],
-            error_message=raw['text'],
-            path=Path(raw['filename']),
+            error_code=raw["code"],
+            row_start=raw["line_number"],
+            error_message=raw["text"],
+            path=Path(raw["filename"]),
             error_type=None,
             row_end=None,
-            col_start=raw.get('column_number'),
-            col_end=None   
+            col_start=raw.get("column_number"),
+            col_end=None,
         )
-
 
 
 class MypyParser(BaseParser):
     linter_type = LinterType.MYPY
     _line_regex = re.compile(
-        r"^(?P<path>[^:]+?):(?P<row_start>\d+):(?P<col_start>\d+):(?P<row_end>\d+):(?P<col_end>\d+):(?P<error_type>[\w\s]+):(?P<error_message>[^\:\[]+)\[(?P<error_code>[^\]]+)]$")
+        r"^(?P<path>[^:]+?):(?P<row_start>\d+):(?P<col_start>\d+):(?P<row_end>\d+):(?P<col_end>\d+):(?P<error_type>[\w\s]+):(?P<error_message>[^\:\[]+)\[(?P<error_code>[^\]]+)]$"
+    )
 
     @staticmethod
     def parse_line(raw: Union[str, dict]) -> ParseResult:
@@ -88,21 +88,21 @@ class MypyParser(BaseParser):
 
         match_dict = match.groupdict()
 
-        raw_error_type = match_dict['error_type']
-        if 'error' in raw_error_type:
+        raw_error_type = match_dict["error_type"]
+        if "error" in raw_error_type:
             error_type = ErrorType.ERROR
-        elif 'warning' in raw_error_type:
+        elif "warning" in raw_error_type:
             error_type = ErrorType.WARNING
         else:
             error_type = None
-            
+
         return ParseResult(
-            error_code=match_dict['error_code'],
-            row_start=int(match_dict['row_start']),
-            row_end=int(match_dict['row_end']),
-            col_start=int(match_dict['col_start']),
-            col_end=int(match_dict['col_end']),
-            path=Path(match_dict['path']),
+            error_code=match_dict["error_code"],
+            row_start=int(match_dict["row_start"]),
+            row_end=int(match_dict["row_end"]),
+            col_start=int(match_dict["col_start"]),
+            col_end=int(match_dict["col_end"]),
+            path=Path(match_dict["path"]),
             error_type=error_type,
-            error_message=match_dict['error_message']
+            error_message=match_dict["error_message"],
         )

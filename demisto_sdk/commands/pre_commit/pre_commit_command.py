4890ce7208e6e3fbe472958f6587dbc76e219829
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-13 12:34:44 +0200

    fix doubles

--- demisto_sdk/commands/pre_commit/pre_commit_command.py
+++ demisto_sdk/commands/pre_commit/pre_commit_command.py
@@ -153,6 +153,7 @@ def pre_commit(
         files_to_run = staged_files | git_util._get_all_changed_files()
     elif all_files:
         files_to_run = git_util.get_all_files()
+    files_to_run = {file.relative_to(CONTENT_PATH) for file in files_to_run}
     return categorize_files(files_to_run).run(test, skip_hooks)
 
 

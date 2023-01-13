090bdae9521ba6a0194a985f19e51d44780ac60c
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-13 12:39:26 +0200

    try now

--- demisto_sdk/commands/pre_commit/runner.sh
+++ demisto_sdk/commands/pre_commit/runner.sh
@@ -2,4 +2,4 @@
 echo "Running tests..."
 set -e
 python -m pip install --no-cache-dir -q pytest pytest-mock requests-mock pytest-asyncio pytest-xdist pytest-datadir-ng freezegun pytest-json pytest-cov pytest-github-actions-annotate-failures
-pytest . -v --json=pytest_report.json
+pytest . -v

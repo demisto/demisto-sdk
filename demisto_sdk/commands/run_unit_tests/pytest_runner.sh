e6ea8af95531db4a5e5ea15f92be70f13999f391
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-14 12:26:33 +0200

    addrootdir

--- demisto_sdk/commands/run_unit_tests/pytest_runner.sh
+++ demisto_sdk/commands/run_unit_tests/pytest_runner.sh
@@ -4,4 +4,4 @@ set -e
 echo "Running tests..."
 
 python -m pip install --no-cache-dir -q pytest pytest-mock requests-mock pytest-asyncio pytest-xdist pytest-datadir-ng freezegun pytest-cov 
-pytest . -v --junitxml=.report_pytest.xml --cov-report= --cov=.
+pytest . -v --rootdir=/content --junitxml=.report_pytest.xml --cov-report= --cov=. 

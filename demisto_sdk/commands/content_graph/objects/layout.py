6d3dbb8bb751ee2d59c842b43cb19308a14a268d
dorschw <81086590+dorschw@users.noreply.github.com>
2023-01-17 10:52:00 +0000

    initial attempt

--- demisto_sdk/commands/content_graph/objects/layout.py
+++ demisto_sdk/commands/content_graph/objects/layout.py
@@ -1,3 +1,5 @@
+import re
+from logging import getLogger
 from typing import List, Optional, Set
 
 from pydantic import Field
@@ -5,6 +7,9 @@ from pydantic import Field
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
+
+logger = getLogger("demisto-sdk")
+incident_regex = re.compile("\bincident[\bs\\s]", flags=re.IGNORECASE)
 
 
 class Layout(ContentItem, content_type=ContentType.LAYOUT):  # type: ignore[call-arg]
@@ -30,4 +35,39 @@ class Layout(ContentItem, content_type=ContentType.LAYOUT):  # type: ignore[call
         data = super().prepare_for_upload(marketplace, **kwargs)
         data["fromServerVersion"] = self.fromversion
         data["toServerVersion"] = self.toversion
+
+        if marketplace == MarketplaceVersions.MarketplaceV2:
+            # replaces the word `Incident` with `Alert`, in all places where and _x2 field was not manually specified
+            data = replace_incidents_alerts(data)
+
         return data
+
+
+def replace_incidents_alerts(data: dict) -> dict:
+    """
+    XSOAR Incidents are XSIAM Alerts. This replaces them recursively, matching the original case (lower, UPPER, Title)
+    """
+
+    def _replace_string(string: str) -> str:
+        if not incident_regex.match(string):
+            # the word `incident` doesn't appear at all, in any casese
+            return string
+
+        for incident_case, alert_case in {
+            "incident": "alert",
+            "Incident": "Alert",
+            "INCIDENT": "ALERT",
+        }.items():
+            if incident_case in string:
+                string = string.replace(incident_case, alert_case)
+        return string
+
+    for key, value in data.items():
+        if isinstance(value, dict):
+            data[key] = replace_incidents_alerts(value)
+        elif isinstance(value, list):
+            data[key] = list(map(replace_incidents_alerts, value))
+        elif isinstance(value, str):
+            data[key] = _replace_string(value)
+
+    return data

feff0e721dea326bc545002fc74010de9620946f
Dan Tavori <dtavori@paloaltonetworks.com>
2023-02-03 21:17:44 +0200

    ba115 fix

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -2015,9 +2015,12 @@ class ValidateManager:
                     for file in added_files:
                         file = str(file)
                         file_type = find_type(file)
-                        file_dict = get_file(file, file_type.value)
-                        if deleted_file_id == _get_file_id(file_type.value, file_dict):
-                            return True
+                        if file_type == deleted_file_type:
+                            file_dict = get_file(file, "yml")
+                            if deleted_file_id == _get_file_id(
+                                file_type.value, file_dict
+                            ):
+                                return True
         return False
 
     @error_codes("BA115")

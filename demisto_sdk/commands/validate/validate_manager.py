b017d94b9247f1e57c55dcc301691941a0c8f100
Dan Tavori <dtavori@paloaltonetworks.com>
2023-02-05 12:11:55 +0200

    cr

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -2016,7 +2016,8 @@ class ValidateManager:
                         file = str(file)
                         file_type = find_type(file)
                         if file_type == deleted_file_type:
-                            file_dict = get_file(file, "yml")
+                            file_suffix = Path(deleted_file_path).suffix
+                            file_dict = get_file(file, file_suffix)
                             if deleted_file_id == _get_file_id(
                                 file_type.value, file_dict
                             ):

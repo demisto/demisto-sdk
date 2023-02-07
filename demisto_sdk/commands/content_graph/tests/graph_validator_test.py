088baae706c5395afa10136865418906d8833f6e
Dan Tavori <dtavori@paloaltonetworks.com>
2023-02-07 16:44:38 +0200

    graph validator testfix

--- demisto_sdk/commands/content_graph/tests/graph_validator_test.py
+++ demisto_sdk/commands/content_graph/tests/graph_validator_test.py
@@ -481,10 +481,8 @@ def test_is_file_display_name_already_exists(repository: ContentDTO, capsys):
 
     captured = capsys.readouterr().out
     assert not is_valid
-    assert (
-        "Pack 'SamplePack' has a duplicate display_name as: SamplePack2, SamplePack3"
-        in captured
-    )
+    for i in range(1, 4):
+        assert f"Pack 'SamplePack{i}' has a duplicate display_name" in captured
 
 
 def test_are_marketplaces_relationships_paths_valid(

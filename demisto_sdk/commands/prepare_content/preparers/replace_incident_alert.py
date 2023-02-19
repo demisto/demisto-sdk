4e920086f95201497730a88ea35cd1450ef681a1
dorschw <81086590+dorschw@users.noreply.github.com>
2023-02-19 11:24:40 +0200

    docstrings

--- demisto_sdk/commands/prepare_content/preparers/replace_incident_alert.py
+++ demisto_sdk/commands/prepare_content/preparers/replace_incident_alert.py
@@ -11,12 +11,21 @@ def change_incident_to_alert(data: dict) -> dict:
         raise TypeError(f"expected dictionary, got {type(data)}")
 
     def is_replaceable_by_type(type_: Optional[str]):
+        """
+        Whether the item has a type value that should be replaced
+        """
         return type_ in {"childInv", "relatedIncidents", "linkedIncidents"}
 
     def is_replaceable_name(name: str):
+        """
+        Whether the name is replacable - only a few are.
+        """
         return name in {"Child Incidents", "Linked Incidents", "Related Incidents"}
 
     def is_replaceable_in_layoutscontainer(datum_: dict):
+        """
+        Whether the value is an internal part of a layoutcontainer, that should be replaced
+        """
         return {"i", "x", "y", "h", "w"}.issubset(datum_.keys())
 
     def fix_recursively(datum: Union[list, dict]) -> Union[list, dict]:
@@ -24,10 +33,10 @@ def change_incident_to_alert(data: dict) -> dict:
             if datum.get("name_x2") is not None:
                 # already has a xsiam name, then we have nothing to do
                 return datum
-            if (
+            if is_replaceable_name(name := datum.get("name", "")) and (
                 is_replaceable_by_type(datum.get("type"))
                 or is_replaceable_in_layoutscontainer(datum)
-            ) and is_replaceable_name(name := datum.get("name", "")):
+            ):
 
                 datum["name"] = name.replace("Incidents", "Alerts")
                 return datum

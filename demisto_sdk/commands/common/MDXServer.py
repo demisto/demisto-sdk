fd03b5cb66791a27fc6ea2c3dec501746744f519
Judah Schwartz <juschwartz@paloaltonetworks.com>
2023-02-19 09:23:37 +0200

    log dont print

--- demisto_sdk/commands/common/MDXServer.py
+++ demisto_sdk/commands/common/MDXServer.py
@@ -138,7 +138,7 @@ def start_local_MDX_server(
     """
     click.secho("Starting local mdx server")
 
-    click.secho(subprocess.check_output(["npm", "list", "--json"]))
+    logging.info(subprocess.check_output(["npm", "list", "--json"]))
     process = subprocess.Popen(
         ["node", str(server_script_path())], stdout=subprocess.PIPE, text=True
     )

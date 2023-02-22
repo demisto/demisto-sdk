0d151bdf232c9cee4060ad54050ae1ee5a570bb0
tkatzir <tkatzir@paloaltonetworks.com>
2023-02-22 11:33:43 +0200

    Fix test

--- demisto_sdk/commands/format/update_integration.py
+++ demisto_sdk/commands/format/update_integration.py
@@ -14,6 +14,7 @@ from demisto_sdk.commands.common.constants import (
     ParameterType,
 )
 from demisto_sdk.commands.common.handlers import JSON_Handler
+from demisto_sdk.commands.common.logger import secho_and_info
 from demisto_sdk.commands.common.tools import find_type, get_item_marketplaces, get_json
 from demisto_sdk.commands.format.format_constants import (
     ERROR_RETURN_CODE,
@@ -144,7 +145,7 @@ class IntegrationYMLFormat(BaseUpdateYML):
                     if name == command_name:
                         is_array = argument.get("isArray", False)
                         if not is_array:
-                            logger.info(
+                            secho_and_info(
                                 f"isArray field in {name} command is set to False. Fix the command to support that function and set it to True."
                             )
                         argument.update(
@@ -160,7 +161,7 @@ class IntegrationYMLFormat(BaseUpdateYML):
                         "required": True,
                         "secret": False,
                     }
-                    logger.info(
+                    secho_and_info(
                         f"Command {command_name} has no arguemnts. Setting them: {json.dumps(default_bang_args, indent=4)}"
                     )
                     argument_list: list = command.get("arguments", [])
@@ -250,7 +251,7 @@ class IntegrationYMLFormat(BaseUpdateYML):
 
     def run_format(self) -> int:
         try:
-            logger.info(
+            secho_and_info(
                 f"\n[blue]================= Updating file {self.source_file} =================[/blue]"
             )
             super().update_yml(

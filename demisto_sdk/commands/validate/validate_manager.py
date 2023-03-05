6cccff67382c8cc08518804bf54d5b52e773f98a
tkatzir <tkatzir@paloaltonetworks.com>
2023-03-05 15:47:00 +0200

    Remove debug messages

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -1314,18 +1314,8 @@ class ValidateManager:
                     modified_files, old_format_files, added_files
                 )
             )
-            logger.info(f"*** after adding validate_no_missing_release_notes")
 
-        logger.info(f"*** Before all(validation_results)")
-        for current_validation_results in validation_results:
-            logger.info(
-                "*** str(current_validation_results): "
-                + str(current_validation_results)
-            )
-            logger.info(f"*** {current_validation_results=}")
-        ret_value = all(validation_results)
-        logger.info(f"*** After all(validation_results)")
-        return ret_value
+        return all(validation_results)
 
     """ ######################################## Unique Validations ####################################### """
 

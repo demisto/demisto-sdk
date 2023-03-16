f7e7d70873ce316f1dfad4324140af103d61202c
israelpolishook <ipolishuk@paloaltonetworks.com>
2023-03-16 18:13:47 +0200

    first commit

--- lint2/diff.py
+++ lint2/diff.py
@@ -2,21 +2,22 @@ import os
 import re
 from pathlib import Path
 from typing import Dict, Optional, Set, Tuple
+from itertools import chain
 
 from parse_linter import LinterError
 
+ROW_DIFF_REGEX = re.compile(r"\+\+\+ ./(.*?)\n@@ -\d,\d \+(\d,\d+) @@")
 SINGLE_LINE_REGEX = re.compile(r"\d+")
 MULTI_LINE_REGEX = re.compile(r"(?P<start>\d+),(?P<end>\d+)")
 
 
-def _parse_changed_files(raw_diff: str) -> Dict[Path, Set[int]]:
+def _parse_changed_files(raw_diff: list) -> Dict[Path, Set[int]]:
     result: Dict[Path, Set[int]] = {}
 
     current_path: Optional[Path] = None
     current_changed_lines: Set[int] = set()
-    lines = raw_diff.splitlines()
 
-    for row_ix, row in enumerate(lines):
+    for row_ix, row in enumerate(raw_diff):
         if SINGLE_LINE_REGEX.fullmatch(row):
             current_changed_lines.update({int(row)})
         elif match := MULTI_LINE_REGEX.fullmatch(row):
@@ -34,8 +35,10 @@ def _parse_changed_files(raw_diff: str) -> Dict[Path, Set[int]]:
 
                 current_path = new_path
                 current_changed_lines = set()
+            else:
+                current_path = new_path
 
-        if row_ix == len(lines) - 1:  # last line
+        if row_ix == len(raw_diff) - 1:  # last line
             if not current_path:
                 raise RuntimeError(
                     "Reached end of a non-blank diff file without parsing anything"
@@ -47,9 +50,10 @@ def _parse_changed_files(raw_diff: str) -> Dict[Path, Set[int]]:
 
 def get_diff() -> Dict[Path, Set[int]]:
     raw: str = os.popen(
-        r"git diff master...HEAD --unified=0 | grep -Po '^\+\+\+ ./\K.*|^@@ -[0-9]+(,[0-9]+)? \+\K[0-9]+(,[0-9]+)?(?= @@)'"
-    )
-    return _parse_changed_files(raw)
+        r"git diff master...HEAD --unified=0"
+    ).read()
+    raw_diff = list(chain.from_iterable(ROW_DIFF_REGEX.findall(raw, re.IGNORECASE | re.MULTILINE)))
+    return _parse_changed_files(raw_diff)
 
 def filter_errors(errors: Tuple[LinterError, ...]):
     # return errors that occur in lines showing in the diff

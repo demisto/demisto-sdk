96dc1b74c5376a7d6aba0797ee6326c4d2e53a6b
dorschw <81086590+dorschw@users.noreply.github.com>
2023-03-23 16:06:55 +0200

    move files

--- demisto_sdk/commands/content_graph/objects/layout.py
+++ demisto_sdk/commands/content_graph/objects/layout.py
@@ -1,13 +1,10 @@
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Union
 
 from pydantic import Field
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
-from demisto_sdk.commands.prepare_content.preparers.replace_layout_incident_alert import (
-    replace_layout_incident_alert,
-)
 
 
 class Layout(ContentItem, content_type=ContentType.LAYOUT):  # type: ignore[call-arg]
@@ -47,3 +44,43 @@ class Layout(ContentItem, content_type=ContentType.LAYOUT):  # type: ignore[call
         data["fromServerVersion"] = self.fromversion
         data["toServerVersion"] = self.toversion
         return data
+
+
+def replace_layout_incident_alert(data: dict) -> dict:
+    """
+    Changes {"name": "Related/Linked/Chiled Incidents", ... }
+         to {"name": "Related/Linked/Chiled Alerts", ... }
+    """
+
+    if not isinstance(data, dict):
+        raise TypeError(f"expected dictionary, got {type(data)}")
+
+    def fix_recursively(datum: Union[list, dict]) -> Union[list, dict]:
+        if isinstance(datum, dict):
+            if datum.get("name_x2") is not None:
+                # already has a xsiam name, then we have nothing to do
+                return datum
+            if (name := datum.get("name", ""), datum.get("type")) in {
+                ("Child Incidents", "childInv"),
+                ("Linked Incidents", "linkedIncidents"),
+                ("Related Incidents", "relatedIncidents"),
+            }:
+                datum["name"] = name.replace("Incident", "Alert")
+                return datum
+            else:  # not the atomic dictionary that we intend to fix, use recursion instead.
+                return {key: fix_recursively(value) for key, value in datum.items()}
+
+        elif isinstance(datum, list):
+            return [fix_recursively(item) for item in datum]
+
+        else:
+            return datum  # nothing to change
+
+    if not isinstance(result := fix_recursively(data), dict):
+        """
+        the inner function returns a value of the same type as its input,
+        so a dict input should never return a non-dict. this part is just for safety (mypy).
+        """
+        raise ValueError(f"unexpected type for a fixed-dictionary output {type(data)}")
+
+    return result

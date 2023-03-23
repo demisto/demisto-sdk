4abc584cf20719a5d24176aaa4829e898b887fe9
ilan <ierukhimovic@paloaltonetworks.com>
2023-03-23 18:03:51 +0200

    filepaths

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -3314,13 +3314,23 @@ def pre_commit(
 
 @main.command(short_help="Run unit tests in a docker for integrations and scripts")
 @click.help_option("-h", "--help")
+@click.option(
+    "-i",
+    "--input",
+    type=PathsParamType(
+        exists=True, resolve_path=True
+    ),  # PathsParamType allows passing a list of paths
+    help="The path of the content pack/file to validate specifically.",
+)
 @click.option("-v", "--verbose", is_flag=True,  default=False, help="Verbose output of unit tests")
 @click.argument("file_paths", nargs=-1, type=click.Path(exists=True, resolve_path=True))
 @click.pass_context
 @logging_setup_decorator
-def run_unit_tests(file_paths: Tuple[str, ...]: str, verbose: bool):
+def run_unit_tests(input: str, file_paths: Tuple[str, ...], verbose: bool):
+    if input:
+        file_paths = tuple(input.split(","))
     from demisto_sdk.commands.run_unit_tests.unit_tests_runner import unit_test_runner
-    sys.exit(unit_test_runner(file_paths, verbose)
+    sys.exit(unit_test_runner(file_paths, verbose))
 
 
 app = typer.Typer(name="modeling-rules", hidden=True, no_args_is_help=True)

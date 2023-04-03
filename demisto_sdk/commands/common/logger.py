6eedaf35ba1c8f51212227ff1e9301cf7e30d247
tkatzir <tkatzir@paloaltonetworks.com>
2023-04-03 13:59:54 +0300

    Initialize logger

--- demisto_sdk/commands/common/logger.py
+++ demisto_sdk/commands/common/logger.py
@@ -3,10 +3,11 @@ import logging.config
 import os.path
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
+from typing import Optional
 
 from demisto_sdk.commands.common.content_constant_paths import CONTENT_PATH
 
-logger: logging.Logger
+logger: Optional[logging.Logger] = None
 
 LOG_FILE_NAME: str = "demisto_sdk_debug.log"
 

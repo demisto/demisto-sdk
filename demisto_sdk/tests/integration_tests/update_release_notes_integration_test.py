cd9d7b9ed280fb25d8bbb5ec06f8ba7dcfb32af9
dorschw <81086590+dorschw@users.noreply.github.com>
2023-04-09 22:35:56 +0200

    add `RUF100` & fixes

--- demisto_sdk/tests/integration_tests/update_release_notes_integration_test.py
+++ demisto_sdk/tests/integration_tests/update_release_notes_integration_test.py
@@ -6,7 +6,6 @@ from pathlib import Path
 import pytest
 from click.testing import CliRunner
 
-import conftest  # noqa: F401
 from demisto_sdk.__main__ import main
 from demisto_sdk.commands.common.git_util import GitUtil
 from demisto_sdk.commands.common.legacy_git_tools import git_path

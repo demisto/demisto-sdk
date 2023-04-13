6f498ba8d3fcf4265da0ee0240669449fddcec8e
rshunim <rshunim@paloaltonetworks.com>
2023-04-13 12:28:32 +0300

    pre-commit

--- demisto_sdk/commands/lint/helpers.py
+++ demisto_sdk/commands/lint/helpers.py
@@ -293,7 +293,9 @@ def add_tmp_lint_files(
                         shutil.copy(src=module_path, dst=copied_api_module_path)
                     else:
                         url = f"https://raw.githubusercontent.com/demisto/content/master/{api_code_path}"
-                        api_content = get_remote_file(full_file_path=url, return_content=True)
+                        api_content = get_remote_file(
+                            full_file_path=url, return_content=True
+                        )
                         copied_api_module_path.write_bytes(api_content)
 
                 added_modules.append(copied_api_module_path)

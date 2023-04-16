884bbd5feedab4d8a07d427255cc115b1c8e6a78
ilan <ierukhimovic@paloaltonetworks.com>
2023-04-16 09:42:10 +0200

    log the output file

--- demisto_sdk/commands/prepare_content/prepare_upload_manager.py
+++ demisto_sdk/commands/prepare_content/prepare_upload_manager.py
@@ -1,7 +1,7 @@
 import shutil
 from pathlib import Path
 from typing import Optional, Union
-
+import logging
 from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.interface.neo4j.neo4j_graph import (
     Neo4jContentGraphInterface,
@@ -10,7 +10,7 @@ from demisto_sdk.commands.content_graph.objects.base_content import BaseContent
 from demisto_sdk.commands.content_graph.objects.content_item import ContentItem
 from demisto_sdk.commands.content_graph.objects.pack import Pack
 
-
+logger = logging.getLogger("demisto-sdk")
 class PrepareUploadManager:
     @staticmethod
     def prepare_for_upload(
@@ -67,5 +67,6 @@ class PrepareUploadManager:
             )
         with output.open("w") as f:
             content_item.handler.dump(data, f)
-
+            
+        logger.info(f"[green]Output saved in: {str(output.absolute())}[/green]")
         return output

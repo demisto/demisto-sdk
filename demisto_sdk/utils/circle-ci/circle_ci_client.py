485aa6af3356d052a7facb00c75ea027eac8af37
rshunim <rshunim@paloaltonetworks.com>
2023-04-17 17:12:41 +0300

    revert 2

--- demisto_sdk/utils/circle-ci/circle_ci_client.py
+++ demisto_sdk/utils/circle-ci/circle_ci_client.py
@@ -103,7 +103,7 @@ class CircleCIClient:
         base_url: Optional[str] = None,
         verify: bool = True,
     ):
-        self.auth = HTTPBasicAuth(username=token, password="")
+        self.auth = HTTPBasicAuth(username=token or os.getenv("CCI_TOKEN"), password="")
         self.base_url = base_url or API_BASE_URL
         self.verify = verify
 

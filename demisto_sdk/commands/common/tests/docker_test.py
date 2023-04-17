de657d90fd67b8f8bb863e398516ed84cc5853d8
tkatzir <tkatzir@paloaltonetworks.com>
2023-04-17 14:24:52 +0300

    From print to logger

--- demisto_sdk/commands/common/tests/docker_test.py
+++ demisto_sdk/commands/common/tests/docker_test.py
@@ -673,7 +673,7 @@ class TestDockerImage:
                 ],
             )
             docker_image_validator = mock_docker_image_validator()
-            print(docker_image_validator.is_docker_image_deprecated("demisto/aiohttp"))
+            print(docker_image_validator.is_docker_image_deprecated("demisto/aiohttp"))  # noqa: T201
             assert (
                 "demisto/aiohttp",
                 "Use the demisto/py3-tools docker image instead.",

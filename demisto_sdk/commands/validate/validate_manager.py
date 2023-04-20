66fc2c5de1f8da1b645fb8185b201754e3819874
epintzov <epintzov@paloaltonetworks.com>
2023-04-20 14:56:18 +0300

    removed unnecessary logging

--- demisto_sdk/commands/validate/validate_manager.py
+++ demisto_sdk/commands/validate/validate_manager.py
@@ -1901,7 +1901,6 @@ class ValidateManager:
             self.id_set_validations
         )
         if pack_errors:
-            logger.info(f"[red]{pack_errors}[/red]")
             files_valid = False
 
         # check author image

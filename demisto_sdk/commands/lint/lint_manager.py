ba8133edde7bb986a4f8860f53e9c216dd5a3b93
Judah Schwartz <juschwartz@paloaltonetworks.com>
2023-04-23 14:19:34 -0400

    docker exception caught

--- demisto_sdk/commands/lint/lint_manager.py
+++ demisto_sdk/commands/lint/lint_manager.py
@@ -15,6 +15,7 @@ import urllib3.exceptions
 from packaging.version import Version
 from wcmatch.pathlib import Path, PosixPath
 
+import demisto_sdk
 from demisto_sdk.commands.common.constants import (
     API_MODULES_PACK,
     PACKS_PACK_META_FILE_NAME,
@@ -266,6 +267,7 @@ class LintManager:
             docker_client.ping()
         except (
             docker.errors.DockerException,
+            demisto_sdk.commands.common.docker_helper.DockerException,
             requests.exceptions.ConnectionError,
             urllib3.exceptions.ProtocolError,
             docker.errors.APIError,

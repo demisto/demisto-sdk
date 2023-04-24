34d0e4cb2812b6338384e4263d9029e2f392e7ab
Shachar Kidor <82749224+ShacharKidor@users.noreply.github.com>
2023-04-24 12:41:02 +0300

    Update demisto_sdk/commands/init/tests/contribution_converter_test.py
    Co-authored-by: ShirleyDenkberg <62508050+ShirleyDenkberg@users.noreply.github.com>

--- demisto_sdk/commands/init/tests/contribution_converter_test.py
+++ demisto_sdk/commands/init/tests/contribution_converter_test.py
@@ -735,7 +735,7 @@ def test_create_contribution_items_version_note():
 > In case you are requested by your reviewer to improve the code or to make changes, submit them through the **GitHub Codespaces** and **Not through the XSOAR UI**.
 >
 > **To use the GitHub Codespaces, do the following:**
-> 1. Click on the **'Code'** button in the right upper corner of this PR.
+> 1. Click the **'Code'** button in the right upper corner of this PR.
 > 2. Click **'Create codespace on Transformers'**.
 > 3. Click **'Authorize and continue'**.
 > 4. Wait until your Codespace environment is generated, once it is, you can edit your code.

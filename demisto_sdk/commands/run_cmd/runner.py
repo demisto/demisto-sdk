d9b1435b7d0bbf9de3e5428e5633886ac831b66f
eli sharf <57587340+esharf@users.noreply.github.com>
2023-04-30 13:54:07 +0300

    Added paging to playground search in run command (#2951)

--- demisto_sdk/commands/run_cmd/runner.py
+++ demisto_sdk/commands/run_cmd/runner.py
@@ -106,12 +106,16 @@ class Runner:
 
     def _get_playground_id(self):
         """Retrieves Playground ID from the remote Demisto instance."""
-        playground_filter = {"filter": {"type": [9]}}
-        answer = self.client.search_investigations(filter=playground_filter)
+
+        def playground_filter(page: int = 0):
+            return {"filter": {"type": [9], "page": page}}
+
+        answer = self.client.search_investigations(filter=playground_filter())
         if answer.total == 0:
             raise RuntimeError("No playgrounds were detected in the environment.")
-        playgrounds = answer.data
-        if len(playgrounds) > 1:
+        elif answer.total == 1:
+            result = answer.data[0].id
+        else:
             # if found more than one playground, try to filter to results against the current user
             user_data, response, _ = self.client.generic_request(
                 path="/user",
@@ -122,18 +126,28 @@ class Runner:
             if response != 200:
                 raise RuntimeError("Cannot find username")
             username = user_data.get("username")
-            playgrounds = [
-                playground
-                for playground in playgrounds
-                if playground.creating_user_id == username
-            ]
+
+            def filter_by_creating_user_id(playground):
+                return playground.creating_user_id == username
+
+            playgrounds = list(filter(filter_by_creating_user_id, answer.data))
+
+            for i in range(int((answer.total - 1) / len(answer.data))):
+                playgrounds.extend(
+                    filter(
+                        filter_by_creating_user_id,
+                        self.client.search_investigations(
+                            filter=playground_filter(i + 1)
+                        ).data,
+                    )
+                )
+
             if len(playgrounds) != 1:
                 raise RuntimeError(
                     f"There is more than one playground to the user. "
                     f"Number of playgrounds is: {len(playgrounds)}"
                 )
-
-        result = playgrounds[0].id
+            result = playgrounds[0].id
 
         logger.debug(f"Playground ID: {result}")
 

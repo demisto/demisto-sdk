55f0860065957637afc29c5c548ea0b9e393e16e
esharf <57587340+esharf@users.noreply.github.com>
2023-04-30 10:59:06 +0300

    added paging to playground search in run command

--- demisto_sdk/commands/run_cmd/runner.py
+++ demisto_sdk/commands/run_cmd/runner.py
@@ -97,7 +97,8 @@ class Runner:
                         f.seek(0)
                         file_path = f.name
                         command = self.query.split(" ")[0]
-                        json_to_outputs(command, json=file_path, prefix=self.prefix)
+                        json_to_outputs(command, json=file_path,
+                                        prefix=self.prefix)
                 else:
                     logger.info(
                         "[red]Could not extract raw output as JSON from command[/red]"
@@ -106,12 +107,16 @@ class Runner:
 
     def _get_playground_id(self):
         """Retrieves Playground ID from the remote Demisto instance."""
-        playground_filter = {"filter": {"type": [9]}}
-        answer = self.client.search_investigations(filter=playground_filter)
+        def playground_filter(page: int = 0):
+            return {"filter": {"type": [9], "page": page}}
+
+        answer = self.client.search_investigations(filter=playground_filter())
         if answer.total == 0:
-            raise RuntimeError("No playgrounds were detected in the environment.")
-        playgrounds = answer.data
-        if len(playgrounds) > 1:
+            raise RuntimeError(
+                "No playgrounds were detected in the environment.")
+        elif answer.total == 1:
+            result = answer.data[0].id
+        else:
             # if found more than one playground, try to filter to results against the current user
             user_data, response, _ = self.client.generic_request(
                 path="/user",
@@ -122,18 +127,25 @@ class Runner:
             if response != 200:
                 raise RuntimeError("Cannot find username")
             username = user_data.get("username")
-            playgrounds = [
-                playground
-                for playground in playgrounds
-                if playground.creating_user_id == username
-            ]
+
+            def filter_by_creating_user_id(playground):
+                return playground.creating_user_id == username
+
+            playgrounds = list(filter(filter_by_creating_user_id, answer.data))
+
+            for i in range(int((answer.total - 1) / len(answer.data))):
+                playgrounds.extend(filter(
+                    filter_by_creating_user_id,
+                    self.client.search_investigations(
+                        filter=playground_filter(i + 1)).data
+                ))
+
             if len(playgrounds) != 1:
                 raise RuntimeError(
                     f"There is more than one playground to the user. "
                     f"Number of playgrounds is: {len(playgrounds)}"
                 )
-
-        result = playgrounds[0].id
+            result = playgrounds[0].id
 
         logger.debug(f"Playground ID: {result}")
 
@@ -150,7 +162,8 @@ class Runner:
         """
         update_entry = {"investigationId": playground_id, "data": self.query}
 
-        answer = self.client.investigation_add_entries_sync(update_entry=update_entry)
+        answer = self.client.investigation_add_entries_sync(
+            update_entry=update_entry)
         if not answer:
             raise DemistoRunTimeError(
                 "Command did not run, make sure it was written correctly."
@@ -200,7 +213,8 @@ class Runner:
                         if self.SECTIONS_HEADER_REGEX.match(line):
                             logger.info(f"[yellow]{line}[/yello]")
                         elif self.FULL_LOG_REGEX.match(line):
-                            logger.info("[yellow]Full Integration Log:[/yellow]")
+                            logger.info(
+                                "[yellow]Full Integration Log:[/yellow]")
                         else:
                             logger.info(line)
 
@@ -287,7 +301,8 @@ class Runner:
 
         # execute the command in playground
         update_entry = {"investigationId": playground_id, "data": command}
-        res = self.client.investigation_add_entries_sync(update_entry=update_entry)
+        res = self.client.investigation_add_entries_sync(
+            update_entry=update_entry)
 
         body = {"query": "${.}"}
         context = self.client.generic_request(

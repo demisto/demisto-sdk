20d617a4cd6b6b9c78f45c28260539d89b41828b
dorschw <81086590+dorschw@users.noreply.github.com>
2023-05-02 13:37:20 +0300

    improve cpu count flow & logging

--- demisto_sdk/commands/common/cpu_count.py
+++ demisto_sdk/commands/common/cpu_count.py
@@ -1,4 +1,3 @@
-import contextlib
 import logging
 import os
 
@@ -11,10 +10,15 @@ DEMISTO_SDK_MAX_CPU_CORES = "DEMISTO_SDK_MAX_CPU_CORES"
 
 
 def cpu_count() -> int:
-    max_available_cpus = os.cpu_count() or 1
-    with contextlib.suppress(TypeError, ValueError, KeyError):
-        max_allowed_cpus = int(os.environ[DEMISTO_SDK_MAX_CPU_CORES])
-        requsted_cpus = min(max_allowed_cpus, max_available_cpus)
-        return max(1, requsted_cpus)
-    logger.debug(f"cpu_count={max_allowed_cpus}")
-    return max_available_cpus
+    result = max_available_cpus = os.cpu_count() or 1
+    if raw_env_var := os.getenv(DEMISTO_SDK_MAX_CPU_CORES) is not None:
+        try:
+            max_allowed_cpus = int(raw_env_var)
+            requsted_cpus = min(max_allowed_cpus, max_available_cpus)
+            result = max(1, requsted_cpus)
+        except (TypeError, ValueError):
+            logger.exception(
+                f"failed converting DEMISTO_SDK_MAX_CPU_CORES={raw_env_var} to integer, using default cpu count instead"
+            )
+    logger.debug(f"cpu_count={result}")
+    return result

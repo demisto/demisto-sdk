cf28a120a1a2aa279d9dd3b394da03c200c1962b
ilan <ierukhimovic@paloaltonetworks.com>
2023-05-03 12:24:21 +0300

    test

--- demisto_sdk/commands/run_unit_tests/unit_tests_runner.py
+++ demisto_sdk/commands/run_unit_tests/unit_tests_runner.py
@@ -117,6 +117,7 @@ def unit_test_runner(file_paths: List[Path], verbose: bool = False) -> int:
                     ],
                     command="sh test_runner.sh",
                     working_dir=working_dir,
+                    user=os.getuid(),
                     detach=True,
                 )
                 logger.debug(f"Running test in container {container.id}")

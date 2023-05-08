41c34108ced307cb2ae4c816b87e1eeaa530a994
ilan <ierukhimovic@paloaltonetworks.com>
2023-05-08 18:39:34 +0300

    fix

--- demisto_sdk/commands/run_unit_tests/pytest_runner.sh
+++ demisto_sdk/commands/run_unit_tests/pytest_runner.sh
@@ -4,4 +4,4 @@ set -e
 echo "Running tests..."
 
 python -m pip install --no-cache-dir -q pytest pytest-mock requests-mock pytest-asyncio pytest-xdist pytest-datadir-ng freezegun pytest-cov pytest-pretty
-python -m pytest . -v --rootdir=/content --junitxml=.report_pytest.xml --cov-report= --cov=.
+python -m pytest . -n auto -v --rootdir=/content --junitxml=.report_pytest.xml --cov-report= --cov=.

1cbcd480bbb2fae5c34337adc61d8217ca8786be
shmuel44 <skroizer@paloaltonetworks.com>
2023-05-09 17:00:02 +0300

    Added hook

--- demisto_sdk/commands/pre_commit/hooks/hook.py
+++ demisto_sdk/commands/pre_commit/hooks/hook.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import List
+from demisto_sdk.commands.common.content_constant_paths import CONTENT_PATH
+from poetry.core.factory import Factory
 
 
 class Hook(ABC):
@@ -8,3 +11,10 @@ class Hook(ABC):
     @abstractmethod
     def prepare_hook(self, **kwargs):
         return self.hook
+
+    def additional_dependencies(self, group_name: str) -> List[str]:
+        factory = Factory()
+        poetry = factory.create_poetry(CONTENT_PATH)
+        pkg = poetry.package
+
+        return [p.to_pep_508() for p in pkg.dependency_group(group_name).dependencies]

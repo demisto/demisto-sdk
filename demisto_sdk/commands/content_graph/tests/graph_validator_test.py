9ecc2da56785c93d5141de6297b7aa12e0c30f09
dorschw <81086590+dorschw@users.noreply.github.com>
2023-05-14 17:01:38 +0300

    add id

--- demisto_sdk/commands/content_graph/tests/graph_validator_test.py
+++ demisto_sdk/commands/content_graph/tests/graph_validator_test.py
@@ -321,6 +321,7 @@ def _get_pack_by_id(repository: ContentDTO, pack_id: str) -> Pack:
 def mock_pack(name, marketplaces):
     return Pack(
         object_id=name,
+        id=name,
         content_type=ContentType.PACK,
         node_id=f"{ContentType.PACK}:{name}",
         path=Path("Packs"),

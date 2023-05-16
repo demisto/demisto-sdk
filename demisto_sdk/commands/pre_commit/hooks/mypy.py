79ba0fa4e1b6c10d2eeb24a104d8826967c67389
shmuel44 <skroizer@paloaltonetworks.com>
2023-05-16 18:25:27 +0300

    mypy hook

--- demisto_sdk/commands/pre_commit/hooks/mypy.py
+++ demisto_sdk/commands/pre_commit/hooks/mypy.py
@@ -3,5 +3,5 @@ from demisto_sdk.commands.pre_commit.hooks.hook import Hook
 
 class MypyHook(Hook):
     def prepare_hook(self, python_version: str, **kwargs):
-        self.hook["args"][-1] = f"--python-version={python_version}"
+        self.hook["args"] = [f"--python-version={python_version}"]
         self.hook["additional_dependencies"] = self.additional_dependencies(group_name="typing")

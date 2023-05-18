d101d030af6d0f39588f01c58497373334be6727
merit <meretmaayta@gmail.com>
2023-05-18 16:46:05 +0300

    edit for checking first fetch

--- demisto_sdk/commands/generate_docs/common.py
+++ demisto_sdk/commands/generate_docs/common.py
@@ -147,7 +147,9 @@ def generate_table_section(
     for item in data:
         tmp_item = "    |" if numbered_section else "|"
         escape_less_greater_signs = (
-            "First fetch time" or "First fetch timestamp" in item
+            True
+            if {"First fetch time", "First fetch timestamp"}.intersection(item.keys())
+            else False
         )  # instead of html escaping
         for key in item:
             escaped_string = string_escape_md(

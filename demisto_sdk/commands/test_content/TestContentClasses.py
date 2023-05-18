550456067f472737e564a71862b6634cf79924b4
adi88d <adaud@paloaltonetworks.com>
2023-05-18 11:48:44 +0300

    run pre commit

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -2706,10 +2706,14 @@ class ServerContext:
         )
 
     def _reset_containers(self):
-        if self.build_context.server_numeric_version == '99.99.98':
-            self.build_context.logging_module.info("Skip reset containers - this API is not supported.", real_time=True)
+        if self.build_context.server_numeric_version == "99.99.98":
+            self.build_context.logging_module.info(
+                "Skip reset containers - this API is not supported.", real_time=True
+            )
         else:
-            self.build_context.logging_module.info("Resetting containers\n", real_time=True)
+            self.build_context.logging_module.info(
+                "Resetting containers\n", real_time=True
+            )
 
             body, status_code, _ = demisto_client.generic_request_func(
                 self=self.client, method="POST", path="/containers/reset"

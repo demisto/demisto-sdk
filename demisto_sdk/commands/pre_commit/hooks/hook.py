2b031acd27c7c54923109d1d0067a9de7ca8b916
shmuel44 <skroizer@paloaltonetworks.com>
2023-05-23 18:21:33 +0300

    hook method

--- demisto_sdk/commands/pre_commit/hooks/hook.py
+++ demisto_sdk/commands/pre_commit/hooks/hook.py
@@ -1,8 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import List
-from demisto_sdk.commands.common.content_constant_paths import CONTENT_PATH
-from poetry.core.factory import Factory
-
 
 class Hook(ABC):
     def __init__(self, hook: dict) -> None:
@@ -11,7 +7,3 @@ class Hook(ABC):
     @abstractmethod
     def prepare_hook(self, **kwargs):
         return self.hook
-
-    def additional_dependencies(self, group_name: str) -> List[str]:
-        dependencies = Factory().create_poetry(CONTENT_PATH).package(group_name).dependencies
-        return [p.to_pep_508() for p in dependencies]
--- demisto_sdk/commands/pre_commit/hooks/mypy.py
+++ demisto_sdk/commands/pre_commit/hooks/mypy.py
@@ -4,4 +4,3 @@ from demisto_sdk.commands.pre_commit.hooks.hook import Hook
 class MypyHook(Hook):
     def prepare_hook(self, python_version: str, **kwargs):
         self.hook["args"] = [f"--python-version={python_version}"]
-        self.hook["additional_dependencies"] = self.additional_dependencies(group_name="typing")

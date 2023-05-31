246e5a571e9ea49c2049171f176821ec62481d93
ilan <ierukhimovic@paloaltonetworks.com>
2023-05-31 18:13:06 +0300

    add -i flag

--- demisto_sdk/commands/pre_commit/hooks/format.py
+++ demisto_sdk/commands/pre_commit/hooks/format.py
@@ -7,4 +7,5 @@ from demisto_sdk.commands.pre_commit.hooks.hook import Hook
 class FormatHook(Hook):
     def prepare_hook(self, input_files: Optional[Iterable[Path]], **kwargs):
         if input_files:
-            self.hook["args"][-1] = ",".join(str(file) for file in input_files)
+            self.hook["args"][-1] = "-i"
+            self.hook["args"].append(",".join(str(file) for file in input_files))

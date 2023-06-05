9ac9c4f8be6a099ce9cafc3fe470c2f36c576c47
tkatzir <tkatzir@paloaltonetworks.com>
2023-06-05 11:27:37 +0300

    Debug messages

--- demisto_sdk/commands/content_graph/tests/graph_validator_test.py
+++ demisto_sdk/commands/content_graph/tests/graph_validator_test.py
@@ -519,6 +519,7 @@ def test_is_file_using_unknown_content(
     Then
     - Check whether the graph is valid or not, based on whether the integration file path was provided
     """
+    logger_info = mocker.patch.object(logging.getLogger("demisto-sdk"), "info")
     logger_warning = mocker.patch.object(logging.getLogger("demisto-sdk"), "warning")
     logger_error = mocker.patch.object(logging.getLogger("demisto-sdk"), "error")
     if should_provide_integration_path:
@@ -528,6 +529,16 @@ def test_is_file_using_unknown_content(
     with GraphValidator(should_update=False, git_files=git_files) as graph_validator:
         create_content_graph(graph_validator.graph)
         assert graph_validator.is_file_using_unknown_content() == is_valid
+
+    for current_call in logger_info.call_args_list:
+        if type(current_call[0]) == tuple:
+            print(f"*** INFO *** {current_call[0][0]=}")
+    for current_call in logger_warning.call_args_list:
+        if type(current_call[0]) == tuple:
+            print(f"*** WARNING *** {current_call[0][0]=}")
+    for current_call in logger_error.call_args_list:
+        if type(current_call[0]) == tuple:
+            print(f"*** ERROR *** {current_call[0][0]=}")
 
     found_level = False
     str_to_search, logger_to_search = (

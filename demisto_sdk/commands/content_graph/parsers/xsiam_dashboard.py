e26bdc55a486cae6cfd87ddfdee109e9044f4cad
yucohen <yucohen@paloaltonetworks.com>
2023-06-07 14:35:43 +0300

    updated versions properties for xsiam conten items

--- demisto_sdk/commands/content_graph/parsers/xsiam_dashboard.py
+++ demisto_sdk/commands/content_graph/parsers/xsiam_dashboard.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Set
 
-from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.common.constants import (
+    DEFAULT_CONTENT_ITEM_FROM_VERSION,
+    DEFAULT_CONTENT_ITEM_TO_VERSION,
+    MarketplaceVersions,
+)
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.parsers.json_content_item import (
     JSONContentItemParser,
@@ -15,6 +19,7 @@ class XSIAMDashboardParser(
         self, path: Path, pack_marketplaces: List[MarketplaceVersions]
     ) -> None:
         super().__init__(path, pack_marketplaces)
+        self.original_json_data = self.get_json()
         self.json_data: Dict[str, Any] = self.json_data.get("dashboards_data", [{}])[0]
 
     @property
@@ -24,3 +29,16 @@ class XSIAMDashboardParser(
     @property
     def object_id(self) -> Optional[str]:
         return self.json_data.get("global_id")
+
+    @property
+    def fromversion(self) -> str:
+        return (
+            self.original_json_data.get("fromVersion")
+            or DEFAULT_CONTENT_ITEM_FROM_VERSION
+        )
+
+    @property
+    def toversion(self) -> str:
+        return (
+            self.original_json_data.get("toVersion") or DEFAULT_CONTENT_ITEM_TO_VERSION
+        )
--- demisto_sdk/commands/content_graph/parsers/xsiam_report.py
+++ demisto_sdk/commands/content_graph/parsers/xsiam_report.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Set
 
-from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.common.constants import (
+    DEFAULT_CONTENT_ITEM_FROM_VERSION,
+    DEFAULT_CONTENT_ITEM_TO_VERSION,
+    MarketplaceVersions,
+)
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.parsers.json_content_item import (
     JSONContentItemParser,
@@ -13,6 +17,7 @@ class XSIAMReportParser(JSONContentItemParser, content_type=ContentType.XSIAM_RE
         self, path: Path, pack_marketplaces: List[MarketplaceVersions]
     ) -> None:
         super().__init__(path, pack_marketplaces)
+        self.original_json_data = self.get_json()
         self.json_data: Dict[str, Any] = self.json_data.get("templates_data", [{}])[0]
 
     @property
@@ -26,3 +31,16 @@ class XSIAMReportParser(JSONContentItemParser, content_type=ContentType.XSIAM_RE
     @property
     def supported_marketplaces(self) -> Set[MarketplaceVersions]:
         return {MarketplaceVersions.MarketplaceV2}
+
+    @property
+    def fromversion(self) -> str:
+        return (
+            self.original_json_data.get("fromVersion")
+            or DEFAULT_CONTENT_ITEM_FROM_VERSION
+        )
+
+    @property
+    def toversion(self) -> str:
+        return (
+            self.original_json_data.get("toVersion") or DEFAULT_CONTENT_ITEM_TO_VERSION
+        )

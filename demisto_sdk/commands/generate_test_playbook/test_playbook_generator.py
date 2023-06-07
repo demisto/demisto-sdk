af00043d2248ae4533747aead5edf959d2c05814
Shahaf Ben Yakir <44666568+ShahafBenYakir@users.noreply.github.com>
2023-06-07 11:57:07 +0300

    Update test_playbook_generator.py
    Added `**kwargs` to the function init

--- demisto_sdk/commands/generate_test_playbook/test_playbook_generator.py
+++ demisto_sdk/commands/generate_test_playbook/test_playbook_generator.py
@@ -286,6 +286,7 @@ class PlaybookTestsGenerator:
         commands: str = None,
         examples: str = None,
         upload: bool = False,
+         **kwargs
     ):
         self.integration_yml_path = input
         self.output = output

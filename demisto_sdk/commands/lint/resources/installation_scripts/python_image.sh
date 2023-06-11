1863711492483c518824643d55c2a0ec3fa1c11d
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-11 12:28:35 +0300

    add unsafelegacy

--- demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
+++ demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
@@ -9,6 +9,15 @@ cd /devwork
 chown -R :4000 /devwork/
 chmod -R 775 /devwork
 . /etc/os-release
+sed -i 's/providers = provider_sect/providers = provider_sect\n\
+ssl_conf = ssl_sect\n\
+\n\
+[ssl_sect]\n\
+system_default = system_default_sect\n\
+\n\
+[system_default_sect]\n\
+Options = UnsafeLegacyRenegotiation/' /etc/ssl/openssl.cnf
+
 if [ "$ID" = "alpine" ]
 then
     apk add --no-cache --virtual .build-deps python3-dev gcc build-base;

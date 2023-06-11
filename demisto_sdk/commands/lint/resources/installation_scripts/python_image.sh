f6c1229b90e47d521741ad12dd103b402b10ed8e
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-11 14:52:10 +0300

    apk update

--- demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
+++ demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
@@ -9,18 +9,10 @@ cd /devwork
 chown -R :4000 /devwork/
 chmod -R 775 /devwork
 . /etc/os-release
-sed -i 's/providers = provider_sect/providers = provider_sect\n\
-ssl_conf = ssl_sect\n\
-\n\
-[ssl_sect]\n\
-system_default = system_default_sect\n\
-\n\
-[system_default_sect]\n\
-Options = UnsafeLegacyRenegotiation/' /etc/ssl/openssl.cnf
 
 if [ "$ID" = "alpine" ]
 then
-    apk add --no-cache --virtual .build-deps python3-dev gcc build-base;
+    apk update && apk add --no-cache --virtual .build-deps python3-dev gcc build-base;
 elif [ "$ID" = "debian" ]
 then
     apt-get update && apt-get install -y --no-install-recommends gcc python3-dev

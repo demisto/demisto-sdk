55901121102816f333297085f3d1ca01de29f925
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-19 11:54:31 +0300

    add preprocessrule content item

--- demisto_sdk/commands/content_graph/parsers/pre_process_rule.py
+++ demisto_sdk/commands/content_graph/parsers/pre_process_rule.py
@@ -1,10 +1,15 @@
+from typing import Set
+
+from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.parsers.json_content_item import (
     JSONContentItemParser,
 )
 
 
-class PreprocessRuleParser(
+class PreProcessRuleParser(
     JSONContentItemParser, content_type=ContentType.PREPROCESS_RULE
 ):
-    pass
+    @property
+    def supported_marketplaces(self) -> Set[MarketplaceVersions]:
+        return {MarketplaceVersions.XSOAR}

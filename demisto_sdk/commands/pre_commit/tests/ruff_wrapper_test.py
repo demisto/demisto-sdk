edb007c7e35d36d75d47ee9ddb5b06e13fff3549
dorschw <81086590+dorschw@users.noreply.github.com>
2023-06-20 13:20:29 +0200

    more cases

--- demisto_sdk/commands/pre_commit/tests/ruff_wrapper_test.py
+++ demisto_sdk/commands/pre_commit/tests/ruff_wrapper_test.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 
 import pytest
 
@@ -12,32 +12,52 @@ path = Path("dummy/foo.py")
 
 
 DUMMY_VIOLATIONS = (
-        LinterViolation(
-            error_code="TEST001",
-            path=path,
-            row_start=0,
-            message="dummy message",
-            violation_type=ViolationType.ERROR,
-            is_autofixable=False,
-        ),
-        LinterViolation(
-            error_code="TEST002",
-            path=path,
-            row_start=1,
-            message="dummy message",
-            violation_type=ViolationType.ERROR,
-            is_autofixable=True,
-            fix_suggestion="dummy fix suggestion",
-        ),
-    )
+    LinterViolation(
+        error_code="TEST001",
+        path=path,
+        row_start=0,
+        message="dummy message",
+        violation_type=ViolationType.ERROR,
+        is_autofixable=False,
+    ),
+    LinterViolation(
+        error_code="TEST002",
+        path=path,
+        row_start=1,
+        message="dummy message",
+        violation_type=ViolationType.ERROR,
+        is_autofixable=True,
+        fix_suggestion="dummy fix suggestion",
+    ),
+)
 
-@pytest.mark.parametrize("modified_lines,expected", [((0,), (DUMMY_VIOLATIONS[0],))])
-def test_filter_violations(modified_lines:Tuple[int,...], expected: List[LinterViolation]):
+
+@pytest.mark.parametrize(
+    "modified_lines,expected",
+    [
+        pytest.param((0,), (DUMMY_VIOLATIONS[0],), id="only one line changed"),
+        pytest.param(
+            (0, 1),
+            (
+                DUMMY_VIOLATIONS[0],
+                DUMMY_VIOLATIONS[1],
+            ),
+            id="two lines, two violations",
+        ),
+        pytest.param((), (), id="no lines changed"),
+        pytest.param((3,), (), id="lines without violations changed"),
+    ],
+)
+def test_filter_violations(
+    modified_lines: Tuple[int, ...], expected: List[LinterViolation]
+):
     from demisto_sdk.commands.pre_commit.ruff_wrapper import filter_violations
 
-    git_modified_files = {path: modified_lines}
-    assert filter_violations(
-        violations=DUMMY_VIOLATIONS,
-        git_modified_lines=git_modified_files,
-        fail_autofixable=True,
-    ) == expected  # TODO autofixable 
+    assert (
+        filter_violations(
+            violations=DUMMY_VIOLATIONS,
+            git_modified_lines={path: modified_lines},
+            fail_autofixable=False,  # TODO autofixable
+        )
+        == expected
+    )

4d0f46df54bc2a97da5ed3828052be823ff71069
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-21 11:27:01 +0300

    update

--- demisto_sdk/scripts/update_additional_dependencies.py
+++ demisto_sdk/scripts/update_additional_dependencies.py
@@ -34,11 +34,13 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument(
         "pre_commit_config",
         help="The path to the pre-commit config file",
+        required=False,
         default=".pre-commit-config.yaml",
     )
     parser.add_argument(
         "requirements",
         help="The path to the requirements file",
+        required=False,
         default="requirements.txt",
     )
     parser.add_argument(

4b7cedfa7c66090cfbf5ecdda8abc044156663d1
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2023-06-23 03:27:46 +0300

    adding Pydantic config class

--- demisto_sdk/commands/changelog/changelog_obj.py
+++ demisto_sdk/commands/changelog/changelog_obj.py
@@ -4,11 +4,16 @@ from pydantic import BaseModel
 
 
 class ChangelogType(str, Enum):
-    fix = 'fix'
-    feature = 'feature'
-    breaking = 'breaking'
+    fix = "fix"
+    feature = "feature"
+    breaking = "breaking"
 
 
 class ChangelogObject(BaseModel):
     description: str
     type: ChangelogType
+
+    class Config:
+        """Pydantic config class"""
+
+        use_enum_values = True

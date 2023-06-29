2ee41bc5f7807d23dcc522ca67f2c4439bfc4f86
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-29 12:11:56 +0300

    dup line

--- demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
@@ -519,7 +519,6 @@ class Neo4jContentGraphInterface(ContentGraphInterface):
             with self.driver.session() as session:
                 session.execute_write(drop_constraints)
                 session.execute_write(import_graphml, graphml_filenames)
-                session.execute_write(import_graphml, graphml_filenames)
                 session.execute_write(merge_duplicate_commands)
                 session.execute_write(merge_duplicate_content_items)
                 session.execute_write(create_constraints)

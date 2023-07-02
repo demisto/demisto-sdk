79412e190c256f59913cabef653533a34d2fee2b
israelpoli <72099621+israelpoli@users.noreply.github.com>
2023-07-02 16:30:01 +0300

    add support for Path input type in upload command (#3123)
    * add support for Path input type in upload command
    
    * add UT and comment corrections
    
    * more UTs
    
    * one more UT, CL
    
    ---------
    
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -3665,3 +3665,23 @@ def parse_marketplace_kwargs(kwargs: Dict[str, Any]) -> MarketplaceVersions:
         "neither marketplace nor is_xsiam provided, using default marketplace=XSOAR"
     )
     return MarketplaceVersions.XSOAR  # default
+
+
+def parse_multiple_path_inputs(
+    input_path: Optional[Union[Path, str, List[Path], Tuple[Path]]]
+) -> Optional[Tuple[Path, ...]]:
+    if not input_path:
+        return ()
+
+    if isinstance(input_path, Path):
+        return (input_path,)
+
+    if isinstance(input_path, str):
+        return tuple(Path(path_str) for path_str in input_path.split(","))
+
+    if isinstance(input_path, (list, tuple)) and isinstance(
+        (result := tuple(input_path))[0], Path
+    ):
+        return result
+
+    raise ValueError(f"Cannot parse paths from {input_path}")

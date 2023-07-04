1d75190bffe0c883d281b0f96aa902c3020744ac
rshunim <rshunim@paloaltonetworks.com>
2023-07-04 17:20:05 +0300

    hgyfhj

--- demisto_sdk/commands/common/hook_validations/base_validator.py
+++ demisto_sdk/commands/common/hook_validations/base_validator.py
@@ -264,7 +264,7 @@ class BaseValidator:
         self.json_output(file_path, error_code, error_message, warning)
         self.add_to_report_error_list(error_code, file_path, FOUND_FILES_AND_ERRORS)
         if str2bool(os.getenv("GITHUB_ACTIONS")):
-            print("In the condition")
+            logger.info("****************** In the condition ***************************")
             print(f"::notice file={file_path},line=1,endLine=1,title=Validation Error::{error_message}")
 
         return formatted_error

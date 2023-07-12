b500e14f33ec09cda5898971aaa0fad6b0fd2025
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-07-12 10:07:42 +0300

    test

--- demisto_sdk/commands/common/tests/tools_test.py
+++ demisto_sdk/commands/common/tests/tools_test.py
@@ -90,7 +90,7 @@ from demisto_sdk.commands.common.tools import (
     server_version_compare,
     str2bool,
     string_to_bool,
-    to_kebab_case,
+    to_kebab_case, SDK_PYPI_VERSION,
 )
 from demisto_sdk.tests.constants_test import (
     DUMMY_SCRIPT_PATH,
@@ -1472,8 +1472,12 @@ def test_get_last_remote_release_version(requests_mock):
     os.environ["DEMISTO_SDK_SKIP_VERSION_CHECK"] = ""
     os.environ["CI"] = ""
     expected_version = "1.3.8"
+    # requests_mock.get(
+    #     r"https://test.org/pypi/demisto-sdk/json",
+    #     json={"info": {"version": expected_version}},
+    # )
     requests_mock.get(
-        r"https://test.org/pypi/demisto-sdk/json",
+        SDK_PYPI_VERSION,
         json={"info": {"version": expected_version}},
     )
     assert get_last_remote_release_version() == expected_version

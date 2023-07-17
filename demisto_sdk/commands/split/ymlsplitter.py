78494c285d7d833ca297ffb1f6afdb9850ad0331
dansterenson <dansterenson@gmail.com>
2023-07-17 14:21:52 +0300

    fix regex to remove all types of pack versions.

--- demisto_sdk/commands/split/ymlsplitter.py
+++ demisto_sdk/commands/split/ymlsplitter.py
@@ -353,7 +353,7 @@ class YmlSplitter:
         """
         remove the auto-generated section headers if they exist.
         """
-        script = re.sub(r"(?:###|//) pack version: (\d+\.\d+\.\d+)", "", script)
+        script = re.sub(r"(?:#|//)+ pack version: (\d+\.\d+\.\d+)(\n|$)", "", script, flags=re.MULTILINE)
         return re.sub(
             r"register_module_line\('.+', '(?:start|end)', __line__\(\)\)(\n|$)",
             "",

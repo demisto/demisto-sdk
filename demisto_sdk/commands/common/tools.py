87153465d0587d102076aadc4ed1291bbe76fc6d
rshunim <rshunim@paloaltonetworks.com>
2023-07-19 16:48:51 +0300

    get_yaml keep_order to false by default

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -878,7 +878,7 @@ def get_file_or_remote(file_path: Path, clear_cache=False):
         return get_remote_file(str(relative_file_path))
 
 
-def get_yaml(file_path, cache_clear=False, keep_order: bool = True):
+def get_yaml(file_path, cache_clear=False, keep_order: bool = False):
     if cache_clear:
         get_file.cache_clear()
     return get_file(file_path, "yml", clear_cache=cache_clear, keep_order=keep_order)

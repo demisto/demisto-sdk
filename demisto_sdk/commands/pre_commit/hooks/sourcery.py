b5bc8005639e78196e20fe6a6f0cb3a002b7230a
Jas Beilin <jgranot@paloaltonetworks.com>
2023-07-20 14:33:59 +0300

    test

--- demisto_sdk/commands/pre_commit/hooks/sourcery.py
+++ demisto_sdk/commands/pre_commit/hooks/sourcery.py
@@ -7,5 +7,5 @@ class SourceryHook(Hook):
         config_file = tools.get_file_or_remote(config_file_path)
         config_file['rule_settings']['python_version'] = python_version
         tmp_file_path = config_file_path.with_name("sourcery_tmp.yml")
-        self.hook['args'] = [f"--config={tmp_file_path}", '--fix', '--no-summary', "--diff=git diff HEAD"]
+        self.hook['args'] = [f"--config={tmp_file_path}", '--no-summary', '--fix', "--diff=git diff HEAD"]
         tools.write_yml(str(tmp_file_path), config_file)

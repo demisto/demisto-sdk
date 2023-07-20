8c4c4dbfd04ad5141bc0e1fd83cf3b26629326c2
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-20 12:51:08 +0300

    test

--- Utils/github_workflow_scripts/handle_external_pr.sh
+++ Utils/github_workflow_scripts/handle_external_pr.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
 reviewers=$(curl -s "https://raw.githubusercontent.com/demisto/content/master/.github/content_roles.json")
-contribution_tl=$(echo $reviewers | jq -r '.CONTRIBUTION_TL')
+contribution_tl=$(echo "$reviewers" | jq -r '.CONTRIBUTION_TL')
 
-echo "Adding contribution TL $contribution_tl as assignee and reviewer"
+echo "Adding contributions TL $contribution_tl as assignee and reviewer"
 # gh pr edit "$PR_LINK" --add-assignee "$contribution_tl"
 # gh pr edit "$PR_LINK" --add-reviewer "$contribution_tl"
 
-security_items=("*Playbooks*" "*IncidentTypes*" "*IncidentFields*" "*IndicatorTypes*" "*IndicatorFields*" "*Layouts*" "*Classifiers*" "*sourcery*")
+security_items="Playbooks|IncidentTypes|IncidentFields|IndicatorTypes|IndicatorFields|Layouts|Classifiers|sourcery"
 
-function is_security_needed() {
-    for file in "${CHANGED_FILES[@]}"; do
-        echo $file
-        for name in "${security_items[@]}"; do
-            echo $name
-            [[ "$file" == "$name" ]] && return 
-        done
-    done
-    false
-}
-
-if is_security_needed; then
-    security_rev=$(echo $reviewers | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
+if git diff --name-only $HEAD..$BASE | grep "$security_items"; then
+    security_reviewer=$(echo "$reviewers" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
     echo "Security needed"
-    echo "Adding security reviewer $security_rev as assignee and reviewer"
-    # gh pr edit "$PR_LINK" --add-assignee $security_rev
-    # gh pr edit "$PR_LINK" --add-reviewer $security_rev
+    echo "Adding security reviewer $security_reviewer as assignee and reviewer"
+    # gh pr edit "$PR_LINK" --add-assignee $security_reviewer
+    # gh pr edit "$PR_LINK" --add-reviewer $security_reviewer
 fi

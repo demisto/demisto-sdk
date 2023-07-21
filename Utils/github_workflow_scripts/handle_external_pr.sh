9a377ef03a13d596ea5ea07e23d8f0f31284d5c7
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-21 14:33:34 +0300

    test

--- Utils/github_workflow_scripts/handle_external_pr.sh
+++ Utils/github_workflow_scripts/handle_external_pr.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+gh pr comment "$PR_LINK" --body "$(cat Utils/github_workflow_scripts/contribution_comment.md)"
+
 reviewers=$(curl -s "https://raw.githubusercontent.com/demisto/content/master/.github/content_roles.json")
 contribution_tl=$(echo "$reviewers" | jq -r '.CONTRIBUTION_TL')
 
 echo "Adding contributions TL $contribution_tl as assignee and reviewer"
 gh pr edit "$PR_LINK" --add-assignee jlevypaloalto  # "$contribution_tl"
-gh pr edit "$PR_LINK" --add-reviewer jlevypaloalto  # "$contribution_tl"
+# gh pr edit "$PR_LINK" --add-reviewer jlevypaloalto  # "$contribution_tl"
 gh pr edit "$PR_LINK" --add-label "Contribution"
-gh pr comment "$PR_LINK" --body "$(cat Utils/github_workflow_scripts/contribution_comment.md)"
 
-security_items="Playbooks|IncidentTypes|IncidentFields|IndicatorTypes|IndicatorFields|Layouts|Classifiers"
+# should be a temporary fix until GH API supports "gh pr edit "$PR_LINK" --add-reviewer", link: https://github.com/cli/cli/issues/4844
+curl -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_TOKEN"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "$API_URL" \
+    -d "{\"reviewers\": \"$contribution_tl\"}"
+
+# security_items="Playbooks|IncidentTypes|IncidentFields|IndicatorTypes|IndicatorFields|Layouts|Classifiers"
 
 # if git diff --name-only $HEAD..$BASE | grep -E "$security_items"; then
 #     security_reviewer=$(echo "$reviewers" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')

7fd5543fe2e8a6daf63d8720ad46ebce6d4cfc48
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-21 18:56:40 +0300

    test

--- Utils/github_workflow_scripts/handle_external_pr.sh
+++ Utils/github_workflow_scripts/handle_external_pr.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-gh pr comment "$PR_LINK" --body "$(cat Utils/github_workflow_scripts/contribution_comment.md)"
-
-reviewers=$(curl -s "https://raw.githubusercontent.com/demisto/content/master/.github/content_roles.json")
-contribution_tl=$(echo "$reviewers" | jq -r '.CONTRIBUTION_TL')
+welcome_message=$(cat Utils/github_workflow_scripts/contribution_comment.md | awk "{gsub(/<contribution_tl>/,\"$contribution_tl\")}1")
+roles=$(curl -s "https://raw.githubusercontent.com/demisto/content/master/.github/content_roles.json")
+contribution_tl=$(echo "$roles" | jq -r '.CONTRIBUTION_TL')
 
 echo "Adding contributions TL $contribution_tl as assignee and reviewer"
-gh pr edit "$PR_LINK" --add-assignee jlevypaloalto  # "$contribution_tl"
-# gh pr edit "$PR_LINK" --add-reviewer jlevypaloalto  # "$contribution_tl"
+gh pr comment "$PR_LINK" --body "$welcome_message"
+gh pr edit "$PR_LINK" --add-assignee "$contribution_tl"
 gh pr edit "$PR_LINK" --add-label "Contribution"
 
-# should be a temporary fix until GH API supports "gh pr edit "$PR_LINK" --add-reviewer", link: https://github.com/cli/cli/issues/4844
+# should be a temporary fix until GH API supports "gh pr edit "$PR_LINK" --add-reviewer $contribution_tl", link: https://github.com/cli/cli/issues/4844
 curl -X POST \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN"\
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "$API_URL" \
-    -d "{\"reviewers\": [\"$contribution_tl\"]}"
+    "https://api.github.com/repos$OWNER/$REPO/pulls/$PR_NUMBER/requested_reviewers" \
+    -d "{\"reviewers\": [\"$contribution_tl\"]}" \
+    -o /dev/null
 
 # security_items="Playbooks|IncidentTypes|IncidentFields|IndicatorTypes|IndicatorFields|Layouts|Classifiers"
 
-# if git diff --name-only $HEAD..$BASE | grep -E "$security_items"; then
-#     security_reviewer=$(echo "$reviewers" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
+# if git diff --name-only $HEAD..$BASE | grep -E "$security_items"; then  # use: ${{ github.event.pull_request.diff_url }}
+#     security_reviewer=$(echo "$roles" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
 #     echo "Security needed"
 #     echo "Adding security reviewer $security_reviewer as assignee and reviewer"
-#     gh pr edit "$PR_LINK" --add-assignee $security_reviewer
 #     gh pr edit "$PR_LINK" --add-reviewer $security_reviewer
 # fi

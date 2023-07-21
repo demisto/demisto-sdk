6adb979c062c8711674dae0b4537b5bad7436f20
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-21 13:47:02 +0300

    test

--- Utils/github_workflow_scripts/handle_external_pr.sh
+++ Utils/github_workflow_scripts/handle_external_pr.sh
@@ -4,17 +4,17 @@ reviewers=$(curl -s "https://raw.githubusercontent.com/demisto/content/master/.g
 contribution_tl=$(echo "$reviewers" | jq -r '.CONTRIBUTION_TL')
 
 echo "Adding contributions TL $contribution_tl as assignee and reviewer"
-# gh pr edit "$PR_LINK" --add-assignee "$contribution_tl"
-# gh pr edit "$PR_LINK" --add-reviewer "$contribution_tl"
+gh pr edit "$PR_LINK" --add-assignee jlevypaloalto  # "$contribution_tl"
+gh pr edit "$PR_LINK" --add-reviewer jlevypaloalto  # "$contribution_tl"
 gh pr edit "$PR_LINK" --add-label "Contribution"
 gh pr comment "$PR_LINK" --body "$(cat Utils/github_workflow_scripts/contribution_comment.md)"
 
 security_items="Playbooks|IncidentTypes|IncidentFields|IndicatorTypes|IndicatorFields|Layouts|Classifiers"
 
-if git diff --name-only $HEAD..$BASE | grep -E "$security_items"; then
-    security_reviewer=$(echo "$reviewers" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
-    echo "Security needed"
-    echo "Adding security reviewer $security_reviewer as assignee and reviewer"
-    # gh pr edit "$PR_LINK" --add-assignee $security_reviewer
-    # gh pr edit "$PR_LINK" --add-reviewer $security_reviewer
-fi
+# if git diff --name-only $HEAD..$BASE | grep -E "$security_items"; then
+#     security_reviewer=$(echo "$reviewers" | jq -r '.CONTRIBUTION_SECURITY_REVIEWER')
+#     echo "Security needed"
+#     echo "Adding security reviewer $security_reviewer as assignee and reviewer"
+#     gh pr edit "$PR_LINK" --add-assignee $security_reviewer
+#     gh pr edit "$PR_LINK" --add-reviewer $security_reviewer
+# fi

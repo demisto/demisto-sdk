86d1af484ed6c32c0133b6d43e2b8bf1395d9bd6
Jas Beilin <jgranot@paloaltonetworks.com>
2023-07-23 13:58:20 +0300

    using tmpfile

--- demisto_sdk/commands/pre_commit/hooks/sourcery.py
+++ demisto_sdk/commands/pre_commit/hooks/sourcery.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import tempfile
 
 from demisto_sdk.commands.common import tools
 from demisto_sdk.commands.pre_commit.hooks.hook import Hook
@@ -8,6 +9,8 @@ class SourceryHook(Hook):
     def prepare_hook(self, python_version: str, config_file_path: Path, **kwargs):
         config_file = tools.get_file_or_remote(config_file_path)
         config_file["rule_settings"]["python_version"] = python_version
-        tmp_file_path = config_file_path.with_name(".sourcery_tmp.yaml")
-        self.hook["args"] += [ f"--config={tmp_file_path}"]
-        tools.write_yml(str(tmp_file_path), config_file)
+        # tmp_file_path = config_file_path.with_name(".sourcery_tmp.yaml")
+        tf = tempfile.NamedTemporaryFile(delete=False)
+        tools.write_yml(tf.name, config_file)
+        self.hook["args"] += [f"--config={tf.name}"]
+

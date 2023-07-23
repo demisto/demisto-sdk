4483a1fd02a9ee681679ba8db100f27d0b5d640b
dorschw <81086590+dorschw@users.noreply.github.com>
2023-07-23 16:55:38 +0300

    Update demisto_sdk/commands/download/downloader.py

--- demisto_sdk/commands/download/downloader.py
+++ demisto_sdk/commands/download/downloader.py
@@ -184,7 +184,7 @@ class Downloader:
         self.keep_empty_folders = keep_empty_folders
         if is_sdk_defined_working_offline() and self.run_format:
             self.run_format = False
-            logger.info(f"format is not supported when the DEMISTO_SDK_OFFLINE_ENV environment variable is set.")
+            logger.info(f"format is not supported when the DEMISTO_SDK_OFFLINE_ENV environment variable is set, skipping it.")
 
     def download(self) -> int:
         """

399fa70d855792601309b0e8e342fc838a06ee85
ilan <ierukhimovic@paloaltonetworks.com>
2023-07-24 09:56:37 +0300

    fix

--- demisto_sdk/commands/content_graph/neo4j_service.py
+++ demisto_sdk/commands/content_graph/neo4j_service.py
@@ -94,7 +94,7 @@ def _docker_start():
         name="neo4j-content",
         ports={"7474/tcp": 7474, "7687/tcp": 7687, "7473/tcp": 7473},
         volumes=[
-            f"{REPO_PATH / NEO4J_FOLDER / NEO4J_DATA_FOLDER}:/{NEO4J_DATA_FOLDER}:ro,z",
+            f"{REPO_PATH / NEO4J_FOLDER / NEO4J_DATA_FOLDER}:/{NEO4J_DATA_FOLDER}",
             f"{REPO_PATH / NEO4J_FOLDER / NEO4J_IMPORT_FOLDER}:{LOCAL_NEO4J_PATH / NEO4J_IMPORT_FOLDER}:ro,z",
             f"{REPO_PATH / NEO4J_FOLDER / NEO4J_PLUGINS_FOLDER}:/{NEO4J_PLUGINS_FOLDER}:ro,z",
         ],

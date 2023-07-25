1a373d63c66efb87163151ab6bee42a05eda52d1
dorschw <81086590+dorschw@users.noreply.github.com>
2023-07-25 16:11:17 +0300

    remove unused code

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -3208,7 +3208,6 @@ def get_item_marketplaces(
         return [MarketplaceVersions.MarketplaceV2.value]
 
     if not item_data:
-        file_type = Path(item_path).suffix
         item_data = get_file(item_path)
 
     # first check, check field 'marketplaces' in the item's file

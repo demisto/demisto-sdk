5b8408ed490fc4ad376c04a10eee6593ba7b048f
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-07-30 14:50:01 +0300

    test

--- demisto_sdk/commands/coverage_analyze/helpers.py
+++ demisto_sdk/commands/coverage_analyze/helpers.py
@@ -193,8 +193,7 @@ class CoverageSummary:
         json_path = (
             os.path.join(self.cache_dir, "coverage-min.json") if self.cache_dir else ""
         )
-        if self.use_cache and self.cache_dir and False:
-            logger.info("here")
+        if self.use_cache and self.cache_dir:
             try:
                 with open(json_path) as coverage_summary_file:
                     full_coverage_summary = json.load(coverage_summary_file)
@@ -215,9 +214,7 @@ class CoverageSummary:
         resp = requests.get(self.url)
         resp.raise_for_status()
         data = resp.json()
-        logger.info("here2")
-        if self.use_cache and self.cache_dir and False:
-            logger.info("here3")
+        if self.use_cache and self.cache_dir:
             with open(json_path, "w") as fp:
                 json.dump(data, fp)
 
--- demisto_sdk/commands/coverage_analyze/tests/helpers_test.py
+++ demisto_sdk/commands/coverage_analyze/tests/helpers_test.py
@@ -269,7 +269,8 @@ class TestCoverageSummary:
                 self.default_url, json=read_file(JSON_MIN_DATA_FILE)
             )
             not_mocked_open = builtins.open
-            open_file_mocker = mocker.patch("builtins.open")
+            # open_file_mocker = mocker.patch("builtins.open")
+            open_file_mocker = mocker.patch.object(builtins, 'open')
             files_data = CoverageSummary(
                 previous_coverage_report_url=TestCoverageSummary.TestGetFilesSummary.default_url,
                 no_cache=True,

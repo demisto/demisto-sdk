eb43989051026d99807accaf5d909636bf0e52df
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-08-13 17:49:30 +0300

    fixed logger

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -3430,7 +3430,7 @@ def get_url_with_retries(url: str, retries: int, backoff_factor: int = 1, **kwar
             response.raise_for_status()
         except HTTPError as error:
             logger.debug(
-                f"Got error ({error}) trying to fetch {url}. {retries - i} retries left."
+                f"Got error ({error}) trying to fetch {url}. {retries - i - 1} retries left."
             )
             exception = error
         else:

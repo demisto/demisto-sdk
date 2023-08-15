16ac8a9c40aef53367677805d92c8733ad2a3270
tkatzir <tkatzir@paloaltonetworks.com>
2023-08-15 19:35:14 +0300

    Use tools_core

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -40,7 +40,6 @@ from packaging.version import Version
 from pebble import ProcessFuture, ProcessPool
 from requests.exceptions import HTTPError
 
-import demisto_sdk.commands.common.tools_core as tools_core
 from demisto_sdk.commands.common.constants import (
     ALL_FILES_VALIDATION_IGNORE_WHITELIST,
     API_MODULES_PACK,
@@ -117,6 +116,11 @@ from demisto_sdk.commands.common.handlers import DEFAULT_JSON_HANDLER as json
 from demisto_sdk.commands.common.handlers import DEFAULT_YAML_HANDLER as yaml
 from demisto_sdk.commands.common.handlers import YAML_Handler
 from demisto_sdk.commands.common.logger import logger
+from demisto_sdk.commands.common.tools_core import (
+    get_content_path,
+    is_external_repository,
+    string_to_bool,
+)
 
 if TYPE_CHECKING:
     from demisto_sdk.commands.content_graph.interface import ContentGraphInterface
@@ -409,7 +413,7 @@ def get_marketplace_to_core_packs() -> Dict[MarketplaceVersions, Set[str]]:
     Returns:
         A mapping from marketplace versions to their core packs.
     """
-    if tools_core.is_external_repository():
+    if is_external_repository():
         return {}  # no core packs in external repos.
 
     mp_to_core_packs: Dict[MarketplaceVersions, Set[str]] = {}
@@ -444,7 +448,7 @@ def get_core_pack_list(marketplaces: List[MarketplaceVersions] = None) -> list:
         The core packs list.
     """
     result: Set[str] = set()
-    if tools_core.is_external_repository():
+    if is_external_repository():
         return []  # no core packs in external repos.
 
     if marketplaces is None:
@@ -525,7 +529,7 @@ def get_remote_file_from_api(
             str(exc).replace(github_token, "XXX") if github_token else str(exc)
         )
         err_msg = err_msg.replace(gitlab_token, "XXX") if gitlab_token else err_msg
-        if tools_core.is_external_repository():
+        if is_external_repository():
             logger.debug(
                 f'[yellow]You are working in a private repository: "{git_content_config.current_repository}".\n'
                 f"The github/gitlab token in your environment is undefined.\n"
@@ -831,7 +835,7 @@ def get_file(
     type_of_file = file_path.suffix.lower()
 
     if not file_path.exists():
-        file_path = Path(tools_core.get_content_path()) / file_path  # type: ignore[arg-type]
+        file_path = Path(get_content_path()) / file_path  # type: ignore[arg-type]
 
     if not file_path.exists():
         raise FileNotFoundError(file_path)
@@ -862,7 +866,7 @@ def get_file(
 
 
 def get_file_or_remote(file_path: Path, clear_cache=False):
-    content_path = tools_core.get_content_path()
+    content_path = get_content_path()
     relative_file_path = None
     if file_path.is_absolute():
         absolute_file_path = file_path
@@ -1042,7 +1046,7 @@ def str2bool(v):
     """
     Deprecated. Use string_to_bool instead
     """
-    return tools_core.string_to_bool(v, default_when_empty=False)
+    return string_to_bool(v, default_when_empty=False)
 
 
 def to_dict(obj):
@@ -1394,7 +1398,7 @@ def filter_files_by_type(file_paths=None, skip_file_types=None) -> set:
 
 
 def pack_name_to_path(pack_name):
-    return os.path.join(tools_core.get_content_path(), PACKS_DIR, pack_name)  # type: ignore
+    return os.path.join(get_content_path(), PACKS_DIR, pack_name)  # type: ignore
 
 
 def pack_name_to_posix_path(pack_name):
@@ -1402,7 +1406,7 @@ def pack_name_to_posix_path(pack_name):
 
 
 def get_pack_ignore_file_path(pack_name):
-    return os.path.join(tools_core.get_content_path(), PACKS_DIR, pack_name, PACKS_PACK_IGNORE_FILE_NAME)  # type: ignore
+    return os.path.join(get_content_path(), PACKS_DIR, pack_name, PACKS_PACK_IGNORE_FILE_NAME)  # type: ignore
 
 
 def get_test_playbook_id(test_playbooks_list: list, tpb_path: str) -> Tuple:  # type: ignore
@@ -2103,7 +2107,7 @@ def is_file_from_content_repo(file_path: str) -> Tuple[bool, str]:
         git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
         remote_url = git_repo.remote().urls.__next__()
         is_fork_repo = "content" in remote_url
-        is_external_repo = tools_core.is_external_repository()
+        is_external_repo = is_external_repository()
 
         if not is_fork_repo and not is_external_repo:
             return False, ""
@@ -2961,7 +2965,7 @@ def get_current_usecases() -> list:
     Returns:
         List of approved usecases from current branch
     """
-    if not tools_core.is_external_repository():
+    if not is_external_repository():
         approved_usecases_json, _ = get_dict_from_file(
             "Tests/Marketplace/approved_usecases.json"
         )
@@ -2975,7 +2979,7 @@ def get_approved_tags_from_branch() -> Dict[str, List[str]]:
     Returns:
         Dict of approved tags from current branch
     """
-    if not tools_core.is_external_repository():
+    if not is_external_repository():
         approved_tags_json, _ = get_dict_from_file(
             "Tests/Marketplace/approved_tags.json"
         )
@@ -3001,7 +3005,7 @@ def get_current_categories() -> list:
     Returns:
         List of approved categories from current branch
     """
-    if tools_core.is_external_repository():
+    if is_external_repository():
         return []
     try:
         approved_categories_json, _ = get_dict_from_file(

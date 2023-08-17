17c3f30aa0d51d57bea8e1796426129244a461f8
GuyAfik <guyafik11@gmail.com>
2023-08-17 15:28:36 +0300

    remove irrelevant code

--- demisto_sdk/commands/content_graph/commands/create.py
+++ demisto_sdk/commands/content_graph/commands/create.py
@@ -104,9 +104,6 @@ def create(
     relationships. Then, the parsed content objects are mapped to
     a Repository model and uploaded to the graph database.
     """
-    import time
-
-    start_time = time.time()
     logging_setup(
         console_log_threshold=console_log_threshold,
         file_log_threshold=file_log_threshold,
@@ -119,7 +116,6 @@ def create(
             dependencies=not no_dependencies,
             output_path=output_path,
         )
-    logger.info(f"Creating the graph took {time.time() - start_time} seconds")
 
 
 def stop_content_graph() -> None:

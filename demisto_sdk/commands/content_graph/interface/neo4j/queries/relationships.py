cd582b073bdcc559a4aada94a26b810af4fb9397
ilan <ierukhimovic@paloaltonetworks.com>
2023-08-21 17:40:50 +0300

    add with

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/relationships.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/relationships.py
@@ -92,6 +92,7 @@ ON CREATE
     SET target.not_in_repository = true
 
 // Get or create the relationship and set its "mandatorily" field based on relationship data
+WITH source, target, rel_data
 CALL apoc.merge.relationship(source, "{RelationshipType.USES}", {{mandatorily: rel_data.mandatorily}}, {{}}, target) YIELD r
 SET r.mandatorily = coalesce(r.mandatorily, false) OR rel_data.mandatorily
 RETURN count(r) AS relationships_merged"""

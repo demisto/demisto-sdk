b8d6d84a634f86cef69776e569230f20b9c4c31b
dorschw <81086590+dorschw@users.noreply.github.com>
2023-08-31 12:07:10 +0300

    black

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -199,6 +199,7 @@ def main(ctx, config, version, release_notes, **kwargs):
 
     config.configuration = Configuration()
     import dotenv
+
     dotenv.load_dotenv(CONTENT_PATH / ".env", override=True)  # type: ignore # load .env file from the cwd
     if (
         (not os.getenv("DEMISTO_SDK_SKIP_VERSION_CHECK")) or version

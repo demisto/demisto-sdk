dac94be173d7bdbe8691193941a10621fcc0f1d6
GuyAfik <guyafik11@gmail.com>
2023-09-03 17:01:08 +0300

    try to fix uts

--- demisto_sdk/commands/create_artifacts/content_artifacts_creator.py
+++ demisto_sdk/commands/create_artifacts/content_artifacts_creator.py
@@ -1446,7 +1446,7 @@ def content_files_handler(
             and content_object.code_path.name == "CommonServerPython.py"
         ):
             # Modify CommonServerPython.py global variables
-            repo = artifact_manager.Content.git().repo
+            repo = artifact_manager.content.git().repo
             modify_common_server_constants(
                 content_object.code_path,
                 artifact_manager.content_version,

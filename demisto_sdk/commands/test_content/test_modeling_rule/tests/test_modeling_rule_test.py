c084911eb6a57a5f4eba7747bf3a7f08eba39500
MosheEichler <meichler@paloaltonetworks.com>
2023-09-19 15:55:32 +0300

    docstring

--- demisto_sdk/commands/test_content/test_modeling_rule/tests/test_modeling_rule_test.py
+++ demisto_sdk/commands/test_content/test_modeling_rule/tests/test_modeling_rule_test.py
@@ -1695,6 +1695,19 @@ def test_day_suffix(day, suffix):
     ('call a', True),
 ])
 def test_call_rule_regex(mr_text, expected_result):
+    """
+    Test the CALL_RULE_REGEX regex matches text containing 'call'.
+
+    Given:
+        - mr_text: Text to search for 'call'
+        - expected_result: Whether we expect mr_text to match
+
+    When:
+        - Search mr_text with ModelingRule.CALL_RULE_REGEX
+
+    Then:
+        - The search result should match expected_result
+    """
     from demisto_sdk.commands.common.content.objects.pack_objects.modeling_rule.modeling_rule import ModelingRule
     mr = ModelingRule
     assert bool(mr.CALL_RULE_REGEX.search(mr_text)) == expected_result

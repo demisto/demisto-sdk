7c5f489fd6b116015406646f29d63cc7a3cbe8b7
GuyAfik <guyafik11@gmail.com>
2023-10-24 11:15:19 +0300

    change handler_file to json/yml file

--- demisto_sdk/commands/common/files/json_file.py
+++ demisto_sdk/commands/common/files/json_file.py
@@ -1,11 +1,11 @@
 from pydantic import Field, validator
 
-from demisto_sdk.commands.common.files.handler_file import HandlerFile
+from demisto_sdk.commands.common.files.json_yml_file import JsonYmlFile
 from demisto_sdk.commands.common.handlers import DEFAULT_JSON_HANDLER as json
 from demisto_sdk.commands.common.handlers import JSON_Handler
 
 
-class JsonFile(HandlerFile):
+class JsonFile(JsonYmlFile):
     handler: JSON_Handler = Field(None, exclude=True)
 
     @validator("handler", always=True)
--- demisto_sdk/commands/common/files/json_yml_file.py
+++ demisto_sdk/commands/common/files/json_yml_file.py
@@ -10,7 +10,7 @@ from demisto_sdk.commands.common.files.text_file import TextFile
 from demisto_sdk.commands.common.handlers.xsoar_handler import XSOAR_Handler
 
 
-class HandlerFile(TextFile, ABC):
+class JsonYmlFile(TextFile, ABC):
 
     handler: XSOAR_Handler = Field(None, exclude=True)
 

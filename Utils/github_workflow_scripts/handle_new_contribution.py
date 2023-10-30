9c189c6947844cb84b35255e69c07c3626588fe3
RotemAmit <ramit@paloaltonetworks.com>
2023-10-30 15:04:19 +0200

    script updated according to cr

--- Utils/github_workflow_scripts/handle_new_contribution.py
+++ Utils/github_workflow_scripts/handle_new_contribution.py
@@ -1,5 +1,8 @@
+import os
+
 from demisto_sdk.commands.common.git_content_config import GitContentConfig, GitProvider
 from demisto_sdk.commands.common.tools import get_remote_file
+from github import Github
 
 
 CONTENT_ROLES_FILE_PATH = ".github/content_roles.json"
@@ -18,6 +21,17 @@ def main():
     # save the contrib_tl username to a file for a later use in the workflow
     with open("contrib_tl.txt", "w") as f:
         f.write(contrib_tl_username)
+
+    org_name = 'demisto'
+    repo_name = 'demisto-sdk'
+    gh = Github(os.getenv('GITHUB_TOKEN'), verify=False)
+    sdk_repo = gh.get_repo(f'{org_name}/{repo_name}')
+    pr_number = os.getenv('PR_NUMBER')
+    print(f'{pr_number=}')
+    pr = sdk_repo.get_pull(pr_number)
+    pr.add_to_assignees(contrib_tl_username)
+    pr.add_to_labels('Contribution')
+    print('***End of script***')
 
 
 if __name__ == "__main__":

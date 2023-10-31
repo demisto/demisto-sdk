f292fe55ec8b5469bac8b501aeaeedfef9e9dda2
Koby Meir <kobymeir@users.noreply.github.com>
2023-10-31 13:44:01 +0200

    Fixed an issue where the **modeling-rules test** command failed report and error when test data didn't exist. (#3754)
    * Fixed an issue where the **modeling-rules test** command failed report and error when test data didn't exist.

--- demisto_sdk/commands/test_content/test_modeling_rule/test_modeling_rule.py
+++ demisto_sdk/commands/test_content/test_modeling_rule/test_modeling_rule.py
@@ -1225,11 +1225,21 @@ def validate_modeling_rule(
                     extra={"markup": True},
                 )
         else:
+            err = (
+                f"Please create a test data file for {get_relative_path_to_content(modeling_rule_directory)} "
+                f"and then rerun\n{executed_command}"
+            )
             logger.error(
-                f"[red]Please create a test data file for "
-                f"{get_relative_path_to_content(modeling_rule_directory)} and then rerun\n{executed_command}[/red]",
+                f"[red]{err}[/red]",
                 extra={"markup": True},
             )
+            test_data_test_case = TestCase(
+                "Test data file does not exist",
+                classname=f"Modeling Rule {get_relative_path_to_content(modeling_rule.schema_path)}",
+            )
+            test_data_test_case.result += [Error(err)]
+            modeling_rule_test_suite.add_testcase(test_data_test_case)
+            return False, modeling_rule_test_suite
         return False, None
 
 

f69951d571a6fd28b5d17fa0a95daad74293f5d7
YuvHayun <yhayun011@gmail.com>
2023-11-06 11:42:55 +0200

    added content types dict

--- init_validation_script.py
+++ init_validation_script.py
@@ -3,44 +3,138 @@ from string import Template
 
 from demisto_sdk.commands.common.logger import logger
 
-validation_template = f"""
+CONTENT_TYPES_DICT = {
+    "1": {
+        "import": "from demisto_sdk.commands.content_graph.objects.integration import Integration",
+        "content_type": "Integration"
+    },
+    "2": {
+        "import": "from demisto_sdk.commands.content_graph.objects.script import Script",
+        "content_type": "Script"
+    },
+    "3": {
+        "import": "from demisto_sdk.commands.content_graph.objects.playbook import Playbook",
+        "content_type": "Playbook"
+    },
+    "4": {
+        "import": "from demisto_sdk.commands.content_graph.objects.pack import Pack",
+        "content_type": "Pack"
+    },
+    "5": {
+        "import": "from demisto_sdk.commands.content_graph.objects.dashboard import Dashboard",
+        "content_type": "Dashboard"
+    },
+    "6": {
+        "import": "from demisto_sdk.commands.content_graph.objects.classifier import Classifier",
+        "content_type": "Classifier"
+    },
+    "7": {
+        "import": "from demisto_sdk.commands.content_graph.objects.incident_type import IncidentType",
+        "content_type": "IncidentType"
+    },
+    "8": {
+        "import": "from demisto_sdk.commands.content_graph.objects.layout import Layout",
+        "content_type": "Layout"
+    },
+    "9": {
+        "import": "from demisto_sdk.commands.content_graph.objects.mapper import Mapper",
+        "content_type": "Mapper"
+    },
+    "10": {
+        "import": "from demisto_sdk.commands.content_graph.objects.wizard import Wizard",
+        "content_type": "Wizard"
+    },
+    "11": {
+        "import": "from demisto_sdk.commands.content_graph.objects.correlation_rule import CorrelationRule",
+        "content_type": "CorrelationRule"
+    },
+    "12": {
+        "import": "from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField",
+        "content_type": "IncidentField"
+    },
+    "13": {
+        "import": "from demisto_sdk.commands.content_graph.objects.incident_type import IncidentType",
+        "content_type": "IncidentType"
+    },
+    "14": {
+        "import": "from demisto_sdk.commands.content_graph.objects.indicator_field import IndicatorField",
+        "content_type": "IndicatorField"
+    },
+    "15": {
+        "import": "from demisto_sdk.commands.content_graph.objects.indicator_type import IndicatorType",
+        "content_type": "IndicatorType"
+    },
+    "16": {
+        "import": "from demisto_sdk.commands.content_graph.objects.layout_rule import LayoutRule",
+        "content_type": "LayoutRule"
+    },
+    "17": {
+        "import": "from demisto_sdk.commands.content_graph.objects.layout import Layout",
+        "content_type": "Layout"
+    },
+    "18": {
+        "import": "from demisto_sdk.commands.content_graph.objects.modeling_rule import ModelingRule",
+        "content_type": "ModelingRule"
+    },
+    "19": {
+        "import": "from demisto_sdk.commands.content_graph.objects.parsing_Rule import ParsingRule",
+        "content_type": "ParsingRule"
+    },
+    "20": {
+        "import": "from demisto_sdk.commands.content_graph.objects.report import Report",
+        "content_type": "Report"
+    },
+    "21": {
+        "import": "from demisto_sdk.commands.content_graph.objects.test_playbook import TestPlaybook",
+        "content_type": "TestPlaybook"
+    },
+    "22": {
+        "import": "from demisto_sdk.commands.content_graph.objects.trigger import Trigger",
+        "content_type": "Trigger"
+    },
+    "23": {
+        "import": "from demisto_sdk.commands.content_graph.objects.widget import Widget",
+        "content_type": "Widget"
+    },
+    "24": {
+        "import": "from demisto_sdk.commands.content_graph.objects.generic_definition import GenericDefinition",
+        "content_type": "GenericDefinition"
+    },
+    "25": {
+        "import": "from demisto_sdk.commands.content_graph.objects.generic_field import GenericField",
+        "content_type": "GenericField"
+    },
+    "26": {
+        "import": "from demisto_sdk.commands.content_graph.objects.generic_module import GenericModule",
+        "content_type": "GenericModule"
+    },
+    "27": {
+        "import": "from demisto_sdk.commands.content_graph.objects.generic_type import GenericType",
+        "content_type": "GenericType"
+    },
+    "28": {
+        "import": "from demisto_sdk.commands.content_graph.objects.xsiam_dashboard import XSIAMDashboard",
+        "content_type": "XSIAMDashboard"
+    },
+    "29": {
+        "import": "from demisto_sdk.commands.content_graph.objects.xsiam_report import XSIAMReport",
+        "content_type": "XSIAMReport"
+    }
+}
+
+VALIDATION_TEMPLATE = """
 from __future__ import annotations
 
-from typing import Iterable, List, Union
+$imports
 
-from demisto_sdk.commands.content_graph.objects.classifier import Classifier
-from demisto_sdk.commands.content_graph.objects.dashboard import Dashboard
-from demisto_sdk.commands.content_graph.objects.incident_type import IncidentType
-from demisto_sdk.commands.content_graph.objects.integration import Integration
-from demisto_sdk.commands.content_graph.objects.layout import Layout
-from demisto_sdk.commands.content_graph.objects.mapper import Mapper
-from demisto_sdk.commands.content_graph.objects.playbook import Playbook
-from demisto_sdk.commands.content_graph.objects.script import Script
-from demisto_sdk.commands.content_graph.objects.wizard import Wizard
-from demisto_sdk.commands.validate.validators.base_validator import (
-    BaseValidator,
-    FixingResult,
-    ValidationResult,
-)
-
-ContentTypes = Union[
-    Integration,
-    Dashboard,
-    IncidentType,
-    Layout,
-    Mapper,
-    Playbook,
-    Script,
-    Wizard,
-    Classifier,
-]
+$content_types
 
 
 class IDNameValidator(BaseValidator[ContentTypes]):
     error_code = "$error_code"
     description = "$error_description"
     error_message = "$error_message"
-    fixing_message = "Changing name to be equal to id ({0})."
+    fixing_message = $fix_message
     related_field = "$related_field"
     $expected_git_statuses
 
@@ -59,22 +153,20 @@ class IDNameValidator(BaseValidator[ContentTypes]):
             if content_item.object_id != content_item.name
         ]
 
-    def fix(self, content_item: ContentTypes, _) -> FixingResult:
-        content_item.name = content_item.object_id
-        return FixingResult(
-            validator=self,
-            message=self.fixing_message.format(content_item.object_id),
-            content_object=content_item,
-        )
+    $fix_method
 """
 class ValidationInitializer():
     def init(self):
         self.error_code = None
         self.description = ""
         self.error_message = ""
+        self.fix_method = ""
+        self.include_old_format_files = False
         self.get_error_details()
         self.get_validation_details()
         # self.get_file_name()
+        self.get_fix_info()
+        self.process_inputs()
         self.generate_new_py_file()
 
     def get_error_details(self):
@@ -152,7 +244,7 @@ class ValidationInitializer():
         file_name = "new_script.py"
         with open(file_name, "w") as file:
             # Write the content into the file
-            new_file_content = Template(validation_template).safe_substitute(error_code = self.error_code)
+            new_file_content = Template(VALIDATION_TEMPLATE).safe_substitute(error_code = self.error_code)
             file.write(new_file_content)
         
     def get_validation_details(self):
@@ -185,10 +277,67 @@ class ValidationInitializer():
         )
         if git_statuses:
             self.git_statuses = git_statuses.split(",")
-            if not "A" in self.git_statuses and not "D" in self.git_statuses:
-                self.include_old_format_files = True
-            else:
-                self.git_statuses = 
+            if "A" not in self.git_statuses and "D" not in self.git_statuses:
+                self.include_old_format_files_is_valid_method  = ", old_content_items: Iterable[Optional[ContentTypes]]"
+                self.include_old_format_files_fix_method = ", old_content_item: ContentTypes"
+        else:
+            self.include_old_format_files_is_valid_method = ", _"
+            self.include_old_format_files_fix_method = ", _"
+            self.git_statuses = None
+    
+    def get_content_types(self):
+        """Makes sure a name is given for the created object
+        """
+        supported_content_types = [f"{key}: {value.get("content_type")}" for key, value in CONTENT_TYPES_DICT.items()]
+        content_types = str(
+            input("Enter a comma separated list of content types to be supported by the validation.\n"
+                  f"The supported types are {supported_content_types.split("\n")}")
+        )
+        while not content_types:
+            content_types = str(input("Please enter at least one content type:"))
+        self.content_types = content_types.split(",")
+        
+    def process_inputs(self):
+        self.process_content_types()
+        
+    def process_content_types(self):
+        if len (self.content_types) == 1:
+            self.supported_content_types = f"ContentTypes = {self.content_types}"
+            self.imports = "from typing import Iterable, List\n\n"
+        else:
+            self.supported_content_types = f"ContentTypes = {','.join(self.content_types)}"
+            self.imports = "from typing import Iterable, List, Union\n\n"
+        for content_type in self.content_types:
+            self.imports += f"{CONTENT_TYPES_DICT.get(content_type, {}).get('import', "")}\n"
+            
+    def process_fix_result_related_fields(self):
+        if self.support_fix:
+            self.imports += """from demisto_sdk.commands.validate.validators.base_validator import (
+        BaseValidator,
+        FixingResult,
+        ValidationResult,
+    )"""
+            self.fix_method = f"""def fix(self, content_item: ContentTypes{self.include_old_format_files_fix_method}) -> FixResult:
+        # Add your fix right here
+        pass
+            """
+        else:
+            self.imports += """from demisto_sdk.commands.validate.validators.base_validator import (
+        BaseValidator,
+        ValidationResult,
+    )"""
+
+    def get_fix_info(self):
+        support_fix = str(
+            input("does the validation support fix? (Y/N):")
+        )
+        while not support_fix and support_fix not in ["Y", "N", "y", "n"]:
+            support_fix = str(input("Please enter wether the validation support fix or not? (Y/N):"))
+        if support_fix in ["Y", "y"]:
+            self.support_fix = True
+        else:
+            self.support_fix = False
+                    
                 
 def main():
     logger.info("Creating a new validation")

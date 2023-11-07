2f445209accf77f160fb596c526b005e5d50bedf
ilaner <88267954+ilaner@users.noreply.github.com>
2023-11-07 17:47:27 +0200

    Update demisto_sdk/commands/content_graph/commands/update.py
    Co-authored-by: Dan Tavori <38749041+dantavori@users.noreply.github.com>

--- demisto_sdk/commands/content_graph/commands/update.py
+++ demisto_sdk/commands/content_graph/commands/update.py
@@ -90,7 +90,7 @@ def update_content_graph(
         content_graph_interface, use_git, git_util, imported_path, packs_to_update
     ):
         logger.info(
-            f"Content graph is up to date, no need to update. Make sure to add/commit your changes. UI representation is available at {NEO4J_DATABASE_HTTP} "
+            f"Content graph is up-to-date. If you expected an update, make sure your changes are added/committed to git. UI representation is available at {NEO4J_DATABASE_HTTP} "
             f"(username: {NEO4J_USERNAME}, password: {NEO4J_PASSWORD})"
         )
         content_graph_interface.export_graph(

f5debfe68edaad50c12ac904c240f8a2c3d6e681
ilan <ierukhimovic@paloaltonetworks.com>
2023-11-08 14:51:05 +0200

    remove clean

--- demisto_sdk/commands/content_graph/content_graph_builder.py
+++ demisto_sdk/commands/content_graph/content_graph_builder.py
@@ -93,6 +93,5 @@ class ContentGraphBuilder:
         self.content_graph.create_nodes(self.nodes)
         gc.collect()
         self.content_graph.create_relationships(self.relationships)
-        self.content_graph.clean()
         gc.collect()
         self.content_graph.finalize()

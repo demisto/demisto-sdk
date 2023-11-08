e71ea4e38b9b82b8b893584a21f15de7fbdb974e
ilan <ierukhimovic@paloaltonetworks.com>
2023-11-08 13:48:01 +0200

    create constraints after import

--- demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/neo4j_graph.py
@@ -636,6 +636,7 @@ class Neo4jContentGraphInterface(ContentGraphInterface):
         with self.driver.session() as session:
             session.execute_write(drop_constraints)
             session.execute_write(import_graphml, graphml_filenames)
+            session.execute_write(create_constraints)
 
         has_infra_graph_been_changed = self._has_infra_graph_been_changed()
         self._id_to_obj = {}

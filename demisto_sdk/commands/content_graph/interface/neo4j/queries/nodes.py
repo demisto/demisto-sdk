6bb21252c306ea244d12e3c280218ee23a189313
ilan <ierukhimovic@paloaltonetworks.com>
2023-11-08 16:12:42 +0200

    syntax

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/nodes.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/nodes.py
@@ -286,5 +286,5 @@ MATCH (m:BaseContent{content_type: n.content_type, not_in_repository: false})
 WHERE ((m.object_id = n.object_id AND m.object_id <> "") OR (m.name = n.name AND m.name <> ""))
 WITH m, n
 CALL apoc.refactor.mergeNodes([m, n], {properties: "discard", mergeRels: true}) YIELD node
-RETURN count(*)'),{limit: 1000}""",
+RETURN count(*)',{limit: 1000})""",
     )

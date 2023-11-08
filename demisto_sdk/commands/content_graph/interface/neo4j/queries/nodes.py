8265e21360023aa3bf16a53553d9f5e31b4b58af
ilan <ierukhimovic@paloaltonetworks.com>
2023-11-08 15:59:53 +0200

    try periodic commit

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/nodes.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/nodes.py
@@ -280,10 +280,11 @@ def merge_duplicate_content_items(tx: Transaction) -> None:
     run_query(
         tx,
         """// Merges possible duplicate content item nodes after import
+CALL apoc.periodic.commit('
 MATCH (n:BaseContent{not_in_repository: true})
 MATCH (m:BaseContent{content_type: n.content_type, not_in_repository: false})
 WHERE ((m.object_id = n.object_id AND m.object_id <> "") OR (m.name = n.name AND m.name <> ""))
 WITH m, n
 CALL apoc.refactor.mergeNodes([m, n], {properties: "discard", mergeRels: true}) YIELD node
-RETURN count(*)""",
+RETURN count(*)'),{limit: 1000}""",
     )

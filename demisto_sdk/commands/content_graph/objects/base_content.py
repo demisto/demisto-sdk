13dca5046744b6c9a5509baa5a20281c1bdacb42
YuvHayun <yhayun011@gmail.com>
2023-11-09 16:55:54 +0200

    pre commit fixes

--- demisto_sdk/commands/content_graph/objects/base_content.py
+++ demisto_sdk/commands/content_graph/objects/base_content.py
@@ -46,7 +46,7 @@ from demisto_sdk.commands.content_graph.parsers.pack import PackParser
 if TYPE_CHECKING:
     from demisto_sdk.commands.content_graph.objects.relationship import RelationshipData
 
-CONTENT_TYPE_TO_MODEL: Dict[ContentType, Type["BaseNode"]] = {}
+CONTENT_TYPE_TO_MODEL: Dict[ContentType, Type["BaseContent"]] = {}
 json = JSON_Handler()
 
 
@@ -75,7 +75,7 @@ class BaseContentMetaclass(ModelMetaclass):
         """
         super_cls: BaseContentMetaclass = super().__new__(cls, name, bases, namespace)
         # for type checking
-        model_cls: Type["BaseNode"] = cast(Type["BaseNode"], super_cls)
+        model_cls: Type["BaseContent"] = cast(Type["BaseContent"], super_cls)
         if content_type:
             CONTENT_TYPE_TO_MODEL[content_type] = model_cls
             model_cls.content_type = content_type
@@ -252,8 +252,7 @@ class BaseContent(BaseNode):
             or path.name == PACKS_PACK_META_FILE_NAME
         ):  # if the path given is a pack
             try:
-                content_type_to_model = cast(Type["BaseContent"], CONTENT_TYPE_TO_MODEL)
-                return content_type_to_model[ContentType.PACK].from_orm(
+                return CONTENT_TYPE_TO_MODEL[ContentType.PACK].from_orm(
                     PackParser(path, git_sha=git_sha)
                 )
             except InvalidContentItemException:

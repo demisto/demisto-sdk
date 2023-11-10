7cc1e54933471a66af0671812df3fe01a2de4321
YuvHayun <yhayun011@gmail.com>
2023-11-10 16:11:58 +0200

    Tests

--- demisto_sdk/commands/content_graph/common.py
+++ demisto_sdk/commands/content_graph/common.py
@@ -46,7 +46,8 @@ class RelationshipType(str, enum.Enum):
 
 
 class ContentType(str, enum.Enum):
-    BASE_CONTENT = "BaseNode"
+    BASE_CONTENT = "BaseContent"
+    BASE_NODE = "BaseNode"
     CLASSIFIER = "Classifier"
     COMMAND = "Command"
     COMMAND_OR_SCRIPT = "CommandOrScript"
@@ -84,7 +85,9 @@ class ContentType(str, enum.Enum):
 
     @property
     def labels(self) -> List[str]:
-        labels: Set[str] = {ContentType.BASE_CONTENT.value, self.value}
+        labels: Set[str] = {ContentType.BASE_NODE.value, self.value}
+        if self.value != ContentType.COMMAND:
+            labels.add(ContentType.BASE_CONTENT.value)
 
         if self.value == ContentType.TEST_PLAYBOOK.value:
             labels.add(ContentType.PLAYBOOK.value)

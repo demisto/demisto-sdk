6c7b520c3661361f0c9d3cf1494b94cbdd6bf2f0
MLainer1 <mlainer@paloaltonetworks.com>
2023-11-15 14:33:16 +0200

    doc strings

--- tests_end_to_end/xsiam/test_e2e_demisto_sdk_flow_playbook_xsiam.py
+++ tests_end_to_end/xsiam/test_e2e_demisto_sdk_flow_playbook_xsiam.py
@@ -20,6 +20,14 @@ from TestSuite.test_tools import ChangeCWD
 
 
 def test_e2e_demisto_sdk_flow_playbook_testsuite(tmpdir):
+    """This flow checks:
+        1. Creates a new playbook and uploads it demisto-sdk upload command.
+        2. Downloads the playbook using demisto-sdk upload command.
+        3. Generates docs for the playbook using demisto-sdk generate-docs command.
+        4. Formatting the playbook using the demisto-sdk format command.
+        5. Validates the playbook using the demisto-sdk validate command.
+        6. Uploads the playbook using the demisto-sdk upload command.
+    """
     # Importing TestSuite classes from Demisto-SDK, as they are excluded when pip installing the SDK.
     e2e_tests_utils.cli(f"mkdir {tmpdir}/git")
     e2e_tests_utils.git_clone_demisto_sdk(
@@ -81,10 +89,15 @@ def test_e2e_demisto_sdk_flow_playbook_testsuite(tmpdir):
 
 
 def test_e2e_demisto_sdk_flow_playbook_client(tmpdir, verify_ssl: bool = False):
-    """The flow the test is using:
-        1. Creates a new playbook and uploading it to the machine using an http request
-        2. Downloads the playbook using demisto-sdk upload command
-        3. 
+    """This flow checks:
+        1. Creates a new playbook and uploading it to the machine using an http request.
+        2. Downloads the playbook using demisto-sdk download command.
+        3. Downloads the script CommonServerUserPowerShell using demisto-sdk upload command.
+        4. Generates docs for the playbook using demisto-sdk generate-docs command.
+        5. Formatting the playbook using the demisto-sdk format command.
+        6. Validates the playbook using the demisto-sdk validate command.
+        7. Uploads the playbook using the demisto-sdk upload command.
+        8. Deletes the playbook using an http request.
     """
     demisto_client = get_client_from_server_type(verify_ssl=verify_ssl)
 
@@ -109,13 +122,16 @@ def test_e2e_demisto_sdk_flow_playbook_client(tmpdir, verify_ssl: bool = False):
     # Preparing updated pack folder
     e2e_tests_utils.cli(f"mkdir -p {tmpdir}/Packs/{pack_name}_client")
 
+    logger.info(
+        f"Checking which files we can download from the machine."
+    )
     Downloader(
         list_files=True,
         insecure=True,
     ).download()
 
     logger.info(
-        f"Trying to download the updated playbook from {playbook_name} to {tmpdir}/Packs/{pack_name}_client/Playbooks"
+        f"Trying to download the updated playbook from {playbook_name} to {tmpdir}/Packs/{pack_name}_client/Playbooks."
     )
     Downloader(
         output=f"{tmpdir}/Packs/{pack_name}_client",
@@ -130,7 +146,7 @@ def test_e2e_demisto_sdk_flow_playbook_client(tmpdir, verify_ssl: bool = False):
     assert not dest_playbook_path.exists()
 
     logger.info(
-        f"Trying to download the CommonServerUserPowerShell file to {tmpdir}/Packs/{pack_name}_client/Playbooks"
+        f"Trying to download the CommonServerUserPowerShell file to {tmpdir}/Packs/{pack_name}_client/Playbooks."
     )
     Downloader(
         output=f"{tmpdir}/Packs/{pack_name}_client",
@@ -139,27 +155,35 @@ def test_e2e_demisto_sdk_flow_playbook_client(tmpdir, verify_ssl: bool = False):
     ).download()
 
     logger.info(
-        f"Generating docs (creating a readme file for the playbook {source_playbook_path}"
+        f"Generating docs (creating a readme file for the playbook {source_playbook_path}."
     )
     generate_playbook_doc.generate_playbook_doc(input_path=str(source_playbook_path))
     assert Path(
         f"{tmpdir}/Packs/{pack_name}/Playbooks/{playbook_name}_README.md"
     ).exists()
 
-    logger.info(f"Formating playbook {source_playbook_path}")
+    logger.info(f"Formating playbook {source_playbook_path}.")
 
     with ChangeCWD(str(Path(source_playbook_path).parent)):
         format_manager(
             input=source_playbook_path,
             assume_answer=True,
         )
-        logger.info(f"Validating playbook {source_playbook_path}")
+        logger.info(f"Validating playbook {source_playbook_path}.")
         ValidateManager(file_path=source_playbook_path).run_validation()
 
-        logger.info(f"Uploading updated playbook {source_playbook_path}")
+        logger.info(f"Uploading updated playbook {source_playbook_path}.")
         Uploader(input=Path(source_playbook_path), insecure=True, zip=True, marketplace=MarketplaceVersions.MarketplaceV2).upload()
 
     try:
         demisto_client.delete_playbook(playbook_name, playbook_name)
     except ApiException as ae:
-        logger.info(f"*** Failed to delete playbook {playbook_name}, reason: {ae}")
+        logger.info(f"*** Failed to delete playbook {playbook_name}, reason: {ae}.")
+
+
+def test_e2e_demisto_sdk_flow_modeling_rules(tmpdir):
+    """This flow checks:
+    1. Uploads the pack HelloWorld with the modeling rules HelloWorldModelingRule using the demisto-sdk upload command
+    2. Tests the modeling rules using the demisto-sdk modeling-rules test command
+    """
+    

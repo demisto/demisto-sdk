7c7b61b8530eb9b41b52d830504f8a48801d2a30
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2023-11-20 17:19:28 +0200

    Add debug level log if version fetch failed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # Changelog
 ## Unreleased
+* Added a debug-level log with more information in case connection to the server failed while trying to fetch its version.
 
 ## 1.23.0
 * Added support for inputs sections and outputs sections in a playbook.
--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -2551,7 +2551,8 @@ def get_demisto_version(client: demisto_client) -> Version:
         resp = client.generic_request("/about", "GET")
         about_data = json.loads(resp[0].replace("'", '"'))
         return Version(Version(about_data.get("demistoVersion")).base_version)
-    except Exception:
+    except Exception as e:
+        logger.debug(f"Failed to get server version. Error: {e}")
         logger.warning(
             "Could not parse server version, please make sure the environment is properly configured."
         )

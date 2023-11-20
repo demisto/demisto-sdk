90e30b25fbc501f1365b956a564a53ed0388145d
yaakovpraisler <ypreisler@paloaltonetworks.com>
2023-11-20 10:25:47 +0200

    black

--- demisto_sdk/commands/content_graph/objects/pack_metadata.py
+++ demisto_sdk/commands/content_graph/objects/pack_metadata.py
@@ -229,7 +229,8 @@ class PackMetadata(BaseModel):
             or any(
                 [
                     playbook.name.startswith("TIM ")
-                    for playbook in content_items.playbook if not playbook.is_test
+                    for playbook in content_items.playbook
+                    if not playbook.is_test
                 ]
             )
             else set()
@@ -237,12 +238,24 @@ class PackMetadata(BaseModel):
         tags |= {PackTags.USE_CASE} if self.use_cases else set()
         tags |= (
             {PackTags.TRANSFORMER}
-            if any(["transformer" in script.tags for script in content_items.script if not script.is_test])
+            if any(
+                [
+                    "transformer" in script.tags
+                    for script in content_items.script
+                    if not script.is_test
+                ]
+            )
             else set()
         )
         tags |= (
             {PackTags.FILTER}
-            if any(["filter" in script.tags for script in content_items.script if not script.is_test])
+            if any(
+                [
+                    "filter" in script.tags
+                    for script in content_items.script
+                    if not script.is_test
+                ]
+            )
             else set()
         )
         tags |= (

734737b89f925718062ac67287c1f753808e5303
MosheEichler <meichler@paloaltonetworks.com>
2023-11-26 15:34:30 +0200

    code review fixes

--- demisto_sdk/scripts/update_demisto_sdk_version.sh
+++ demisto_sdk/scripts/update_demisto_sdk_version.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-# Get CD path list from argument
+# Get repositories path list from argument
 repositories=("$@")
 
-# Validate CD path list
+# Validate repositories path list
 if [ ${#repositories[@]} -eq 0 ]; then
   echo "Usage: $0 <repository1> <repository2> ..."
   exit 1
 fi
 
-# Save current path
-current_path="$PWD"
+valid_repositories=()
+invalid_repositories=()
 
-# Loop through paths
 for repository in "${repositories[@]}"
 do
   # Validate path
   if [ ! -d "$repository" ]; then
-    echo "Error: $repository is not a valid directory."
-    exit 1
+    invalid_repositories+=("$repository")
+  else
+    valid_repositories+=("$repository")
   fi
+done
 
-  # CD to path
-  cd "$repository" || exit
+# Validate repositories path list
+if [ ${#valid_repositories[@]} -eq 0 ]; then
+  echo "No valid repositories are found"
+  exit 1
+fi
+
+# Loop through valid repositories
+for valid_repository in "${valid_repositories[@]}"
+do
+  # Move to repository path
+  pushd "$valid_repository" || exit
 
   # Do git commands...
   current_content_branch=$(git branch --show-current)
@@ -34,5 +44,14 @@ do
   git checkout "$current_content_branch"
 done
 
-# CD back to original path
-cd "$current_path" || exit
+# Move back to original path
+popd || exit
+
+# Check if invalid_repositories is not empty and print its items
+if [ ${#invalid_repositories[@]} -gt 0 ]; then
+  echo "The following invalid repositories are found:"
+  for invalid_repo in "${invalid_repositories[@]}"
+  do
+    echo "  - $invalid_repo"
+  done
+fi

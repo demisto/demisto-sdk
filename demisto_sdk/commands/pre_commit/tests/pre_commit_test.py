aef6cf496f5c622bb80a6e1ee3aacde60cc192e3
ilan <ierukhimovic@paloaltonetworks.com>
2023-11-29 12:41:27 +0200

    fix test

--- demisto_sdk/commands/pre_commit/tests/pre_commit_test.py
+++ demisto_sdk/commands/pre_commit/tests/pre_commit_test.py
@@ -130,7 +130,7 @@ def test_config_files(mocker, repo: Repo, is_test: bool):
     if not is_test:
         tests_we_should_skip.add("run-unit-tests")
         tests_we_should_skip.add("coverage-analyze")
-        tests_we_should_skip.add("merge-coverage-report")
+        tests_we_should_skip.add("merge-pytest-reports")
     for m in mock_subprocess.call_args_list:
         assert set(m.kwargs["env"]["SKIP"].split(",")) == tests_we_should_skip
 

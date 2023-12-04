a9c7bb3ec0b02644ed8e79e16cb47cd21244bd48
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-04 22:07:44 +0200

    fix additional reqs

--- demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
+++ demisto_sdk/commands/lint/resources/installation_scripts/python_image.sh
@@ -18,7 +18,7 @@ then
     apt-get update && apt-get install -y --no-install-recommends gcc python3-dev
 fi
 pip install --no-cache-dir --progress-bar off -r /test-requirements.txt
-pip install --no-cache-dir --progress-bar off -r /additonal-requirements.txt
+pip install --no-cache-dir --progress-bar off -r /additional-requirements.txt
 
 if [ "$ID" = "alpine" ]
 then

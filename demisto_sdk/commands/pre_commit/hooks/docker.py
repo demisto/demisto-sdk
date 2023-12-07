a79b02c22a51b262075121bff7498ef49f1a401e
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-07 14:24:52 +0200

    add split_on_file option

--- demisto_sdk/commands/pre_commit/hooks/docker.py
+++ demisto_sdk/commands/pre_commit/hooks/docker.py
@@ -165,6 +165,7 @@ def get_environment_flag(env: dict) -> str:
 def _split_by_objects(
     files_with_objects: List[Tuple[Path, IntegrationScript]],
     config_arg: Optional[Tuple],
+    split_by_file: Optional[str],
 ) -> Dict[Optional[IntegrationScript], Set[Tuple[Path, IntegrationScript]]]:
     """
     Will group files into groups that share the same configuration file.
@@ -172,6 +173,7 @@ def _split_by_objects(
     Args:
         files: the files to split
         config_arg: a tuple, argument_name, file_name
+        split_by_file: if this file exists, split it to a seperate hook
 
     Returns:
         a dict where the keys are the names of the folder of the config and the value is a set of files for that config
@@ -182,8 +184,10 @@ def _split_by_objects(
 
     for file, obj in files_with_objects:
         if (
-            config_arg and (obj.path.parent / config_arg[1]).exists()
-        ) or obj.additional_test_requirements:
+            (config_arg and (obj.path.parent / config_arg[1]).exists())
+            or (split_by_file and (obj.path.parent / split_by_file).exists())
+            or obj.additional_test_requirements
+        ):
             object_to_files[obj].add((file, obj))
         else:
             object_to_files[None].add((file, obj))
@@ -238,14 +242,16 @@ class DockerHook(Hook):
             f"Elapsed time to gather tags to files: {end_time - start_time} seconds"
         )
         config_arg = self._get_config_file_arg()
-
+        split_by_file = self._get_property("split_by_file")
         start_time = time.time()
         logger.info(f"{len(tag_to_files_objs)} images were collected from files")
         logger.debug(f'collected images: {" ".join(tag_to_files_objs.keys())}')
         for image, files_with_objects in sorted(
             tag_to_files_objs.items(), key=lambda item: item[0]
         ):
-            object_to_files = _split_by_objects(files_with_objects, config_arg)
+            object_to_files = _split_by_objects(
+                files_with_objects, config_arg, split_by_file
+            )
             image_is_powershell = any(
                 obj.is_powershell for _, obj in files_with_objects
             )

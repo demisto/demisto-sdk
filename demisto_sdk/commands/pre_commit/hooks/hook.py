a4e0c688e0c36b8a0927c05b250391b3fc5632bb
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-10 15:07:40 +0200

    parenthe

--- demisto_sdk/commands/pre_commit/hooks/hook.py
+++ demisto_sdk/commands/pre_commit/hooks/hook.py
@@ -145,7 +145,7 @@ def join_files(files: Set[Path], separator: str = "|") -> str:
         str: The joined string.
     """
     # qoute files to avoid issues with special characters
-    return separator.join(f"'{str(file)}'" for file in files)
+    return separator.join(f"({str(file)})" for file in files)
 
 
 def safe_update_hook_args(hook: Dict, value: Any) -> None:

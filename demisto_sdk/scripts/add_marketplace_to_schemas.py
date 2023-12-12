496a6709d543e05333e776c716102ede4ce65fb2
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-12 11:00:37 +0200

    update schemas and script

--- demisto_sdk/scripts/add_marketplace_to_schemas.py
+++ demisto_sdk/scripts/add_marketplace_to_schemas.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from pathlib import Path
+from typing import List
 
 from demisto_sdk.commands.common.legacy_git_tools import git_path
 from demisto_sdk.commands.common.tools import get_file, write_dict
@@ -7,22 +8,35 @@ from demisto_sdk.commands.common.tools import get_file, write_dict
 GIT_ROOT = Path(git_path())
 SCHEMA_FOLDER = GIT_ROOT / "demisto_sdk" / "commands" / "common" / "schemas"
 NON_SUPPORTED_KEYS = ["id"]
+SUPPORTED_KEYS = [
+    "isfetch",
+    "defaultValue",
+    "defaultvalue",
+    "required",
+    "description",
+    "name",
+]
 
 
 def add_key(mapping):
-    if not mapping:
+    if not mapping or not isinstance(mapping, dict):
         return
     keys_to_add = []
-    keys_to_delete = []
+    keys_to_delete: List[str] = []
     for key, value in mapping.items():
-        if key in NON_SUPPORTED_KEYS:
-            for key, value in mapping.items():
-                if f"regex;{key}:" in key:
-                    keys_to_delete.append(key)
-            continue
+        if key in NON_SUPPORTED_KEYS or (SUPPORTED_KEYS and key not in SUPPORTED_KEYS):
+            keys_to_delete.extend(
+                k for k, _ in mapping.items() if f"regex;({key}:" in k
+            )
         if value.get("type") != "map":
+
             if "regex;" in key and "(.+)" in key:
                 continue
+            if key in NON_SUPPORTED_KEYS or (
+                SUPPORTED_KEYS and key not in SUPPORTED_KEYS
+            ):
+                continue
+
             value = deepcopy(value)
             value.pop("required", None)
             if "regex;" in key:
@@ -44,7 +58,13 @@ def main():
     ]
     for schema_file in schema_files:
         schema = get_file(schema_file, keep_order=True)
-        add_key(schema["mapping"])
+        for key in schema:
+            if key == "mapping":
+                add_key(schema[key])
+            else:
+                if not isinstance(schema[key], dict):
+                    continue
+                add_key(schema[key].get("mapping"))
         write_dict(schema_file, schema)
 
 

66d3741d93c8a6428c5ae5c136af9c7072c2801e
Yehuda <yrosenberg@paloaltonetworks.com>
2023-12-12 12:53:23 +0200

    final

--- demisto_sdk/commands/validate/validators/PB_validators/PB118_is_input_key_not_in_tasks.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB118_is_input_key_not_in_tasks.py
@@ -1,5 +1,6 @@
 
 from __future__ import annotations
+import pathlib
 import re
 
 from typing import Iterable, List
@@ -17,43 +18,63 @@ ContentTypes = Playbook
 
 class IsInputKeyNotInTasksValidator(BaseValidator[ContentTypes]):
     error_code = "PB118"
-    description = ""
-    error_message = ""
+    description = "Validate that all inputs described in the playbooks input section are used in tasks."
+    error_message = "The playbook '{playbook_name}' contains inputs that are not used in any of its tasks: {inputs_not_in_use} "
     fix_message = ""
     related_field = ""
     is_auto_fixable = False
     expected_git_statuses = [GitStatuses.ADDED, GitStatuses.MODIFIED]
 
+
+    def collect_all_inputs_in_use(self, content_item:Playbook) -> set[str]:
+        """
+        Args:
+            - content_item (Playbook): The content item to collect inputs from.
+        Returns:
+            - Set of all inputs used in playbook.
+        """
+        result: set = set()
+        playbook_text = pathlib.Path(str(content_item.path)).read_text()
+        all_inputs_occurrences = re.findall(r"inputs\.[-\w ?!():]+", playbook_text)
+        for input in all_inputs_occurrences:
+            input = input.strip()
+            splitted = input.split(".")
+            if len(splitted) > 1 and splitted[1] and not splitted[1].startswith(" "):
+                result.add(splitted[1])
+        return result
+    
+    
+    def collect_all_inputs_from_inputs_section(self, content_item: Playbook) -> set[str]:
+        """
+        Args:
+            - content_item (Playbook): The content item to collect inputs from.
+        Returns:
+            - A set of all inputs defined in the 'inputs' section of playbook.
+        """
+        inputs: dict = content_item.data.get("inputs", {})
+        inputs_keys = [input["key"].strip() for input in inputs if input["key"]]
+        return set(inputs_keys)
+    
+    
     def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        """Check whether all playbook inputs (defined in the "inputs" section) are in use in any of the tasks
+        Args:
+            - content_items (Iterable[ContentTypes]): The content items to check.
+        Return:
+            - List[ValidationResult]. List of ValidationResults objects.
+        """
         results: List[ValidationResult] = []
         for content_item in content_items:
-            # def collect_all_inputs_in_use(self) -> Set[str]:
-            result: set = set()
-            with open(str(content_item.path)) as f:
-                playbook_text = f.read()
-            all_inputs_occurrences = re.findall(r"inputs\.[-\w ?!():]+", playbook_text)
-            for input in all_inputs_occurrences:
-                input = input.strip()
-                splitted = input.split(".")
-                if len(splitted) > 1 and splitted[1] and not splitted[1].startswith(" "):
-                    result.add(splitted[1])
-            inputs_in_use =  result
-            
-            #def collect_all_inputs_from_inputs_section(self) -> Set[str]:
-            inputs: dict = content_item.data.get("inputs", {})
-            inputs_keys = []
-            for input in inputs:
-                if input["key"]:
-                    inputs_keys.append(input["key"].strip())
-            inputs_in_section = set(inputs_keys)
-            
-            inputs_not_in_section = inputs_in_use.difference(inputs_in_section)
-            if inputs_not_in_section:
-                results.append(
+            inputs_in_use =  self.collect_all_inputs_in_use(content_item)
+            inputs_in_section = self.collect_all_inputs_from_inputs_section(content_item)
+
+            if inputs_not_in_use := inputs_in_section.difference(inputs_in_use):
+                results.append(ValidationResult(
                 validator=self,
-                message=self.error_message,
+                message=self.error_message.format(
+                            playbook_name=content_item.name,
+                            inputs_not_in_use=', '.join(inputs_not_in_use)
+                        ),
                 content_object=content_item
-                )
- 
-        
+                ))
         return results

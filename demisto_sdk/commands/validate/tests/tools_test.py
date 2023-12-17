b8e43596370cd904e4362e4588e99d7e89a53794
Yehuda <yrosenberg@paloaltonetworks.com>
2023-12-17 10:54:38 +0200

    finally tests

--- demisto_sdk/commands/validate/tests/tools_test.py
+++ demisto_sdk/commands/validate/tests/tools_test.py
@@ -6,32 +6,34 @@ from demisto_sdk.commands.validate.validators.tools import (
 )
 
 @pytest.mark.parametrize("input_name, expected_result", [
-    ("inputs.hello", {"hello"}),
-    ("inputs", {}),
+    ({"0":{"inputs.hello":"test"}, "1":{"inputs.example":"test"}}, {'hello: test', 'example: test'}),
+    ({"0":{"inputs":"test"}, "1":{"inputs":"test"}}, set())
 ])
 def test_collect_all_inputs_in_use(input_name, expected_result):
     """
     Given:
-        - A playbook with inputs 
+        - A playbook with inputs in some tasks
     When:
-        - 
+        - Running collect_all_inputs_in_use
     Then:
-        - A set of all inputs defined in the 'inputs' section of playbook should be returned
+        - Return a set of input names and values from any task in the playbook, if the inputs match the pattern inputs.<input_name>
+        scenario 1: inputs.hello: test
+        scenario 2: inputs: test
     """
-    playbook = create_playbook_object(paths=["name"], values=[input_name])
+    playbook = create_playbook_object(paths=["tasks"], values=[input_name])
+
     assert collect_all_inputs_in_use(playbook) == expected_result
 
 
 def test_collect_all_inputs_from_inputs_section():
     """
     Given:
-        - A playbook with input1 and input2 defined
+        - A playbook with inputs defined in the inputs section
     When:
         - Running collect_all_inputs_from_inputs_section
     Then:
-        - A set with input1 and input2 should be returned
+        - A set with all inputs defined in the inputs section with no duplicates or empty spaces are returned.
     """
-    playbook = create_playbook_object()
-    playbook.data["inputs"] =[{"key": "inputs.test     ", "test text": "test"}, {"key": "inputs.test2", "test text": "test"}]
+    playbook = create_playbook_object(paths=["inputs"], values=[[{"key": "inputs.test     ", "test text": "test"}, {"key": "inputs.test", "test text": "test"}, {"key": "inputs.test2", "test text": "test"}]])
 
-    assert collect_all_inputs_from_inputs_section(playbook) == {'inputs.test2', 'inputs.test'}
+    assert collect_all_inputs_from_inputs_section(playbook) == {'inputs.test', 'inputs.test2'}

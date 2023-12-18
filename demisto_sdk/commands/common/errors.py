10e4a5a31f19658cf0630779a54f445255c39865
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-18 17:32:41 +0200

    add server nodes

--- demisto_sdk/commands/common/errors.py
+++ demisto_sdk/commands/common/errors.py
@@ -4372,6 +4372,7 @@ class Errors:
     @staticmethod
     @error_code_decorator
     def using_unknown_content(content_name: str, unknown_content_names: List[str]):
+        unknown_content_names = [f"'{name}'" for name in unknown_content_names]
         return f"Content item '{content_name}' using content items: {', '.join(unknown_content_names)} which cannot be found in the repository."
 
     @staticmethod
@@ -4379,6 +4380,7 @@ class Errors:
     def multiple_packs_with_same_display_name(
         content_name: str, pack_display_names: List[str]
     ):
+        pack_display_names = [f"'{name}'" for name in pack_display_names]
         return f"Pack '{content_name}' has a duplicate display_name as: {', '.join(pack_display_names)} "
 
     @staticmethod

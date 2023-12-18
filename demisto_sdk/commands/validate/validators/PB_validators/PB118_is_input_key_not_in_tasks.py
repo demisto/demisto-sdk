bed74a4b0737c88f95c0d5624fbafcf7aae98dc1
Yehuda <yrosenberg@paloaltonetworks.com>
2023-12-18 10:03:13 +0200

    ruff

--- demisto_sdk/commands/validate/validators/PB_validators/PB118_is_input_key_not_in_tasks.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB118_is_input_key_not_in_tasks.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
+
 from typing import Iterable, List
 
 from demisto_sdk.commands.common.constants import GitStatuses
 from demisto_sdk.commands.common.hook_validations.playbook import PlaybookValidator
-
 from demisto_sdk.commands.content_graph.objects.playbook import Playbook
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,

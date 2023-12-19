3500ed74f6cff821a0ce51b385465741666529f4
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-19 10:11:47 +0200

    fix typo

--- demisto_sdk/commands/content_graph/interface/neo4j/queries/relationships.py
+++ demisto_sdk/commands/content_graph/interface/neo4j/queries/relationships.py
@@ -89,7 +89,7 @@ MERGE (target:{target_type}{
 
 // If created, mark "not in repository" (all repository nodes were created already)
 ON CREATE
-    SET cmd:{labels_of(target_type)}
+    SET target:{labels_of(target_type)},
         target.not_in_repository = true,
         target.object_id = CASE WHEN target.object_id IS NULL THEN target.name ELSE target.object_id END,
         target.name = CASE WHEN target.name IS NULL THEN target.object_id ELSE target.name END

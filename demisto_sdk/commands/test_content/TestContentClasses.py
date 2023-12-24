763da33db288223bce2c1d0363d3c814286b90b5
samuelFain <65926551+samuelFain@users.noreply.github.com>
2023-12-24 17:35:17 +0200

    Change lo_warning to log_info

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -2629,11 +2629,11 @@ class TestContext:
         """
         # we want to test first playback only once (we want to skip it when using retries mechanism)
 
-        self.playbook.log_warning("################ running _execute_mockable_test function")
-        self.playbook.log_warning(f"{self.playbook=}")
-        self.playbook.log_warning(f"{proxy=}")
-        self.playbook.log_warning(f"{self.playbook.configuration.playbook_id=}")
-        self.playbook.log_warning(f"{proxy.has_mock_file(self.playbook.configuration.playbook_id)}")
+        self.playbook.log_info("################ running _execute_mockable_test function")
+        self.playbook.log_info(f"{self.playbook=}")
+        self.playbook.log_info(f"{proxy=}")
+        self.playbook.log_info(f"{self.playbook.configuration.playbook_id=}")
+        self.playbook.log_info(f"{proxy.has_mock_file(self.playbook.configuration.playbook_id)}")
         if (
             not self.playbook.configuration.is_first_playback_failed
             and proxy.has_mock_file(self.playbook.configuration.playbook_id)
@@ -2665,7 +2665,7 @@ class TestContext:
             with run_with_mock(
                 proxy, self.playbook.configuration.playbook_id, record=True
             ) as result_holder:
-                self.playbook.log_warning("################ _execute_mockable_test | running run_with_mock with record=True")
+                self.playbook.log_info("################ _execute_mockable_test | running run_with_mock with record=True")
                 status = self._incident_and_docker_test()
                 self.playbook.configuration.number_of_executions += 1
                 status = self._update_playbook_status(status, is_record_run=True)
@@ -2678,7 +2678,7 @@ class TestContext:
             with run_with_mock(
                 proxy, self.playbook.configuration.playbook_id
             ) as result_holder:
-                self.playbook.log_warning("################ running run_with_mock with proxy with record=False")
+                self.playbook.log_info("################ running run_with_mock with proxy with record=False")
                 status = self._run_incident_test()
                 self._update_playbook_status(status, is_second_playback_run=True)
                 result_holder[RESULT] = status == PB_Status.COMPLETED

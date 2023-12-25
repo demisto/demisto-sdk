241b1fcf88ed795e792087efae44b62b7ce9a243
ilan <ierukhimovic@paloaltonetworks.com>
2023-12-25 13:24:51 +0200

    remove unused method

--- demisto_sdk/commands/pre_commit/hooks/hook.py
+++ demisto_sdk/commands/pre_commit/hooks/hook.py
@@ -35,9 +35,6 @@ class Hook:
         """
         self.hooks.append(deepcopy(self.base_hook))
 
-    def filter_by_support_level(self):
-        ...
-
     def _set_files_on_hook(
         self, hook: dict, files: Iterable[Path], should_filter: bool = True
     ) -> int:

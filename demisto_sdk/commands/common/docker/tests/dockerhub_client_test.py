f8d3811b2ad86a4df9fd91aa1ed9c64324523b70
GuyAfik <guyafik11@gmail.com>
2023-12-31 15:47:19 +0200

    latest docker image tag ut

--- demisto_sdk/commands/common/docker/tests/dockerhub_client_test.py
+++ demisto_sdk/commands/common/docker/tests/dockerhub_client_test.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
+from typing import List
 
 import pytest
 from freezegun import freeze_time
+from packaging.version import Version
 
-from demisto_sdk.commands.common.docker.dockerhub_api import DockerHubClient
+from demisto_sdk.commands.common.docker.dockerhub_client import DockerHubClient
 
 
 @pytest.fixture()
@@ -55,3 +57,41 @@ def test_get_token_with_existing_expired_token(
     )
     assert dockerhub_client.get_token(repo="test") == "token_from_api"
     assert requests_mock.called
+
+
+@pytest.mark.parametrize(
+    "tags, expected_highest_tag",
+    [
+        (
+            [
+                "3.10.13.78960",
+                "3.10.13.81631",
+                "3.10.12.68300",
+                "3.10.11.61265",
+                "3.8.3.9324",
+                "3.9.8.24399",
+            ],
+            "3.10.13.81631",
+        ),
+        (["1.0.0.81877", "1.0.0.78900", "1.0.0.72295"], "1.0.0.81877"),
+        (["1.0.0.81877", "1.0.0.78900", "2.0.0.72295"], "2.0.0.72295"),
+    ],
+)
+def test_get_latest_docker_image_tag(
+    requests_mock,
+    dockerhub_client: DockerHubClient,
+    tags: List[str],
+    expected_highest_tag: str,
+):
+    requests_mock.get(
+        "https://auth.docker.io/token",
+        json={"token": "1234", "issued_at": "1234", "expires_in": 300},
+    )
+    docker_image = "demisto/python3"
+    requests_mock.get(
+        f"{dockerhub_client.DEFAULT_REGISTRY}/{docker_image}/tags/list",
+        json={"tags": tags},
+    )
+    assert dockerhub_client.get_latest_docker_image_tag(docker_image) == Version(
+        expected_highest_tag
+    )

11ed34a6ec596e0bbe630f598f77b285b342e1a5
GuyAfik <guyafik11@gmail.com>
2024-01-04 18:33:15 +0200

    DO110

--- demisto_sdk/commands/validate/validators/DO_validators/DO109_docker_image_is_not_deprecated.py
+++ demisto_sdk/commands/validate/validators/DO_validators/DO109_docker_image_is_not_deprecated.py
@@ -16,7 +16,7 @@ ContentTypes = Union[Integration, Script]
 
 class DockerImageIsNotDeprecatedValidator(BaseValidator[ContentTypes]):
     error_code = "DO109"
-    description = "Validate that the given content item has a docker_image."
+    description = "Validate that the given content item uses a docker image that is not deprecated"
     error_message = "The {0} docker image is deprecated, {1}"
     related_field = "Docker image"
     is_auto_fixable = False
---
+++ demisto_sdk/commands/validate/validators/DO_validators/DO110_docker_image_is_not_native_image.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Iterable, List, Union
+
+from demisto_sdk.commands.common.files.json_file import JsonFile
+from demisto_sdk.commands.common.git_content_config import GitContentConfig
+from demisto_sdk.commands.content_graph.objects.integration import Integration
+from demisto_sdk.commands.content_graph.objects.script import Script
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+
+ContentTypes = Union[Integration, Script]
+
+
+class DockerImageIsNotNativeImageValidator(BaseValidator[ContentTypes]):
+    error_code = "DO110"
+    description = "Validate that the given content item uses a docker image that is not the native image."
+    error_message = "The docker image is {1} native-image, use a different image"
+    related_field = "Docker image"
+    is_auto_fixable = False
+
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message.format(content_item.docker_image),
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if not content_item.is_javascript
+            and content_item.docker_image_object.name != "demisto/py3-native"
+        ]

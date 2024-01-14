f80a16d0624597383b84325eab3e35e73d97d31c
GuyAfik <guyafik11@gmail.com>
2024-01-14 15:59:51 +0200

    mocks

--- demisto_sdk/commands/content_graph/tests/GR_validators_test.py
+++ demisto_sdk/commands/content_graph/tests/GR_validators_test.py
@@ -35,12 +35,14 @@ GIT_PATH = Path(git_path())
 def setup_method(mocker):
     """Auto-used fixture for setup before every test run"""
     import demisto_sdk.commands.content_graph.objects.base_content as bc
+    from demisto_sdk.commands.common.files.file import File
 
     bc.CONTENT_PATH = GIT_PATH
     mocker.patch.object(neo4j_service, "REPO_PATH", GIT_PATH)
     mocker.patch.object(ContentGraphInterface, "repo_path", GIT_PATH)
-    mocker.patch(
-        "demisto_sdk.commands.common.docker_images_metadata.get_remote_file_from_api",
+    mocker.patch.object(
+        File,
+        "read_from_github_api",
         return_value={
             "docker_images": {
                 "python3": {

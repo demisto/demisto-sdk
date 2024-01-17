c7a8de7e73963bb69120bb2dd3622f5a9dd47799
GuyAfik <guyafik11@gmail.com>
2024-01-17 10:41:40 +0200

    SC109 test

--- demisto_sdk/commands/validate/tests/SC_validators_test.py
+++ demisto_sdk/commands/validate/tests/SC_validators_test.py
@@ -1,5 +1,13 @@
+from demisto_sdk.commands.common.constants import (
+    SKIP_PREPARE_SCRIPT_NAME,
+    MarketplaceVersions,
+)
 from demisto_sdk.commands.validate.tests.test_tools import (
     create_script_object,
+)
+from demisto_sdk.commands.validate.validators.base_validator import BaseValidator
+from demisto_sdk.commands.validate.validators.SC_validators import (
+    SC109_script_name_is_not_unique_validator,
 )
 from demisto_sdk.commands.validate.validators.SC_validators.SC105_incident_not_in_args_validator import (
     IsScriptArgumentsContainIncidentWordValidator,
@@ -11,6 +19,13 @@ from demisto_sdk.commands.validate.validators.SC_validators.SC109_script_name_is
     DuplicatedScriptNameValidator,
 )
 from TestSuite.repo import Repo
+
+MP_XSOAR = [MarketplaceVersions.XSOAR.value]
+MP_V2 = [MarketplaceVersions.MarketplaceV2.value]
+MP_XSOAR_AND_V2 = [
+    MarketplaceVersions.XSOAR.value,
+    MarketplaceVersions.MarketplaceV2.value,
+]
 
 
 def test_IsScriptArgumentsContainIncidentWordValidator_is_valid():
@@ -70,7 +85,7 @@ def test_ScriptRunAsIsNotDBotRoleValidator_is_valid():
     assert results[0].content_object.name == "InvalidScript"
 
 
-def test_DuplicatedScriptNameValidator_is_valid(graph_repo: Repo):
+def test_DuplicatedScriptNameValidator_is_valid(mocker, graph_repo: Repo):
     """
     Given
         - A content repo with 8 scripts:
@@ -83,20 +98,33 @@ def test_DuplicatedScriptNameValidator_is_valid(graph_repo: Repo):
     Then
         - Validate that only the first pair of scripts appear in the results, and teh rest of the scripts is valid.
     """
+    mocker.patch.object(
+        SC109_script_name_is_not_unique_validator, "CONTENT_PATH", new=graph_repo.path
+    )
+
     pack = graph_repo.create_pack()
 
-    script1 = pack.create_script()
-    script2 = pack.create_script()
+    pack.create_script("test_incident_1").set_data(marketplaces=MP_XSOAR_AND_V2)
+    pack.create_script("test_alert_1").set_data(marketplaces=MP_XSOAR_AND_V2)
 
-    script1.set_data(name="test")
-    script2.set_data(name="test")
+    pack.create_script("test_incident_2").set_data(marketplaces=MP_XSOAR)
+    pack.create_script("test_alert_2").set_data(marketplaces=MP_XSOAR)
 
-    graph_repo.create_graph()
+    pack.create_script("test_incident_3").set_data(
+        skipprepare=[SKIP_PREPARE_SCRIPT_NAME], marketplaces=MP_V2
+    )
+    pack.create_script("test_alert_3").set_data(
+        skipprepare=[SKIP_PREPARE_SCRIPT_NAME], marketplaces=MP_V2
+    )
 
-    from TestSuite.test_tools import ChangeCWD
+    pack.create_script("test_incident_4").set_data(marketplaces=MP_XSOAR_AND_V2)
+    pack.create_script("test_alert_4").set_data(marketplaces=MP_XSOAR)
 
-    with ChangeCWD(graph_repo.path):
-        results = DuplicatedScriptNameValidator().is_valid([script1.object, script2.object])
+    BaseValidator.graph_interface = graph_repo.create_graph()
+
+    results = DuplicatedScriptNameValidator().is_valid(
+        [script.object for script in pack.scripts]
+    )
 
     assert len(results) == 1
     assert "test_alert_1.yml" == results[0].content_object.path.name

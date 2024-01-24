7a4a3e7eb5d139fdb091d8c6d1de5fe2469d9d61
merit-maita <49760643+merit-maita@users.noreply.github.com>
2024-01-24 10:33:54 +0200

    fix validate failing on list data.json (#3971)
    * fixed a bug with validating pack lists
    
    * added changelog note
    
    * reverted changes to changelog
    
    * added notes

--- demisto_sdk/scripts/changelog/changelog.py
+++ demisto_sdk/scripts/changelog/changelog.py
@@ -237,13 +237,13 @@ def _validate_branch(pr_number: str) -> None:
     if is_changelog_modified():
         raise ValueError(
             "Do not modify changelog.md\n"
-            "Run `demisto-sdk changelog --init -n <pr number>`"
+            "Run `sdk-changelog  --init -n <pr number>`"
             " to create a changelog file instead."
         )
     if not is_log_yml_exist(pr_number):
         raise ValueError(
             "Missing changelog file.\n"
-            "Run `demisto-sdk changelog --init -n <pr number>` and fill it."
+            "Run `sdk-changelog  --init -n <pr number>` and fill it."
         )
     validate_log_yml(pr_number)
 

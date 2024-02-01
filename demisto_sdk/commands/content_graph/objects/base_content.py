06a7199f0c5ed61c10b5feb70b4275b6712ce191
dansterenson <dansterenson@gmail.com>
2024-02-01 18:50:01 +0200

    added output argument to item saving

--- demisto_sdk/commands/content_graph/objects/base_content.py
+++ demisto_sdk/commands/content_graph/objects/base_content.py
@@ -197,7 +197,7 @@ class BaseContent(BaseNode):
         for key, val in self.field_mapping.items():
             attr = getattr(self, key)
             if isinstance(attr, dict):
-                attr = {task_id: elem.dict(exclude_unset=True) for task_id, elem in attr.items()}
+                attr = {key: elem.dict(exclude_unset=True) for key, elem in attr.items()}
             if key == "marketplaces":
                 if (
                     MarketplaceVersions.XSOAR_SAAS in attr

2463ed0f028da8a9b1ef72a8e174d4e5297c2323
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2024-02-01 16:14:41 +0200

    [clients] - Refactor classes not to use pydantic (#3985)
    * [clients] - Check health on both xsiam/xsoar-saas
    
    * fix client tests
    
    * changelog
    
    * validate health of xsoar6 too
    
    * align env vars + raise unhealthy exceptions
    
    * update changelog
    
    * fix existing tests
    
    * some fixes
    
    * pydantic to normal objects
    
    * config
    
    * add _str__ to xsoar-clients
    
    * implement is_server_type
    
    * refactor get_client_from_server_type
    
    * update get_from_server_type
    
    * ServerAbout class
    
    * fix most of tests
    
    * if no auth_id - do not validate
    
    * add tests
    
    * remove build num property
    
    * refactor get_server_type
    
    * logs improvments
    
    * logs
    
    * test_get_xsoar_on_prem_client_from_server_type_with_auth_id test
    
    * update __str__ and fix some bugs
    
    * warning if /about is not retrievable
    
    * changelog
    
    * fix tests

--- demisto_sdk/commands/common/clients/errors.py
+++ demisto_sdk/commands/common/clients/errors.py
@@ -4,3 +4,13 @@ import requests
 class UnAuthorized(RuntimeError):
     def __init__(self, message: str, status_code: int = requests.codes.unauthorized):
         super().__init__(f"Unauthorized: {message} - code {status_code}")
+
+
+class UnHealthyServer(RuntimeError):
+    def __init__(self, server: str):
+        super().__init__(f"The {server} is unhealthy")
+
+
+class InvalidServerType(ValueError):
+    def __init__(self, server: str, server_type: str):
+        super().__init__(f"The server {server} is not {server_type} server")

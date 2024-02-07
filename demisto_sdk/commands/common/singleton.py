d3a452334f3a1979972a973821a9e1f45d7ab1e1
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2024-02-07 14:47:44 +0200

    Update `Neo4jContentGraphInterface` to a Singleton class

--- demisto_sdk/commands/common/singleton.py
+++ demisto_sdk/commands/common/singleton.py
@@ -1,6 +1,16 @@
 from abc import abstractmethod
 
 
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+
+        return cls._instances[cls]
+
+
 class PydanticSingleton:
     _instance = None
 

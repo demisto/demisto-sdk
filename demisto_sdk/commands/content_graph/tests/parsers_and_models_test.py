e4c377095be0eb79e989ae0868b7745898ef8240
Yehuda Rosenberg <90599084+RosenbergYehuda@users.noreply.github.com>
2024-02-08 19:40:59 +0200

    Fixed an issue where upload failed when trying to upload a content item that was missing a description or type field/ XSUP-33213 (#4020)
    * init
    
    * changelog
    
    * changelog
    
    * CR fix
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * unit tests
    
    * Merge remote-tracking branch 'origin/master' into YR/Fix-by-changing-description-to-be-optinal
    
    * delete
    
    ---------
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>

--- demisto_sdk/commands/content_graph/tests/parsers_and_models_test.py
+++ demisto_sdk/commands/content_graph/tests/parsers_and_models_test.py
@@ -2831,3 +2831,27 @@ def test_updated_marketplaces_set(marketplace, expected_market_place_set):
         expected_market_place_set
         == ContentItemParser.update_marketplaces_set_with_xsoar_values(marketplace)
     )
+
+
+def test_argument_object__default_description():
+    # validate that the 'description' attribute of the Argument object is set to an empty string by default
+    from demisto_sdk.commands.content_graph.objects.integration_script import Argument
+
+    arg = Argument(name="test")
+    assert arg.description == ""
+
+
+def test_output_object__default_description():
+    # validate that the 'description' attribute of the Output object is set to an empty string by default
+    from demisto_sdk.commands.content_graph.objects.integration import Output
+
+    output = Output()
+    assert output.description == ""
+
+
+def test_parameter_object__default_type():
+    # validate that the 'type' attribute of the Parameter object is set to 0 by default
+    from demisto_sdk.commands.content_graph.objects.integration import Parameter
+
+    param = Parameter(name="test")
+    assert param.type == 0

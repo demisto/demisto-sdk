e0017f2b601f51f8f54a1ce478c1e28c83cb4115
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2024-02-08 11:00:43 +0200

    Resolve mypy errors

--- demisto_sdk/commands/common/singleton.py
+++ demisto_sdk/commands/common/singleton.py
@@ -1,12 +1,13 @@
 from abc import abstractmethod
+from typing import Any, Dict
 
 
 class SingletonMeta(type):
-    _instances = {}
+    _instances: Dict[type, Any] = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super().__call__(*args, **kwargs)
 
         return cls._instances[cls]
 

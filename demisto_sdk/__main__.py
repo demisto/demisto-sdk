e06dbc0d975ca9bf28b9b538d592dc86b2bd8132
ilan <ierukhimovic@paloaltonetworks.com>
2024-02-08 22:11:35 +0200

    Add dotenv configuration and update IDE settings

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -20,6 +20,7 @@ import os
 from pathlib import Path
 from typing import IO, Any, Dict, List, Optional, Tuple, Union
 
+import dotenv
 import typer
 from pkg_resources import DistributionNotFound, get_distribution
 
@@ -73,6 +74,8 @@ SDK_OFFLINE_ERROR_MESSAGE = (
     "[red]An internet connection is required for this command. If connected to the "
     "internet, un-set the DEMISTO_SDK_OFFLINE_ENV environment variable.[/red]"
 )
+
+dotenv.load_dotenv(CONTENT_PATH / ".env", override=True)  # type: ignore # load .env file from the cwd
 
 
 # Third party packages
@@ -209,9 +212,6 @@ def main(ctx, config, version, release_notes, **kwargs):
     handle_deprecated_args(ctx.args)
 
     config.configuration = Configuration()
-    import dotenv
-
-    dotenv.load_dotenv(CONTENT_PATH / ".env", override=True)  # type: ignore # load .env file from the cwd
 
     if platform.system() == "Windows":
         logger.warning(

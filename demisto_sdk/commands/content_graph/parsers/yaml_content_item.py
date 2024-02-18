5feef1e39a3c98eca712c0e354761b136521e74b
Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
2024-02-18 15:32:28 +0200

    Fix object items collection method (#4012)
    * test
    
    * in progress
    
    * in progress
    
    * in progress
    
    * in progress
    
    * success fetch ymls
    
    * code loading for renamed files
    
    * initalize related paths dict
    
    * added related file initalization
    
    * fix non relative paths
    
    * mid save
    
    * mid save
    
    * demo fixes
    
    * mid save
    
    * tests fixes
    
    * tests fixes
    
    * test
    
    * mid fix
    
    * test
    
    * added readme test
    
    * fixes
    
    * fixes
    
    * adding tests in progress
    
    * adding tests in progress
    
    * add support for ignored errors on related files
    
    * is_error_ignored_fix
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * cr fixes
    
    * Update demisto_sdk/commands/content_graph/parsers/pack.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * cr fixes
    
    * test fix
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/pack.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/content_item.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/pack.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/pack.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/base_content.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * Update demisto_sdk/commands/content_graph/objects/content_item.py
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>
    
    * cr fixes
    
    * pre commit fixes
    
    ---------
    
    Co-authored-by: Guy Afik <53861351+GuyAfik@users.noreply.github.com>

--- demisto_sdk/commands/content_graph/parsers/yaml_content_item.py
+++ demisto_sdk/commands/content_graph/parsers/yaml_content_item.py
@@ -24,9 +24,14 @@ class YAMLContentItemParser(ContentItemParser):
         pack_marketplaces: List[MarketplaceVersions],
         git_sha: Optional[str] = None,
     ) -> None:
-        super().__init__(path, pack_marketplaces)
-        self.path = self.get_path_with_suffix(".yml")
-        self.git_sha = git_sha
+        super().__init__(path, pack_marketplaces, git_sha)
+        self.path = (
+            self.get_path_with_suffix(".yml")
+            if not git_sha
+            else self.path / f"{self.path.name}.yml"
+            if not self.path.suffix == ".yml"
+            else self.path
+        )  # If git_sha is given then we know we're running on the old_content_object copy and we can assume that the file_path is either the actual item path or the path to the item's dir.
 
         if not isinstance(self.yml_data, dict):
             raise InvalidContentItemException(

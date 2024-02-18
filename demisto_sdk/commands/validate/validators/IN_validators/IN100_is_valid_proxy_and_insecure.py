40417f8afbd6b1c010a0ecfd8da80d7f48dbe2da
YuvHayun <yhayun@paloaltonetworks.com>
2024-02-18 14:18:51 +0200

    fix typo

--- demisto_sdk/commands/validate/validators/IN_validators/IN100_is_valid_proxy_and_insecure.py
+++ demisto_sdk/commands/validate/validators/IN_validators/IN100_is_valid_proxy_and_insecure.py
@@ -35,7 +35,7 @@ class IsValidProxyAndInsecureValidator(BaseValidator[ContentTypes]):
                 message=self.error_message.format(
                     "\n".join(
                         [
-                            f"The {key} param display name should be '{val['display']}', the 'defaultvalue' field should be 'false', the 'required' field should be 'False', and the 'required' field should be 8."
+                            f"The {key} param display name should be '{val['display']}', the 'defaultvalue' field should be 'false', the 'required' field should be 'False', and the 'required' field should be false."
                             for key, val in self.fixed_params[content_item.name].items()
                         ]
                     )

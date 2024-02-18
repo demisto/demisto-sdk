a9d28ff244b885c0ecd621b9abe9ee216efcc43a
dorschw <81086590+dorschw@users.noreply.github.com>
2024-02-18 17:20:27 +0200

    add missing comma

--- demisto_sdk/commands/content_graph/objects/conf_json.py
+++ demisto_sdk/commands/content_graph/objects/conf_json.py
@@ -111,7 +111,7 @@ class ConfJSON(StrictBaseModel):
             (ContentType.PLAYBOOK, ((test.playbookID for test in self.tests),)),
             (
                 ContentType.SCRIPT,
-                ((test.scripts for test in self.tests)),
+                ((test.scripts for test in self.tests),),
             ),
             (
                 ContentType.TEST_PLAYBOOK,

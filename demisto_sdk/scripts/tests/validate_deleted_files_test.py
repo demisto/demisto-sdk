7518930ff845d95fdf2db20d9e27216c7b467872
GuyAfik <guyafik11@gmail.com>
2024-02-18 19:44:45 +0200

    add tests

---
+++ demisto_sdk/scripts/tests/validate_deleted_files_test.py
@@ -0,0 +1,106 @@
+import shutil
+from pathlib import Path
+
+import pytest
+
+from demisto_sdk.commands.common.git_util import Repo as GitRepo
+from TestSuite.repo import Repo
+from TestSuite.test_tools import ChangeCWD
+
+
+@pytest.fixture(autouse=True)
+def setup(mocker):
+    from demisto_sdk.scripts.validate_deleted_files import GitUtil
+
+    mocker.patch.object(GitRepo, "remote", return_value="")
+    mocker.patch.object(GitUtil, "fetch", return_value=None)
+
+
+def test_validate_deleted_files_when_deleting_integration_folder(git_repo: Repo):
+    """
+    Given:
+        - an integration folder which was deleted
+
+    When:
+        - running the validate-deleted-files script
+
+    Then:
+        - TODO
+    """
+    from demisto_sdk.scripts.validate_deleted_files import main
+
+    pack = git_repo.create_pack("Test")
+    integration = pack.create_integration("Test")
+    git_repo.git_util.commit_files("create pack and integration")
+    git_repo.git_util.repo.git.checkout("-b", "delete_integration")
+    shutil.rmtree(integration.path)
+    git_repo.git_util.commit_files("delete integration")
+
+    with ChangeCWD(git_repo.path):
+        assert main() == 1
+
+
+def test_validate_deleted_files_when_deleting_from_tests_folder(git_repo: Repo):
+    """
+    Given:
+        - conf.json that was deleted
+
+    When:
+        - running the validate-deleted-files script
+
+    Then:
+        - TODO
+    """
+    from demisto_sdk.scripts.validate_deleted_files import main
+
+    git_repo.git_util.repo.git.checkout("-b", "delete_conf.json")
+    Path.unlink(Path(git_repo.path) / "Tests/conf.json")
+    git_repo.git_util.commit_files("delete conf.json")
+
+    with ChangeCWD(git_repo.path):
+        assert main() == 1
+
+
+def test_validate_deleted_files_when_modifying_pack_metadata(git_repo: Repo):
+    """
+    Given:
+        - pack metadata that was updated
+
+    When:
+        - running the validate-deleted-files script
+
+    Then:
+        - make sure that the script returns error code 0, which means it didn't identify any deleted files
+    """
+    from demisto_sdk.scripts.validate_deleted_files import main
+
+    git_repo.git_util.repo.git.checkout("-b", "modify_file")
+    pack = git_repo.create_pack("Test")
+    pack.pack_metadata.update({"support": "community"})
+    git_repo.git_util.commit_files("update packmetadata.json")
+
+    with ChangeCWD(git_repo.path):
+        assert main() == 0
+
+
+def test_validate_deleted_files_when_adding_integration(git_repo: Repo):
+    """
+    Given:
+        - adding a new integration
+
+    When:
+        - running the validate-deleted-files script
+
+    Then:
+        - make sure that the script returns error code 0, which means it didn't identify any deleted files
+    """
+    from demisto_sdk.scripts.validate_deleted_files import main
+
+    pack = git_repo.create_pack("Test")
+    git_repo.git_util.commit_files("create pack")
+    git_repo.git_util.repo.git.checkout("-b", "add_integration")
+    pack.create_integration("Test")
+    git_repo.git_util.commit_files("add integration")
+
+    with ChangeCWD(git_repo.path):
+        assert main() == 0
--- demisto_sdk/scripts/validate_deleted_files.py
+++ demisto_sdk/scripts/validate_deleted_files.py
@@ -1,16 +1,19 @@
 import os
-
-from demisto_sdk.commands.common.logger import logger
-from demisto_sdk.commands.common.git_util import GitUtil
 from pathlib import Path
-from demisto_sdk.commands.common.constants import PACKS_DIR, FileType_ALLOWED_TO_DELETE, TESTS_DIR, UTILS_DIR
-from demisto_sdk.commands.common.tools import find_type
-from demisto_sdk.commands.common.files.file import File
+
+from demisto_sdk.commands.common.constants import (
+    DEMISTO_GIT_PRIMARY_BRANCH,
+    DEMISTO_GIT_UPSTREAM,
+    PACKS_DIR,
+    TESTS_DIR,
+    UTILS_DIR,
+    FileType_ALLOWED_TO_DELETE,
+)
 from demisto_sdk.commands.common.files.errors import FileReadError
-from demisto_sdk.commands.common.constants import DEMISTO_GIT_PRIMARY_BRANCH
-
-
-GIT_UTIL = GitUtil.from_content_path()
+from demisto_sdk.commands.common.files.file import File
+from demisto_sdk.commands.common.git_util import GitUtil
+from demisto_sdk.commands.common.logger import logger
+from demisto_sdk.commands.common.tools import find_type
 
 
 def is_file_allowed_allowed_to_be_deleted_(file_path: Path) -> bool:
@@ -18,11 +21,21 @@ def is_file_allowed_allowed_to_be_deleted_(file_path: Path) -> bool:
 
     try:
         file_content = File.read_from_git_path(file_path)
+    except FileNotFoundError:
+        logger.warning(
+            f"Could not find {file_path} in branch {DEMISTO_GIT_UPSTREAM}/{DEMISTO_GIT_PRIMARY_BRANCH}"
+        )
+        logger.debug(
+            f"Retrieving {file_path} content from local branch {DEMISTO_GIT_PRIMARY_BRANCH}"
+        )
+        file_content = File.read_from_git_path(file_path, from_remote=False)
     except FileReadError as error:
         logger.warning(
-            f'Could not read file {file_path} from git, error: {error}\ntrying to read {file_path} from github'
+            f"Could not read file {file_path} from git, error: {error}\ntrying to read {file_path} from github"
         )
-        file_content = File.read_from_github_api(file_path, verify_ssl=True if os.getenv("CI") else False)
+        file_content = File.read_from_github_api(
+            file_path, verify_ssl=True if os.getenv("CI") else False
+        )
 
     if file_type := find_type(file_path, file_content):
         return file_type in FileType_ALLOWED_TO_DELETE
@@ -38,7 +51,9 @@ def is_file_allowed_to_be_deleted(file_path: Path) -> bool:
     Returns: True if the file allowed to be deleted, else False.
 
     """
-    if not set(file_path.absolute().parts).intersection({PACKS_DIR, TESTS_DIR, UTILS_DIR}):
+    if not set(file_path.absolute().parts).intersection(
+        {PACKS_DIR, TESTS_DIR, UTILS_DIR}
+    ):
         # if the file is not under Packs/Tests/Utils folder, allow to delete it
         return True
 
@@ -46,13 +61,18 @@ def is_file_allowed_to_be_deleted(file_path: Path) -> bool:
 
 
 def validate_deleted_files():
-    deleted_files = GIT_UTIL.deleted_files(DEMISTO_GIT_PRIMARY_BRANCH)
+    git_util = GitUtil.from_content_path()
+    deleted_files = git_util.deleted_files(DEMISTO_GIT_PRIMARY_BRANCH)
 
     invalid_deleted_files = [
-        str(file_path) for file_path in deleted_files if not is_file_allowed_to_be_deleted(file_path)
+        str(file_path)
+        for file_path in deleted_files
+        if not is_file_allowed_to_be_deleted(file_path)
     ]
     if invalid_deleted_files:
-        logger.error(f'The following files cannot be deleted: {", ".join(invalid_deleted_files)}, restore them')
+        logger.error(
+            f'The following files cannot be deleted: {", ".join(invalid_deleted_files)}, restore them'
+        )
         return 1
     return 0
 
@@ -61,7 +81,9 @@ def main():
     try:
         return validate_deleted_files()
     except Exception as error:
-        logger.error(f'Unexpected error occurred while validating deleted files {error}')
+        logger.error(
+            f"Unexpected error occurred while validating deleted files {error}"
+        )
         raise
 
 

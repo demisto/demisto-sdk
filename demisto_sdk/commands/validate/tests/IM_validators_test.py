69a60ebcccf2b32d4f19a91547d4d9324678c8f0
jbabazadeh <jbabazadeh@paloaltonetworks.com>
2024-02-19 21:32:58 +0200

    pre commit

--- demisto_sdk/commands/validate/tests/IM_validators_test.py
+++ demisto_sdk/commands/validate/tests/IM_validators_test.py
@@ -1,17 +1,22 @@
 import pytest
+
 from demisto_sdk.commands.validate.tests.test_tools import (
     create_metadata_object,
 )
 from demisto_sdk.commands.validate.validators.IM_validators.IM108_author_image_is_empty import (
-    AuthorImageIsEmptyValidator
-    )
+    AuthorImageIsEmptyValidator,
+)
 
 
 @pytest.mark.parametrize(
     "content_items, expected_number_of_failures, expected_msgs",
     [
         ([create_metadata_object()], 0, []),
-        ([create_metadata_object(image="")], 1, ["The author image should not be empty. Please provide a relevant image."])
+        (
+            [create_metadata_object(image="")],
+            1,
+            ["The author image should not be empty. Please provide a relevant image."],
+        ),
     ],
 )
 def test_AuthorImageIsEmptyValidator_is_valid(
@@ -22,7 +27,7 @@ def test_AuthorImageIsEmptyValidator_is_valid(
     content_items.
         - Case 1: Author image not empty.
         - Case 2: Author image is empty.
-            
+
     When
     - Calling the AuthorImageIsEmptyValidator is_valid function.
     Then
--- demisto_sdk/commands/validate/validators/IM_validators/IM108_author_image_is_empty.py
+++ demisto_sdk/commands/validate/validators/IM_validators/IM108_author_image_is_empty.py
@@ -1,22 +1,27 @@
 from __future__ import annotations
+
 from typing import Iterable, List
+
 from demisto_sdk.commands.common.constants import RelatedFileType
 from demisto_sdk.commands.content_graph.objects.pack import Pack
 from demisto_sdk.commands.validate.validators.base_validator import (
-        BaseValidator,
-        ValidationResult,
+    BaseValidator,
+    ValidationResult,
 )
 
 ContentTypes = Pack
 
+
 class AuthorImageIsEmptyValidator(BaseValidator[ContentTypes]):
     error_code = "IM108"
-    description = "checks that the author image file is not empty"
-    error_message = "The author image should not be empty. Please provide a relevant image."
+    description = "Checks that the author image file is not empty"
+    error_message = (
+        "The author image should not be empty. Please provide a relevant image."
+    )
     related_field = "image"
     is_auto_fixable = False
     related_file_type = [RelatedFileType.AUTHOR_IMAGE]
- 
+
     def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
         return [
             ValidationResult(
@@ -25,8 +30,5 @@ class AuthorImageIsEmptyValidator(BaseValidator[ContentTypes]):
                 content_object=content_item,
             )
             for content_item in content_items
-            if (
-               content_item.author_image_path.stat().st_size == 0
-            )
+            if (content_item.author_image_path.stat().st_size == 0)
         ]
-         

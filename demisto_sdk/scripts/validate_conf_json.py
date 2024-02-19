13bc4394c4b958a2d3741397593de73d6f255d3e
dorschw <81086590+dorschw@users.noreply.github.com>
2024-02-19 13:02:39 +0200

    print

--- demisto_sdk/scripts/validate_conf_json.py
+++ demisto_sdk/scripts/validate_conf_json.py
@@ -29,8 +29,8 @@ class ConfJsonValidator:
         is_valid = True
 
         for content_type, ids in self.conf.linked_content_items.items():
-            logger.info(f"{content_type=}, {len(ids)=}")
-            logger.info(f"{ids=}")
+            print(f"{content_type=}, {len(ids)=}")
+            print(f"{ids=}")
             if found_missing := ids.difference(
                 {
                     item.object_id

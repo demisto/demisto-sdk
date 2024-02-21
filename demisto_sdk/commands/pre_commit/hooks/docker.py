b845feb90660f8ae8f48d31bcc49265788e1164b
ilan <ierukhimovic@paloaltonetworks.com>
2024-02-21 12:06:26 +0200

    Refactor DockerHook class to support pack ignore config

--- demisto_sdk/commands/pre_commit/hooks/docker.py
+++ demisto_sdk/commands/pre_commit/hooks/docker.py
@@ -5,10 +5,12 @@ import subprocess
 import time
 from collections import defaultdict
 from copy import deepcopy
-from functools import lru_cache
+from functools import lru_cache, partial
+from multiprocessing import Pool
 from pathlib import Path
 from typing import Dict, Iterable, List, Optional, Set, Tuple
 
+import more_itertools
 from docker.errors import DockerException
 from packaging.version import Version
 
@@ -18,6 +20,7 @@ from demisto_sdk.commands.common.constants import (
     TYPE_PYTHON,
 )
 from demisto_sdk.commands.common.content_constant_paths import CONTENT_PATH, PYTHONPATH
+from demisto_sdk.commands.common.cpu_count import cpu_count
 from demisto_sdk.commands.common.docker_helper import (
     docker_login,
     get_docker,
@@ -159,7 +162,7 @@ def _split_by_objects(
     files_with_objects: List[Tuple[Path, IntegrationScript]],
     config_arg: Optional[Tuple],
     isolate_container: bool = False,
-    support_require_network: bool = False,
+    support_pack_ignore_config: bool = False,
 ) -> Dict[Optional[IntegrationScript], Set[Tuple[Path, IntegrationScript]]]:
     """
     Will group files into groups that share the same configuration file.
@@ -168,7 +171,7 @@ def _split_by_objects(
         files: the files to split
         config_arg: a tuple, argument_name, file_name
         isolate_container: a boolean. If true it will split all the objects into separate hooks.
-        support_require_network: a boolean. If true it will support the `require_network` field in pack ignore.
+        support_pack_ignore_config: a boolean. If true it will read the support the config in pack ignore, for example networking.
 
     Returns:
         a dict where the keys are the names of the folder of the config and the value is a set of files for that config
@@ -181,7 +184,7 @@ def _split_by_objects(
         if (
             isolate_container
             or (config_arg and (obj.path.parent / config_arg[1]).exists())
-            or (support_require_network and should_enable_network(obj))
+            or (support_pack_ignore_config and should_enable_network(obj))
         ):
             object_to_files[obj].add((file, obj))
 
@@ -223,6 +226,28 @@ class DockerHook(Hook):
             hook.pop("copy_files", None)
             hook.pop("isolate_container", None)
             hook.pop("support_require_network", None)
+
+    def process_image(
+        self,
+        image_files_with_objects: Tuple[str, List[Tuple[Path, IntegrationScript]]],
+        config_arg: Optional[Tuple],
+        isolate_container: bool,
+        support_pack_ignore_config: bool,
+    ) -> List[Dict]:
+        image, files_with_objects = image_files_with_objects
+        object_to_files = _split_by_objects(
+            files_with_objects,
+            config_arg,
+            isolate_container,
+            support_pack_ignore_config,
+        )
+        image_is_powershell = any(obj.is_powershell for _, obj in files_with_objects)
+
+        dev_image = devtest_image(image, image_is_powershell, self.context.dry_run)
+        hooks = self.get_new_hooks(
+            dev_image, image, object_to_files, config_arg, support_pack_ignore_config
+        )
+        return hooks
 
     def prepare_hook(
         self,
@@ -267,32 +292,25 @@ class DockerHook(Hook):
                             CONTENT_PATH / file, obj.path.parent / Path(file).name
                         )
         isolate_container = self._get_property("isolate_container", False)
-        support_require_network = self._get_property("support_require_network", False)
+        support_pack_ignore_config = self._get_property(
+            "support_pack_ignore_config", False
+        )
         config_arg = self._get_config_file_arg()
         start_time = time.time()
         logger.debug(f"{len(tag_to_files_objs)} images were collected from files")
         logger.debug(f'collected images: {" ".join(tag_to_files_objs.keys())}')
-        for image, files_with_objects in sorted(
-            tag_to_files_objs.items(), key=lambda item: item[0]
-        ):
-            object_to_files = _split_by_objects(
-                files_with_objects,
-                config_arg,
-                isolate_container,
-                support_require_network,
-            )
-            image_is_powershell = any(
-                obj.is_powershell for _, obj in files_with_objects
-            )
 
-            dev_image = devtest_image(image, image_is_powershell, self.context.dry_run)
-            hooks = self.get_new_hooks(
-                dev_image,
-                image,
-                object_to_files,
-                config_arg,
+        with Pool(processes=cpu_count()) as pool:
+            hooks = pool.map(
+                partial(
+                    self.process_image,
+                    config_arg=config_arg,
+                    isolate_container=isolate_container,
+                    support_pack_ignore_config=support_pack_ignore_config,
+                ),
+                sorted(tag_to_files_objs.items(), key=lambda item: item[0]),
             )
-            self.hooks.extend(hooks)
+            self.hooks = list(more_itertools.flatten(hooks))
         end_time = time.time()
         logger.debug(
             f"DockerHook - prepared images in {round(end_time - start_time, 2)} seconds"
@@ -306,6 +324,7 @@ class DockerHook(Hook):
             Optional[IntegrationScript], Set[Tuple[Path, IntegrationScript]]
         ],
         config_arg: Optional[Tuple],
+        support_pack_ignore_config,
     ):
         """
         Given the docker image and files to run on it, create new hooks to insert
@@ -314,6 +333,8 @@ class DockerHook(Hook):
             image: name of the base image (for naming)
             object_to_files_with_objects: A dict where the key is the object (or None) and value is the set of files to run together.
             config_arg: The config arg to set where relevant. This will be appended to the end of "args"
+            support_pack_ignore_config: a boolean. If true it will read the support the config in pack ignore, for example networking.
+
         Returns:
             All the hooks to be appended for this image
         """
@@ -360,16 +381,21 @@ class DockerHook(Hook):
                 hook[
                     "name"
                 ] = f"{hook['name']}-{integration_script.object_id}"  # for uniqueness
+
+                if support_pack_ignore_config and should_enable_network(
+                    integration_script
+                ):
+                    hook["entry"] = hook["entry"].replace("--network none", "")
+                    # We should isolate. We can run in the working directory of the integration/script and we can't use the `script_runner.py`
+                    change_working_directory = True
+                    if "script_runner" in hook["args"][0]:
+                        hook["args"] = ["-m"] + hook["args"][1:-1]
+
                 if change_working_directory:
                     hook[
                         "entry"
                     ] = f"-w {Path('/src') / integration_script.path.parent.relative_to(CONTENT_PATH)} {hook['entry']}"
 
-            if should_enable_network(integration_script):
-                hook["entry"] = hook["entry"].replace("--network none", "")
-                # We should isolate, so we can't use the `script_runner.py`
-                change_working_directory = True
-                hook["args"] = ["-m"] + hook["args"][1:-1]
             if self._set_files_on_hook(
                 hook,
                 files,

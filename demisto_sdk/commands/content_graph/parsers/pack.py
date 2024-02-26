b22d64bb3c62a4f3d823be311f03adbb99ad6cc9
adi88d <adaud@paloaltonetworks.com>
2024-02-26 13:28:39 +0200

    update PackMetadataParser init

--- demisto_sdk/commands/content_graph/parsers/pack.py
+++ demisto_sdk/commands/content_graph/parsers/pack.py
@@ -138,7 +138,10 @@ class PackMetadataParser:
         self.server_min_version: str = metadata.get("serverMinVersion", "")
         self.current_version: str = metadata.get("currentVersion", "")
         self.version_info: str = ""
-        self.commit: str = GitUtil().get_current_commit_hash() or ""
+        try:
+            self.commit: str = GitUtil().get_current_commit_hash() or ""
+        except ValueError:
+            self.commit = ""
         self.downloads: int = 0
         self.tags: List[str] = metadata.get("tags") or []
         self.keywords: List[str] = metadata.get("keywords", [])

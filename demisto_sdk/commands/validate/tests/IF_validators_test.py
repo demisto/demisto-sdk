65280f984b09b5bda518031f0aa085858ee89baa
israelpolishook <ipolishuk@paloaltonetworks.com>
2024-03-04 18:27:29 +0200

    Refactor IF100 validate and UT

---
+++ demisto_sdk/commands/validate/tests/IF_validators_test.py
@@ -0,0 +1,40 @@
+import pytest
+
+from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
+from demisto_sdk.commands.validate.tests.test_tools import create_incident_field_object
+from demisto_sdk.commands.validate.validators.IF_validators.IF100_is_valid_name_and_cli_name import (
+    IsValidNameAndCliNameValidator,
+)
+
+
+@pytest.mark.parametrize(
+    "content_items, expected_number_of_failures, expected_msgs",
+    (
+        [
+            create_incident_field_object(["name", "cliName"], ["case 1", "case1"]),
+            create_incident_field_object(["name", "cliName"], ["test 1", "test1"]),
+        ],
+        1,
+        [
+            (
+                "The words: case cannot be used as a name.\n"
+                "To fix the problem, remove the words case, "
+                "or add them to the whitelist named argsExceptionsList in:\n"
+                "https://github.com/demisto/server/blob/57fbe417ae420c41ee12a9beb850ff4672209af8/services/servicemodule_test.go#L8273"
+            )
+        ],
+    ),
+)
+def test_IsValidNameAndCliNameValidator_is_valid(
+    content_items: list[IncidentField],
+    expected_number_of_failures: int,
+    expected_msgs: list[str],
+):
+    results = IsValidNameAndCliNameValidator().is_valid(content_items=content_items)
+    assert len(results) == expected_number_of_failures
+    assert all(
+        [
+            result.message == expected_msg
+            for result, expected_msg in zip(results, expected_msgs)
+        ]
+    )
---
+++ demisto_sdk/commands/validate/validators/IF_validators/IF100_is_valid_name_and_cli_name.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import Iterable, List
+
+from demisto_sdk.commands.common.constants import RelatedFileType
+from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+
+ContentTypes = IncidentField
+
+BAD_WORDS = {
+    "incident",
+    "case",
+    "alert",
+    "event",
+    "playbook",
+    "ticket",
+    "issue",
+    "incidents",
+    "cases",
+    "alerts",
+    "events",
+    "playbooks",
+    "tickets",
+    "issues",
+}
+
+
+class IsValidNameAndCliNameValidator(BaseValidator[ContentTypes]):
+    error_code = "IF100"
+    description = "Validate that the name and cliName does not contain any potential incident synonyms."
+    error_message = (
+        "The words: {words} cannot be used as a name.\n"
+        "To fix the problem, remove the words {words}, "
+        "or add them to the whitelist named argsExceptionsList in:\n"
+        "https://github.com/demisto/server/blob/57fbe417ae420c41ee12a9beb850ff4672209af8/services/servicemodule_test.go#L8273"
+    )
+    related_field = "name,cliName"
+    is_auto_fixable = False
+    related_file_type = [RelatedFileType.JSON]
+
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message.format(words=', '.join(words)),
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if (
+                words := [
+                    word for word in content_item.name.split() if word in BAD_WORDS
+                ]
+            )
+        ]

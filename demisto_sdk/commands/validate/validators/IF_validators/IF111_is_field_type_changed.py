d66c0c611a6200a9d7fb384f256c89c40dc01dd4
israelpolishook <ipolishuk@paloaltonetworks.com>
2024-03-11 16:14:47 +0200

    added IF111 validate

---
+++ demisto_sdk/commands/validate/validators/IF_validators/IF111_is_field_type_changed.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Iterable, List, cast
+
+from demisto_sdk.commands.common.constants import GitStatuses, RelatedFileType
+from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    FixResult,
+    ValidationResult,
+)
+
+ContentTypes = IncidentField
+
+
+class IsFieldTypeChangedValidator(BaseValidator[ContentTypes]):
+    error_code = "IF111"
+    description = "Validates that the field type was not changed"
+    error_message = "Changing incident field type is not allowed"
+    fix_message = "The type field has changed back"
+    related_field = "type"
+    is_auto_fixable = True
+    expected_git_statuses = [GitStatuses.MODIFIED]
+    related_file_type = [RelatedFileType.JSON]
+
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message,
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if (
+                content_item.field_type
+                != cast(ContentTypes, content_item.old_base_content_object).field_type
+            )
+        ]
+
+    def fix(self, content_item: ContentTypes) -> FixResult:
+        content_item.field_type = cast(
+            ContentTypes, content_item.old_base_content_object
+        ).field_type
+        return FixResult(
+            validator=self,
+            message=self.fix_message,
+            content_object=content_item,
+        )

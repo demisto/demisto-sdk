612e46c7ab782d66fe4924aeeae537f9086d58aa
dorschw <81086590+dorschw@users.noreply.github.com>
2024-03-12 15:35:56 +0000

    fix `BA110` UT

--- demisto_sdk/commands/common/StrEnum.py
+++ demisto_sdk/commands/common/StrEnum.py
@@ -5,7 +5,6 @@ https://tomwojcik.com/posts/2023-01-02/python-311-str-enum-breaking-change
 
 try:
     from enum import StrEnum
-
     # On Python3.11, the (backwards incompatible) StrEnum was added
 
 except ImportError:

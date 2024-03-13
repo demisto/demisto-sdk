55084934fda6e09dd7ed10946336c37279f166aa
GuyAfik <guyafik11@gmail.com>
2024-03-13 09:49:11 +0200

    update file name

--- demisto_sdk/commands/pre_commit/hooks/docker.py
+++ demisto_sdk/commands/pre_commit/hooks/docker.py
@@ -275,7 +275,8 @@ class DockerHook(Hook):
             All the hooks to be appended for this image
         """
         new_hook = deepcopy(self.base_hook)
-        new_hook["id"] = f"{new_hook.get('id')}-{image.replace('/', '-')}"
+        _image = image.replace('/', '-').replace(':', "-")
+        new_hook["id"] = f"{new_hook.get('id')}-{_image}"
         new_hook["name"] = f"{new_hook.get('name')}-{image.replace('/', '-')}"
         new_hook["language"] = "docker_image"
         env = new_hook.pop("env", {})

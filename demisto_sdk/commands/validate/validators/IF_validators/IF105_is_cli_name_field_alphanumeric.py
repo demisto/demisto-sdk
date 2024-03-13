3709a80ceabb3db5cb1ec153c1c968c482703da6
israelpolishook <ipolishuk@paloaltonetworks.com>
2024-03-13 14:15:40 +0200

    fix

--- demisto_sdk/commands/validate/validators/IF_validators/IF105_is_cli_name_field_alphanumeric.py
+++ demisto_sdk/commands/validate/validators/IF_validators/IF105_is_cli_name_field_alphanumeric.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import re
 from typing import Iterable, List
 
-from demisto_sdk.commands.common.constants import RelatedFileType
 from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,

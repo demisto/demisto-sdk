e2ef63cf37a44feda433fee424404b077d03b532
GuyAfik <guyafik11@gmail.com>
2024-03-14 12:23:07 +0200

    update

--- demisto_sdk/commands/pre_commit/pre_commit_context.py
+++ demisto_sdk/commands/pre_commit/pre_commit_context.py
@@ -68,7 +68,7 @@ class PreCommitContext:
         self.hooks = self._get_hooks(self.precommit_template)
         self.split_hooks: Dict[
             str, Set[str]
-        ] = {}  # mapping between original hook IDs to their newly created hooks
+        ] = {}  # mapping between original hook IDs to their newly created hook IDs
         self.hooks_need_docker = self._hooks_need_docker()
         logger.debug(f"PreCommitContext: {self.asdict()}")
 

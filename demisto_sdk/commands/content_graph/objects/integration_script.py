126ff1e381616c2f9f222e67ffb69699f89c4234
YuvHayun <yhayun@paloaltonetworks.com>
2024-03-17 17:14:25 +0200

    test

--- demisto_sdk/commands/content_graph/objects/integration_script.py
+++ demisto_sdk/commands/content_graph/objects/integration_script.py
@@ -31,7 +31,7 @@ from demisto_sdk.commands.prepare_content.integration_script_unifier import (
 
 
 class Output(BaseModel):
-    description: str = ""
+    description: Optional[str] = None
     contentPath: Optional[str] = None
     contextPath: Optional[str] = None
     type: Optional[str] = None

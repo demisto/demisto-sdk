691de2738f3a623cfbaad840c207d3f5fa281cad
YuvHayun <yhayun@paloaltonetworks.com>
2024-03-17 14:46:54 +0200

    Converting validations

--- demisto_sdk/commands/content_graph/parsers/base_script.py
+++ demisto_sdk/commands/content_graph/parsers/base_script.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Dict, List, Optional, Set
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.common.tools import get_value
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.parsers.integration_script import (
     IntegrationScriptParser,
@@ -41,9 +42,14 @@ class BaseScriptParser(IntegrationScriptParser, content_type=ContentType.BASE_SC
                 "type": "type",
                 "subtype": "subtype",
                 "alt_docker_images": "alt_dockerimages",
+                "outputs": "outputs",
             }
         )
         return super().field_mapping
+
+    @property
+    def outputs(self) -> Optional[List]:
+        return get_value(self.yml_data, self.field_mapping.get("outputs", ""), [])
 
     def connect_to_dependencies(self) -> None:
         """Creates USES_COMMAND_OR_SCRIPT mandatory relationships with the commands/scripts used.

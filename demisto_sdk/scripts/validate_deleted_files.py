8e9528484c721cd2fc5cc7645d541974e4ffdd00
GuyAfik <guyafik11@gmail.com>
2024-03-17 14:48:36 +0200

    add import of __future__

--- demisto_sdk/scripts/validate_deleted_files.py
+++ demisto_sdk/scripts/validate_deleted_files.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import os
 from pathlib import Path
-from typing import Annotated, List, Set  # type: ignore[attr-defined]
+from typing import List, Set
 
 import typer
+from typing_extensions import Annotated
 
 from demisto_sdk.commands.common.constants import (
     DEMISTO_GIT_PRIMARY_BRANCH,

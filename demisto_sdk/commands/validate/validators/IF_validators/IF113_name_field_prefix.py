f8a33828c1db0aa542e7fb5b34d44329810c6dea
israelpolishook <ipolishuk@paloaltonetworks.com>
2024-03-19 11:16:47 +0200

    commit

--- demisto_sdk/commands/validate/validators/IF_validators/IF113_name_field_prefix.py
+++ demisto_sdk/commands/validate/validators/IF_validators/IF113_name_field_prefix.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import Iterable, List, cast
 
-from demisto_sdk.commands.common.constants import RelatedFileType
 from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,

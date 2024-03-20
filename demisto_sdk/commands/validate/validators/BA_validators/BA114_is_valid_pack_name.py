04f9c945ff273b470c6ada01729cfe53d7a809c4
Tal <tcarmeli@paloaltonetworks.com>
2024-03-20 13:59:43 +0200

    changes

--- demisto_sdk/commands/validate/validators/BA_validators/BA114_is_valid_pack_name.py
+++ demisto_sdk/commands/validate/validators/BA_validators/BA114_is_valid_pack_name.py
@@ -1,52 +1,11 @@
 
 from __future__ import annotations
 
-from typing import Iterable, List, Union
+from typing import Iterable, List
 
-from demisto_sdk.commands.common.constants import PACKS_FOLDER
 from demisto_sdk.commands.common.content_constant_paths import CONTENT_PATH
 from demisto_sdk.commands.common.tools import get_pack_name
-from demisto_sdk.commands.content_graph.objects.assets_modeling_rule import (
-    AssetsModelingRule,
-)
 from demisto_sdk.commands.content_graph.objects.base_content import BaseContent
-from demisto_sdk.commands.content_graph.objects.base_playbook import BasePlaybook
-from demisto_sdk.commands.content_graph.objects.base_script import BaseScript
-from demisto_sdk.commands.content_graph.objects.classifier import Classifier
-from demisto_sdk.commands.content_graph.objects.correlation_rule import CorrelationRule
-from demisto_sdk.commands.content_graph.objects.dashboard import Dashboard
-from demisto_sdk.commands.content_graph.objects.generic_definition import (
-    GenericDefinition,
-)
-from demisto_sdk.commands.content_graph.objects.generic_field import GenericField
-from demisto_sdk.commands.content_graph.objects.generic_module import GenericModule
-from demisto_sdk.commands.content_graph.objects.generic_type import GenericType
-from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
-from demisto_sdk.commands.content_graph.objects.incident_type import IncidentType
-from demisto_sdk.commands.content_graph.objects.indicator_field import IndicatorField
-from demisto_sdk.commands.content_graph.objects.indicator_type import IndicatorType
-from demisto_sdk.commands.content_graph.objects.integration import Integration
-from demisto_sdk.commands.content_graph.objects.job import Job
-from demisto_sdk.commands.content_graph.objects.layout import Layout
-from demisto_sdk.commands.content_graph.objects.layout_rule import LayoutRule
-from demisto_sdk.commands.content_graph.objects.list import List
-from demisto_sdk.commands.content_graph.objects.mapper import Mapper
-from demisto_sdk.commands.content_graph.objects.modeling_rule import ModelingRule
-from demisto_sdk.commands.content_graph.objects.pack import Pack
-from demisto_sdk.commands.content_graph.objects.parsing_rule import ParsingRule
-from demisto_sdk.commands.content_graph.objects.playbook import Playbook
-from demisto_sdk.commands.content_graph.objects.pre_process_rule import PreProcessRule
-from demisto_sdk.commands.content_graph.objects.relationship import RelationshipData
-from demisto_sdk.commands.content_graph.objects.report import Report
-from demisto_sdk.commands.content_graph.objects.script import Script
-from demisto_sdk.commands.content_graph.objects.test_playbook import TestPlaybook
-from demisto_sdk.commands.content_graph.objects.test_script import TestScript
-from demisto_sdk.commands.content_graph.objects.trigger import Trigger
-from demisto_sdk.commands.content_graph.objects.widget import Widget
-from demisto_sdk.commands.content_graph.objects.wizard import Wizard
-from demisto_sdk.commands.content_graph.objects.xdrc_template import XDRCTemplate
-from demisto_sdk.commands.content_graph.objects.xsiam_dashboard import XSIAMDashboard
-from demisto_sdk.commands.content_graph.objects.xsiam_report import XSIAMReport
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,
     GitStatuses,

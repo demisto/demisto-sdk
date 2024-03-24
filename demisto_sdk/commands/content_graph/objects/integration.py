9fab26d908b7da48f7320c53c59dda67a41d6dee
merit-maita <meretmaayta@gmail.com>
2024-03-24 13:09:44 +0200

    edits

--- demisto_sdk/commands/content_graph/objects/integration.py
+++ demisto_sdk/commands/content_graph/objects/integration.py
@@ -140,7 +140,7 @@ class Integration(IntegrationScript, content_type=ContentType.INTEGRATION):  # t
         incident_to_alert: bool = False,
     ) -> dict:
         summary = super().summary(marketplace, incident_to_alert)
-        if marketplace == MarketplaceVersions.XSOAR:
+        if marketplace != MarketplaceVersions.MarketplaceV2:
             summary["isfetchevents"] = False
             summary["isfetchassets"] = False
         if self.unified_data:

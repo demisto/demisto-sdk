6086f6ccf48e29929ed5c37b81c870770a2cba85
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-03-26 18:00:24 +0200

    black format

--- demisto_sdk/commands/validate/tests/BC_validators_test.py
+++ demisto_sdk/commands/validate/tests/BC_validators_test.py
@@ -731,7 +731,7 @@ def test_IsBreakingContextOutputBackwardsValidator_is_valid(
             result.message == expected_msg
             for result, expected_msg in zip(results, expected_msgs)
         ]
-)
+    )
 
 
 @pytest.mark.parametrize(
@@ -803,4 +803,5 @@ def test_IsValidFromversionOnModifiedValidator_is_valid_fails(
     assert (
         len(result) == 1
         and result[0].message
-        == "Changing the minimal supported version field `fromversion` is not allowed. Please undo, or request a force merge.")
+        == "Changing the minimal supported version field `fromversion` is not allowed. Please undo, or request a force merge."
+    )

5701dcdd3a6e9ae0555fe0c8128af419726b7ae0
ilan <ierukhimovic@paloaltonetworks.com>
2024-03-27 18:13:26 +0200

    try create coverage

--- demisto_sdk/commands/pre_commit/pre_commit_context.py
+++ demisto_sdk/commands/pre_commit/pre_commit_context.py
@@ -56,8 +56,7 @@ class PreCommitContext:
         shutil.rmtree(PRECOMMIT_FOLDER, ignore_errors=True)
         shutil.rmtree(PRE_COMMIT_FOLDER_SHARED, ignore_errors=True)
 
-        PRE_COMMIT_FOLDER_SHARED.mkdir(parents=True, mode=0o777)
-
+        (PRE_COMMIT_FOLDER_SHARED / "coverage").mkdir(parents=True, mode=0o777)
         PRECOMMIT_FOLDER.mkdir(parents=True)
         PRECOMMIT_CONFIG.mkdir()
         PRECOMMIT_DOCKER_CONFIGS.mkdir()

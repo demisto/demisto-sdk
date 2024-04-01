7985b2d9c02ad6b74a76591a78a35a8dac80abf7
BEAdi <72088126+BEAdi@users.noreply.github.com>
2024-04-01 09:44:29 +0300

    ImageTooLargeValidator

---
+++ demisto_sdk/commands/validate/validators/IM_validators/IM101_image_too_large.py
@@ -0,0 +1,37 @@
+
+from __future__ import annotations
+
+from typing import Iterable, List
+
+from demisto_sdk.commands.common.constants import GitStatuses
+from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
+from demisto_sdk.commands.content_graph.objects.integration import Integration
+from demisto_sdk.commands.validate.validators.base_validator import (
+        BaseValidator,
+        ValidationResult,
+)
+
+ContentTypes = Integration
+
+
+class ImageTooLargeValidator(BaseValidator[ContentTypes]):
+    error_code = "IM101"
+    description = "Checks that the image file dimensions are matching the requirements."
+    rationale = "Image needs to fit its place in the UI. For more information see: https://xsoar.pan.dev/docs/integrations/integration-logo"
+    error_message = "You've created/modified a yml or package with a large sized image. Please make sure to change the image dimensions at:"
+    related_field = "image"
+    is_auto_fixable = False
+    expected_git_statuses = [GitStatuses.ADDED, GitStatuses.MODIFIED]
+    related_file_type = [RelatedFileType.IMAGE]
+    
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        image_max_size = 10 * 1024  # 10kB
+        return [
+            ValidationResult(
+                validator=self,
+                message=f"{self.error_message} {content_item.name}_image.png.",
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if content_item.image.get_file_size().st_size > image_max_size
+        ]

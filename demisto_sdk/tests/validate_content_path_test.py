13c21ea820b88b81cdd706788e75f490593e47fd
Anas Yousef <44998563+anas-yousef@users.noreply.github.com>
2024-04-09 17:21:14 +0300

    pre-commit

--- demisto_sdk/tests/validate_content_path_test.py
+++ demisto_sdk/tests/validate_content_path_test.py
@@ -153,7 +153,11 @@ def test_depth_one_pass(folder: str):
         _validate(Path(DUMMY_PACK_PATH, folder, "nested", "nested_deeper", "file"))
     except PathIsTestOrDocData:
         pass
-    except (InvalidIntegrationScriptFileType, InvalidIntegrationScriptFileName, InvalidXDRCTemplatesFileName):
+    except (
+        InvalidIntegrationScriptFileType,
+        InvalidIntegrationScriptFileName,
+        InvalidXDRCTemplatesFileName,
+    ):
         # In Integration/script, InvalidIntegrationScriptFileType will be raised but is irrelevant for this test.
         # InvalidXDRCTemplatesFileName will be raised but it is irrelevant for this test.
         pass

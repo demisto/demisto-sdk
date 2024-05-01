ed26646f3dff6722ee44677297c46a68106dde9c
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2024-05-01 03:41:49 +0300

    fix UT

--- demisto_sdk/commands/validate/tests/LO_validators_test.py
+++ demisto_sdk/commands/validate/tests/LO_validators_test.py
@@ -2,42 +2,39 @@ from typing import List
 
 import pytest
 
+from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.validate.tests.test_tools import create_layout_object
-from demisto_sdk.commands.validate.validators.LO_validators.LO107_is_valid_type import (
-    ContentTypes as ContentTypes107,
-)
 from demisto_sdk.commands.validate.validators.LO_validators.LO107_is_valid_type import (
     IsValidTypeValidator,
 )
 
 
 @pytest.mark.parametrize(
-    "content_items, expected_field_error_messages",
+    "paths, values, expected_field_error_messages",
     [
         pytest.param(
-            create_layout_object(paths=["detailsV2.tabs[0].type"], values=["canvas"]),
+            ["detailsV2.tabs[0].type"],
+            ["canvas"],
             ["canvas"],
             id="Case1: Single invalid type in first tab",
         ),
         pytest.param(
-            create_layout_object(
-                paths=["detailsV2.tabs[1].sections[0].type"], values=["evidence"]
-            ),
+            ["detailsV2.tabs[1].sections[0].type"],
+            ["evidence"],
             ["evidence"],
             id="Case2: Single invalid type in a section of the second tab",
         ),
         pytest.param(
-            create_layout_object(
-                paths=["detailsV2.tabs[1].sections[0].type", "detailsV2.tabs[0].type"],
-                values=["evidence", "canvas"],
-            ),
+            ["detailsV2.tabs[1].sections[0].type", "detailsV2.tabs[0].type"],
+            ["evidence", "canvas"],
             ["canvas", "evidence"],
             id="Case3: Multiple invalid types in different tabs",
         ),
     ],
 )
-def test_IIsValidTypeValidator_is_valid_failure(
-    content_items: ContentTypes107,
+def test_IsValidTypeValidator_is_valid_failure(
+    paths: List[str],
+    values: List[str],
     expected_field_error_messages: List[str],
 ):
     """
@@ -46,11 +43,34 @@ def test_IIsValidTypeValidator_is_valid_failure(
         Case2: a layout object with a single invalid type in a section of the second tab,
         Case3: a layout object with multiple invalid types in different tabs,
     When
-        the IsValidTypeValidator's is_valid method is called with the layout object,
+        the IsValidTypeValidator's is_valid method is called with the layout object.
     Then
         it should return a list of results with one failure message that matches the expected error message.
+    Note
+        The create_layout_object() function returns an invalid layout object by default.
+        Therefore, we manually change the values to only check the values that came from the parametrize.
     """
-    results = IsValidTypeValidator().is_valid([content_items])
+    # Extend the paths and values to make the layout object valid for some values
+    paths.extend(
+        [
+            "detailsV2.tabs[1].sections[2].type",
+            "detailsV2.tabs[1].sections[3].type",
+            "detailsV2.tabs[4].type",
+            "detailsV2.tabs[5].type",
+            "detailsV2.tabs[6].type",
+        ]
+    )
+    values.extend(
+        [
+            "dynamic",
+            "dynamic",
+            "dynamic",
+            "dynamic",
+            "dynamic",
+        ]
+    )
+    content_items = create_layout_object(paths=paths, values=values)
+    results = IsValidTypeValidator().is_valid([content_items])  # type: ignore[reportArgumentType]
     assert len(results) == 1  # one failure
     assert (
         results[0].message
@@ -58,12 +78,50 @@ def test_IIsValidTypeValidator_is_valid_failure(
     )
 
 
-def test_IsContentItemNameContainTrailingSpacesValidator_is_valid_success():
+def test_IsValidTypeValidator_is_valid_success():
     """
-    Given a layout object created by the create_layout_object function,
-    When the IsValidTypeValidator's is_valid method is called with the layout object,
-    Then it should return no failures, indicating that the layout object is valid.
+    Given
+        a layout object created by the create_layout_object function,
+    When
+        the IsValidTypeValidator's is_valid method is called with the layout object,
+    Then
+        it should return no failures, indicating that the layout object is valid.
+    Note
+        The create_layout_object() function is used to create a layout object with valid types.
     """
+    valid_layout_object = create_layout_object(  # Create a valid layout object
+        paths=[
+            "detailsV2.tabs[1].sections[2].type",
+            "detailsV2.tabs[1].sections[3].type",
+            "detailsV2.tabs[4].type",
+            "detailsV2.tabs[5].type",
+            "detailsV2.tabs[6].type",
+        ],
+        values=["dynamic", "dynamic", "dynamic", "dynamic", "dynamic"],
+    )
     assert not IsValidTypeValidator().is_valid(
-        [create_layout_object()]  # type: ignore[reportArgumentType]
+        [valid_layout_object]  # type: ignore[reportArgumentType]
     )  # no failures
+
+
+def test_IsValidTypeValidator_returns_no_failures_after_removal_of_MarketplaceV2():
+    """
+    Given
+        a layout object that initially includes MarketplaceV2 in its marketplaces,
+    When
+        MarketplaceV2 in the marketplaces of the layout object,
+    Then
+        the IsValidTypeValidator's is_valid method should initially return failures,
+        but after the removal of MarketplaceV2, it should return no failures, indicating that the layout object is now valid.
+    """
+    layout_object = create_layout_object()
+
+    assert IsValidTypeValidator().is_valid(
+        [layout_object]  # type: ignore[reportArgumentType]
+    ), "Expected initial validation to fail due to presence of MarketplaceV2"
+
+    layout_object.marketplaces.remove(MarketplaceVersions.MarketplaceV2)  # type: ignore
+
+    assert not IsValidTypeValidator().is_valid(
+        [layout_object]  # type: ignore[reportArgumentType]
+    ), "Expected validation to pass after removal of MarketplaceV2"

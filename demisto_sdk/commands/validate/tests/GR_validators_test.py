f71cf929a40d9094c69a9a9fed80b4149171cc25
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-05-16 20:13:08 +0300

    gr100 validation

---
+++ demisto_sdk/commands/validate/tests/GR_validators_test.py
@@ -0,0 +1,107 @@
+import pytest
+import logging
+from pathlib import Path
+from TestSuite.repo import Repo
+from demisto_sdk.commands.common.legacy_git_tools import git_path
+import demisto_sdk.commands.content_graph.neo4j_service as neo4j_service
+from demisto_sdk.commands.content_graph.interface import ContentGraphInterface
+from demisto_sdk.commands.content_graph.objects.classifier import Classifier
+from demisto_sdk.commands.content_graph.objects.integration import Command, Integration
+from demisto_sdk.commands.content_graph.objects.pack import Pack
+from demisto_sdk.commands.content_graph.objects.playbook import Playbook
+from demisto_sdk.commands.content_graph.objects.repository import ContentDTO
+from demisto_sdk.commands.content_graph.objects.script import Script
+from demisto_sdk.commands.common.docker.docker_image import DockerImage
+from demisto_sdk.commands.content_graph.common import ContentType, RelationshipType
+from demisto_sdk.commands.validate.validators.GR_validators import GR100_uses_items_not_in_market_place
+
+from demisto_sdk.commands.validate.validators.GR_validators.GR100_uses_items_not_in_market_place import \
+    MarketplaceFieldsValidator
+
+from demisto_sdk.commands.content_graph.tests.create_content_graph_test import (
+    mock_relationship,
+    mock_test_playbook,
+)
+
+from demisto_sdk.commands.common.constants import (
+    GENERAL_DEFAULT_FROMVERSION,
+    SKIP_PREPARE_SCRIPT_NAME,
+    MarketplaceVersions,
+)
+
+from demisto_sdk.commands.content_graph.commands.create import (
+    create_content_graph,
+)
+
+MP_XSOAR = [MarketplaceVersions.XSOAR.value]
+MP_V2 = [MarketplaceVersions.MarketplaceV2.value]
+MP_XSOAR_AND_V2 = [
+    MarketplaceVersions.XSOAR.value,
+    MarketplaceVersions.MarketplaceV2.value,
+]
+
+
+def test_MarketplaceFieldsValidator_is_valid(mocker, graph_repo: Repo):
+    """
+    Given
+    - A content repo
+    When
+    - running MarketplaceFieldsValidator is_valid function.
+    Then
+    - Validate the existence of invalid marketplaces usages.
+    """
+
+    # pack2 = graph_repo.create_pack()
+    # pack2.set_data(marketplaces=MP_XSOAR)
+
+    pack = graph_repo.create_pack()
+    pack.set_data(marketplaces=MP_V2)
+
+    test_integration_0 = pack.create_integration("test_integration_0")
+    test_integration_0.set_data(marketplaces=MP_XSOAR)
+    test_integration_0.set_commands(["command_1"])
+
+    test_integration_1 = pack.create_integration("test_integration_1")
+    test_integration_1.set_data(marketplaces=MP_V2)
+    test_integration_1.set_commands(["command_2", "command_3"])
+    pass
+
+    net4j_content_graph_interface = graph_repo.create_graph()
+    pack_graph_object = pack.get_graph_object(interface=net4j_content_graph_interface)
+    # integrations = pack_graph_object.content_items.integration
+    MarketplaceFieldsValidator.graph_interface = net4j_content_graph_interface
+    validator = MarketplaceFieldsValidator()
+    validator.is_valid([pack_graph_object] + pack_graph_object.content_items.integration)
+    pass
+    # results = MarketplaceFieldsValidator().is_valid([pack_graph_object])
+
+    # command_1 = test_integration
+    # command_1.set_data(marketplaces=MP_V2)
+
+    # MarketplaceFieldsValidator.graph_interface = graph_repo.create_graph()
+    # pack.create_script("script_1").
+    # pack.create_integration("test_integration_1").set_data(marketplaces=MP_V2)
+    # pack.create_integration("test_integration_2/").set_data(marketplaces=MP_XSOAR_AND_V2)
+
+
+    # graph.create_relationships()
+    pass
+    # pack.set_data()
+    # logger_error = mocker.patch.object(logging.getLogger("demisto-sdk"), "error")
+    #
+    # validator = MarketplaceFieldsValidator()
+    # create_content_graph(validator.graph)
+    # pass
+    # # MarketplaceFieldsValidator().is_valid()
+    # #
+    # # with GraphValidator(update_graph=False) as graph_validator:
+    # #     create_content_graph(graph_validator.graph)
+    # #     is_valid = graph_validator.validate_marketplaces_fields()
+    # #ss
+    # # assert not is_valid
+    # # assert str_in_call_args_list(
+    # #     logger_error.call_args_list,
+    # #     "Content item 'SamplePlaybook' can be used in the 'xsoar, xpanse' marketplaces"
+    # #     ", however it uses content items: 'SamplePlaybook2' which are not supported in"
+    # #     " all of the marketplaces of 'SamplePlaybook'",
+    # # )
---
+++ demisto_sdk/commands/validate/validators/GR_validators/GR100_uses_items_not_in_market_place.py
@@ -0,0 +1,75 @@
+
+from __future__ import annotations
+
+from typing import Iterable, List, Union
+
+from demisto_sdk.commands.common.constants import GitStatuses
+from demisto_sdk.commands.common.errors import Errors
+from demisto_sdk.commands.content_graph.objects.integration import Integration
+from demisto_sdk.commands.content_graph.objects.script import Script
+from demisto_sdk.commands.content_graph.objects.playbook import Playbook
+from demisto_sdk.commands.content_graph.objects.pack import Pack
+from demisto_sdk.commands.content_graph.objects.dashboard import Dashboard
+from demisto_sdk.commands.content_graph.objects.classifier import Classifier
+from demisto_sdk.commands.content_graph.objects.job import Job
+from demisto_sdk.commands.content_graph.objects.layout import Layout
+from demisto_sdk.commands.content_graph.objects.mapper import Mapper
+from demisto_sdk.commands.content_graph.objects.wizard import Wizard
+from demisto_sdk.commands.content_graph.objects.correlation_rule import CorrelationRule
+from demisto_sdk.commands.content_graph.objects.incident_field import IncidentField
+from demisto_sdk.commands.content_graph.objects.incident_type import IncidentType
+from demisto_sdk.commands.content_graph.objects.indicator_field import IndicatorField
+from demisto_sdk.commands.content_graph.objects.indicator_type import IndicatorType
+from demisto_sdk.commands.content_graph.objects.layout_rule import LayoutRule
+from demisto_sdk.commands.content_graph.objects.layout import Layout
+from demisto_sdk.commands.content_graph.objects.modeling_rule import ModelingRule
+from demisto_sdk.commands.content_graph.objects.parsing_rule import ParsingRule
+from demisto_sdk.commands.content_graph.objects.report import Report
+from demisto_sdk.commands.content_graph.objects.test_playbook import TestPlaybook
+from demisto_sdk.commands.content_graph.objects.trigger import Trigger
+from demisto_sdk.commands.content_graph.objects.widget import Widget
+from demisto_sdk.commands.content_graph.objects.generic_definition import GenericDefinition
+from demisto_sdk.commands.content_graph.objects.generic_field import GenericField
+from demisto_sdk.commands.content_graph.objects.generic_module import GenericModule
+from demisto_sdk.commands.content_graph.objects.generic_type import GenericType
+from demisto_sdk.commands.content_graph.objects.xsiam_dashboard import XSIAMDashboard
+from demisto_sdk.commands.content_graph.objects.xsiam_report import XSIAMReport
+from demisto_sdk.commands.validate.validators.base_validator import (
+        BaseValidator,
+        ValidationResult,
+)
+
+ContentTypes = Union[Integration, Script, Playbook, Pack, Dashboard, Classifier, Job, Layout, Mapper, Wizard, CorrelationRule, IncidentField, IncidentType, IndicatorField, IndicatorType, LayoutRule, Layout, ModelingRule, ParsingRule, Report, TestPlaybook, Trigger, Widget, GenericDefinition, GenericField, GenericModule, GenericType, XSIAMDashboard, XSIAMReport]
+
+
+class MarketplaceFieldsValidator(BaseValidator[ContentTypes]):
+    error_code = "GR100"
+    description = ("Content item attempts to use other content items which are not supported in all of the "
+                   "marketplaces of the content item.")
+    rationale = "Content graph proper construction."
+    error_message = ""
+    related_field = ""
+    is_auto_fixable = False
+    expected_git_statuses = [GitStatuses.RENAMED, GitStatuses.ADDED, GitStatuses.MODIFIED]
+
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        validation_results = []
+
+        for content_item in self.graph.find_uses_paths_with_invalid_marketplaces([
+                item.pack_id for item in content_items]):
+
+            used_content_items = [relationship.content_item_to.object_id for relationship in content_item.uses]
+            error_message = Errors.uses_items_not_in_marketplaces(
+                content_item.name, content_item.marketplaces, used_content_items
+            )
+            validation_results.append(
+                ValidationResult(
+                    validator=self,
+                    message=error_message,
+                    content_object=content_item,
+                )
+            )
+        return validation_results
+    
+
+    

080677751aed80c17589b88340af4d24b6dc196e
merit-maita <49760643+merit-maita@users.noreply.github.com>
2024-05-16 12:15:02 +0300

    refactored validate for DA (#4264)
    * refactored validate for DA
    
    * added validations to config.toml
    
    * added support to widgets in forbidden fields
    
    * added support to widgets in necessary fields
    
    * fixed unittests
    
    * revert changes
    
    * added to validation config
    
    * added changelog
    
    * fixed pr number
    
    * added flag newly added
    
    * updated validations
    
    * Update demisto_sdk/commands/validate/validators/DA_validators/DA101_dashboard_contains_necessary_fields.py
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * Update demisto_sdk/commands/validate/validators/DA_validators/DA101_dashboard_contains_necessary_fields.py
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * updated the unittests
    
    * added data dict object
    
    * update to fix unittest
    
    * e
    
    * edited the objects fields
    
    * added field mapping
    
    * Update demisto_sdk/commands/validate/validators/DA_validators/DA101_dashboard_contains_necessary_fields.py
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * added related field
    
    * fixed the unittest
    
    ---------
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>

---
+++ demisto_sdk/commands/validate/tests/DA_validators_test.py
@@ -0,0 +1,79 @@
+from demisto_sdk.commands.validate.tests.test_tools import create_dashboard_object
+from demisto_sdk.commands.validate.validators.DA_validators.DA100_dashboard_contains_forbidden_fields import (
+    IsDashboardContainForbiddenFieldsValidator,
+)
+from demisto_sdk.commands.validate.validators.DA_validators.DA101_dashboard_contains_necessary_fields import (
+    IsDashboardContainNecessaryFieldsValidator,
+)
+
+
+def test_IsDashboardContainForbiddenFieldsValidator_is_valid():
+    """
+    Given:
+        - Dashboard content items
+    When:
+        - run is_valid method
+    Then:
+        - Ensure that the ValidationResult returned
+          for the Dashboard who has a field with name 'system'
+        - Ensure that no ValidationResult returned
+          when `system` field does not exist
+    """
+    dashboard = create_dashboard_object(
+        paths=["system", "layout"],
+        values=[
+            "foo",
+            [
+                {
+                    "id": "layout test",
+                    "widget": {"owner": "owner test", "id": "widget test"},
+                }
+            ],
+        ],
+    )
+
+    # not valid
+    results = IsDashboardContainForbiddenFieldsValidator().is_valid([dashboard])
+    assert (
+        "The 'system' fields need to be removed from Confluera Dashboard."
+        in results[0].message
+    )
+    assert (
+        "The 'owner' fields need to be removed from widget test Widget listed under Confluera Dashboard."
+        in results[0].message
+    )
+
+    # valid
+    dashboard.data_dict["system"] = None
+    dashboard.layout[0]["widget"]["owner"] = None
+    assert not IsDashboardContainForbiddenFieldsValidator().is_valid([dashboard])
+
+
+def test_IsDashboardContainNecessaryFieldsValidator_is_valid():
+    """
+    Given:
+        - Dashboard content items
+    When:
+        - run is_valid method
+    Then:
+        - Ensure that no ValidationResult returned when all required fields exist.
+        - Ensure that the ValidationResult returned
+          for the dashboard whose 'fromDate' field is missing.
+    """
+    # valid
+    dashboard = create_dashboard_object()
+    assert not IsDashboardContainNecessaryFieldsValidator().is_valid([dashboard])
+
+    # not valid
+    dashboard.data_dict["fromDate"] = None
+    dashboard.layout[0]["widget"]["toDate"] = None
+    result = IsDashboardContainNecessaryFieldsValidator().is_valid([dashboard])
+
+    assert (
+        "The 'fromDate' fields are missing from Confluera Dashboard and need to be added."
+        in result[0].message
+    )
+    assert (
+        "The 'toDate' fields are missing from detcount Widget listed under Confluera Dashboard and need to be added."
+        in result[0].message
+    )

a9e3cc0c2ee7b2567b0873d8cad599afea647d2b
Anas Yousef <44998563+anas-yousef@users.noreply.github.com>
2024-05-20 17:19:12 +0300

    Added fix

--- demisto_sdk/commands/content_graph/parsers/integration_script.py
+++ demisto_sdk/commands/content_graph/parsers/integration_script.py
@@ -91,4 +91,4 @@ class IntegrationScriptParser(YAMLContentItemParser):
     def auto_update_docker_image(self):
         return (
             get_value(self.yml_data, "autoUpdateDockerImage", "")
-        ).lower() != "false"
+        ).lower() not in ("false", False)

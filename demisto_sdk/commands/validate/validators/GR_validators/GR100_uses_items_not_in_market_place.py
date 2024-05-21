5297ef0029894d77596e1b22c49f38cb5bc3c64f
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-05-21 09:55:06 +0300

    PR fixes

--- demisto_sdk/commands/validate/validators/GR_validators/GR100_uses_items_not_in_market_place.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR100_uses_items_not_in_market_place.py
@@ -1,16 +1,51 @@
 from __future__ import annotations
 
-from typing import Iterable, List
+from typing import Iterable, List, Union
 
 from demisto_sdk.commands.content_graph.common import RelationshipType
-
-from demisto_sdk.commands.content_graph.objects.base_content import BaseContent
+from demisto_sdk.commands.content_graph.objects import (Classifier, CorrelationRule, Dashboard, GenericDefinition,
+                                                        GenericField, GenericModule, GenericType, IncidentField,
+                                                        IncidentType, IndicatorField, IndicatorType, Integration, Job,
+                                                        Layout, LayoutRule, Mapper, ModelingRule, Pack, ParsingRule,
+                                                        Playbook, Report, Script, TestPlaybook, Trigger, Widget, Wizard,
+                                                        XSIAMDashboard, XSIAMReport
+                                                        )
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,
     ValidationResult,
 )
 
-ContentTypes = BaseContent
+ContentTypes = Union[
+    Integration,
+    Script,
+    Playbook,
+    Pack,
+    Dashboard,
+    Classifier,
+    Job,
+    Layout,
+    Mapper,
+    Wizard,
+    CorrelationRule,
+    IncidentField,
+    IncidentType,
+    IndicatorField,
+    IndicatorType,
+    LayoutRule,
+    Layout,
+    ModelingRule,
+    ParsingRule,
+    Report,
+    TestPlaybook,
+    Trigger,
+    Widget,
+    GenericDefinition,
+    GenericField,
+    GenericModule,
+    GenericType,
+    XSIAMDashboard,
+    XSIAMReport,
+]
 
 
 class MarketplacesFieldValidator(BaseValidator[ContentTypes]):
@@ -20,9 +55,11 @@ class MarketplacesFieldValidator(BaseValidator[ContentTypes]):
         "marketplaces of the content item."
     )
     rationale = "Content graph proper construction."
-    error_message = ("Content item '{content_name}' can be used in the '{marketplaces}' marketplaces,"
-                     " however it uses content items: '{used_content_items}'"
-                     " which are not supported in all of the marketplaces of '{content_name}'.")
+    error_message = (
+        "Content item '{content_name}' can be used in the '{marketplaces}' marketplaces,"
+        " however it uses content items: '{used_content_items}'"
+        " which are not supported in all of the marketplaces of '{content_name}'."
+    )
 
     related_field = "marketplaces"
     is_auto_fixable = False
@@ -31,9 +68,8 @@ class MarketplacesFieldValidator(BaseValidator[ContentTypes]):
         validation_results = []
 
         for content_item in self.graph.find_uses_paths_with_invalid_marketplaces(
-            [item.pack_id for item in content_items]
+                [item.pack_id for item in content_items]
         ):
-
             used_content_items = [
                 item.content_item_to.object_id
                 for item in content_item.relationships_data.get(RelationshipType.USES)
@@ -42,9 +78,11 @@ class MarketplacesFieldValidator(BaseValidator[ContentTypes]):
             validation_results.append(
                 ValidationResult(
                     validator=self,
-                    message=self.error_message.format(content_name=content_item.name,
-                                                      marketplaces=', '.join(content_item.marketplaces),
-                                                      used_content_items=', '.join(used_content_items)),
+                    message=self.error_message.format(
+                        content_name=content_item.name,
+                        marketplaces=", ".join(content_item.marketplaces),
+                        used_content_items=", ".join(used_content_items),
+                    ),
                     content_object=content_item,
                 )
             )

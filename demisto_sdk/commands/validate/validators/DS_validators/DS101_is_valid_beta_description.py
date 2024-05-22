bd3a1e0ac178a27256e352eb180e336d19786c72
Moshe <mgalitzki@paloaltonetworks.com>
2024-05-22 09:58:14 +0300

    ruff fixes

--- demisto_sdk/commands/validate/validators/DS_validators/DS101_is_valid_beta_description.py
+++ demisto_sdk/commands/validate/validators/DS_validators/DS101_is_valid_beta_description.py
@@ -3,14 +3,15 @@ from __future__ import annotations
 from typing import Iterable, List
 
 from demisto_sdk.commands.common.constants import (
-    GitStatuses, BETA_INTEGRATION_DISCLAIMER,
+    BETA_INTEGRATION_DISCLAIMER,
+    GitStatuses,
 )
 from demisto_sdk.commands.content_graph.objects.integration import Integration
+from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,
     ValidationResult,
 )
-from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
 
 ContentTypes = Integration
 

53367507f239b1ccf8da5609e19625046d99a03b
epintzov <epintzov@paloaltonetworks.com>
2024-05-26 13:14:26 +0300

    test xml load

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -1367,8 +1367,10 @@ class TestResults:
         storage_bucket = storage_client.bucket(ARTIFACTS_BUCKET)
         blob = storage_bucket.blob(f'content-playbook-reports/{repository_name}/{file_name}')
 
+        data_to_load = (ET.tostring(self.test_results_xml_file, encoding='UTF-8',xml_declaration=True, method='xml').
+                        decode('UTF-8'))
         # Upload the JSON string to the blob
-        blob.upload_from_string(self.test_results_xml_file, content_type="application/xml")
+        blob.upload_from_string(data_to_load, content_type="application/xml")
 
     @staticmethod
     def delete_oldest_file(repository_name: str,

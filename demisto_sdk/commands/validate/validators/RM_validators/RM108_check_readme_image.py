293575324afbf7d21cefe622be203f9eddf9bd62
maimorag <mmorag@paloaltonetworks.com>
2024-06-04 17:37:27 +0300

    make validation shorter

--- demisto_sdk/commands/validate/validators/RM_validators/RM108_check_readme_image.py
+++ demisto_sdk/commands/validate/validators/RM_validators/RM108_check_readme_image.py
@@ -24,12 +24,6 @@ from demisto_sdk.commands.common.constants import (
     DOC_FILE_IMAGE_REGEX
 )
 
-from demisto_sdk.commands.common.tools import (
-    extract_image_paths_from_str,
-    get_full_image_paths_from_relative,
-    get_pack_name,
-)
-
 
 ContentTypes = Union[Integration, Script, Playbook, Pack]
 
@@ -54,8 +48,8 @@ class CheckReadmeImageValidator(BaseValidator[ContentTypes]):
             )
             for content_item in content_items
             if (
-                error_message := self.verify_absolute_images_not_exist(content_item.description) +
-                                 self.verify_relative_saved_in_doc_files(content_item.description) +
+                error_message := self.verify_absolute_images_not_exist(content_item.description_file) +
+                                 self.verify_relative_saved_in_doc_files(content_item.description_file) +
                                  self.verify_absolute_images_not_exist(content_item.readme.file_content) +
                                  self.verify_relative_saved_in_doc_files(content_item.readme.file_content)
             )
@@ -63,59 +57,24 @@ class CheckReadmeImageValidator(BaseValidator[ContentTypes]):
 
 
     def verify_absolute_images_not_exist(self, content_item: str) -> str:
-        """Check if the content item contains exists image paths.
+        """Check for existing absolute image paths."""
+        error_message = ""
+        absolute_links = re.findall(URL_IMAGE_LINK_REGEX + r'|' + HTML_IMAGE_LINK_REGEX, content_item, re.IGNORECASE | re.MULTILINE)
+        if absolute_links:
+            error_message = f"Invalid image path(s): {absolute_links}. Use relative paths instead.\n"
+        return error_message
         
         
-        Arguments:
-            content_item {ContentTypes} -- The content item to check.
-
-        Returns:
-            str -- The error message if the content item contains absolute paths.
-        """
+    def verify_relative_saved_in_doc_files(self, content_item: str) -> str:
+        """Check for relative image paths not saved in the pack's doc_files folder."""
         if not content_item:
             return ""
         error_message = ""
-        absolute_links = re.findall(
-                URL_IMAGE_LINK_REGEX,
-                content_item,
-                re.IGNORECASE | re.MULTILINE,
-            )
-        absolute_links += re.findall(
-            HTML_IMAGE_LINK_REGEX,
-            content_item,
-            re.IGNORECASE | re.MULTILINE,
-        )
-        if absolute_links:
-            error_message = f"Detected the following invalid image path:{absolute_links}. Absolute paths are not supported in pack README files. Please use relative paths instead. \n"
-
+        relative_images = re.findall(r"(\!\[.*?\])\(((?!http).*?)\)$" +
+                                    r'|' + r'(<img.*?src\s*=\s*"((?!http).*?)")',
+                                    content_item,
+                                    re.IGNORECASE | re.MULTILINE)
+        invalid_links = [rel_img for rel_img in relative_images if not re.match(DOC_FILE_IMAGE_REGEX, rel_img)]
+        if invalid_links:
+            error_message += f"Relative image paths not in pack's doc_files:{invalid_links}. Move them to the folder."
         return error_message
-    
-    
-    def verify_relative_saved_in_doc_files(self, content_item: str) -> str:
-            """Check if the content item contains exists image paths.
-            
-            
-            Arguments:
-                content_item {ContentTypes} -- The content item to check.
-
-            Returns:
-                str -- The error message if the content item contains relative images that are not saved in the pack's doc_files folder.
-            """
-            if not content_item:
-                return ""
-            error_message = ""
-            relative_images = re.findall(
-                r"(\!\[.*?\])\(((?!http).*?)\)$",
-                content_item,
-                re.IGNORECASE | re.MULTILINE,
-            )
-            relative_images += re.findall(  # HTML image tag
-                r'(<img.*?src\s*=\s*"((?!http).*?)")',
-                content_item,
-                re.IGNORECASE | re.MULTILINE,
-            )
-            if relative_images:
-                invalid_links = [relative_image_path for relative_image_path in relative_images if not re.match(DOC_FILE_IMAGE_REGEX, relative_image_path)]
-                if invalid_links:
-                    error_message += f"Detected that the relative image paths are not in the pack's doc_files:{invalid_links}. Please move them to the pack's doc_files folder."
-            return error_message

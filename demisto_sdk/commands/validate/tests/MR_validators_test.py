020c99ebcccd30f1d661f5e66c54de29341d4c20
sberman <sberman@paloaltonetworks.com>
2024-06-09 17:28:03 +0300

    Added validation MR100

---
+++ demisto_sdk/commands/validate/tests/MR_validators_test.py
@@ -0,0 +1,29 @@
+from demisto_sdk.commands.validate.tests.test_tools import (
+    create_modeling_rule_object,
+)
+from demisto_sdk.commands.validate.validators.MR_validators.MR100_validate_schema_file_exists import (
+    ValidateSchemaFileExistsValidator,
+)
+
+
+def test_ValidateSchemaFileExistsValidator_is_valid():
+    """
+    Given:
+        - Modeling Rules content items
+    When:
+        - run is_valid method
+    Then:
+        - Ensure that no ValidationResult returned when schema file exists.
+        - Ensure that the ValidationResult returned when there is no schema file.
+    """
+    modeling_rule = create_modeling_rule_object()
+    # Valid
+    assert not ValidateSchemaFileExistsValidator().is_valid([modeling_rule])
+
+    # Schema file does not exist
+    modeling_rule.schema_file.exist = False
+    results = ValidateSchemaFileExistsValidator().is_valid([modeling_rule])
+    assert (
+        "Packs/pack_0/ModelingRules/modelingrule_0/modelingrule_0.yml is missing a schema file."
+        in results[0].message
+    )

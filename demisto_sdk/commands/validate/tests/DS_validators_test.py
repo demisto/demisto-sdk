346ea811df37d56942bd6971d43955716b7ccb2c
Moshe <mgalitzki@paloaltonetworks.com>
2024-06-17 10:42:33 +0300

    ruff fix

--- demisto_sdk/commands/validate/tests/DS_validators_test.py
+++ demisto_sdk/commands/validate/tests/DS_validators_test.py
@@ -1,9 +1,9 @@
 import pytest
 
+from demisto_sdk.commands.common.constants import BETA_INTEGRATION_DISCLAIMER
 from demisto_sdk.commands.validate.tests.test_tools import (
     create_integration_object,
 )
-from demisto_sdk.commands.common.constants import BETA_INTEGRATION_DISCLAIMER
 
 
 @pytest.mark.parametrize(

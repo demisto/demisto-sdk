60663ff6855e1b3c1c4bbbd5247b3d79da4d5b1c
yaakovpraisler <ypreisler@paloaltonetworks.com>
2024-06-17 17:38:27 +0300

    Added PB125 validation to the new format

--- demisto_sdk/commands/validate/tests/PB_validators_test.py
+++ demisto_sdk/commands/validate/tests/PB_validators_test.py
@@ -17,6 +17,9 @@ from demisto_sdk.commands.validate.validators.PB_validators.PB118_is_input_key_n
 from demisto_sdk.commands.validate.validators.PB_validators.PB123_is_conditional_task_has_unhandled_reply_options import (
     IsAskConditionHasUnhandledReplyOptionsValidator,
 )
+from demisto_sdk.commands.validate.validators.PB_validators.PB125_playbook_only_default_next import (
+    PlaybookOnlyDefaultNextValidator,
+)
 
 
 @pytest.mark.parametrize(
@@ -222,3 +225,45 @@ def test_IsAskConditionHasUnhandledReplyOptionsValidator():
         )
     }
     assert IsAskConditionHasUnhandledReplyOptionsValidator().is_valid([playbook])
+
+
+def test_PB125_playbook_only_default_next_valid():
+    """
+    Given:
+    - A default standard playbook.
+
+    When:
+    - calling PlaybookOnlyDefaultNextValidator.is_valid.
+
+    Then:
+    - The results should be empty as expected without validation error results.
+    """
+    playbook = create_playbook_object()
+    assert not PlaybookOnlyDefaultNextValidator().is_valid([playbook])
+
+
+def test_PB125_playbook_only_default_next_not_valid():
+    """
+    Given:
+    - A playbook with a condition task with only a default nexttask.
+
+    When:
+    - calling PlaybookOnlyDefaultNextValidator.is_valid.
+
+    Then:
+    - The results should contain a validation error object.
+    """
+    playbook = create_playbook_object()
+    playbook.tasks = {
+        "0": TaskConfig(
+            **{
+                "id": "test task",
+                "taskid": "27b9c747-b883-4878-8b60-7f352098a631",
+                "type": "condition",
+                "message": {"replyOptions": ["yes"]},
+                "nexttasks": {"#default#": ["1"]},
+                "task": {"id": "27b9c747-b883-4878-8b60-7f352098a63c"},
+            }
+        )
+    }
+    assert PlaybookOnlyDefaultNextValidator().is_valid([playbook])
---
+++ demisto_sdk/commands/validate/validators/PB_validators/PB125_playbook_only_default_next.py
@@ -0,0 +1,45 @@
+
+from __future__ import annotations
+
+from typing import Iterable, List
+
+from demisto_sdk.commands.content_graph.objects.playbook import Playbook
+from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+
+ContentTypes = Playbook
+
+
+class PlaybookOnlyDefaultNextValidator(BaseValidator[ContentTypes]):
+    error_code = "PB125"
+    description = "Validates that a condition task doesn't has only a default next-task."
+    rationale = "Validates that a condition task doesn't has only a default next-task."
+    error_message = "Playbook with id:\"{playbook_id}\" has conditional tasks with an only default condition. Tasks IDs: {tasks}.\n" \
+                    "Please remove these tasks or add another non-default condition to these conditional tasks"
+    related_field = "conditions"
+    is_auto_fixable = False
+    related_file_type = [RelatedFileType.YML]
+
+    
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        tasks_with_only_default_nexttasks = dict()
+
+        for content_item in content_items:
+            for task_id, task in content_item.tasks.items():
+                if len(list(task.nexttasks or {})) == 1 and list((task.nexttasks or {}).keys())[0].lower() == "#default#":
+                    tasks_with_only_default_nexttasks.setdefault(content_item.object_id, []).append(task_id)
+
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message.format(playbook_id=playbook_id, tasks=tasks),
+                content_object=content_item,
+            )
+            for playbook_id, tasks in tasks_with_only_default_nexttasks.items()
+        ]
+    
+
+    

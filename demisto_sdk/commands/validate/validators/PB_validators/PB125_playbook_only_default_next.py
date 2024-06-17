de42b26f4302592f9a58d31b978db1451e2cb62b
yaakovpraisler <ypreisler@paloaltonetworks.com>
2024-06-17 17:54:38 +0300

    pre-commit

--- demisto_sdk/commands/validate/validators/PB_validators/PB125_playbook_only_default_next.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB125_playbook_only_default_next.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 from typing import Iterable, List
@@ -15,22 +14,30 @@ ContentTypes = Playbook
 
 class PlaybookOnlyDefaultNextValidator(BaseValidator[ContentTypes]):
     error_code = "PB125"
-    description = "Validates that a condition task doesn't has only a default next-task."
+    description = (
+        "Validates that a condition task doesn't has only a default next-task."
+    )
     rationale = "Validates that a condition task doesn't has only a default next-task."
-    error_message = "Playbook with id:\"{playbook_id}\" has conditional tasks with an only default condition. Tasks IDs: {tasks}.\n" \
-                    "Please remove these tasks or add another non-default condition to these conditional tasks"
+    error_message = (
+        'Playbook with id:"{playbook_id}" has conditional tasks with an only default condition. Tasks IDs: {tasks}.\n'
+        "Please remove these tasks or add another non-default condition to these conditional tasks"
+    )
     related_field = "conditions"
     is_auto_fixable = False
     related_file_type = [RelatedFileType.YML]
 
-    
     def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
-        tasks_with_only_default_nexttasks = dict()
+        tasks_with_only_default_nexttasks: dict = dict()
 
         for content_item in content_items:
             for task_id, task in content_item.tasks.items():
-                if len(list(task.nexttasks or {})) == 1 and list((task.nexttasks or {}).keys())[0].lower() == "#default#":
-                    tasks_with_only_default_nexttasks.setdefault(content_item.object_id, []).append(task_id)
+                if (
+                    len(list(task.nexttasks or {})) == 1
+                    and list((task.nexttasks or {}).keys())[0].lower() == "#default#"
+                ):
+                    tasks_with_only_default_nexttasks.setdefault(
+                        content_item.object_id, []
+                    ).append(task_id)
 
         return [
             ValidationResult(
@@ -40,6 +47,3 @@ class PlaybookOnlyDefaultNextValidator(BaseValidator[ContentTypes]):
             )
             for playbook_id, tasks in tasks_with_only_default_nexttasks.items()
         ]
-    
-
-    

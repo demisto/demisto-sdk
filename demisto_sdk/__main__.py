6a8aa077168719bd626d9b062bf1eb5a695ecdb3
Moshe <mgalitzki@paloaltonetworks.com>
2024-06-17 22:46:11 +0300

    revert

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -28,6 +28,7 @@ from demisto_sdk.commands.common.configuration import Configuration
 from demisto_sdk.commands.common.constants import (
     DEMISTO_SDK_MARKETPLACE_XSOAR_DIST_DEV,
     ENV_DEMISTO_SDK_MARKETPLACE,
+    INTEGRATIONS_README_FILE_NAME,
     ExecutionMode,
     FileType,
     MarketplaceVersions,
@@ -2237,6 +2238,13 @@ def init(ctx, **kwargs):
     is_flag=True,
     default=True,
 )
+@click.option(
+    "-f",
+    "--force",
+    help="Whether to force the generation of documentation (rather than update when it exists in version control)",
+    is_flag=True,
+    default=False,
+)
 @click.pass_context
 @logging_setup_decorator
 def generate_docs(ctx, **kwargs):
@@ -2308,21 +2316,23 @@ def _generate_docs_for_file(kwargs: Dict[str, Any]):
     custom_image_path: str = kwargs.get("custom_image_path", "")
     readme_template: str = kwargs.get("readme_template", "")
     use_graph = kwargs.get("graph", True)
+    force = kwargs.get("force", False)
 
     try:
         if command:
             if (
                 output_path
-                and (not Path(output_path, "README.md").is_file())
+                and (not Path(output_path, INTEGRATIONS_README_FILE_NAME).is_file())
                 or (not output_path)
                 and (
                     not Path(
-                        os.path.dirname(os.path.realpath(input_path)), "README.md"
+                        os.path.dirname(os.path.realpath(input_path)),
+                        INTEGRATIONS_README_FILE_NAME,
                     ).is_file()
                 )
             ):
                 raise Exception(
-                    "[red]The `command` argument must be presented with existing `README.md` docs."
+                    f"[red]The `command` argument must be presented with existing `{INTEGRATIONS_README_FILE_NAME}` docs."
                 )
 
         file_type = find_type(kwargs.get("input", ""), ignore_sub_categories=True)
@@ -2362,6 +2372,7 @@ def _generate_docs_for_file(kwargs: Dict[str, Any]):
                 command=command,
                 old_version=old_version,
                 skip_breaking_changes=skip_breaking_changes,
+                force=force,
             )
         elif file_type == FileType.SCRIPT:
             logger.info(f"Generating {file_type.value.lower()} documentation")
@@ -3013,7 +3024,7 @@ def openapi_codegen(ctx, **kwargs):
     "-ab",
     "--artifacts_bucket",
     help="The artifacts bucket name to upload the results to",
-    required=True,
+    required=False,
 )
 @click.option("-s", "--server", help="The server URL to connect to")
 @click.option("-c", "--conf", help="Path to content conf.json file", required=True)

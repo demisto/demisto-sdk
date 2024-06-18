25954da575f214fe50882f6055ee03dc7ff9be19
sbenyakir <shahaf.benyakir@demisto.com>
2024-06-18 08:59:04 +0300

    fixed cr comments

--- demisto_sdk/commands/validate/validators/PB_validators/PB126_is_default_not_only_condition.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB126_is_default_not_only_condition.py
@@ -15,15 +15,15 @@ ContentTypes = Playbook
 class IsDefaultNotOnlyConditionValidator(BaseValidator[ContentTypes]):
     error_code = "PB126"
     description = (
-        "ensure that conditional tasks have more than path which is not the default one"
+        "Ensure that conditional tasks have an execution path besides for the default"
     )
     rationale = (
         "We want to ensure that conditional tasks have more than path which is not the default one o/w it "
         "make no sense to have such"
     )
     error_message = (
-        "Playbook task with id:{} only has a default option. Please remove this task or add another "
-        "non-default option to the task"
+        "The following playbook conditional tasks only have a default option: {}. Please remove these tasks or add "
+        "another non-default option to each task."
     )
     related_field = "conditions"
     is_auto_fixable = False
@@ -31,6 +31,7 @@ class IsDefaultNotOnlyConditionValidator(BaseValidator[ContentTypes]):
     def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
         validation_results = []
         for content_item in content_items:
+            failed_tasks = []
             for task in content_item.tasks.values():
                 if (
                     task.type == PlaybookTaskType.CONDITION
@@ -41,11 +42,13 @@ class IsDefaultNotOnlyConditionValidator(BaseValidator[ContentTypes]):
                         map(str.upper, task.message.get("replyOptions", []))
                     )
                     if len(reply_options) == 1 and "#default#".upper() in reply_options:
-                        validation_results.append(
-                            ValidationResult(
-                                validator=self,
-                                message=self.error_message.format(task.id),
-                                content_object=content_item,
-                            )
-                        )
+                        failed_tasks.append(f"Task: {task.id}")
+            if failed_tasks:
+                validation_results.append(
+                    ValidationResult(
+                        validator=self,
+                        message=self.error_message.format(failed_tasks),
+                        content_object=content_item,
+                    )
+                )
         return validation_results

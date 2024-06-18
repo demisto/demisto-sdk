b6555ec1cda82f68f460c6449972f22296c70315
RotemAmit <ramit@paloaltonetworks.com>
2024-06-18 10:00:01 +0300

    cr updates

--- demisto_sdk/commands/validate/validators/PB_validators/PB108_is_valid_task_id.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB108_is_valid_task_id.py
@@ -17,7 +17,7 @@ class IsValidTaskIdValidator(BaseValidator[ContentTypes]):
     error_code = "PB108"
     description = "Validate that the task ID and the 'id' under the 'task' field are from UUID format."
     rationale = "Validate that the task ID and the 'id' under the 'task' field are from UUID format."
-    error_message = "This playbook has tasks with invalid 'taskid' or invalid 'id' under the 'task' field."
+    error_message = "This playbook has tasks with invalid 'taskid' or invalid 'id' under the 'task' field.\n{0}"
     related_field = "taskid"
     is_auto_fixable = False
 
@@ -31,17 +31,17 @@ class IsValidTaskIdValidator(BaseValidator[ContentTypes]):
         results: List[ValidationResult] = []
         for content_item in content_items:
             invalid_tasks = self.invalid_tasks(content_item.tasks)
-            tasks_error_message = self.error_message
+            tasks_error_message = ""
             for task_id in invalid_tasks:
                 tasks_error_message = (
-                    f"{tasks_error_message}\nTask {task_id} has invalid UUIDs in the fields "
-                    f"{invalid_tasks[task_id]}."
+                    f"{tasks_error_message}Task {task_id} has invalid UUIDs in the fields "
+                    f"{invalid_tasks[task_id]}.\n"
                 )
             if invalid_tasks:
                 results.append(
                     ValidationResult(
                         validator=self,
-                        message=tasks_error_message,
+                        message=self.error_message.format(tasks_error_message),
                         content_object=content_item,
                     )
                 )
@@ -55,8 +55,7 @@ class IsValidTaskIdValidator(BaseValidator[ContentTypes]):
             - dict[str, TaskConfig] that contains the invalid tasks.
         """
         invalid_tasks = {}
-        for task_id in tasks:
-            task = tasks[task_id]
+        for task_id, task in tasks.items():
             taskid = task.taskid
             inner_id = task.task.id
             is_valid_taskid = is_string_uuid(taskid)

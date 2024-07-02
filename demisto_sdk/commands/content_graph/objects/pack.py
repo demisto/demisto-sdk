65f90f4c9b1e8eef3540fcb0464296d050120bcc
maimorag <mmorag@paloaltonetworks.com>
2024-07-02 15:51:26 +0300

    revert

--- demisto_sdk/commands/content_graph/objects/pack.py
+++ demisto_sdk/commands/content_graph/objects/pack.py
@@ -130,7 +130,7 @@ class Pack(BaseContent, PackMetadata, content_type=ContentType.PACK):
     content_items: PackContentItems = Field(
         PackContentItems(), alias="contentItems", exclude=True
     )
-    pack_metadata_dict: dict = Field({}, exclude=True)
+    pack_metadata_dict: Optional[dict] = Field({}, exclude=True)
 
     @classmethod
     def from_orm(cls, obj) -> "Pack":

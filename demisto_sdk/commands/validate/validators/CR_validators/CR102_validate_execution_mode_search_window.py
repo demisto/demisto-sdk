0182611dc130fd9e00417d8535141484d215ea10
noy <nodavidi.paloaltonetworks.com>
2024-07-08 15:53:48 +0300

    added a new file for CR102 validation

---
+++ demisto_sdk/commands/validate/validators/CR_validators/CR102_validate_execution_mode_search_window.py
@@ -0,0 +1,42 @@
+
+from __future__ import annotations
+
+from typing import Iterable, List
+
+from demisto_sdk.commands.content_graph.objects.correlation_rule import CorrelationRule
+from demisto_sdk.commands.validate.validators.base_validator import (
+        BaseValidator,
+        ValidationResult,
+)
+
+ContentTypes = CorrelationRule
+
+
+class ExecutionModeSearchWindowValidator(BaseValidator[ContentTypes]):
+    error_code = "CR102"
+    description = (
+        "Validates 'search_window' existence and non-emptiness for 'execution_mode' = 'SCHEDULED'."
+    )
+    rationale = ""
+    error_message = "The 'search_window' key must exist and cannot be empty when the 'execution_mode' is set to 'SCHEDULED'."
+    related_field = ""
+    is_auto_fixable = False
+
+    
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message,
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if (
+               "search_window" not in self.current_file) or (
+                self.current_file["execution_mode"] == "SCHEDULED"
+                and not self.current_file["search_window"]
+            )
+        ]
+        
+
+    

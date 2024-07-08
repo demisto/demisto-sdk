21aa7ba86c8fce8e550facec1e7bbb91082647be
Kobbi Gal <kgal@paloaltonetworks.com>
2024-07-08 12:59:18 +0300

    set ci in typer args
    changed files now list of paths

--- demisto_sdk/scripts/validate_mypy_global_ignore.py
+++ demisto_sdk/scripts/validate_mypy_global_ignore.py
@@ -7,35 +7,14 @@ import typer
 from demisto_sdk.commands.common.constants import DEMISTO_GIT_PRIMARY_BRANCH
 from demisto_sdk.commands.common.git_util import GitUtil
 from demisto_sdk.commands.common.logger import logger, logging_setup
-from demisto_sdk.scripts.scripts_common import (
-    HELP_CHANGED_FILES,
-    is_ci,
-    split_files,
-)
+from demisto_sdk.scripts.scripts_common import HELP_CHANGED_FILES
 
 MYPY_GLOBAL_IGNORE_PATTERN = re.compile(r"^#\s*type\s*:\s*ignore")
 
 main = typer.Typer()
 
 
-def get_py_files(input_files: List[str]) -> List[str]:
-    """
-    Helper function to filter a `list` of file `str`
-    to return a list of Python files.
-
-    Args:
-    - `input_files` (``List[str]``): The input files.
-
-    Returns
-    - `List[str]` containing a list of strings of Python files.
-    """
-
-    python_files = [file for file in input_files if file.endswith(".py")]
-
-    return python_files
-
-
-def has_global_type_ignore(file_path_str: str) -> Tuple[bool, Union[int, None]]:
+def has_global_type_ignore(file_path: Path) -> Tuple[bool, Union[int, None]]:
     """
     Helper function to check whether the input file
     has the global mypy ignore set.
@@ -50,12 +29,6 @@ def has_global_type_ignore(file_path_str: str) -> Tuple[bool, Union[int, None]]:
     was found, `None` if it wasn't found.
     """
 
-    file_path = Path(file_path_str)
-
-    if not file_path.exists():
-        logger.error(f"File '{file_path}' doesn't exist")
-        raise typer.Exit(1)
-
     lines = file_path.read_text().splitlines()
 
     for line_num, line in enumerate(lines):
@@ -67,49 +40,54 @@ def has_global_type_ignore(file_path_str: str) -> Tuple[bool, Union[int, None]]:
 
 @main.command(help="Validate the changed Python files don't specify global mypy ignore")
 def validate_mypy_global_ignore(
-    changed_files: List[str] = typer.Argument(
-        default=None, help=HELP_CHANGED_FILES, callback=split_files
-    )
+    changed_files: List[Path] = typer.Argument(
+        default=None,
+        help=HELP_CHANGED_FILES,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+    ),
+    ci: bool = typer.Option(envvar="CI", default=False),
 ) -> None:
     """
     Validate whether the Python file has a global mypy type ignore set.
     Exit code 0 if global mypy type ignore is set, 1 otherwise.
 
     Args:
-    - `changed_files` (``List[str]``): The files to check, e.g. 'test/f1.py f2.py f3.py'.
+    - `changed_files` (``List[Path]``): The files paths to check.
     """
 
     exit_code = 0
 
     logging_setup()
-    git_util = GitUtil.from_content_path()
-
-    ci = is_ci()
-
     logger.debug(f"Running in CI environment: {ci}")
 
-    if changed_files:
-        logger.debug(f"Got {','.join(changed_files)} as input...")
-    else:
+    logger.debug(f"Input {changed_files=}")
+
+    if not changed_files:
         logger.debug(
             f"Getting changed files from git branch '{DEMISTO_GIT_PRIMARY_BRANCH}'..."
         )
+        git_util = GitUtil.from_content_path()
         changed_files = [
-            str(path)
-            for path in git_util.get_all_changed_files(DEMISTO_GIT_PRIMARY_BRANCH)
+            Path(git_util.git_path()) / Path(path)
+            for path in git_util.get_all_changed_files(
+                DEMISTO_GIT_PRIMARY_BRANCH, include_untracked=True
+            )
         ]
 
-    py_files = get_py_files(changed_files)
-    if py_files:
+    logger.debug(
+        f"Iterating over '{changed_files=}' to check for global mypy type ignore..."
+    )
 
-        logger.debug(
-            f"Iterating over {len(py_files)} ({''.join(py_files)}) Python changed files to check if they have global mypy ignore comments..."
-        )
+    if changed_files:
 
         result: Dict[str, Any] = {}
 
         for changed_file in changed_files:
-            result[changed_file] = has_global_type_ignore(changed_file)
+            result[changed_file.absolute().name] = has_global_type_ignore(changed_file)
+
+        logger.debug(f"{result=}")
 
         for filename, (has_global_ignore, line_number) in result.items():
             if has_global_ignore:

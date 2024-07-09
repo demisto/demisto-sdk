58bc0269a55cfb2cd904b8ebe2c2636915bcfc3e
noy <nodavidi.paloaltonetworks.com>
2024-07-09 15:36:11 +0300

    added fields in CorrelationRuleParser

--- demisto_sdk/commands/validate/validators/CR_validators/CR102_validate_execution_mode_search_window.py
+++ demisto_sdk/commands/validate/validators/CR_validators/CR102_validate_execution_mode_search_window.py
@@ -19,12 +19,8 @@ class ExecutionModeSearchWindowValidator(BaseValidator[ContentTypes]):
     error_message = "The 'search_window' key must exist and cannot be empty when the 'execution_mode' is set to 'SCHEDULED'."
     related_field = ""
     is_auto_fixable = False
-
-    def __init__(self):
-        self.execution_mode = self.current_file.get("execution_mode", None)
-        self.search_window = self.current_file.get("search_window", None)
         
-    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+    def is_valid(self, content_items: Iterable[CorrelationRule]) -> List[ValidationResult]:
         return [
             ValidationResult(
                 validator=self,
@@ -34,7 +30,7 @@ class ExecutionModeSearchWindowValidator(BaseValidator[ContentTypes]):
             for content_item in content_items
             if (
                ("search_window" not in self.current_file) or (
-                self.execution_mode == "SCHEDULED"
+                content_item.execution_mode == "SCHEDULED"
                 and not self.search_window)
             )
         ]

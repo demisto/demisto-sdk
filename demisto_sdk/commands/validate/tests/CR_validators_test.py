8d088d04305bb7a45d6f6c9af6a50a2ba9864dcd
noy <nodavidi.paloaltonetworks.com>
2024-07-10 09:31:15 +0300

    added 2 unittests

--- demisto_sdk/commands/validate/tests/CR_validators_test.py
+++ demisto_sdk/commands/validate/tests/CR_validators_test.py
@@ -1,7 +1,37 @@
+from demisto_sdk.commands.content_graph.objects.correlation_rule import CorrelationRule
+from demisto_sdk.commands.validate.tests.test_tools import (
+    create_correlation_rule_object,
+)
 from demisto_sdk.commands.validate.validators.CR_validators.CR102_validate_execution_mode_search_window import (
     ExecutionModeSearchWindowValidator,
 )
 
 
-def test_validate_execution_mode_search_window():
-    pass
+def test_validate_execution_mode_search_window_with_no_fields():
+    """
+    Given:
+        An default correlation rule with no search_window and execution_mode fields.
+    When:
+        Calling Validate
+    Then:
+        The validation should fail
+    """
+    correlation_rule = create_correlation_rule_object()
+    assert len(ExecutionModeSearchWindowValidator().is_valid([correlation_rule])) == 1
+    
+
+def test_validate_execution_mode_search_window_with_empty_search_window():
+    """
+    Given:
+        A correlation rule with empty search_window but execution_mode == SCHEDULED.
+    When:
+        Calling Validate
+    Then:
+        The validation should fail
+    """
+    correlation_rule = create_correlation_rule_object(
+        ["execution_mode", "search_window"],
+        ["SCHEDULED", ""]
+    )
+    assert len(ExecutionModeSearchWindowValidator().is_valid([correlation_rule])) == 1
+

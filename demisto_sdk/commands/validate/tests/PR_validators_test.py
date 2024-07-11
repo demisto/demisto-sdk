45a2a4207706dafe79c23174c40ccdc5335cb18c
noy <nodavidi.paloaltonetworks.com>
2024-07-11 14:59:27 +0300

    added unitests and change the content type comparation

--- demisto_sdk/commands/validate/tests/PR_validators_test.py
+++ demisto_sdk/commands/validate/tests/PR_validators_test.py
@@ -1,10 +1,3 @@
-from demisto_sdk.commands.validate.tests.test_tools import (
-    create_parsing_rule_object,
-    create_modeling_rule_object,
-)
-from demisto_sdk.commands.validate.validators.PR_validators.PR101_invalid_parsing_or_modeling_rule_suffix_name import (
-    ParsingAndModelingRuleSuffixNameValidator,
-)
 from demisto_sdk.commands.common.constants import (
     MODELING_RULE,
     MODELING_RULE_ID_SUFFIX,
@@ -13,34 +6,50 @@ from demisto_sdk.commands.common.constants import (
     PARSING_RULE_ID_SUFFIX,
     PARSING_RULE_NAME_SUFFIX,
 )
+from demisto_sdk.commands.validate.tests.test_tools import (
+    create_modeling_rule_object,
+    create_parsing_rule_object,
+)
+from demisto_sdk.commands.validate.validators.PR_validators.PR101_invalid_parsing_or_modeling_rule_suffix_name import (
+    ParsingAndModelingRuleSuffixNameValidator,
+)
+
 
 def test_parsing_rule_with_valid_suffixes():
     parsing_rule = create_parsing_rule_object (
-        ["object_id", "name"], ["Example_ParsingRule", "Example Parsing Rule"]
+        paths=["id", "name"], values=["Example_" + PARSING_RULE_ID_SUFFIX, "Example " + PARSING_RULE_NAME_SUFFIX]
     )
     assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 0
     
 def test_modeling_rule_with_valid_suffixes():
     modeling_rule = create_modeling_rule_object (
-        ["object_id", "name"], ["Example_ModelingRule", "Example Modeling Rule"]
+        ["id", "name"], ["Example_" + MODELING_RULE_ID_SUFFIX, "Example " + MODELING_RULE_NAME_SUFFIX]
     )
     assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([modeling_rule])) == 0
 
 
 def test_parsing_rule_with_invalid_id_suffix():
     parsing_rule = create_parsing_rule_object (
-        ["object_id", "name"], ["Example_", "Example Parsing Rule"]
+        ["id", "name"], ["Example_", "Example " + PARSING_RULE_NAME_SUFFIX]
     )
     assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 1
 
 def test_parsing_rule_with_invalid_name_suffix():
     parsing_rule = create_parsing_rule_object (
-        ["object_id", "name"], ["Example_ParsingRule", "Example Parsing"]
+        ["id", "name"], ["Example_" + PARSING_RULE_ID_SUFFIX, "Example Parsing"]
     )
-    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 0
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 1
     
 
 def test_modeling_rule_with_invalid_id_suffix():
+    modeling_rule = create_modeling_rule_object (
+        ["id", "name"], ["Example_", "Example " + MODELING_RULE_NAME_SUFFIX]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([modeling_rule])) == 1
     
     
 def test_modeling_rule_with_invalid_name_suffix():
+    modeling_rule = create_modeling_rule_object (
+        ["id", "name"], ["Example_" + MODELING_RULE_ID_SUFFIX, "Example "]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([modeling_rule])) == 1
--- demisto_sdk/commands/validate/validators/PR_validators/PR101_invalid_parsing_or_modeling_rule_suffix_name.py
+++ demisto_sdk/commands/validate/validators/PR_validators/PR101_invalid_parsing_or_modeling_rule_suffix_name.py
@@ -12,6 +12,9 @@ from demisto_sdk.commands.common.constants import (
     PARSING_RULE_ID_SUFFIX,
     PARSING_RULE_NAME_SUFFIX,
 )
+from demisto_sdk.commands.content_graph.common import (
+    ContentType,
+)
 from demisto_sdk.commands.content_graph.objects.modeling_rule import ModelingRule
 from demisto_sdk.commands.content_graph.objects.parsing_rule import ParsingRule
 from demisto_sdk.commands.validate.validators.base_validator import (
@@ -30,7 +33,7 @@ class ParsingAndModelingRuleSuffixNameValidator(BaseValidator[ContentTypes]):
     is_auto_fixable = False
 
     
-    def is_valid_using_graph(self, content_items: Iterable[ContentTypes], validate_all_files: bool) -> List[ValidationResult]:
+    def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
         return [
             ValidationResult(
                 validator=self,
@@ -40,10 +43,10 @@ class ParsingAndModelingRuleSuffixNameValidator(BaseValidator[ContentTypes]):
             )
             for content_item in content_items
             if (
-                (content_item.content_type == MODELING_RULE and not content_item.name.endswith(MODELING_RULE_NAME_SUFFIX)) or
-                (content_item.content_type == MODELING_RULE and not content_item.object_id.endswith(MODELING_RULE_ID_SUFFIX)) or
-                (content_item.content_type == PARSING_RULE and not content_item.name.endswith(PARSING_RULE_NAME_SUFFIX)) or
-                (content_item.content_type == PARSING_RULE and not content_item.object_id.endswith(PARSING_RULE_ID_SUFFIX))
+                (content_item.content_type == ContentType.MODELING_RULE and not content_item.name.endswith(MODELING_RULE_NAME_SUFFIX)) or
+                (content_item.content_type == ContentType.MODELING_RULE and not content_item.object_id.endswith(MODELING_RULE_ID_SUFFIX)) or
+                (content_item.content_type == ContentType.PARSING_RULE and not content_item.name.endswith(PARSING_RULE_NAME_SUFFIX)) or
+                (content_item.content_type == ContentType.PARSING_RULE and not content_item.object_id.endswith(PARSING_RULE_ID_SUFFIX))
             )
         ]
         

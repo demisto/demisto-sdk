67b80ccf28df280460b03f05a7f5b01bdcf32242
noy <nodavidi.paloaltonetworks.com>
2024-07-11 13:24:18 +0300

    unittests writing

--- demisto_sdk/commands/validate/tests/PR_validators_test.py
+++ demisto_sdk/commands/validate/tests/PR_validators_test.py
@@ -1,3 +1,7 @@
+from demisto_sdk.commands.validate.tests.test_tools import (
+    create_parsing_rule_object,
+    create_modeling_rule_object,
+)
 from demisto_sdk.commands.validate.validators.PR_validators.PR101_invalid_parsing_or_modeling_rule_suffix_name import (
     ParsingAndModelingRuleSuffixNameValidator,
 )
@@ -10,11 +14,30 @@ from demisto_sdk.commands.common.constants import (
     PARSING_RULE_NAME_SUFFIX,
 )
 
-def test_parsing_rule_with_invalid_id_suffix():
-    parsing_rule = 
+def test_parsing_rule_with_valid_suffixes():
+    parsing_rule = create_parsing_rule_object (
+        ["object_id", "name"], ["Example_ParsingRule", "Example Parsing Rule"]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 0
     
+def test_modeling_rule_with_valid_suffixes():
+    modeling_rule = create_modeling_rule_object (
+        ["object_id", "name"], ["Example_ModelingRule", "Example Modeling Rule"]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([modeling_rule])) == 0
+
+
+def test_parsing_rule_with_invalid_id_suffix():
+    parsing_rule = create_parsing_rule_object (
+        ["object_id", "name"], ["Example_", "Example Parsing Rule"]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 1
 
 def test_parsing_rule_with_invalid_name_suffix():
+    parsing_rule = create_parsing_rule_object (
+        ["object_id", "name"], ["Example_ParsingRule", "Example Parsing"]
+    )
+    assert len(ParsingAndModelingRuleSuffixNameValidator().is_valid([parsing_rule])) == 0
     
 
 def test_modeling_rule_with_invalid_id_suffix():

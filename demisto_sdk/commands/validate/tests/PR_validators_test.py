3fddcb0636a768e49a2b2c912541e7117234bcc4
noy <nodavidi.paloaltonetworks.com>
2024-07-11 12:52:51 +0300

    unittests

---
+++ demisto_sdk/commands/validate/tests/PR_validators_test.py
@@ -0,0 +1,23 @@
+from demisto_sdk.commands.validate.validators.PR_validators.PR101_invalid_parsing_or_modeling_rule_suffix_name import (
+    ParsingAndModelingRuleSuffixNameValidator,
+)
+from demisto_sdk.commands.common.constants import (
+    MODELING_RULE,
+    MODELING_RULE_ID_SUFFIX,
+    MODELING_RULE_NAME_SUFFIX,
+    PARSING_RULE,
+    PARSING_RULE_ID_SUFFIX,
+    PARSING_RULE_NAME_SUFFIX,
+)
+
+def test_parsing_rule_with_invalid_id_suffix():
+    parsing_rule = 
+    
+
+def test_parsing_rule_with_invalid_name_suffix():
+    
+
+def test_modeling_rule_with_invalid_id_suffix():
+    
+    
+def test_modeling_rule_with_invalid_name_suffix():

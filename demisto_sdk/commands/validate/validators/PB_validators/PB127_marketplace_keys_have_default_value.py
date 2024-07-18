04d7540fec1d06f3b71cd849a8ee4e4d34c695dd
dmeylakh <dmeylakh@paloaltonetworks.com>
2024-07-18 12:07:15 +0300

    committing befor merging with master

--- demisto_sdk/commands/validate/validators/PB_validators/PB127_marketplace_keys_have_default_value.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB127_marketplace_keys_have_default_value.py
@@ -9,6 +9,7 @@ from demisto_sdk.commands.prepare_content.preparers.marketplace_suffix_preparer 
 )
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,
+    FixResult,
     ValidationResult,
 )
 
@@ -18,18 +19,21 @@ ContentTypes = Playbook
 class MarketplaceKeysHaveDefaultValidator(BaseValidator[ContentTypes]):
     error_code = "PB127"
     description = "Ensure that any yml keys that are marketplace only have a default non-marketplace counterpart."
-    rationale = "To ensure the required yml keys, we have to have a default value for any marketplace only keys."
+    rationale = "To be able to validate the existence of required yml keys, we need to make sure that in addition to a specific marketplace key (say mykey:xsoar) there is also a basic key with no specific marketplace (for example: mykey)"
     error_message = (
         "The following playbook yml keys only do not have a default option: {}. Please remove these keys or add "
         "another default option to each key."
     )
-    fix_message = "Added default value to the following playbook keys: {0}."
+    fix_message = "Added default value to the following playbook keys:\n{0}"
     marketplace_suffixes = [
         marketplace for marketplace in MarketplaceVersions.__members__.values()
     ]
+    bad_paths_dict: dict = dict()
+    is_auto_fixable = True
 
     def is_valid(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
         validation_results = []
+
         for content_item in content_items:
             marketplace_keys_with_no_default = self.check_recursively(
                 content_item.data, bad_keys=[]
@@ -44,44 +48,106 @@ class MarketplaceKeysHaveDefaultValidator(BaseValidator[ContentTypes]):
                         content_object=content_item,
                     )
                 )
+                self.bad_paths_dict[
+                    content_item.name
+                ] = marketplace_keys_with_no_default
 
         return validation_results
 
-    def check_recursively(self, datum, path="root", bad_keys=[], fix=False) -> list:
+    def check_recursively(self, datum, path="root", bad_keys=[]) -> list:
+        """If marketplace suffix is encountered in the data, check for default value. Do
+
+        Args:
+            datum (dict or list): The root data node to check.
+            path (str): The path to the current data node.
+            bad_keys (list): The bad keys that are already known.
+
+        Return:
+            (list) All data paths with no default value.
+        """
         # If the datum is a list, iterate over its values and check recursively.
         if isinstance(datum, list):
             for index, item in enumerate(datum):
-                bad_inner_keys = self.check_recursively(
-                    item, path=f"{path}.[{index}]", bad_keys=bad_keys, fix=fix
-                )
-                bad_keys.extend(
-                    bad_key for bad_key in bad_inner_keys if bad_key not in bad_keys
+                bad_keys = self.check_recursively(
+                    item,
+                    path=f"{path}.[{index}]",
+                    bad_keys=bad_keys,
                 )
             return bad_keys
 
         # If the datum is a dictionary, iterate over its keys.
         elif isinstance(datum, dict):
-            for key in datum.keys():
-                value = datum[key]
+            for key, value in datum.items():
                 if isinstance(value, (list, dict)):
                     # If the value is a list or a dictionary recursively check.
-                    bad_inner_keys = self.check_recursively(
-                        value, path=f"{path}.{key}", bad_keys=bad_keys, fix=fix
-                    )
-                    bad_keys.extend(
-                        bad_key for bad_key in bad_inner_keys if bad_key not in bad_keys
+                    bad_keys = self.check_recursively(
+                        value,
+                        path=f"{path}.{key}",
+                        bad_keys=bad_keys,
                     )
                 if isinstance(key, str) and SEPARATOR in str(key):
                     for suffix in self.marketplace_suffixes:
                         if key.casefold().endswith(suffix):
                             # Construct the default key without the suffix and check if it exists in the datum.
-                            clean_key = key[: -(len(suffix) + len(SEPARATOR))]
+                            clean_key = key.split(SEPARATOR)[0]
                             if (
                                 clean_key not in datum
-                                and f"{path}.{key}" not in bad_keys
+                                and f"{path}.{clean_key}" not in bad_keys
                             ):
-                                bad_keys.append(f"{path}.{key}")
+                                bad_keys.append(f"{path}.{clean_key}")
                             # No need to keep matching marketplaces to the suffix once one is found.
                             break
 
         return bad_keys
+
+    def get_parent_path_from_data(self, data, key_path):
+        keys = key_path.split(".")
+        last_key = keys[-1]
+        keys = keys[1:-1]  # No root and no last key
+        data_node = data
+        for key_index, key in enumerate(keys):
+            if (
+                key.startswith("[")
+                and key.endswith("]")
+                and isinstance(data_node, list)
+            ):
+                key = int(key[1:-1])
+            data_node = data_node[key]
+
+        if (
+            last_key.startswith("[")
+            and last_key.endswith("]")
+            and isinstance(data_node, list)
+        ):
+            last_key = int(key[1:-1])
+
+        return data_node, last_key
+
+    def fix(self, content_item: ContentTypes) -> FixResult:
+        new_content_data = content_item.data
+        bad_paths = self.bad_paths_dict[content_item.name]
+        default_paths_used = {}
+        for bad_path in bad_paths:
+            parent_datum, last_key = self.get_parent_path_from_data(
+                new_content_data, bad_path
+            )
+            for suffix in self.marketplace_suffixes:
+                key_with_suffix = f"{last_key}{SEPARATOR}{suffix}"
+                if key_with_suffix in parent_datum.keys():
+                    new_default_value = parent_datum[key_with_suffix]
+                    parent_datum[last_key] = new_default_value
+                    default_paths_used[bad_path] = key_with_suffix
+                    break
+
+        return FixResult(
+            validator=self,
+            message=self.fix_message.format(
+                "\n".join(
+                    [
+                        f"For {bad_path} used {key_used}."
+                        for bad_path, key_used in default_paths_used.items()
+                    ]
+                )
+            ),
+            content_object=content_item,
+        )

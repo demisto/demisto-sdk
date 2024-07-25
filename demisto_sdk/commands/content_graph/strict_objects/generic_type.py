ca1d255bd2cca99084bd097b7ab080bad3c612b5
rshunim <rshunim@paloaltonetworks.com>
2024-07-25 11:06:01 +0300

    no more "type:ignore[misc,valid-type]"

--- demisto_sdk/commands/content_graph/strict_objects/generic_type.py
+++ demisto_sdk/commands/content_graph/strict_objects/generic_type.py
@@ -3,8 +3,18 @@ from pydantic import Field
 from demisto_sdk.commands.content_graph.strict_objects.base_strict_model import (
     StrictGenericIncidentType,
 )
+from demisto_sdk.commands.content_graph.strict_objects.common import (
+    BaseStrictModel,
+    create_model,
+)
 
 
-class StrictGenericType(StrictGenericIncidentType):  # type:ignore[valid-type,misc]
+class _StrictGenericType(BaseStrictModel):
     definition_id: str = Field(..., alias="definitionId")
     generic_module_id: str = Field(..., alias="genericModuleId")
+
+
+StrictGenericType = create_model(
+    model_name="StrictGenericType",
+    base_models=(_StrictGenericType, StrictGenericIncidentType),
+)

1b6ba81723e3c9fda7f5c4876fd1984f7c6a68d2
rshunim <rshunim@paloaltonetworks.com>
2024-07-25 16:50:31 +0300

    works

--- demisto_sdk/scripts/monkeytype.py
+++ demisto_sdk/scripts/monkeytype.py
@@ -11,16 +11,11 @@ app = typer.Typer()
 
 @app.command("run")
 def run(path: Path = Path.cwd()):
-    path = Path("/Users/rshunim/dev/demisto/content/Packs/ipinfo/Integrations/ipinfo_v2")
-    modules = subprocess.run(
-        ["monkeytype", "list-modules"], text=True, check=True, capture_output=True, cwd=path,
-    ).stdout.splitlines()
     runner_path = path / "runner.py"
-    python_path = ':'.join(str(path) for path in PYTHONPATH)
+    python_path = ':'.join(str(path_) for path_ in PYTHONPATH + [path])
     env = os.environ.copy() | {'PYTHONPATH': os.environ['PYTHONPATH'] + ":" + python_path}
     subprocess.run(
         [
-            # "/src/Tests/scripts/script_runner.py",
             "pytest",
             str(path),
             "--monkeytype-output=./monkeytype.sqlite3",
@@ -30,13 +25,13 @@ def run(path: Path = Path.cwd()):
         cwd=path
     )
     modules = subprocess.run(
-        ["monkeytype", "list-modules"], text=True, check=True, capture_output=True, cwd=path,
+        ["monkeytype", "list-modules"], text=True, check=True, capture_output=True, cwd=path, env=env
     ).stdout.splitlines()
     filtered_modules = set(modules).difference(("demistomock", "CommonServerPython"))
     runner_path.write_text("\n".join(f"import {module}\n{module}.main()" for module in filtered_modules))
     for module in filtered_modules:
-        subprocess.run(["monkeytype", "-v", "stub", module], check=True, cwd=path)
-        subprocess.run(["monkeytype", "-v", "apply", module], check=True, cwd=path)
+        subprocess.run(["monkeytype", "-v", "stub", module], check=True, cwd=path, env=env)
+        subprocess.run(["monkeytype", "-v", "apply", module], check=True, cwd=path, env=env)
     runner_path.unlink()
 
 
@@ -46,20 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""pip install pytest-monkeytype
-
-# Generate annotations by running your pytest tests as usual:
-py.test --monkeytype-output=./monkeytype.sqlite3
-
-# Get a listing of modules annotated by monkeytype
-monkeytype list-modules
-
-# Generate a stub file for those annotations using monkeytype:
-monkeytype stub some.module
-
-# Apply these annotations directly
-monkeytype apply some.module"""
-
-
-# TODO - running pytest-monkeytypeon Arkime_test.py, now the next step is to solve our problem.
-# we want to run it on a single script, not

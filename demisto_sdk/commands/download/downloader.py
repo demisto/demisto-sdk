2323454e744a111f6c30c857963f4ca9f35ebc9d
maimorag <mmorag@paloaltonetworks.com>
2024-07-25 10:16:31 +0300

    fix

--- demisto_sdk/commands/download/downloader.py
+++ demisto_sdk/commands/download/downloader.py
@@ -115,20 +115,15 @@ def format_playbook_task(content_item_data: dict[str, dict]) -> dict[str, dict]:
         Dict: The updated content_item_data where 'playbookId' is replaced with 'playbookName' in tasks running sub-playbooks.
     """
     content_data = content_item_data.get("data", {})
-    tasks_data = content_data.get("tasks", {})
-
-    for task_id, task_data in tasks_data.items():
-        if task_data.get("type") == "playbook":
+    all_tasks_data = content_data.get("tasks", {})
+    if isinstance(all_tasks_data, dict) and all_tasks_data:
+        for task_id, task_data in all_tasks_data.items():
             playbook_id_value = task_data.get("task", {}).get("playbookId")
-            if playbook_id_value:
-                new_task_data = {
-                    "task": {
-                        "playbookName": playbook_id_value,
-                    }
-                }
-                tasks_data[task_id] = new_task_data
-
-    content_data["tasks"] = tasks_data
+            if task_data.get("type") == "playbook" and playbook_id_value:
+                task_data["task"]["playbookName"] = playbook_id_value
+                task_data["task"].pop("playbookId", None)
+                all_tasks_data[task_id]=task_data
+    content_data["tasks"] = all_tasks_data
     return content_item_data
 
 
@@ -369,9 +364,7 @@ class Downloader:
                 or (compiled_regex and re.match(compiled_regex, content_item_name))
                 or content_item_name in self.input_files
             ):
-                filtered_custom_content_objects[file_name] = format_playbook_task(
-                    content_item_data
-                )
+                filtered_custom_content_objects[file_name] = content_item_data
 
         logger.info(
             f"Filtering process completed, {len(filtered_custom_content_objects)}/{original_count} items remain."

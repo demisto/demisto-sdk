671bbd56ebd8f646e1a9b3d3f159401d5d0cb81e
BEAdi <72088126+BEAdi@users.noreply.github.com>
2024-07-25 12:31:25 +0300

    support previous value

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -1216,10 +1216,12 @@ class CloudServerContext(ServerContext):
         super().__init__(build_context, server_private_ip, use_retries_mechanism)
         self.machine = cloud_machine
         self.server_url = self.server_ip
-        self.api_key = self.build_context.api_key.get(cloud_machine, {}).get("api-key")
+        self.api_key = self.build_context.api_key.get(cloud_machine, {}).get(
+            "api-key"
+        ) or self.build_context.api_key.get(cloud_machine)
         self.auth_id = self.build_context.api_key.get(cloud_machine, {}).get(
             "x-xdr-auth-id"
-        )
+        ) or self.build_context.env_json.get(cloud_machine, {}).get("x-xdr-auth-id")
         os.environ.pop(
             "DEMISTO_USERNAME", None
         )  # we use client without demisto username

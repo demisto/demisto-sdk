4069e1b1828c6d70a50cd556da81f7a8889549aa
rshunim <rshunim@paloaltonetworks.com>
2024-07-25 16:31:50 +0300

    classifier

--- demisto_sdk/commands/content_graph/strict_objects/classifier.py
+++ demisto_sdk/commands/content_graph/strict_objects/classifier.py
@@ -1,20 +1,40 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 
 from pydantic import Field
 
 from demisto_sdk.commands.common.constants import MarketplaceVersions
-from demisto_sdk.commands.content_graph.strict_objects.base_strict_model import (
-    StrictBaseClassifier,
-)
 from demisto_sdk.commands.content_graph.strict_objects.common import (
     DESCRIPTION_DYNAMIC_MODEL,
     ID_DYNAMIC_MODEL,
     NAME_DYNAMIC_MODEL,
+    BaseStrictModel,
     create_model,
 )
 
 
-class _StrictClassifier(StrictBaseClassifier):
+class _StrictClassifier(BaseStrictModel):
+    feed: Optional[bool] = None
+    incident_samples: Optional[List[str]] = Field(None, alias="incidentSamples")
+    indicator_samples: Optional[List[str]] = Field(None, alias="indicatorSamples")
+    propagation_labels: Optional[Any] = Field(None, alias="propagationLabels")
+    is_default: Optional[bool] = Field(None, alias="isDefault")
+    sort_values: Optional[Any] = Field(None, alias="sortValues")
+    id_: str = Field(..., alias="id")
+    version: int
+    from_version: Optional[str] = Field(None, alias="fromVersion")
+    to_version: Optional[str] = Field(None, alias="toVersion")
+    modified: Optional[str] = None
+    default_incident_type: Optional[str] = Field(None, alias="defaultIncidentType")
+
+    # TODO - the filed 'mapping' does not exist in the classifier.yml Schema, but appears as a field in the classifier
+    #  files in our Content repository
+    mapping: Optional[dict] = None
+
+    unclassified_cases: Optional[dict] = Field(None, alias="unclassifiedCases")
+    transformer: Optional[dict] = None
+    key_type_map: Optional[dict] = Field(None, alias="keyTypeMap")
+    custom: Optional[bool] = None
+
     name: str
     type_: str = Field(..., alias="type")
     description: str

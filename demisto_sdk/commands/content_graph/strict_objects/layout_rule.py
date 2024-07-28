a2108b367ed7b589c3d8fde22d2eeec917ada8d7
rshunim <rshunim@paloaltonetworks.com>
2024-07-28 12:42:16 +0300

    add Layout Rule

---
+++ demisto_sdk/commands/content_graph/strict_objects/layout_rule.py
@@ -0,0 +1,46 @@
+from typing import List, Optional, Union
+
+from demisto_sdk.commands.content_graph.strict_objects.common import (
+    DESCRIPTION_DYNAMIC_MODEL,
+    BaseStrictModel,
+    create_model,
+)
+
+
+class FilterSchema(BaseStrictModel):
+    SEARCH_FIELD: str
+    SEARCH_TYPE: str
+    SEARCH_VALUE: str
+
+
+class AndSchema(BaseStrictModel):
+    AND: Optional[List[Union["FilterSchema", "OrSchema", "AndSchema"]]] = None
+
+
+class OrSchema(BaseStrictModel):
+    OR: Optional[List[Union["FilterSchema", "OrSchema", "AndSchema"]]] = None
+
+
+# Forward references to resolve circular dependencies
+FilterSchema.update_forward_refs()
+AndSchema.update_forward_refs()
+OrSchema.update_forward_refs()
+
+
+class AlertsFilter(BaseStrictModel):
+    filter: Optional[Union[OrSchema, AndSchema]] = None
+
+
+class _StrictLayoutRule(BaseStrictModel):
+    rule_id: str
+    rule_name: str
+    layout_id: str
+    fromVersion: str
+    description: Optional[str] = None
+    alerts_filter: Optional[AlertsFilter] = None
+
+
+StrictLayoutRule = create_model(
+    model_name="StrictLayoutRule",
+    base_models=(_StrictLayoutRule, DESCRIPTION_DYNAMIC_MODEL),
+)

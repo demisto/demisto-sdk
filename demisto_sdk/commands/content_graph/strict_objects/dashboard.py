4f60584fd4f72d2a82ad1885796a0c72605006ce
rshunim <rshunim@paloaltonetworks.com>
2024-07-30 17:04:28 +0300

    convert widget to pydantic object

--- demisto_sdk/commands/content_graph/strict_objects/dashboard.py
+++ demisto_sdk/commands/content_graph/strict_objects/dashboard.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 
 from pydantic import Field
 
@@ -15,6 +15,44 @@ from demisto_sdk.commands.content_graph.strict_objects.common import (
     create_model,
 )
 
+class DashboardWidget(BaseStrictModel):
+    cache: Optional[str] = Field(None, alias="Cache")
+    cache_versn: Optional[int] = Field(None, alias="cacheVersn")
+    category: Optional[str] = None
+    created: Optional[str] = None
+    data_type: Optional[str] = Field(None, alias="dataType")
+    date_range: Optional[Dict[str, Any]] = Field(alias="dateRange", default_factory=dict)
+    definition_id: Optional[str] = Field(None, alias="definitionId")
+    from_server_version: Optional[str] = Field(None, alias="fromServerVersion")
+    id_: Optional[str] = Field(None, alias="id")
+    index_name: Optional[str] = Field(None, alias="indexName")
+    is_predefined: Optional[bool] = Field(None, alias="isPredefined")
+    item_version: Optional[str] = Field(None, alias="itemVersion")
+    modified: Optional[str] = None
+    name: Optional[str] = None
+    pack_id: Optional[str] = Field(None, alias="packID")
+    pack_name: Optional[str] = Field(None, alias="packName")
+    pack_propagation_labels: Optional[List[str]] = Field(None, alias="packPropagationLabels")
+    params: Optional[Dict[str, Any]] = Field(default_factory=dict)
+    prev_name: Optional[str] = Field(None, alias="prevName")
+    primary_term: Optional[int] = Field(None, alias="primaryTerm")
+    propagation_labels: Optional[List[str]] = Field(None, alias="propagationLabels")
+    query: Optional[str] = None
+    sequence_number: Optional[int] = Field(None, alias="sequenceNumber")
+    size_in_bytes: Optional[int] = Field(None, alias="sizeInBytes")
+    to_server_version: Optional[str] = Field(None, alias="toServerVersion")
+    vc_should_keep_item_legacy_prod_machine: Optional[str] = Field(None, alias="vcShouldKeepItemLegacyProdMachine")
+    version: Optional[int] = None
+    widget_type: Optional[str] = Field(None, alias="widgetType")
+    commit_message: Optional[str] = Field(None, alias="commitMessage")
+    should_commit: Optional[bool] = Field(None, alias="shouldCommit")
+    size: Optional[int] = None
+    sort: Optional[Any] = None
+    sort_values: Optional[Any] = Field(None, alias="sortValues")
+    vc_should_ignore: Optional[bool] = Field(None, alias="vcShouldIgnore")
+    description: Optional[str] = None
+    from_version: Optional[str] = Field(None, alias="fromVersion")
+    locked: Optional[bool] = None
 
 class _DashboardLayout(BaseStrictModel):
     force_range: bool = Field(alias="forceRange")
@@ -23,7 +61,7 @@ class _DashboardLayout(BaseStrictModel):
     h: int
     w: int
     i: str
-    widget: dict = Field(default_factory=dict)
+    widget: DashboardWidget = Field(default_factory=dict)
     reflect_dimensions: Optional[bool] = Field(None, alias="reflectDimensions")
 
 

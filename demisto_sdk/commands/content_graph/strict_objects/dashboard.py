7ef21f83a3f862049a826c8194335d2aa4ef2971
rshunim <rshunim@paloaltonetworks.com>
2024-07-30 15:06:23 +0300

    strict dashboard

---
+++ demisto_sdk/commands/content_graph/strict_objects/dashboard.py
@@ -0,0 +1,54 @@
+from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.content_graph.strict_objects.base_strict_model import BaseOptionalVersionJson
+from demisto_sdk.commands.content_graph.strict_objects.common import BaseStrictModel, create_model, ID_DYNAMIC_MODEL, \
+    DESCRIPTION_DYNAMIC_MODEL, NAME_DYNAMIC_MODEL, create_dynamic_model
+
+from typing import List, Optional
+from pydantic import Field
+
+
+class _DashboardLayout(BaseStrictModel):
+    force_range: bool = Field(alias="forceRange")
+    x: int
+    y: int
+    h: int
+    w: int
+    i: str
+    widget: dict = Field(default_factory=dict)
+    reflect_dimensions: Optional[bool] = Field(None, alias="reflectDimensions")
+
+
+DashboardLayout = create_model(model_name="DashboardLayout",
+                               base_models=(_DashboardLayout,
+                                            ID_DYNAMIC_MODEL,
+                                            )
+                               )
+
+
+class _StrictDashboard(BaseStrictModel):
+    id: str
+    version: int
+    name: str
+    description: str
+    from_date_license: Optional[str] = Field(None, alias="fromDateLicense")
+    is_predefined: bool = Field(alias="isPredefined")
+    from_date: Optional[str] = Field(None, alias="fromDate")
+    to_date: Optional[str] = Field(None, alias="toDate")
+    period: Optional[dict] = Field(default_factory=dict)
+    layout: Optional[List[DashboardLayout]] = Field(default_factory=list)
+    marketplaces: Optional[List[MarketplaceVersions]] = None
+
+
+ID_DASHBOARD_DYNAMIC_MODEL = create_dynamic_model(
+    # creating here with include_without_suffix == False
+    field_name="id",
+    type_=Optional[str],
+    default=None,
+)
+
+StrictDashboard = create_model(model_name="StrictDashboard",
+                               base_models=(_StrictDashboard,
+                                            BaseOptionalVersionJson,
+                                            DESCRIPTION_DYNAMIC_MODEL,
+                                            NAME_DYNAMIC_MODEL,
+                                            ID_DASHBOARD_DYNAMIC_MODEL))

af29351e4e083c3456ebe36c39284241d33535ad
yrosenberg <yrosenberg@paloaltonetworks.com>
2024-08-07 13:59:31 +0300

    verify

--- demisto_sdk/commands/content_graph/parsers/content_item.py
+++ demisto_sdk/commands/content_graph/parsers/content_item.py
@@ -311,7 +311,7 @@ class ContentItemParser(BaseContentParser, metaclass=ParserMetaclass):
         )
 
     def is_above_marketplace_min_version(self) -> bool:
-        return Version(self.toversion) >= Version("6.0.0")
+        return Version(self.toversion) >= Version(MARKETPLACE_MIN_VERSION)
 
     def add_relationship(
         self,

23368d8dd6bef0be95438888e5331ae3f8e581de
BEAdi <72088126+BEAdi@users.noreply.github.com>
2024-08-11 16:04:27 +0300

    remove backward support

--- demisto_sdk/commands/test_content/TestContentClasses.py
+++ demisto_sdk/commands/test_content/TestContentClasses.py
@@ -1216,16 +1216,9 @@ class CloudServerContext(ServerContext):
         super().__init__(build_context, server_private_ip, use_retries_mechanism)
         self.machine = cloud_machine
         self.server_url = self.server_ip
-        cloud_machine_details = self.build_context.api_key.get(cloud_machine, {})
-        self.api_key = (
-            cloud_machine_details.get("api-key")
-            if isinstance(cloud_machine_details, dict)
-            else self.build_context.api_key.get(cloud_machine)
-        )
-        self.auth_id = (
-            cloud_machine_details.get("x-xdr-auth-id")
-            if isinstance(cloud_machine_details, dict)
-            else self.build_context.env_json.get(cloud_machine, {}).get("x-xdr-auth-id")
+        self.api_key = self.build_context.api_key.get(cloud_machine, {}).get("api-key")
+        self.auth_id = self.build_context.api_key.get(cloud_machine, {}).get(
+            "x-xdr-auth-id"
         )
         os.environ.pop(
             "DEMISTO_USERNAME", None

8eaeefd723737286e003e24122db78fd4519e84d
rshunim <rshunim@paloaltonetworks.com>
2024-08-18 10:51:56 +0300

    pre commit

--- demisto_sdk/commands/content_graph/parsers/repository.py
+++ demisto_sdk/commands/content_graph/parsers/repository.py
@@ -9,7 +9,8 @@ from demisto_sdk.commands.common.constants import PACKS_FOLDER
 from demisto_sdk.commands.common.cpu_count import cpu_count
 from demisto_sdk.commands.common.logger import logger
 from demisto_sdk.commands.content_graph.parsers.content_item import (
-    NotAContentItemException, StrictObjectNotExistException,
+    NotAContentItemException,
+    StrictObjectNotExistException,
 )
 from demisto_sdk.commands.content_graph.parsers.pack import PackParser
 

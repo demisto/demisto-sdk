014295f545f837beb90b4e6452ed378c876007de
sberman <sberman@paloaltonetworks.com>
2024-08-18 17:21:12 +0300

    Fixed PA102 for modeling_rules

--- demisto_sdk/commands/validate/tests/PA_validators_test.py
+++ demisto_sdk/commands/validate/tests/PA_validators_test.py
@@ -19,6 +19,7 @@ from demisto_sdk.commands.common.constants import (
 from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFile
 from demisto_sdk.commands.validate.tests.test_tools import (
     create_integration_object,
+    create_modeling_rule_object,
     create_old_file_pointers,
     create_pack_object,
     create_playbook_object,
@@ -1521,7 +1522,8 @@ def test_IsValidUseCasesValidator_fix():
 
 
 @pytest.mark.parametrize(
-    "pack, is_deprecated_pack, integrations, playbooks, scripts, expected_number_of_failures, expected_msgs",
+    "pack, is_deprecated_pack, integrations, playbooks, scripts, modeling_rules, expected_number_of_failures, "
+    "expected_msgs",
     [
         (create_pack_object(), False, [], [], [], 0, []),
         (
@@ -1553,6 +1555,15 @@ def test_IsValidUseCasesValidator_fix():
             0,
             [],
         ),
+        (
+            create_pack_object(),
+            False,
+            [],
+            [create_integration_object(["deprecated"], [True])],
+            [create_modeling_rule_object()],
+            0,
+            [],
+        ),
     ],
 )
 def test_ShouldPackBeDeprecatedValidator_obtain_invalid_content_items(
@@ -1561,6 +1572,7 @@ def test_ShouldPackBeDeprecatedValidator_obtain_invalid_content_items(
     integrations,
     playbooks,
     scripts,
+    modeling_rules,
     expected_number_of_failures,
     expected_msgs,
 ):
@@ -1571,6 +1583,7 @@ def test_ShouldPackBeDeprecatedValidator_obtain_invalid_content_items(
         - Case 2: A non deprecated pack with a deprecated integration.
         - Case 3: A non deprecated pack with a deprecated integration and a non deprecated script
         - Case 4: A deprecated pack with a deprecated integration.
+        - Case 3: A non deprecated pack with a deprecated integration and a non deprecated modeling_rule
     When
     - Calling the ShouldPackBeDeprecatedValidator obtain_invalid_content_items function.
     Then
@@ -1579,11 +1592,13 @@ def test_ShouldPackBeDeprecatedValidator_obtain_invalid_content_items(
         - Case 2: Should fail.
         - Case 3: Shouldn't fail.
         - Case 4: Shouldn't fail.
+        - Case 5: Shouldn't fail.
     """
     pack.deprecated = is_deprecated_pack
     pack.content_items.integration.extend(integrations)
     pack.content_items.playbook.extend(playbooks)
     pack.content_items.script.extend(scripts)
+    pack.content_items.modeling_rule.extend(modeling_rules)
     content_items = [pack]
     results = ShouldPackBeDeprecatedValidator().obtain_invalid_content_items(
         content_items

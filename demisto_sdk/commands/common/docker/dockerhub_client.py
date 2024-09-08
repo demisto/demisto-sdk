018ae53222b9e03a8e643c5b3279b36d20ccfbfa
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-09-08 17:08:53 +0300

    add log

--- demisto_sdk/commands/common/docker/dockerhub_client.py
+++ demisto_sdk/commands/common/docker/dockerhub_client.py
@@ -51,6 +51,7 @@ class DockerHubClient:
         verify_ssl: bool = False,
     ):
         self.registry_api_url = registry or self.DEFAULT_REGISTRY
+        logger.info("################################################# self.registry_api_url: {self.registry_api_url}")
         self.docker_hub_api_url = docker_hub_api_url or self.DOCKER_HUB_API_BASE_URL
         self.username = username or os.getenv(DOCKERHUB_USER, "")
         self.password = password or os.getenv(DOCKERHUB_PASSWORD, "")

39b5ff7202384acc2b4ce49c5d4d222bd1968249
yrosenberg <yrosenberg@paloaltonetworks.com>
2024-09-15 01:57:42 +0300

    cr

--- demisto_sdk/commands/validate/validators/GR_validators/GR103_is_using_unknown_content.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR103_is_using_unknown_content.py
@@ -86,7 +86,7 @@ class IsUsingUnknownContentValidator(BaseValidator[ContentTypes], ABC):
     is_auto_fixable = False
 
     def obtain_invalid_content_items_using_graph(
-        self, content_items: Iterable[ContentTypes], validate_all_files: bool
+        self, content_items: Iterable[ContentTypes]| None, validate_all_files: bool
     ) -> List[ValidationResult]:
         results: List[ValidationResult] = []
         file_paths_to_validate = (

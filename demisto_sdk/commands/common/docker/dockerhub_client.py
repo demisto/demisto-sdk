1099f4b562d37563077ac38ab776a497ecece870
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-09-17 00:20:28 +0300

    Import and init a DockerBase attr in docker_image.py

--- demisto_sdk/commands/common/docker/dockerhub_client.py
+++ demisto_sdk/commands/common/docker/dockerhub_client.py
@@ -8,8 +8,7 @@ import requests
 from packaging.version import InvalidVersion, Version
 from requests.exceptions import ConnectionError, RequestException, Timeout
 
-from demisto_sdk.commands.common.handlers.xsoar_handler import
-from demisto_sdk.commands.common.docker_helper import DockerBase
+from demisto_sdk.commands.common.handlers.xsoar_handler import JSONDecodeError
 from demisto_sdk.commands.common.logger import logger
 from demisto_sdk.commands.common.StrEnum import StrEnum
 from demisto_sdk.commands.common.tools import retry

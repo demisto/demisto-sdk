0cd475716b7628586da872f20c871d26c0a5818e
rshunim <rshunim@paloaltonetworks.com>
2024-09-17 12:56:31 +0300

    change it to models

--- demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule.py
+++ demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule.py
@@ -1,12 +1,13 @@
+from demisto_sdk.commands.content_graph.strict_objects.common import create_model
 from demisto_sdk.commands.content_graph.strict_objects.modeling_rule import (
     StrictModelingRule,
 )
 
+# This model/class represents the StrictAssetsModelingRule.
+# The AssetsModelingRule and ModelingRule objects have the same schema, but they are separate for future changes.
 
-class StrictAssetsModelingRule(StrictModelingRule):  # type:ignore[misc,valid-type]
-    """
-    This class represents the StrictAssetsModelingRule.
-    The AssetsModelingRule and ModelingRule objects have the same schema, but they are separate for future changes.
-    """
 
-    pass
+StrictAssetsModelingRule = create_model(
+    model_name="StrictAssetsModelingRule",
+    base_models=(StrictModelingRule,),
+)
--- demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule_schema.py
+++ demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule_schema.py
@@ -1,13 +1,14 @@
+from demisto_sdk.commands.content_graph.strict_objects.common import create_model
 from demisto_sdk.commands.content_graph.strict_objects.modeling_rule_schema import (
     StrictModelingRuleSchema,
 )
 
+# This model/class represents the StrictAssetsModelingRuleSchema.
+# The AssetsModelingRuleSchema and ModelingRuleSchema objects have the same schema,
+# but they are separate for future changes.
 
-class StrictAssetsModelingRuleSchema(StrictModelingRuleSchema):
-    """
-    This class represents the StrictAssetsModelingRuleSchema.
-    The AssetsModelingRuleSchema and ModelingRuleSchema objects have the same schema,
-    but they are separate for future changes.
-    """
 
-    pass
+StrictAssetsModelingRuleSchema = create_model(
+    model_name="StrictAssetsModelingRule",
+    base_models=(StrictModelingRuleSchema,),
+)

6fbadbd6b88a69a7ea492f631cd98e82dbb69e1b
rshunim <rshunim@paloaltonetworks.com>
2024-09-18 11:22:25 +0300

    reverting to class instead of model

--- demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule.py
+++ demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule.py
@@ -1,14 +1,12 @@
-from demisto_sdk.commands.content_graph.strict_objects.common import create_model
 from demisto_sdk.commands.content_graph.strict_objects.modeling_rule import (
     StrictModelingRule,
 )
 
-"""
-The `AssetsModelingRule` and `ModelingRule` objects use the same schema, use use separate classes to make future diversions easier.
-"""
 
+class StrictAssetsModelingRule(StrictModelingRule):  # type: ignore[misc, valid-type]
+    """
+    The `AssetsModelingRule` and `ModelingRule` objects use the same schema, use separate classes to make
+    future diversions easier.
+    """
 
-StrictAssetsModelingRule = create_model(
-    model_name="StrictAssetsModelingRule",
-    base_models=(StrictModelingRule,),
-)
+    pass
--- demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule_schema.py
+++ demisto_sdk/commands/content_graph/strict_objects/asset_modeling_rule_schema.py
@@ -1,13 +1,11 @@
-from demisto_sdk.commands.content_graph.strict_objects.common import create_model
 from demisto_sdk.commands.content_graph.strict_objects.modeling_rule_schema import (
     StrictModelingRuleSchema,
 )
 
-"""
-The `AssetsModelingRule` and `ModelingRule` objects use the same schema, use use separate classes to make future diversions easier.
-"""
 
-StrictAssetsModelingRuleSchema = create_model(
-    model_name="StrictAssetsModelingRule",
-    base_models=(StrictModelingRuleSchema,),
-)
+class StrictAssetsModelingRuleSchema(StrictModelingRuleSchema):
+    """
+    The `AssetsModelingRule` and `ModelingRule` objects use the same schema, use use separate classes to make future diversions easier.
+    """
+
+    pass

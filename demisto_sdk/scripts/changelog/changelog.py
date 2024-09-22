2a21a8aca41771c0b17a67942a12a2dede2f8197
Shmuel Kroizer <69422117+shmuel44@users.noreply.github.com>
2024-09-22 10:45:25 +0300

    [changelog] Sort By Category (#4555)
    * init
    
    * revert
    
    * revert
    
    * revert
    
    * revert
    
    * improve
    
    * changelog

--- demisto_sdk/scripts/changelog/changelog.py
+++ demisto_sdk/scripts/changelog/changelog.py
@@ -285,8 +285,10 @@ def compile_changelog_md(
     # Collecting the new log entries in the following order:
     # breaking, feature, fix, internal
     for log_type in (LogType.breaking, LogType.feature, LogType.fix, LogType.internal):
-        new_changelog.extend(log.to_string() for log in new_logs.get(log_type, ()))
-    # A new line separates versions
+        if logs := new_logs.get(log_type, []):
+            new_changelog.append(f"### {log_type.capitalize()}")
+            new_changelog.extend(log.to_string() for log in logs)
+            new_changelog.append("")  # Add an empty line after each category
     new_changelog.append("")
     # Collecting the old changelog
     new_changelog.extend(old_changelog)

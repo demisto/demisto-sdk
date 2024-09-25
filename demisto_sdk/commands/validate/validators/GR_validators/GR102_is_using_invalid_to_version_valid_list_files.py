fb06ac6b0229ea96da459c62147f3c5456a896c7
rshunim <rshunim@paloaltonetworks.com>
2024-09-25 18:46:44 +0300

    pre-commit

--- demisto_sdk/commands/validate/validators/GR_validators/GR102_is_using_invalid_to_version_valid_list_files.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR102_is_using_invalid_to_version_valid_list_files.py
@@ -36,7 +36,10 @@ from demisto_sdk.commands.content_graph.objects import (
     XSIAMDashboard,
     XSIAMReport,
 )
-from demisto_sdk.commands.validate.validators.base_validator import ValidationResult
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
 from demisto_sdk.commands.validate.validators.GR_validators.GR102_is_using_invalid_to_version_valid import (
     IsUsingInvalidToVersionValidator,
 )
@@ -77,10 +80,12 @@ ContentTypes = Union[
 ]
 
 
-class IsUsingInvalidToVersionValidatorListFiles(IsUsingInvalidToVersionValidator):
+class IsUsingInvalidToVersionValidatorListFiles(
+    IsUsingInvalidToVersionValidator, BaseValidator[ContentTypes]
+):
     expected_execution_mode = [ExecutionMode.SPECIFIC_FILES, ExecutionMode.USE_GIT]
 
     def obtain_invalid_content_items(
         self, content_items: Iterable[ContentTypes]
     ) -> List[ValidationResult]:
-        return self.obtain_invalid_content_items_using_graph(content_items, False)
+        return self.obtain_invalid_content_items_using_graph(content_items)

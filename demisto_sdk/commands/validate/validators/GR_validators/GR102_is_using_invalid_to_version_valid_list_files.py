60316eafc5d08e2b8e703c81122aad9c70fb6bbb
rshunim <rshunim@paloaltonetworks.com>
2024-09-25 18:10:46 +0300

    GR102 validation

---
+++ demisto_sdk/commands/validate/validators/GR_validators/GR102_is_using_invalid_to_version_valid_list_files.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import Iterable, List, Union
+
+from demisto_sdk.commands.common.constants import ExecutionMode
+from demisto_sdk.commands.content_graph.objects import (
+    CaseField,
+    CaseLayout,
+    CaseLayoutRule,
+    Classifier,
+    CorrelationRule,
+    Dashboard,
+    GenericDefinition,
+    GenericField,
+    GenericModule,
+    GenericType,
+    IncidentField,
+    IncidentType,
+    IndicatorField,
+    IndicatorType,
+    Integration,
+    Job,
+    Layout,
+    LayoutRule,
+    Mapper,
+    ModelingRule,
+    Pack,
+    ParsingRule,
+    Playbook,
+    Report,
+    Script,
+    TestPlaybook,
+    Trigger,
+    Widget,
+    Wizard,
+    XSIAMDashboard,
+    XSIAMReport,
+)
+from demisto_sdk.commands.validate.validators.base_validator import ValidationResult
+from demisto_sdk.commands.validate.validators.GR_validators.GR102_is_using_invalid_to_version_valid import (
+    IsUsingInvalidToVersionValidator,
+)
+
+ContentTypes = Union[
+    Integration,
+    Script,
+    Playbook,
+    Pack,
+    Dashboard,
+    Classifier,
+    Job,
+    Layout,
+    Mapper,
+    Wizard,
+    CorrelationRule,
+    IncidentField,
+    IncidentType,
+    IndicatorField,
+    IndicatorType,
+    LayoutRule,
+    Layout,
+    ModelingRule,
+    ParsingRule,
+    Report,
+    TestPlaybook,
+    Trigger,
+    Widget,
+    GenericDefinition,
+    GenericField,
+    GenericModule,
+    GenericType,
+    XSIAMDashboard,
+    XSIAMReport,
+    CaseField,
+    CaseLayout,
+    CaseLayoutRule,
+]
+
+
+class IsUsingInvalidToVersionValidatorListFiles(IsUsingInvalidToVersionValidator):
+    expected_execution_mode = [ExecutionMode.SPECIFIC_FILES, ExecutionMode.USE_GIT]
+
+    def obtain_invalid_content_items(
+        self, content_items: Iterable[ContentTypes]
+    ) -> List[ValidationResult]:
+        return self.obtain_invalid_content_items_using_graph(content_items, False)

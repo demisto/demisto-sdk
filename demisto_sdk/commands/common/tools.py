7a212d26c6be217f63a8e14f51ebd6aee5ea2b93
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-10-27 12:01:39 +0200

    logging

--- demisto_sdk/commands/common/tools.py
+++ demisto_sdk/commands/common/tools.py
@@ -2124,6 +2124,7 @@ def get_content_path(relative_path: Optional[Path] = None) -> Path:
     Returns:
         str: Absolute content path
     """
+    logger.debug(f'get_content_path')
     # ValueError can be suppressed since as default, the environment variable or git.Repo can be used to find the content path.
     with contextlib.suppress(ValueError):
         if relative_path:

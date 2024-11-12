ce79e2f7393f8a2bb8869e13c20331424f19b152
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-11-12 15:28:55 +0200

    Initial Commit

--- demisto_sdk/commands/common/logger.py
+++ demisto_sdk/commands/common/logger.py
@@ -40,7 +40,7 @@ logger.disable(None)  # enabled at setup_logging()
 
 class PropagateHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
-        
+        record.msg = COLOR_TAG_PATTERN.sub("", record.msg)
         logging.getLogger(record.name).handle(record)
 
 

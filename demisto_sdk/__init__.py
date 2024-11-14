3fa8e86eae2bbf627d9882bbf365e4ae7142ed6a
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-11-14 10:49:24 +0200

    Introducing 'DEMISTO_SDK_SKIP_LOGGER_SETUP' envvar.

--- demisto_sdk/__init__.py
+++ demisto_sdk/__init__.py
@@ -1,4 +1,6 @@
-if __name__ in ["__main__", "demisto_sdk"]:
+import os
+
+if not os.environ.get("DEMISTO_SDK_SKIP_LOGGER_SETUP", False):
     from demisto_sdk.commands.common.logger import logging_setup
 
     logging_setup(initial=True, calling_function="__init__")

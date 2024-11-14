157040f254c8b031c789f15e7327a4cf8600104b
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-11-14 12:22:51 +0200

    ruff

--- demisto_sdk/__init__.py
+++ demisto_sdk/__init__.py
@@ -1,6 +1,10 @@
 import os
 
-if not os.environ.get("DEMISTO_SDK_SKIP_LOGGER_SETUP").lower() in ['true', 'yes', '1']:
+if os.environ.get("DEMISTO_SDK_SKIP_LOGGER_SETUP", "False").lower() not in [
+    "true",
+    "yes",
+    "1",
+]:
     from demisto_sdk.commands.common.logger import logging_setup
 
     logging_setup(initial=True, calling_function="__init__")

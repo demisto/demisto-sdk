e51c26788f424eca630c2a7e81214c1924002691
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-11-25 14:30:28 +0200

    introduce content paths update functionallity

--- demisto_sdk/commands/common/content_constant_paths.py
+++ demisto_sdk/commands/common/content_constant_paths.py
@@ -43,3 +43,61 @@ else:
     )
 
 PYTHONPATH_STR = ":".join(str(path) for path in PYTHONPATH)
+
+
+def update_content_paths(content_path: str | Path):
+    """
+    Update content paths globally and reload dependent modules.
+    Args:
+        content_path:
+
+    Returns:
+
+    """
+    global CONTENT_PATH
+    global ALL_PACKS_DEPENDENCIES_DEFAULT_PATH
+    global CONF_PATH
+    global DEFAULT_ID_SET_PATH
+    global MP_V2_ID_SET_PATH
+    global XPANSE_ID_SET_PATH
+    global LANDING_PAGE_SECTIONS_PATH
+    global NATIVE_IMAGE_PATH
+    global COMMON_SERVER_PYTHON_PATH
+    global DEMISTO_MOCK_PATH
+    global API_MODULES_SCRIPTS_DIR
+    global PYTHONPATH
+    global PYTHONPATH_STR
+
+    CONTENT_PATH: Path = Path(content_path)
+    ALL_PACKS_DEPENDENCIES_DEFAULT_PATH = CONTENT_PATH / "all_packs_dependencies.json"
+    CONF_PATH = CONTENT_PATH / TESTS_DIR / "conf.json"
+    DEFAULT_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set.json"
+    MP_V2_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set_mp_v2.json"
+    XPANSE_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set_xpanse.json"
+    LANDING_PAGE_SECTIONS_PATH = (
+        CONTENT_PATH / TESTS_DIR / "Marketplace" / "landingPage_sections.json"
+    )
+    NATIVE_IMAGE_PATH = CONTENT_PATH / "Tests" / NATIVE_IMAGE_FILE_NAME
+    COMMON_SERVER_PYTHON_PATH = (
+        CONTENT_PATH / "Packs" / "Base" / "Scripts" / "CommonServerPython"
+    )
+    DEMISTO_MOCK_PATH = CONTENT_PATH / TESTS_DIR / "demistomock"
+    API_MODULES_SCRIPTS_DIR = CONTENT_PATH / "Packs" / "ApiModules" / "Scripts"
+    PYTHONPATH = [
+        path.absolute()
+        for path in [
+            Path(CONTENT_PATH),
+            COMMON_SERVER_PYTHON_PATH,
+            DEMISTO_MOCK_PATH,
+            Path(__file__).parent.parent / "lint" / "resources" / "pylint_plugins",
+        ]
+    ]
+    if API_MODULES_SCRIPTS_DIR.exists():
+        PYTHONPATH.extend(path.absolute() for path in API_MODULES_SCRIPTS_DIR.iterdir())
+
+    else:
+        logger.debug(
+            "Could not add API modules to 'PYTHONPATH' as the base directory does not exist."
+        )
+
+    PYTHONPATH_STR = ":".join(str(path) for path in PYTHONPATH)

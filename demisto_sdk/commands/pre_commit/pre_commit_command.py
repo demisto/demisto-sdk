b760ec7465aeb03d508b545ab122dfc20dd61c00
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-12-01 17:39:13 +0200

    group_by_language

--- demisto_sdk/commands/pre_commit/pre_commit_command.py
+++ demisto_sdk/commands/pre_commit/pre_commit_command.py
@@ -447,17 +447,29 @@ def group_by_language(
 
     logger.debug(f'group_by_language {str(iterable)}')
     for integration_script_paths in iterable:
-        with multiprocessing.Pool(processes=cpu_count()) as pool:
+        logger.debug(f'group_by_language {integration_script_paths=}')
 
-            # content_items = pool.map(print, [8])
-            logger.debug(f'group_by_language {integration_script_paths=}')
-            logger.debug(f'group_by_language {content_items=}')
-            content_items = pool.map(BaseContent.from_path, integration_script_paths)
-            # for content_item in content_items:
-            #     if not content_item or not isinstance(content_item, IntegrationScript):
-            #         continue
-                # content-item is a script/integration
-                # integrations_scripts.add(content_item)
+        # Process each path sequentially
+        content_items = [BaseContent.from_path(path) for path in integration_script_paths]
+        logger.debug(f'group_by_language {content_items=}')
+
+        for content_item in content_items:
+            if not content_item or not isinstance(content_item, IntegrationScript):
+                continue
+            # content-item is a script/integration
+            integrations_scripts.add(content_item)
+    # for integration_script_paths in iterable:
+    #     with multiprocessing.Pool(processes=cpu_count()) as pool:
+    #
+    #         # content_items = pool.map(print, [8])
+    #         logger.debug(f'group_by_language {integration_script_paths=}')
+    #         logger.debug(f'group_by_language {content_items=}')
+    #         content_items = pool.map(BaseContent.from_path, integration_script_paths)
+    #         for content_item in content_items:
+    #             if not content_item or not isinstance(content_item, IntegrationScript):
+    #                 continue
+    #             # content-item is a script/integration
+    #             integrations_scripts.add(content_item)
     logger.debug("Pre-Commit: Finished parsing all integrations and scripts")
     exclude_integration_script = set()
     # for integration_script in integrations_scripts:

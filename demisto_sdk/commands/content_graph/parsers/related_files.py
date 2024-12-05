f97f2df7ccbf5a11abca6c7276fc23c6196e73c2
Dan Tavori <dtavori@paloaltonetworks.com>
2024-12-05 14:39:11 +0200

    fix rn108

--- demisto_sdk/commands/content_graph/parsers/related_files.py
+++ demisto_sdk/commands/content_graph/parsers/related_files.py
@@ -141,21 +141,19 @@ class RNRelatedFile(TextFiles):
     @property
     def all_rns(self) -> List[str]:
         if not self.rns_list:
+            rns_dir_path = self.main_file_path / RELEASE_NOTES_DIR
             if self.git_sha:
                 git_util = GitUtil.from_content_path()
-                if all(
-                    RELEASE_NOTES_DIR not in f
-                    for f in git_util.list_files_in_dir(
-                        self.main_file_path, self.git_sha
-                    )
+                if not git_util.is_file_exist_in_commit_or_branch(
+                    rns_dir_path, self.git_sha
                 ):
                     self.rns_list = []
                 else:
                     self.rns_list = git_util.list_files_in_dir(
-                        self.main_file_path / RELEASE_NOTES_DIR, self.git_sha
+                        rns_dir_path, self.git_sha
                     )
             else:
-                self.rns_list = get_child_files(self.main_file_path / RELEASE_NOTES_DIR)
+                self.rns_list = get_child_files(rns_dir_path)
         return self.rns_list
 
 

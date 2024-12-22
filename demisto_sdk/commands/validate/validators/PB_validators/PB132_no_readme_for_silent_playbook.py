ad0dd428045536889d7d33fcc5f454a1c08b4cb7
Moshe <mgalitzki@paloaltonetworks.com>
2024-12-22 08:52:20 +0200

    added silent readme check

--- demisto_sdk/commands/validate/validators/PB_validators/PB132_no_readme_for_silent_playbook.py
+++ demisto_sdk/commands/validate/validators/PB_validators/PB132_no_readme_for_silent_playbook.py
@@ -10,11 +10,11 @@ from demisto_sdk.commands.validate.validators.base_validator import (
     ValidationResult,
 )
 
-ContentTypes = Union[Playbook]
+ContentTypes = Playbook
 
 
 class NoReadmeForSilentPlaybook(BaseValidator[ContentTypes]):
-    error_code = "PB131"
+    error_code = "PB132"
     description = "A silent playbook is not allowed to have a README file."
     rationale = "To ensure that nothing about the playbook appears in the documentation.."
     error_message = (
@@ -26,7 +26,6 @@ class NoReadmeForSilentPlaybook(BaseValidator[ContentTypes]):
     def obtain_invalid_content_items(
         self, content_items: Iterable[ContentTypes]
     ) -> List[ValidationResult]:
-
         return [
             ValidationResult(
                 validator=self,
@@ -34,31 +33,5 @@ class NoReadmeForSilentPlaybook(BaseValidator[ContentTypes]):
                 content_object=content_item,
             )
             for content_item in content_items
-            if content_item.is_silent
+            if content_item.is_silent and content_item.readme.exist
         ]
-
-
-def is_valid_relationship(content_item: ContentTypes) -> bool:
-    """
-    Validates the relationship between a content item and its associated playbook/trigger.
-    """
-    if not content_item.is_silent:
-        # If the content item is not marked as silent, it's automatically valid
-        return True
-
-    if content_item.content_type == ContentType.PLAYBOOK:
-        return any(
-            trigger.data.get("playbook_id") == content_item.data.get("id")
-            and trigger.is_silent
-            for trigger in content_item.pack.content_items.trigger
-        )
-
-    if content_item.content_type == ContentType.TRIGGER:
-        return any(
-            playbook.data.get("id") == content_item.data.get("playbook_id")
-            and playbook.is_silent
-            for playbook in content_item.pack.content_items.playbook
-        )
-
-    # Default case if content type is not PLAYBOOK or TRIGGER
-    return True

394cbb336bfa501270665fd63a105a8c3a15e7dc
inbalapt1 <164751454+inbalapt1@users.noreply.github.com>
2025-01-05 13:50:24 +0200

    update-release-notes --text special chars (#4751)
    * update-release-notes --text special chars
    
    * update changelog
    
    * fix pre commit
    
    * fix pre commit
    
    * fix + unittests

--- demisto_sdk/commands/update_release_notes/update_rn.py
+++ demisto_sdk/commands/update_release_notes/update_rn.py
@@ -746,7 +746,7 @@ class UpdateRN:
         :return
         The release notes description
         """
-
+        text = text.encode("utf-8").decode("unicode_escape")
         if self.is_force:
             rn_desc = f"## {content_name}\n\n"
             rn_desc += f'- {text or "%%UPDATE_RN%%"}\n'
@@ -792,7 +792,6 @@ class UpdateRN:
                         rn_desc += deprecate_rn
                     else:
                         rn_desc += f'- {text or "%%UPDATE_RN%%"}\n'
-
         if docker_image:
             rn_desc += f"- Updated the Docker image to: *{docker_image}*.\n"
         return rn_desc

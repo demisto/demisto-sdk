980c87e96e763370373cf0bb7a6b47b8413efdcc
merit-maita <meretmaayta@gmail.com>
2025-01-15 15:01:08 +0200

    added validation

--- demisto_sdk/commands/validate/validators/BC_validators/BC115_is_valid_toversion.py
+++ demisto_sdk/commands/validate/validators/BC_validators/BC115_is_valid_toversion.py
@@ -50,18 +50,7 @@ class IsValidToversionOnModifiedValidator(BaseValidator[ContentTypes]):
         if modified_item.toversion != old_file.toversion:  # file toversion was updated
             if modified_item.content_type.value in ['ModelingRule', 'ParsingRule']:
                 return self.is_valid_item(modified_item.pack_name, modified_item, new_items)
-                # # Found a new content item that's replacing it
-                # if modified_item.pack_name in new_items and modified_item.content_type.value == new_items.get('type'):
-                #     new_item = new_items[modified_item.pack_name]
-                #     is_valid_results = is_valid_versions(Version(modified_item.fromversion),
-                #                                          Version(modified_item.toversion),
-                #                                          Version(new_item.get('from')))
-                #     if not all(is_valid_results):
-                #         return (f"Invalid Change in the {modified_item.content_type.value} versions please validate the"
-                #                 f" following points:\n{self.valid_cases.format(modified_item.content_type.value)}")
-                # else:
-                #     return ("Changing the maximal supported version field `toversion` is not allowed without adding"
-                #             " a new content item to replace it.")
+
             elif modified_item.content_type.value in ['CorrelationRule']:
                 return self.is_valid_item(modified_item.object_id, modified_item, new_items)
 
@@ -69,14 +58,24 @@ class IsValidToversionOnModifiedValidator(BaseValidator[ContentTypes]):
         return
 
     def is_valid_item(self, item_key, modified_item: ContentTypes, new_items: dict):
+        """
+        Check if modified item `toversion` field was updated and if it's a valid update.
 
+        Args:
+            item_key: the modified item identifier key (the field to indicate if a new version of it was added).
+            modified_item: the modified item.
+            new_items: dict of all newly added items.
+
+        Returns:
+           an error message if the modification is not valid, or none in  case it is.
+        """
         # Found a new content item that's replacing it
         if item_key in new_items and modified_item.content_type.value == new_items[item_key].get('type'):
             new_item = new_items[item_key]
-            is_valid_results = is_valid_versions(Version(modified_item.fromversion),
+            is_valid = is_valid_versions(Version(modified_item.fromversion),
                                                  Version(modified_item.toversion),
                                                  Version(new_item.get('from')))
-            if not all(is_valid_results):
+            if not is_valid:
                 return (f"Invalid Change in the {modified_item.content_type.value} versions please validate the"
                         f" following points:\n{self.valid_cases.format(modified_item.content_type.value)}")
         else:
@@ -85,6 +84,16 @@ class IsValidToversionOnModifiedValidator(BaseValidator[ContentTypes]):
 
 
 def sort_content_items(content_items: Iterable[ContentTypes]):
+    """
+    Sort Content Items into two lists of Modified content items and newly added content items.
+
+    Args:
+        content_items (Iterable[ContentTypes]): The content items list
+
+    Returns:
+        modified_items: modified items.
+        new_items: newly added items.
+    """
     modified_items: list = []
     new_items: dict = {}
     for item in content_items:
@@ -99,16 +108,28 @@ def sort_content_items(content_items: Iterable[ContentTypes]):
     return modified_items, new_items
 
 
-def is_valid_versions(old_from: Version, old_to: Version, new_from: Version) -> list[bool]:
+def is_valid_versions(old_from: Version, old_to: Version, new_from: Version) -> bool:
+    """
+    Check if two content items which one is replacing the other have valid from/to version.
+
+    Args:
+        old_from: the modified file `fromversion` field.
+        old_to: the modified file `toversion` field.
+        new_from: the newly added file `fromversion` field.
+
+    Returns:
+        whether the versions are valid or not.
+    """
     res = [
         (old_to.major + 1 == new_from.major and new_from.minor == 0 and new_from.micro == 0),
         (old_to.major == new_from.major and old_to.minor + 1 == new_from.minor and new_from.micro == 0),
         (old_to.major == new_from.major and old_from.minor == new_from.minor and old_to.micro + 1 == new_from.micro)
     ]
     is_continuous = any(res)
-    results = [
-        old_from < new_from,
-        old_to < new_from,
-        is_continuous,
-    ]
-    return results
+    return all(
+        [
+            old_from < new_from,
+            old_to < new_from,
+            is_continuous,
+        ]
+    )

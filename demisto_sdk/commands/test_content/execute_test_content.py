5b6c859ddae17dddd4e361e90d46a54a6aad5a6a
meichler <meichler@paloaltonetworks.com>
2025-02-09 13:22:19 +0200

    update version message

--- demisto_sdk/commands/test_content/execute_test_content.py
+++ demisto_sdk/commands/test_content/execute_test_content.py
@@ -80,7 +80,7 @@ def execute_test_content(**kwargs):
     logging_manager = ParallelLoggingManager(
         "Run_Tests.log", real_time_logs_only=not kwargs["nightly"]
     )
-    logging_manager.info("custom branch for testing number 1")
+    logging_manager.info("custom branch for testing number 2")
     logging_manager.info(
         f'Starting to run tests on Server version:{kwargs["server_version"]} Server Type:{kwargs["server_type"]}'
     )

c4da2aee6fa1d9e67df38257447d00226d97d82c
barryyosi-panw <158817412+barryyosi-panw@users.noreply.github.com>
2025-02-13 16:09:19 +0200

    Update demisto_sdk/commands/content_graph/parsers/pre_process_rule.py

--- demisto_sdk/commands/content_graph/parsers/pre_process_rule.py
+++ demisto_sdk/commands/content_graph/parsers/pre_process_rule.py
@@ -19,7 +19,6 @@ class PreProcessRuleParser(
             MarketplaceVersions.XSOAR,
             MarketplaceVersions.XSOAR_SAAS,
             MarketplaceVersions.XSOAR_ON_PREM,
-            MarketplaceVersions.PLATFORM,
         }
 
     @property

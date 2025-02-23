d94bef7a38bb85a676c0c3c950d053cecc20e550
YuvHayun <yhayun@paloaltonetworks.com>
2025-02-23 16:57:48 +0200

    fix tests

--- demisto_sdk/scripts/tests/validate_validation_config_file_test.py
+++ demisto_sdk/scripts/tests/validate_validation_config_file_test.py
@@ -1,8 +1,12 @@
 from unittest.mock import patch
 
 import pytest
+import toml
 
-from demisto_sdk.commands.validate.config_reader import ConfiguredValidations
+from demisto_sdk.commands.validate.config_reader import (
+    ConfigReader,
+    ConfiguredValidations,
+)
 from demisto_sdk.commands.validate.validators.BA_validators.BA100_is_valid_version import (
     IsValidVersionValidator,
 )
@@ -24,49 +28,83 @@ from demisto_sdk.scripts.validate_validation_config_file import (
 
 
 @pytest.mark.parametrize(
-    "configured_validations, expected_exit_code, expected_error_codes_per_msg",
+    "config_file_content, expected_exit_code, expected_error_codes_per_msg",
     [
         (
-            ConfiguredValidations(
-                selected_path_based_section=["PA100", "PA101"],
-                selected_use_git_section=["GR100", "GR101"],
-                warning_path_based_section=["WA100", "WA101"],
-                warning_use_git_section=["GR102", "GR103"],
-                ignorable_errors=["GR100", "GR102"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["WA100", "WA101"],
+                },
+                "use_git": {
+                    "select": ["GR100", "GR101"],
+                    "warning": ["GR102", "GR103"],
+                },
+                "ignorable_errors": ["GR100", "GR102"],
+            },
             1,
             ["WA100", "WA101"],
         ),
         (
-            ConfiguredValidations(
-                selected_path_based_section=["PA100", "PA101"],
-                selected_use_git_section=["GR100", "GR101"],
-                warning_path_based_section=["BA100", "BA101"],
-                warning_use_git_section=["GR102", "GR103"],
-                ignorable_errors=["GR100", "GR102"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["BA100", "BA101"],
+                },
+                "use_git": {
+                    "select": ["GR100", "GR101"],
+                    "warning": ["GR102", "GR103"],
+                },
+                "ignorable_errors": ["GR100", "GR102"],
+            },
             0,
             [],
+        ),
+        (
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["BA100", "BA101"],
+                },
+                "use_git": {
+                    "select": ["GR100", "GR101"],
+                    "warning": ["GR102", "GR103"],
+                },
+                "ignorable_errors": ["GR100", "GR102"],
+                "custom_section": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["WA100", "WA101"],
+                },
+            },
+            1,
+            ["WA100", "WA101"],
         ),
     ],
 )
 def test_validate_all_configured_error_codes_exist(
+    mocker,
     caplog,
-    configured_validations,
+    config_file_content,
     expected_exit_code,
     expected_error_codes_per_msg,
 ):
     """
-    Given: ConfiguredValidations mock
-    - case 1: ConfiguredValidations contains invalid error codes.
-    - case 2: ConfiguredValidations contains only valid error codes.
+    Given: validation config file mock
+    - Case 1: validation config file contains invalid error codes.
+    - Case 2: validation config file contains only valid error codes.
+    - Case 3: validation config file contains a custom section with invalid error codes.
     When: Running validate_all_configured_error_codes_exist.
     Then: Ensure the exit code and the error codes in the error msg are correct.
     - Case 1: should throw an error.
     - Case 2: should pass the validation.
+    - Case 3: should throw an error.
     """
     errors = caplog.records
-    exit_code = validate_all_configured_error_codes_exist(configured_validations)
+    mocker.patch.object(toml, "load", return_value=config_file_content)
+    config_reader = ConfigReader()
+    exit_code = validate_all_configured_error_codes_exist(
+        ConfiguredValidations(), config_reader.config_file_content
+    )
     assert exit_code == expected_exit_code
     for expected_error_code_per_msg in expected_error_codes_per_msg:
         assert expected_error_code_per_msg in errors[0].msg
@@ -109,16 +147,16 @@ def test_validate_all_validations_run_on_git_mode(
 ):
     """
     Given: ConfiguredValidations mock
-    - case 1: No correlation between use_git and path_based sections.
-    - case 2: use_git contains path_based section.
-    - case 3: Some path_based error codes appears in use_git, but not all.
+    - Case 1: No correlation between use_git and path_based sections.
+    - Case 2: use_git contains path_based section.
+    - Case 3: Some path_based error codes appears in use_git, but not all.
     When: Running validate_all_validations_run_on_git_mode.
     Then: Ensure the exit code and the error codes are correct.
     - Case 1: should throw an error.
     - Case 2: should pass the validation.
     - Case 3: should throw an error.
     """
-    exit_code = validate_all_validations_run_on_git_mode(configured_validations)
+    exit_code = validate_all_validations_run_on_git_mode(configured_validations, {})
     errors = caplog.records
     assert exit_code == expected_exit_code
     for expected_error_code_per_msg in expected_error_codes_per_msg:
@@ -126,79 +164,121 @@ def test_validate_all_validations_run_on_git_mode(
 
 
 @pytest.mark.parametrize(
-    "configured_validations, expected_exit_code, expected_number_of_errors, expected_error_codes_per_msg",
+    "config_file_content, expected_exit_code, expected_number_of_errors, expected_error_codes_per_msg",
     [
         (
-            ConfiguredValidations(
-                selected_use_git_section=["GR100", "GR101"],
-                selected_path_based_section=["PA100", "PA101"],
-                warning_use_git_section=["GR100", "GR101"],
-                warning_path_based_section=["PA100", "PA102"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["PA100", "PA102"],
+                },
+                "use_git": {
+                    "select": ["GR100", "GR101"],
+                    "warning": ["GR100", "GR101"],
+                },
+            },
             1,
             2,
             [
+                ["PA100", "path_based_validations"],
                 ["GR100", "GR101", "use_git"],
-                ["PA100", "path_based"],
             ],
         ),
         (
-            ConfiguredValidations(
-                selected_path_based_section=["PA102", "PA103"],
-                selected_use_git_section=["PA100", "PA101", "GR100", "GR101"],
-                warning_path_based_section=["PA100", "PA101"],
-                warning_use_git_section=["GR100", "GR101"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA102", "PA103"],
+                    "warning": ["PA100", "PA101"],
+                },
+                "use_git": {
+                    "select": ["PA100", "PA101", "GR100", "GR101"],
+                    "warning": ["GR100", "GR101"],
+                },
+            },
             1,
             1,
             [["GR100", "GR101", "use_git"]],
         ),
         (
-            ConfiguredValidations(
-                selected_path_based_section=["PA100", "PA101"],
-                selected_use_git_section=["PA101", "GR100", "GR101"],
-                warning_path_based_section=["PA100", "PA102"],
-                warning_use_git_section=["GR108", "GR107"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["PA100", "PA102"],
+                },
+                "use_git": {
+                    "select": ["PA101", "GR100", "GR101"],
+                    "warning": ["GR108", "GR107"],
+                },
+            },
             1,
             1,
-            [["PA100", "path_based"]],
+            [["PA100", "path_based_validations"]],
         ),
         (
-            ConfiguredValidations(
-                selected_path_based_section=["PA100", "PA101"],
-                selected_use_git_section=["PA101", "GR100", "GR101"],
-                warning_path_based_section=["PA105", "PA106"],
-                warning_use_git_section=["GR108", "GR107"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["PA105", "PA106"],
+                },
+                "use_git": {
+                    "select": ["PA101", "GR100", "GR101"],
+                    "warning": ["GR108", "GR107"],
+                },
+            },
             0,
             0,
             [],
         ),
+        (
+            {
+                "path_based_validations": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["PA105", "PA106"],
+                },
+                "use_git": {
+                    "select": ["PA101", "GR100", "GR101"],
+                    "warning": ["GR108", "GR107"],
+                },
+                "custom_section": {
+                    "select": ["PA100", "PA101"],
+                    "warning": ["PA100", "PA102"],
+                },
+            },
+            1,
+            1,
+            [["PA100", "custom_section"]],
+        ),
     ],
 )
 def test_validate_error_code_not_configured_twice(
+    mocker,
     caplog,
-    configured_validations,
+    config_file_content,
     expected_exit_code,
     expected_number_of_errors,
     expected_error_codes_per_msg,
 ):
     """
-    Given: ConfiguredValidations mock
-    - case 1: correlation between the use_git sections and the path_based sections.
-    - case 2: correlation only between the use_git sections.
-    - case 3: correlation only between the path_based sections.
-    - case 4: No correlation for both sections.
+    Given: config file content mock
+    - Case 1: correlation between the use_git sections and the path_based sections.
+    - Case 2: correlation only between the use_git sections.
+    - Case 3: correlation only between the path_based sections.
+    - Case 4: No correlation for both sections.
+    - Case 5: correlation only between the custom sections.
     When: Running validate_error_code_not_configured_twice.
     Then: Ensure the length of the results the exit code, and the error codes and sections in the error msgs are as expected.
     - Case 1: should fail both sections.
     - Case 2: should fail only path_based section.
     - Case 3: should fail only use_git section.
     - Case 4: should pass.
+    - Case 5: should fail only custom_section section.
     """
     errors = caplog.records
-    exit_code = validate_error_code_not_configured_twice(configured_validations)
+    mocker.patch.object(toml, "load", return_value=config_file_content)
+    config_reader = ConfigReader()
+    exit_code = validate_error_code_not_configured_twice(
+        ConfiguredValidations(), config_reader.config_file_content
+    )
     assert expected_exit_code == exit_code
     assert expected_number_of_errors == len(errors)
     assert all(
@@ -217,41 +297,61 @@ def test_validate_error_code_not_configured_twice(
 
 
 @pytest.mark.parametrize(
-    "configured_validations, expected_exit_code, expected_error_codes_per_msg",
+    "config_file_content, expected_exit_code, expected_error_codes_per_msg",
     [
         (
-            ConfiguredValidations(
-                selected_path_based_section=["BA100", "BA101"],
-                selected_use_git_section=["IN112"],
-                warning_path_based_section=["BA100", "BA101"],
-                warning_use_git_section=[],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["BA100", "BA101"],
+                    "warning": ["BA100", "BA101"],
+                },
+                "use_git": {"select": ["IN112"], "warning": []},
+            },
             1,
             ["BA113"],
         ),
         (
-            ConfiguredValidations(
-                selected_path_based_section=["BA100", "BA101"],
-                selected_use_git_section=["IN112", "BA113"],
-                warning_path_based_section=["BA100", "BA101"],
-                warning_use_git_section=["IN112"],
-            ),
+            {
+                "path_based_validations": {
+                    "select": ["BA100", "BA101"],
+                    "warning": ["BA100", "BA101"],
+                },
+                "use_git": {"select": ["IN112", "BA113"], "warning": ["IN112"]},
+            },
+            0,
+            [],
+        ),
+        (
+            {
+                "path_based_validations": {
+                    "select": ["BA100", "BA101"],
+                    "warning": ["BA100", "BA101"],
+                },
+                "use_git": {"select": ["IN112"], "warning": ["IN112"]},
+                "custom_category": {"select": ["IN112", "BA113"], "warning": ["IN112"]},
+            },
             0,
             [],
         ),
     ],
 )
 def test_validate_all_error_codes_configured(
-    caplog, configured_validations, expected_exit_code, expected_error_codes_per_msg
+    mocker,
+    caplog,
+    config_file_content,
+    expected_exit_code,
+    expected_error_codes_per_msg,
 ):
     """
-    Given: ConfiguredValidations mock
-    - case 1: ConfiguredValidations contains only some of the existing error codes.
-    - case 2: ConfiguredValidations contains all existing error codes.
+    Given: config file content mock
+    - Case 1: config file content contains only some of the existing error codes.
+    - Case 2: config file content contains all existing error codes.
+    - Case 3: config file content contains all existing error codes and additional custom category.
     When: Running validate_all_error_codes_configured.
     Then: Ensure the exit code and the error codes in the error msg are correct.
     - Case 1: should fail with exit code 1 and error code BA113.
     - Case 2: should pass the validation.
+    - Case 3: should pass the validation.
     """
     errors = caplog.records
     existing_error_codes = [
@@ -260,11 +360,15 @@ def test_validate_all_error_codes_configured(
         IsDisplayContainBetaValidator,
         IsContentItemNameContainTrailingSpacesValidator,
     ]
+    mocker.patch.object(toml, "load", return_value=config_file_content)
+    config_reader = ConfigReader()
     with patch(
         "demisto_sdk.scripts.validate_validation_config_file.get_all_validators",
         return_value=existing_error_codes,
     ):
-        exit_code = validate_all_error_codes_configured(configured_validations)
+        exit_code = validate_all_error_codes_configured(
+            ConfiguredValidations(), config_reader.config_file_content
+        )
 
     assert expected_exit_code == exit_code
     assert all(
--- demisto_sdk/scripts/validate_validation_config_file.py
+++ demisto_sdk/scripts/validate_validation_config_file.py
@@ -17,10 +17,10 @@ main = typer.Typer()
 
 
 def validate_all_configured_error_codes_exist(
-    configured_validations: ConfiguredValidations,
+    configured_validations: ConfiguredValidations, config_file_content: dict
 ) -> int:
     """
-    Validate that the set of configured validation errors in sdk_validation_config.toml is equal to the set of all existing validation to ensure we don't misconfigure non-existing validations.
+    Validate that the set of configured validation errors in validation config is equal to the set of all existing validation to ensure we don't misconfigure non-existing validations.
 
     Arguments:
     - configured_validations (ConfiguredValidations): The ConfiguredValidations object
@@ -30,14 +30,15 @@ def validate_all_configured_error_codes_exist(
     """
     exit_code = 0
     configured_errors_set: Set[str] = set()
-    for section in (
-        configured_validations.selected_path_based_section,
-        configured_validations.selected_use_git_section,
-        configured_validations.warning_path_based_section,
-        configured_validations.warning_use_git_section,
-        configured_validations.ignorable_errors,
-    ):
-        configured_errors_set = configured_errors_set.union(set(section))
+    for v in config_file_content.values():
+        if isinstance(v, dict):
+            configured_errors_set = configured_errors_set.union(
+                set(v.get("select", []) + v.get("warning", []))
+            )
+
+    configured_errors_set = configured_errors_set.union(
+        set(configured_validations.ignorable_errors)
+    )
     existing_error_codes: Set[str] = set(
         [validator.error_code for validator in get_all_validators()]
     )
@@ -54,6 +55,7 @@ def validate_all_configured_error_codes_exist(
 
 def validate_all_validations_run_on_git_mode(
     configured_validations: ConfiguredValidations,
+    config_file_content: dict,
 ) -> int:
     """
     Validate that all validations configured in the path_based section are also configured in the use_git section.
@@ -77,6 +79,7 @@ def validate_all_validations_run_on_git_mode(
 
 def validate_error_code_not_configured_twice(
     configured_validations: ConfiguredValidations,
+    config_file_content: dict,
 ) -> int:
     """
     validate that no error code is configured both for warning and select in the same section in the config file.
@@ -88,26 +91,21 @@ def validate_error_code_not_configured_twice(
     - 0 if the validation pass, otherwise return error code.
     """
     exit_code = 0
-    intersected_use_git_error_codes = set(
-        configured_validations.selected_use_git_section
-    ) & set(configured_validations.warning_use_git_section)
-    intersected_path_based_error_codes = set(
-        configured_validations.selected_path_based_section
-    ) & set(configured_validations.warning_path_based_section)
-    for intersected_error_codes, section in [
-        (intersected_use_git_error_codes, "use_git"),
-        (intersected_path_based_error_codes, "path_based"),
-    ]:
-        if intersected_error_codes:
-            logger.error(
-                f"[VA102] The following error codes are configured twice in the {section} both under select & warning: {', '.join(intersected_error_codes)}."
-            )
-            exit_code = 1
+    for k, v in config_file_content.items():
+        if isinstance(v, dict):
+            if intersected_error_codes := set(v.get("select", [])) & set(
+                v.get("warning", [])
+            ):
+                exit_code = 1
+                logger.error(
+                    f"[VA102] The following error codes are configured twice in the {k} both under select & warning: {', '.join(intersected_error_codes)}."
+                )
     return exit_code
 
 
 def validate_all_error_codes_configured(
     configured_validations: ConfiguredValidations,
+    config_file_content: dict,
 ) -> int:
     """
     validate that the set of all validation errors that exist in the new format and the set of all the validation errors configured in the sdk_validation_config are equal to ensure all new validations are being tested.
@@ -120,13 +118,11 @@ def validate_all_error_codes_configured(
     """
     exit_code = 0
     configured_errors_set: Set[str] = set()
-    for section in (
-        configured_validations.selected_path_based_section,
-        configured_validations.selected_use_git_section,
-        configured_validations.warning_path_based_section,
-        configured_validations.warning_use_git_section,
-    ):
-        configured_errors_set = configured_errors_set.union(set(section))
+    for v in config_file_content.values():
+        if isinstance(v, dict):
+            configured_errors_set = configured_errors_set.union(
+                set(v.get("select", []) + v.get("warning", []))
+            )
     existing_error_codes: Set[str] = set(
         [validator.error_code for validator in get_all_validators()]
     )
@@ -156,6 +152,7 @@ def validate_config_file(
     logging_setup(calling_function=__name__)
     config_reader = ConfigReader(path=config_path)
     configured_validations = config_reader.read()
+    config_file_content = config_reader.config_file_content
     validators = {
         "VA100": validate_all_configured_error_codes_exist,
         "VA101": validate_all_validations_run_on_git_mode,
@@ -171,6 +168,6 @@ def validate_config_file(
     else:
         filtered_validators = list(validators.values())
     for validation in filtered_validators:
-        if validation(configured_validations):
+        if validation(configured_validations, config_file_content):
             exit_code = 1
     raise typer.Exit(code=exit_code)

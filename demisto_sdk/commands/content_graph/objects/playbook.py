5d17412f1f9853d6784028578c27730e9da60f2f
barryyosi-panw <byosilevich@paloaltonetworks.com>
2025-02-23 10:54:49 +0200

    revert redundancies

--- demisto_sdk/commands/content_graph/objects/playbook.py
+++ demisto_sdk/commands/content_graph/objects/playbook.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Set
 
 from demisto_sdk.commands.common.constants import TEST_PLAYBOOKS_DIR
 from demisto_sdk.commands.content_graph.common import ContentType
@@ -15,3 +14,4 @@ class Playbook(BasePlaybook, content_type=ContentType.PLAYBOOK):  # type: ignore
             if TEST_PLAYBOOKS_DIR not in path.parts and path.suffix == ".yml":
                 return True
         return False
+    

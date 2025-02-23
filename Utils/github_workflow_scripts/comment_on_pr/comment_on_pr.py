1f79310064fd76d1d826719bc099df9d24c828f4
YuvHayun <yhayun@paloaltonetworks.com>
2025-02-23 15:25:36 +0200

    before testing

--- Utils/github_workflow_scripts/comment_on_pr/comment_on_pr.py
+++ Utils/github_workflow_scripts/comment_on_pr/comment_on_pr.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
 from demisto_sdk.commands.common.constants import CONTENT_REPO
 from demisto_sdk.commands.common.logger import logger
@@ -15,20 +15,17 @@ def comment_changelog_on_pr(pr_num: int, latest_commit: str, github_token: str):
     except Exception:
         logger.exception("Couldn't comment on the changelog.")
         sys.exit(1)
-        
 
 
 def comment_validate_summary(github_token: str, pr_num: int) -> None:
     """
-    Comment on the PR
+    Comment on the PR.
 
-    Checks the following:
-        - If the changelog file has been added in latest commit OR If the changelog file has been
-            modified between the last two commits.
+    If the validate summary exist, comment the summary on the pr.
 
     """
     validate_summary_msg = obtain_validate_summary_msg()
-    
+
     from github import Github
 
     github_client = Github(login_or_token=github_token)
@@ -39,22 +36,25 @@ def comment_validate_summary(github_token: str, pr_num: int) -> None:
     logger.info(f"Successfully commented on PR {pr_num} the validate summary.")
 
 
-def obtain_validate_summary_msg() -> str:
+def obtain_validate_summary_msg():
     validate_summary_msg = ""
     if (artifacts_folder := os.getenv("ARTIFACTS_FOLDER")) and Path(
-            artifacts_folder
-        ).exists():
-            artifacts_validate_summary_path = Path(
-                f"{artifacts_folder}/validate_summary.txt"
-            )
-            logger.info(f"reading from the validate summary results to a txt file at {artifacts_validate_summary_path}.")
-            with open(artifacts_validate_summary_path, "ר") as f:
-                validate_summary_msg = f.read()
+        artifacts_folder
+    ).exists():
+        artifacts_validate_summary_path = Path(
+            f"{artifacts_folder}/validate_summary.txt"
+        )
+        logger.info(
+            f"reading from the validate summary results to a txt file at {artifacts_validate_summary_path}."
+        )
+        with open(artifacts_validate_summary_path, "ר") as f:
+            validate_summary_msg = f.read()
     else:
-        raise Exception(f"could not find validate summary file.")
-    
+        raise Exception("could not find validate summary file.")
+
     if not validate_summary_msg:
         raise Exception("validate_summary_msg is empty.")
+
 
 def comment_validate_summary_on_pr(pr_num: int, github_token: str):
     try:
@@ -86,7 +86,11 @@ def arguments_handler():
         "-ght", "--github_token", help="The token for Github-Api", required=True
     )
     parser.add_argument(
-        "-vs", "--validate_summary", help="Boolean.Whether to run validate summary", required=False, action=argparse.BooleanOptionalAction
+        "-vs",
+        "--validate_summary",
+        help="Boolean.Whether to run validate summary",
+        required=False,
+        action=argparse.BooleanOptionalAction,
     )
 
     return parser.parse_args()

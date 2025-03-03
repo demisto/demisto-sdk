9a30f64cfbcec43698d04733801b79602ea5f323
Moshe Eichler <78307768+MosheEichler@users.noreply.github.com>
2025-03-03 12:40:22 +0200

    Remove mock mechanism (#4764)
    * remove mock mechanism
    
    * remove unmockable integration from conf.json
    
    * update conf json
    
    * remove redundant un-mockable
    
    * remove more redundant code
    
    * remove timestamp replacer
    
    * pre-commit fixes
    
    * add changelog
    
    * fix UTs
    
    * remove isAMI
    
    * ruff fixes
    
    * fixes
    
    * fixed more UT
    
    * fixed all UT
    
    * add a debug message
    
    * ruff fixes
    
    * mypy error
    
    * fix print error
    
    * update version message
    
    * add logs
    
    * remove natured code
    
    * remove redundant log
    
    * CR fixes
    
    * CR fixes

--- demisto_sdk/commands/test_content/tests/server_context_test.py
+++ demisto_sdk/commands/test_content/tests/server_context_test.py
@@ -1,5 +1,4 @@
 from demisto_sdk.commands.common.constants import TEST_PLAYBOOKS
-from demisto_sdk.commands.test_content.mock_server import MITMProxy
 from demisto_sdk.commands.test_content.TestContentClasses import (
     BuildContext,
     OnPremServerContext,
@@ -18,25 +17,23 @@ from demisto_sdk.commands.test_content.tests.DemistoClientMock import DemistoCli
 def test_execute_tests(mocker, tmp_path):
     """
     Given:
-        - A ServerContext instance that should execute three tests:
+        - A ServerContext instance that should execute two tests:
             1) A test with no integrations
-            2) A test with mockable integration
-            3) A test with unmockable integration
+            2) A test with integration
 
     When:
         - Running execute_tests.
 
     Then:
         - Ensure the server context has executed all the tests
-        - Ensure the mockable and unmockable tests queues were emptied
+        - Ensure the test queues were emptied
         - Ensure no test has failed during that test
     """
 
     # Setting up the build context
     filtered_tests = [
         "playbook_without_integrations",
-        "playbook_with_mockable_integration",
-        "playbook_with_unmockable_integration",
+        "playbook_with_integration",
         "skipped_playbook",
     ]
 
@@ -50,22 +47,17 @@ def test_execute_tests(mocker, tmp_path):
     tests = [
         generate_test_configuration(playbook_id="playbook_without_integrations"),
         generate_test_configuration(
-            playbook_id="playbook_with_mockable_integration",
-            integrations=["mockable_integration"],
-        ),
-        generate_test_configuration(
-            playbook_id="playbook_with_unmockable_integration",
-            integrations=["unmockable_integration"],
+            playbook_id="playbook_with_integration",
+            integrations=["integration"],
         ),
         generate_test_configuration(playbook_id="skipped_playbook"),
     ]
     content_conf_json = generate_content_conf_json(
         tests=tests,
-        unmockable_integrations={"unmockable_integration": "reason"},
         skipped_tests={"skipped_playbook": "reason"},
     )
     # Setting up the content-test-conf conf.json
-    integration_names = ["mockable_integration", "unmockable_integration"]
+    integration_names = ["integration"]
     integrations_configurations = [
         generate_integration_configuration(integration_name)
         for integration_name in integration_names
@@ -97,8 +89,7 @@ def test_execute_tests(mocker, tmp_path):
         assert test in server_context.executed_tests
 
     # Validating all queues were emptied
-    assert next(iter(build_context.servers)).mockable_tests_to_run.all_tasks_done
-    assert next(iter(build_context.servers)).unmockable_tests_to_run.all_tasks_done
+    assert next(iter(build_context.servers)).tests_to_run.all_tasks_done
 
     # Validating no failed playbooks
     assert not build_context.tests_data_keeper.failed_playbooks
@@ -127,9 +118,6 @@ def generate_mocked_server_context(
     )
     # Mocking unnecessary calls
     mocker.patch(
-        "demisto_sdk.commands.test_content.mock_server.run_with_mock"
-    ).return_value.__enter__.return_value = {}
-    mocker.patch(
         "demisto_sdk.commands.test_content.IntegrationsLock.safe_lock_integrations",
         return_value=True,
     )
@@ -144,7 +132,6 @@ def generate_mocked_server_context(
         return_value=False,
     )
 
-    mocker.patch.object(MITMProxy, "__init__", lambda *args, **kwargs: None)
     mocker.patch("time.sleep")
     # Executing the test
     server_context = OnPremServerContext(build_context, "1.1.1.1")

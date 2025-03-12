fd54552c11f6776e9eb748e248dc08a4bc097e15
barryyosi-panw <158817412+barryyosi-panw@users.noreply.github.com>
2025-03-12 20:44:18 +0200

    Fix indentation

--- demisto_sdk/commands/pre_commit/pre_commit_command.py
+++ demisto_sdk/commands/pre_commit/pre_commit_command.py
@@ -684,10 +684,10 @@ def add_related_files(file: Path) -> Set[Path]:
             else PS1_TEST_FILE_SUFFIX
         )
 
-    test_files = []
-    if file.parent.exists():
-        test_files = [_file for _file in file.parent.iterdir() if _file.name.endswith(test_file_suffix)]
-        files_to_run.update(test_files)
+        test_files = []
+        if file.parent.exists():
+            test_files = [_file for _file in file.parent.iterdir() if _file.name.endswith(test_file_suffix)]
+            files_to_run.update(test_files)
     return files_to_run
 
 

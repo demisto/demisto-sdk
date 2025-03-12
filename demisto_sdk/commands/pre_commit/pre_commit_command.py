0475e4c3a184ccc4e0ff3d130492dab31377bb46
omerKarkKatz <okarkkatz@paloaltonetworks.com>
2025-03-12 13:28:49 +0200

    chcking if parent folder exists before iterating on it

--- demisto_sdk/commands/pre_commit/pre_commit_command.py
+++ demisto_sdk/commands/pre_commit/pre_commit_command.py
@@ -687,7 +687,7 @@ def add_related_files(file: Path) -> Set[Path]:
         test_files = [
             _file
             for _file in file.parent.iterdir()
-            if _file.name.endswith(test_file_suffix)
+            if file.parent.exists and _file.name.endswith(test_file_suffix)
         ]
         files_to_run.update(test_files)
     return files_to_run

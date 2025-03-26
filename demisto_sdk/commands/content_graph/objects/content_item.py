20ec3f6cb4867715f01a8b13fabe4b87f9edd07e
barryyosi-panw <158817412+barryyosi-panw@users.noreply.github.com>
2025-03-26 07:57:44 +0200

    change log level to dump-file-path line (#4883)
    * change log level to dump file line
    
    * changelog
    
    * Update .changelog/4883.yml
    
    ---------
    
    Co-authored-by: samuelFain <65926551+samuelFain@users.noreply.github.com>

--- demisto_sdk/commands/content_graph/objects/content_item.py
+++ demisto_sdk/commands/content_graph/objects/content_item.py
@@ -365,7 +365,7 @@ class ContentItem(BaseContent):
                 data=self.prepare_for_upload(current_marketplace=marketplace),
                 handler=self.handler,
             )
-            logger.info(f"path to dumped file: {str(dir / self.normalize_name)}")
+            logger.debug(f"path to dumped file: {str(dir / self.normalize_name)}")
         except FileNotFoundError as e:
             logger.warning(f"Failed to dump {self.path} to {dir}: {e}")
 

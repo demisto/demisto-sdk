5300a6da976add62370714780a334eaf14f41e60
rshunim <rshunim@paloaltonetworks.com>
2025-04-28 19:57:30 +0300

    content_type

--- demisto_sdk/commands/content_graph/objects/agentix_agent.py
+++ demisto_sdk/commands/content_graph/objects/agentix_agent.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+
+from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects.agentix_base import AgentixBase
 
 
-class AgentixAgent(AgentixBase):
+class AgentixAgent(AgentixBase, content_type=ContentType.AGENTIX_AGENT):
     color: str
 
     @staticmethod

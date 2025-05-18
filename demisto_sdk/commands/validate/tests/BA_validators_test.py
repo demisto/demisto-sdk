8b252ea0c60bdd4f0c11aa5077b00f0e4030f5f0
Tal <tcarmeli@paloaltonetworks.com>
2025-05-18 18:01:14 +0300

    marketplace validation

--- demisto_sdk/commands/validate/tests/BA_validators_test.py
+++ demisto_sdk/commands/validate/tests/BA_validators_test.py
@@ -10,7 +10,8 @@ from demisto_sdk.commands.common.constants import (
     XSOAR_SUPPORT,
 )
 from demisto_sdk.commands.content_graph.common import ContentType
-from demisto_sdk.commands.content_graph.objects import AgentixAgent
+from demisto_sdk.commands.content_graph.objects import AgentixAction, AgentixAgent
+from demisto_sdk.commands.content_graph.objects.script import Script
 from demisto_sdk.commands.validate.tests.test_tools import (
     REPO,
     create_assets_modeling_rule_object,
@@ -113,9 +114,13 @@ from demisto_sdk.commands.validate.validators.BA_validators.BA127_is_valid_conte
 from demisto_sdk.commands.validate.validators.BA_validators.BA128_is_command_or_script_name_starts_with_digit import (
     IsCommandOrScriptNameStartsWithDigitValidator,
 )
+from demisto_sdk.commands.validate.validators.BA_validators.BA129_is_forbidden_content_item import (
+    IsForbiddenContentItemValidator,
+)
+from demisto_sdk.commands.validate.validators.BA_validators.BA130_is_correct_mp import (
+    IsMarketplaceExistsValidator,
+)
 from TestSuite.repo import ChangeCWD
-from demisto_sdk.commands.validate.validators.BA_validators.BA129_is_forbidden_content_item import\
-    IsForbiddenContentItemValidator
 
 VALUE_WITH_TRAILING_SPACE = "field_with_space_should_fail "
 
@@ -2536,3 +2541,45 @@ def test_is_forbidden_content_item():
 
     assert len(results) == 1
     assert results[0].message == expected_msg
+
+
+def test_is_marketplace_exists():
+    content_items = [AgentixAgent(color="red", description="", display="", path=Path("test.yml"),
+                                marketplaces=["xsoar"], name="test", fromversion="", toversion="",
+                                display_name="", deprecated=False, id="", node_id=""),
+                    AgentixAgent(color="red", description="", display="", path=Path("test.yml"),
+                                marketplaces=["xsoar_saas"], name="test", fromversion="", toversion="",
+                                display_name="", deprecated=False, id="", node_id=""),
+                    AgentixAction(color="red", description="", display="", path=Path("test.yml"),
+                                marketplaces=["xsoar"], name="test", fromversion="", toversion="",
+                                display_name="", deprecated=False, id="", node_id="",
+                                underlyingContentItemId="test", underlyingContentItemName="test",
+                                underlyingContentItemType=1, underlyingContentItemVersion=-1, agent_id="test"),
+                    AgentixAction(color="red", description="", display="", path=Path("test.yml"),
+                                marketplaces=["xsoar_saas"], name="test", fromversion="", toversion="",
+                                display_name="", deprecated=False, id="", node_id="",
+                                underlyingContentItemId="test", underlyingContentItemName="test",
+                                underlyingContentItemType=1, underlyingContentItemVersion=-1, agent_id="test"),
+                    Script(is_llm=True, marketplaces=["xsoar_saas"], id="", script='print("hello world")',
+                           node_id="", path=Path("test.yml"), name="test1", fromversion="6.0.0",
+                           toversion="8.0.0", display_name="test", deprecated=False, type='python',
+                           tags=['test'], skip_prepare=[]),
+                    Script(is_llm=False, marketplaces=["xsoar"], id="", script='print("hello world")',
+                           node_id="", path=Path("test.yml"), name="test1", fromversion="6.0.0",
+                           toversion="8.0.0", display_name="test", deprecated=False, type='python',
+                           tags=['test'], skip_prepare=[]),
+                    Script(is_llm=True, marketplaces=["xsoar"], id="", script='print("hello world")',
+                           node_id="", path=Path("test.yml"), name="test1", fromversion="6.0.0",
+                           toversion="8.0.0", display_name="test", deprecated=False, type='python',
+                           tags=['test'], skip_prepare=[])
+                    ]
+    results = (
+        IsMarketplaceExistsValidator().obtain_invalid_content_items(
+            content_items
+        )
+    )
+
+    assert len(results) == 3
+    assert results[0].message == ("The items AgentixAgent, AgentixAction and Script with isLLM=true"
+                                  " should be uploaded to xsoar_saas only. Please specify only xsoar_saas"
+                                  " under marketplaces.")
---
+++ demisto_sdk/commands/validate/validators/BA_validators/BA130_is_correct_mp.py
@@ -0,0 +1,47 @@
+from typing import Iterable, List, Union
+
+from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.content_graph.common import ContentType
+from demisto_sdk.commands.content_graph.objects import (
+    AgentixAction,
+    AgentixAgent,
+)
+from demisto_sdk.commands.content_graph.objects.script import Script
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+
+ContentTypes = Union[
+    AgentixAgent,
+    AgentixAction,
+    Script
+]
+
+
+class IsMarketplaceExistsValidator(BaseValidator[ContentTypes]):
+    error_code = "BA130"
+    description = f"Content items of type {ContentType.AGENTIX_AGENT}, {ContentType.AGENTIX_ACTION} and {ContentType.SCRIPT} with isLLM=true should be uploaded to xsoar_saas only."
+    rationale = "These types of items should be uploaded to xsoar_saas only."
+    error_message = f"The items {ContentType.AGENTIX_AGENT}, {ContentType.AGENTIX_ACTION} and {ContentType.SCRIPT} with isLLM=true should be uploaded to xsoar_saas only. Please specify only xsoar_saas under marketplaces."
+
+    def obtain_invalid_content_items(
+            self,
+            content_items: Iterable[ContentTypes],
+    ) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message,
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if self.is_invalid_marketplace(content_item)
+        ]
+
+    def is_invalid_marketplace(self, content_item: ContentTypes) -> bool:
+        if (content_item.content_type in [
+            ContentType.AGENTIX_AGENT,
+            ContentType.AGENTIX_ACTION,
+        ]) or (content_item.content_type == ContentType.SCRIPT and content_item.is_llm):
+            return len(content_item.marketplaces) > 1 or len(content_item.marketplaces) == 0 or MarketplaceVersions.XSOAR_SAAS.value != content_item.marketplaces[0].value

5d0584cb0567260d1fb91dcc2a805c1827b9f583
noydavidi <nodavidi.paloaltonetworks.com>
2025-05-20 16:42:23 +0300

    removed spaces

--- demisto_sdk/commands/update_release_notes/update_rn.py
+++ demisto_sdk/commands/update_release_notes/update_rn.py
@@ -1542,7 +1542,6 @@ def update_api_modules_dependents_rn(
     with ContentGraphInterface() as graph:
         update_content_graph(graph, use_git=True, dependencies=True)
         integrations = get_api_module_dependencies_from_graph(api_module_set, graph)
-
         if integrations:
             logger.info("Executing update-release-notes on those as well.")
         for integration in integrations:

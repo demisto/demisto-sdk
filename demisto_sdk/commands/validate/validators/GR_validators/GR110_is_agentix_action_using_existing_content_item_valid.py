b2032930635164430d9fd80b862e0f461322b92b
Tal <tcarmeli@paloaltonetworks.com>
2025-06-17 11:19:38 +0300

    fix ba129 validation

--- demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 
 from abc import ABC
-
 from typing import Iterable, List
 
-from demisto_sdk.commands.common.constants import GitStatuses
 from demisto_sdk.commands.content_graph.objects import AgentixAction
 from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
 from demisto_sdk.commands.validate.validators.base_validator import (
@@ -15,9 +13,13 @@ from demisto_sdk.commands.validate.validators.base_validator import (
 ContentTypes = AgentixAction
 
 
-class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentTypes], ABC):
+class IsAgentixActionUsingExistingContentItemValidator(
+    BaseValidator[ContentTypes], ABC
+):
     error_code = "GR110"
-    description = "Avoid creating Agentix actions that wrap non-existent commands or scripts"
+    description = (
+        "Avoid creating Agentix actions that wrap non-existent commands or scripts"
+    )
     rationale = "Actions in Agentix should wrap only existing commands or scripts"
     error_message = ""
     related_field = ""
@@ -25,9 +27,9 @@ class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentType
     # expected_git_statuses = [GitStatuses.ADDED, GitStatuses.MODIFIED]
     related_file_type = [RelatedFileType.YML]
 
-    def obtain_invalid_content_items_using_graph(self, content_items: Iterable[ContentTypes],
-                                                 validate_all_files: bool) -> List[ValidationResult]:
-
+    def obtain_invalid_content_items_using_graph(
+        self, content_items: Iterable[ContentTypes], validate_all_files: bool
+    ) -> List[ValidationResult]:
         results: List[ValidationResult] = []
 
         for content_item in content_items:
@@ -35,10 +37,16 @@ class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentType
             content_item_type = content_item.underlying_content_item_type
             pack_id = content_item.underlying_content_item_id
 
-            if pack_id in ["_any_", "_builtin_"]:  # no validation when the action wraps a builtin or enrich command
+            if pack_id in [
+                "_any_",
+                "_builtin_",
+            ]:  # no validation when the action wraps a builtin or enrich command
                 continue
 
-            if content_item_type not in ["command", "script"]:  # Validate when the action wraps a command or script
+            if content_item_type not in [
+                "command",
+                "script",
+            ]:  # Validate when the action wraps a command or script
                 results.append(
                     ValidationResult(
                         validator=self,
@@ -50,31 +58,36 @@ class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentType
 
             graph_result = self.graph.search(object_id=name)
 
-            if not graph_result:  # the command or the script does not exist in the Content repo
+            if (
+                not graph_result
+            ):  # the command or the script does not exist in the Content repo
                 results.append(
                     ValidationResult(
                         validator=self,
-                        message=f'The content item {name} does not exist in the Content repository',
+                        message=f"The content item {name} does not exist in the Content repository",
                         content_object=content_item,
                     )
                 )
 
-            elif not self.is_content_item_related_to_the_right_pack(content_item_type=content_item_type,
-                                                                    pack_id=pack_id, graph_result=graph_result):
+            elif not self.is_content_item_related_to_the_right_pack(
+                content_item_type=content_item_type,
+                pack_id=pack_id,
+                graph_result=graph_result,
+            ):
                 results.append(
                     ValidationResult(
                         validator=self,
                         message=f"The action relates the content item '{name}' to this '{pack_id}' pack which is not"
-                                f" the right pack",
+                        f" the right pack",
                         content_object=content_item,
                     )
                 )
 
         return results
 
-    def is_content_item_related_to_the_right_pack(self, content_item_type: str, pack_id: str,
-                                                  graph_result: List) -> bool:
-
+    def is_content_item_related_to_the_right_pack(
+        self, content_item_type: str, pack_id: str, graph_result: List
+    ) -> bool:
         if content_item_type == "command":
             for integration in graph_result[0].integrations:
                 if integration.object_id == pack_id:
--- demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid_all_files.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid_all_files.py
@@ -4,16 +4,20 @@ from typing import Iterable, List
 
 from demisto_sdk.commands.common.constants import ExecutionMode
 from demisto_sdk.commands.content_graph.objects import AgentixAction
-
 from demisto_sdk.commands.validate.validators.base_validator import ValidationResult
-
-from demisto_sdk.commands.validate.validators.GR_validators.GR110_is_agentix_action_using_existing_content_item_valid import \
-    IsAgentixActionUsingExistingContentItemValidator
+from demisto_sdk.commands.validate.validators.GR_validators.GR110_is_agentix_action_using_existing_content_item_valid import (
+    IsAgentixActionUsingExistingContentItemValidator,
+)
 
 ContentTypes = AgentixAction
 
-class IsAgentixActionUsingExistingContentItemValidatorAllFiles(IsAgentixActionUsingExistingContentItemValidator):
+
+class IsAgentixActionUsingExistingContentItemValidatorAllFiles(
+    IsAgentixActionUsingExistingContentItemValidator
+):
     expected_execution_mode = [ExecutionMode.ALL_FILES]
 
-    def obtain_invalid_content_items(self, content_items: Iterable[ContentTypes]) -> List[ValidationResult]:
+    def obtain_invalid_content_items(
+        self, content_items: Iterable[ContentTypes]
+    ) -> List[ValidationResult]:
         return self.obtain_invalid_content_items_using_graph(content_items, True)

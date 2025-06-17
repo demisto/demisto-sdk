18b23d89ceeba6df324b91e34815cde887a04f73
rshunim <rshunim@paloaltonetworks.com>
2025-06-17 14:25:13 +0300

    fix GR110

--- demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import logging
 from abc import ABC
 
 from typing import Iterable, List
 
 from demisto_sdk.commands.common.constants import GitStatuses
+from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects import AgentixAction
 from demisto_sdk.commands.content_graph.parsers.related_files import RelatedFileType
 from demisto_sdk.commands.validate.validators.base_validator import (
@@ -33,9 +35,9 @@ class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentType
         for content_item in content_items:
             name = content_item.underlying_content_item_name
             content_item_type = content_item.underlying_content_item_type
-            pack_id = content_item.underlying_content_item_id
+            content_item_id = content_item.underlying_content_item_id
 
-            if pack_id in ["_any_", "_builtin_"]:  # no validation when the action wraps a builtin or enrich command
+            if content_item_id in ["_any_", "_builtin_"]:  # no validation when the action wraps a builtin or enrich command
                 continue
 
             if content_item_type not in ["command", "script"]:  # Validate when the action wraps a command or script
@@ -54,31 +56,34 @@ class IsAgentixActionUsingExistingContentItemValidator(BaseValidator[ContentType
                 results.append(
                     ValidationResult(
                         validator=self,
-                        message=f'The content item {name} does not exist in the Content repository',
+                        message=f"The content item '{name}' does not exist in the Content repository",
                         content_object=content_item,
                     )
                 )
 
             elif not self.is_content_item_related_to_the_right_pack(content_item_type=content_item_type,
-                                                                    pack_id=pack_id, graph_result=graph_result):
+                                                                    content_item_id=content_item_id,
+                                                                    graph_result=graph_result):
                 results.append(
                     ValidationResult(
                         validator=self,
-                        message=f"The action relates the content item '{name}' to this '{pack_id}' pack which is not"
-                                f" the right pack",
+                        message=f"The action relates the command '{name}' to '{content_item_id}' integration id"
+                                f" which is not the right integration."
+                                f" Please edit the action with the right integration.",
                         content_object=content_item,
                     )
                 )
 
         return results
 
-    def is_content_item_related_to_the_right_pack(self, content_item_type: str, pack_id: str,
+    def is_content_item_related_to_the_right_pack(self, content_item_type: str, content_item_id: str,
                                                   graph_result: List) -> bool:
 
         if content_item_type == "command":
-            for integration in graph_result[0].integrations:
-                if integration.object_id == pack_id:
-                    return True
+            for item in graph_result:
+                if type(item.content_type) == ContentType.COMMAND:
+                    if item.integrations[0].object_id == content_item_id:
+                        return True
         elif content_item_type == "script":
             return True
 

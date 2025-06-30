a9385b5994a7c1619a37925500433a68e039dc83
Tal <tcarmeli@paloaltonetworks.com>
2025-06-30 17:19:25 +0300

    poetry lock

--- demisto_sdk/commands/content_graph/parsers/agentix_action.py
+++ demisto_sdk/commands/content_graph/parsers/agentix_action.py
@@ -32,7 +32,7 @@ class AgentixActionParser(AgentixBaseParser, content_type=ContentType.AGENTIX_AC
         self.underlying_content_item_version: int = self.yml_data.get(
             "underlyingcontentitem").get('version')
         self.requires_user_approval: bool = self.yml_data.get("requiresuserapproval")
-        self.few_shots: Optional[str] = self.yml_data.get("fewshots")
+
     @cached_property
     def field_mapping(self):
         super().field_mapping.update({"display": "name"})

1d5a010eb9392e0e7698d023d47111bf787d02eb
rshunim <rshunim@paloaltonetworks.com>
2025-07-01 11:40:09 +0300

    added GitStatuses to AG100 and AG101

--- demisto_sdk/commands/validate/validators/AG_validators/AG100_is_forbidden_content_item.py
+++ demisto_sdk/commands/validate/validators/AG_validators/AG100_is_forbidden_content_item.py
@@ -1,5 +1,7 @@
 from typing import Iterable, List, Union
 
+from demisto_sdk.commands.common.constants import GitStatuses
+
 from demisto_sdk.commands.content_graph.common import ContentType
 from demisto_sdk.commands.content_graph.objects import (
     AgentixAction,
@@ -22,6 +24,7 @@ class IsForbiddenContentItemValidator(BaseValidator[ContentTypes]):
         f"The items {ContentType.AGENTIX_AGENT} and {ContentType.AGENTIX_ACTION} and {ContentType.SCRIPT} with is_llm=True"
         f" should be stored in content-test-conf, not in Content"
     )
+    expected_git_statuses = [GitStatuses.ADDED, GitStatuses.MODIFIED, GitStatuses.RENAMED]
 
     def obtain_invalid_content_items(
         self,

7b341da5675fb87937df23ff023d9551d48ebb74
rshunim <rshunim@paloaltonetworks.com>
2025-07-03 12:46:58 +0300

    GitStatuses

--- demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR110_is_agentix_action_using_existing_content_item_valid.py
@@ -43,11 +43,11 @@ class IsAgentixActionUsingExistingContentItemValidator(
     error_message = ""
     related_field = ""
     is_auto_fixable = False
-    # expected_git_statuses = [
-    #     GitStatuses.ADDED,
-    #     GitStatuses.MODIFIED,
-    #     GitStatuses.RENAMED,
-    # ]
+    expected_git_statuses = [
+        GitStatuses.ADDED,
+        GitStatuses.MODIFIED,
+        GitStatuses.RENAMED,
+    ]
     related_file_type = [RelatedFileType.YML]
 
     def obtain_invalid_content_items_using_graph(

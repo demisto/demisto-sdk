fc160d6cc47fcd87bc841ad041fb9d97fddf60d6
merit-maita <meretmaayta@gmail.com>
2025-07-08 13:38:37 +0300

    formatted

--- demisto_sdk/commands/validate/validators/IN_validators/IN122_is_valid_feed_integration.py
+++ demisto_sdk/commands/validate/validators/IN_validators/IN122_is_valid_feed_integration.py
@@ -72,7 +72,9 @@ class IsValidFeedIntegrationValidator(BaseValidator[ContentTypes]):
                 msg = f"{msg}\n\tThe field '{key}' must appear and contain '{val}'."
         if must_be_one_of := value.get("must_be_one_of"):
             for key, val in must_be_one_of.items():
-                lists_str = ' or '.join(', '.join(str(item) for item in sublist) for sublist in val)
+                lists_str = " or ".join(
+                    ", ".join(str(item) for item in sublist) for sublist in val
+                )
                 msg = f"{msg}\n\tThe field '{key}' must be one of the following lists: {lists_str}."
         return msg
 

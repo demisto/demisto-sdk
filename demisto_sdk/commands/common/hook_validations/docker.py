2cdd7d2faf2f1e6f5f6d6a068592639f35fa8022
meichler <meichler@paloaltonetworks.com>
2025-07-14 10:42:00 +0300

    fixes

--- demisto_sdk/commands/common/hook_validations/docker.py
+++ demisto_sdk/commands/common/hook_validations/docker.py
@@ -5,7 +5,7 @@ from typing import Optional, Tuple, Union
 
 import requests
 from dateparser import parse
-from packaging.version import parse
+from packaging.version import parse as parse_version
 
 from demisto_sdk.commands.common.constants import (
     NATIVE_IMAGE_DOCKER_NAME,
@@ -268,7 +268,7 @@ class DockerImageValidator(BaseValidator):
         for num_tag in only_numbered_tags:
             # the version_tag.release returns a tuple from the version numbers '1.2.3.45' -> (1, 2, 3, 45)
             # The last place is always a build number, therefore always increasing.
-            if parse(max_tag).release[-1] < parse(num_tag).release[-1]:
+            if parse_version(max_tag).release[-1] < parse_version(num_tag).release[-1]:
                 max_tag = num_tag
 
         return max_tag

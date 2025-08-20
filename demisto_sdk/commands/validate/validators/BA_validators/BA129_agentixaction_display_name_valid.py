32b473a6685e5642ef07b2c87c3a6e5445ac66cc
mayagoldman <mgoldman@paloaltonetworks.com>
2025-08-20 16:08:18 +0300

    pre-commit

---
+++ demisto_sdk/commands/validate/validators/BA_validators/BA129_agentixaction_display_name_valid.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+from typing import Iterable, List, Union
+import re
+
+from demisto_sdk.commands.content_graph.objects.agentix_action import AgentixAction
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+ContentTypes = Union[AgentixAction]
+
+class DisplayNameValidValidator(BaseValidator[ContentTypes]):
+    error_code = "BA129"
+    description = "Ensure display starts with a letter and contains only allowed characters."
+    rationale = "Display names must be user-friendly and conform to standards."
+    error_message = "The following display_name values are invalid: {0}"
+    related_field = "display"
+    is_auto_fixable = False
+
+    # AgentixAction display_name must start with a letter (either lower or upper case) and contain only
+    # the following characters: lowercase letters, uppercase letters, digits, underscores, hyphens, spaces.
+    AGENTIX_ACTION_DISPLAY_NAME_PATTERN = re.compile(r"^[A-Za-z][A-Za-z0-9_\- ]*$")
+
+    def obtain_invalid_content_items(
+        self, content_items: Iterable[ContentTypes]
+    ) -> List[ValidationResult]:
+        validation_results = []
+        for content_item in content_items:
+            valid = True
+            content_type = ""
+            display_name = getattr(content_item, "display_name", None)
+            if not display_name:
+                continue
+            if isinstance(content_item, AgentixAction):
+                if not self.AGENTIX_ACTION_DISPLAY_NAME_PATTERN.match(display_name):
+                    valid = False
+                    content_type = "Agentix Action"
+            if not valid:
+                validation_results.append(
+                    ValidationResult(
+                        validator=self,
+                        message=self.error_message.format(
+                            content_type,
+                            display_name,
+                        ),
+                        content_object=content_item,
+                    )
+                )
+
+        return validation_results

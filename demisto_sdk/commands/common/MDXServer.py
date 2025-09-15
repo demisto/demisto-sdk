70da1956c9757699a2873ed2174763df25015beb
shmuel44 <skroizer@paloaltonetworks.com>
2025-09-15 10:46:52 +0300

    if process.stdout is None

--- demisto_sdk/commands/common/MDXServer.py
+++ demisto_sdk/commands/common/MDXServer.py
@@ -141,6 +141,8 @@ def start_local_MDX_server(
     process = subprocess.Popen(
         ["node", str(server_script_path())], stdout=subprocess.PIPE, text=True
     )
+    if process.stdout is None:
+        raise RuntimeError("Failed to capture stdout from MDX server process")
     line = process.stdout.readline()
     if EXPECTED_SUCCESS_MESSAGE not in line:
         logger.error(f"MDX local server couldnt be started: {line}")

942de5479a4131bf921f5bb559da191433ed14e5
mayagoldman <mgoldman@paloaltonetworks.com>
2025-09-15 14:02:36 +0800

    pre commt

--- demisto_sdk/commands/validate/validators/AG_validators/AG105_is_display_name_valid.py
+++ demisto_sdk/commands/validate/validators/AG_validators/AG105_is_display_name_valid.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import re
 from typing import Iterable, List
 
-from demisto_sdk.commands.common.constants import GitStatuses
 from demisto_sdk.commands.content_graph.objects.agentix_action import AgentixAction
 from demisto_sdk.commands.validate.validators.base_validator import (
     BaseValidator,

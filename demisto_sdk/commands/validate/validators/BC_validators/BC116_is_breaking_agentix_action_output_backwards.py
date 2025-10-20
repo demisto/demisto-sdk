68deb86e150d0aeaad5a2396182cacab56006938
Menachem Weinfeld <90556466+mmhw@users.noreply.github.com>
2025-10-20 13:53:04 +0300

    [validate] Add BC116 validator for AgentixAction outputs (#5100)
    * Add changelog
    
    * [validate] Add BC116 validator for AgentixAction outputs
    
    * Fix pre-commit
    
    * Update demisto_sdk/commands/validate/validators/BC_validators/BC116_is_breaking_agentix_action_output_backwards.py
    
    Co-authored-by: Dan Tavori <38749041+dantavori@users.noreply.github.com>
    
    * Update demisto_sdk/commands/validate/tests/BC_validators_test.py
    
    ---------
    
    Co-authored-by: Dan Tavori <38749041+dantavori@users.noreply.github.com>

---
+++ demisto_sdk/commands/validate/validators/BC_validators/BC116_is_breaking_agentix_action_output_backwards.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import Iterable, List, Set
+
+from demisto_sdk.commands.common.constants import GitStatuses
+from demisto_sdk.commands.content_graph.objects.agentix_action import AgentixAction
+from demisto_sdk.commands.validate.validators.base_validator import (
+    BaseValidator,
+    ValidationResult,
+)
+
+ContentTypes = AgentixAction
+
+
+class IsBreakingAgentixActionOutputBackwardsValidator(BaseValidator[ContentTypes]):
+    error_code = "BC116"
+    description = "Validate that no context output keys were removed from the Agentix action's output section."
+    rationale = "To ensure we don't break backward compatibility."
+    error_message = "The following output keys have been removed, please undo: {0}"
+    related_field = "outputs"
+    is_auto_fixable = False
+    expected_git_statuses = [GitStatuses.RENAMED, GitStatuses.MODIFIED]
+
+    def obtain_invalid_content_items(
+        self, content_items: Iterable[ContentTypes]
+    ) -> List[ValidationResult]:
+        return [
+            ValidationResult(
+                validator=self,
+                message=self.error_message.format(", ".join(removed_outputs)),
+                content_object=content_item,
+            )
+            for content_item in content_items
+            if (removed_outputs := self.get_removed_outputs(content_item))
+        ]
+
+    def get_removed_outputs(self, content_item: ContentTypes) -> List[str]:
+        current_outputs: Set[str] = set()
+        if content_item.outputs:
+            current_outputs = {
+                output.content_item_output_name for output in content_item.outputs
+            }
+
+        old_outputs: Set[str] = set()
+        if (
+            content_item.old_base_content_object
+            and content_item.old_base_content_object.outputs  # type: ignore[attr-defined]
+        ):
+            old_outputs = {
+                output.content_item_output_name
+                for output in content_item.old_base_content_object.outputs  # type: ignore[attr-defined]
+            }
+
+        return list(old_outputs - current_outputs)

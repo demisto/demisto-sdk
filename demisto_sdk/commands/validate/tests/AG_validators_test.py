7629e974ae1872c56d5b42569653ec7cf34694bb
mayagoldman <mgoldman@paloaltonetworks.com>
2025-11-20 10:22:03 +0000

    pre commit

--- demisto_sdk/commands/validate/tests/AG_validators_test.py
+++ demisto_sdk/commands/validate/tests/AG_validators_test.py
@@ -16,11 +16,11 @@ from demisto_sdk.commands.validate.validators.AG_validators.AG100_is_forbidden_c
 from demisto_sdk.commands.validate.validators.AG_validators.AG101_is_correct_mp import (
     IsCorrectMPValidator,
 )
-from demisto_sdk.commands.validate.validators.AG_validators.AG105_is_valid_types import (
-    IsTypeValid,
-)
 from demisto_sdk.commands.validate.validators.AG_validators.AG105_is_display_name_valid import (
     IsDisplayNameValidValidator,
+)
+from demisto_sdk.commands.validate.validators.AG_validators.AG105_is_valid_types import (
+    IsTypeValid,
 )
 
 

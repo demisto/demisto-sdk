68f359cb613b9378ef067dc0dd6aee4a1dd38511
rshunim <rshunim@paloaltonetworks.com>
2026-01-01 18:13:04 +0200

    add a check to the condition

--- demisto_sdk/commands/content_graph/commands/update.py
+++ demisto_sdk/commands/content_graph/commands/update.py
@@ -30,11 +30,11 @@ app = typer.Typer()
 def should_update_graph(
     content_graph_interface: ContentGraphInterface,
     use_git: bool,
-    git_util: GitUtil,
+    git_util: Optional[GitUtil],
     imported_path: Optional[Path] = None,
     packs_to_update: Optional[List[str]] = None,
 ):
-    if content_graph_interface.commit:
+    if content_graph_interface.commit and git_util:
         try:
             changed_pack_ids = git_util.get_all_changed_pack_ids(
                 content_graph_interface.commit

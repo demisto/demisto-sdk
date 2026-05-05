ad548209f1d7d3919c474b4cc9c0c4d3b0fdf59a
Shir Matathias <132361594+Shir2611@users.noreply.github.com>
2026-05-05 06:47:01 -0700

    new validation for non mandatory supported modules (#5327)
    * new validation for non mandatory supported modules
    
    * new validation for non mandatory supported modules
    
    * new validation for non mandatory supported modules
    
    * code review fixes
    
    * code review fixes
    
    * added logic for all use cases
    
    * added logic for all use cases
    
    * added logic for all use cases

--- demisto_sdk/commands/content_graph/interface/graph.py
+++ demisto_sdk/commands/content_graph/interface/graph.py
@@ -328,7 +328,13 @@ class ContentGraphInterface(ABC):
 
     @abstractmethod
     def find_content_items_with_module_mismatch_dependencies(
-        self, content_item_ids: List[str]
+        self, content_item_ids: List[str], mandatory: bool = True
+    ) -> List[BaseNode]:
+        pass
+
+    @abstractmethod
+    def find_content_items_with_module_mismatch_content_items(
+        self, content_item_ids: List[str], mandatory: bool = True
     ) -> List[BaseNode]:
         pass
 

df5b34d7f49861213253a4d1c1b4f824534093f3
Tal <tcarmeli@paloaltonetworks.com>
2026-07-15 12:29:36 +0300

    fix code

--- demisto_sdk/commands/validate/validators/GR_validators/GR102_is_using_invalid_to_version_valid.py
+++ demisto_sdk/commands/validate/validators/GR_validators/GR102_is_using_invalid_to_version_valid.py
@@ -100,6 +100,8 @@ class IsUsingInvalidToVersionValidator(BaseValidator[ContentTypes], ABC):
     def obtain_invalid_content_items_using_graph(
         self, content_items: Iterable[ContentTypes], validate_all_files: bool = False
     ) -> List[ValidationResult]:
+        for content_item in content_items:
+            logger.info(f"Validating content item '{content_item.name}' with to_version '{content_item.toversion}'")
         file_paths_to_validate = (
             []
             if validate_all_files

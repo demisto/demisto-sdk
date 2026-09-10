7b9fab7646e6534e602da51d0c0679fe575bc223
sfish99 <sfish@paloaltonetworks.com>
2026-09-10 10:59:26 +0300

    Allow MR109 to run in specific-files mode (-i) in addition to use-git

--- demisto_sdk/commands/validate/validators/MR_validators/MR109_user_field_missing_identity.py
+++ demisto_sdk/commands/validate/validators/MR_validators/MR109_user_field_missing_identity.py
@@ -33,7 +33,7 @@ class UserFieldMissingIdentityValidator(BaseValidator[ContentTypes]):
     related_field = "XIF"
     is_auto_fixable = False
     related_file_type = [RelatedFileType.XIF]
-    expected_execution_mode = [ExecutionMode.USE_GIT]
+    expected_execution_mode = [ExecutionMode.USE_GIT, ExecutionMode.SPECIFIC_FILES]
 
     def obtain_invalid_content_items(
         self, content_items: Iterable[ContentTypes]
